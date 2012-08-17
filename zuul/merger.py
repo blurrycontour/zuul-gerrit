4ba620212c35ac57622056147361503866472069
Andrew Hutchings <andrew@linuxjedi.co.uk>
2012-08-17 11:13:39 +0100

    Revert "Push zuul-merged refs to gerrit."
    This reverts commit daabed2d7f91e98a73ca85fcfbe1a37f2847d68e.
    
    Change-Id: I7668bfd33ce94cd3f5d637782436018f74459564

--- zuul/merger.py
+++ zuul/merger.py
@@ -65,17 +65,11 @@ class Repo(object):
     def setZuulRef(self, ref, commit):
         self.repo.refs[ref].commit = commit
 
-    def push(self, local, remote):
-        self.log.debug("Pushing %s:%s to %s " % (local, remote,
-                                                 self.remote_url))
-        self.repo.remotes.origin.push('%s:%s' % (local, remote))
-
 
 class Merger(object):
     log = logging.getLogger("zuul.Merger")
 
-    def __init__(self, trigger, working_root):
-        self.trigger = trigger
+    def __init__(self, working_root):
         self.repos = {}
         self.working_root = working_root
         if not os.path.exists(working_root):
@@ -128,20 +122,5 @@ class Merger(object):
             except:
                 self.log.info("Unable to merge %s" % change)
                 return False
-
-        # Push the results upstream to the zuul ref
-        for project, branches in projects.items():
-            repo = self.getRepo(project)
-            for branch in branches:
-                ref = 'refs/zuul/' + branch + '/' + target_ref
-                try:
-                    repo.push(ref, ref)
-                    complete = self.trigger.waitForRefSha(project, ref)
-                except:
-                    self.log.exception("Unable to push %s" % ref)
-                    return False
-                if not complete:
-                    self.log.error("Ref %s did not show up in repo" % ref)
-                    return False
 
         return True
