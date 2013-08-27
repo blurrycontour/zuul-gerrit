be65f65381d313734182e85fd139f5c0d4be8b7f
James E. Blair <jeblair@openstack.org>
2013-08-27 14:59:27 -0700

    Fix checkout when preparing a ref
    The new checkout method was relying on out of date information
    stored in the remote which was not being updated by the fetch
    command.  Instead, just checkout FETCH_HEAD using git directly
    so that the remote does not need to be kept up to date.
    
    Change-Id: Ie47b675512edc36e8aeb9b537ca945ad8d07b780

--- zuul/merger.py
+++ zuul/merger.py
@@ -63,13 +63,11 @@ class Repo(object):
     def checkout(self, ref):
         self._ensure_cloned()
         self.log.debug("Checking out %s" % ref)
-        self.log.debug(repr(ref))
         if self.repo.re_hexsha_only.match(ref):
-            self.repo.head.reference = ref
+            self.repo.git.checkout(ref)
         else:
             self.fetch(ref)
-            self.repo.head.reference = \
-                self.repo.remotes.origin.refs[ref].commit.hexsha
+            self.repo.git.checkout("FETCH_HEAD")
         self.repo.head.reset(index=True, working_tree=True)
         self.repo.git.clean('-x', '-f', '-d')
 
