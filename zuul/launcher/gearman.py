b9a16e6bcf4d0692f673235ce0f028f557300a28
Arx Cruz <apcruz@br.ibm.com>
2013-10-28 19:49:59 -0200

    Add the possibility to pass the zuul url
    This patch adds the possibility to pass ZUUL_URL to jenkins, if
    zuul_url is set on zuul conf file. Otherwise, it skip it, and
    devstack-vm-gate-wrap.sh will use the default
    http://zuul.openstack.org/p url.
    This will enable other infrastructures to test changes with
    their own zuul servers.
    
    Change-Id: I840423cc06fdfdacd301d30be3e0b3e589e563e9

--- zuul/launcher/gearman.py
+++ zuul/launcher/gearman.py
@@ -155,6 +155,10 @@ class Gearman(object):
         self.sched = sched
         self.builds = {}
         self.meta_jobs = {}  # A list of meta-jobs like stop or describe
+        self.zuul_server = None
+
+        if config.has_option('zuul', 'zuul_url')
+            self.zuul_server = config.get('zuul', 'zuul_url')
         server = config.get('gearman', 'server')
         if config.has_option('gearman', 'port'):
             port = config.get('gearman', 'port')
@@ -249,6 +253,9 @@ class Gearman(object):
 
             params['ZUUL_REF'] = item.change.ref
             params['ZUUL_COMMIT'] = item.change.newrev
+
+        if self.zuul_server:
+            params['ZUUL_URL'] = self.config.get('zuul', 'zuul_url')
 
         # This is what we should be heading toward for parameters:
 
