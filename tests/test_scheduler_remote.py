cb66ac49e2b88b4109fd7c7bda169aa755b5d542
Clark Boylan <clark.boylan@gmail.com>
2014-02-24 16:59:21 -0800

    Add a remote url override location
    Zuul constructs a git clone and fetch url from its gerrit connection
    information. There may be cases where zuuld can connect to gerrit for
    events and git info but the zuul merge workers cannot. Add a config
    option to override the git location url for zuul merge workers so that
    they can talk to gerrit replicas instead of gerrit directly. Because the
    merge workers are not talking to gerrit directly also add an optional
    poll for ref existence in the merge worker.
    
    To test this make it possible to subclass TestScheduler and feed CONFIG
    values into the ConfigParser prior to running TestScheduler's setUp.
    This means all scheduler tests can be run under subclasses with
    different configs.
    
    Change-Id: I3e7f2ab07051610e8554555a7c5fecd458e38330

---
+++ tests/test_scheduler_remote.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+# Copyright 2012 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import ConfigParser
+import logging
+import os
+
+import tests.test_scheduler
+
+FIXTURE_DIR = os.path.join(os.path.dirname(__file__),
+                           'fixtures')
+CONFIG = ConfigParser.ConfigParser()
+CONFIG.read(os.path.join(FIXTURE_DIR, "zuul.conf"))
+
+CONFIG.set('zuul', 'layout_config',
+           os.path.join(FIXTURE_DIR, "layout.yaml"))
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-32s '
+                    '%(levelname)-8s %(message)s')
+
+
+class TestSchedulerRemote(tests.test_scheduler.TestScheduler):
+    log = logging.getLogger("zuul.test")
+
+    TEST_CONFIG = CONFIG
+
+    def setUp(self):
+        self.make_dirs()
+        # Preset config values.
+        self.TEST_CONFIG.set('merger', 'poll', 'True')
+        self.TEST_CONFIG.set('gerrit', 'fetch_url', self.upstream_root)
+        super(TestSchedulerRemote, self).setUp()
