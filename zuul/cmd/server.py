5fe59b443018ed412f7aa6fc3ba9906226252469
Joshua Hesketh <josh@nitrotech.org>
2014-04-10 14:14:02 +1000

    Implement a simple mysql reporter.
    I need a way of finding patches I've voted -1 on so that I can
    recheck them when something goes wrong. This reporter just logs
    votes into a simple MySQL table.
    
    Co-Authored-By: Joshua Hesketh <josh@nitrotech.org>
    
    Change-Id: I8c488884073b6890210b4ebf9169d77f80013660

--- zuul/cmd/server.py
+++ zuul/cmd/server.py
@@ -124,6 +124,7 @@ class Server(object):
         logging.basicConfig(level=logging.DEBUG)
         self.sched = zuul.scheduler.Scheduler()
         self.sched.registerReporter(None, 'gerrit')
+        self.sched.registerReporter(None, 'mysql')
         self.sched.registerReporter(None, 'smtp')
         self.sched.registerTrigger(None, 'gerrit')
         self.sched.registerTrigger(None, 'timer')
@@ -182,6 +183,7 @@ class Server(object):
         import zuul.merger.client
         import zuul.lib.swift
         import zuul.reporter.gerrit
+        import zuul.reporter.mysql
         import zuul.reporter.smtp
         import zuul.trigger.gerrit
         import zuul.trigger.timer
@@ -205,7 +207,24 @@ class Server(object):
         timer = zuul.trigger.timer.Timer(self.config, self.sched)
         webapp = zuul.webapp.WebApp(self.sched)
         rpc = zuul.rpclistener.RPCListener(self.config, self.sched)
+
         gerrit_reporter = zuul.reporter.gerrit.Reporter(gerrit)
+
+        try:
+            mysql_reporter = zuul.reporter.mysql.Reporter(
+                (self.config.get('mysql', 'host')
+                 if self.config.has_option('mysql', 'host') else 'localhost'),
+                (int(self.config.get('mysql', 'port'))
+                 if self.config.has_option('mysql', 'port') else 3306),
+                self.config.get('mysql', 'user'),
+                self.config.get('mysql', 'password'),
+                self.config.get('mysql', 'database'),
+                self.config.get('mysql', 'table'))
+        except Exception:
+            log = logging.getLogger("zuul.server.main")
+            log.exception('Unable to configure mysql reporter:')
+            mysql_reporter = None
+
         smtp_reporter = zuul.reporter.smtp.Reporter(
             self.config.get('smtp', 'default_from')
             if self.config.has_option('smtp', 'default_from') else 'zuul',
@@ -222,6 +241,8 @@ class Server(object):
         self.sched.registerTrigger(gerrit)
         self.sched.registerTrigger(timer)
         self.sched.registerReporter(gerrit_reporter)
+        if mysql_reporter:
+            self.sched.registerReporter(mysql_reporter)
         self.sched.registerReporter(smtp_reporter)
 
         self.log.info('Starting scheduler')
