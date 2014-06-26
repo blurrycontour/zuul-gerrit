5c440a4ec71f1172a43ae832564b0c8bb8abda5e
Joshua Hesketh <josh@nitrotech.org>
2014-06-26 15:30:08 +1000

    Add support for negative requirements
    This change adds support for false matching of requirements. To make
    this useful you can now require all approvals match a requirement or
    only one (ie any).
    
    Therefore depreciate require-approvals, replacing with
    require-approvals-any and a new require-approvals-all.
    
    Change-Id: I458e677315ccb90d64cd0c0e734951141324a9c3

--- zuul/scheduler.py
+++ zuul/scheduler.py
@@ -295,7 +295,9 @@ class Scheduler(threading.Thread):
                     open=require.get('open'),
                     current_patchset=require.get('current-patchset'),
                     statuses=toList(require.get('status')),
-                    required_approvals=toList(require.get('approval')))
+                    required_approvals_any=(toList(require.get('approval-any'))
+                                            + toList(require.get('approval'))),
+                    required_approvals_all=toList(require.get('approval-all')))
                 manager.changeish_filters.append(f)
 
             # TODO: move this into triggers (may require pluggable
@@ -324,8 +326,12 @@ class Scheduler(threading.Thread):
                                     comments=comments,
                                     emails=emails,
                                     usernames=usernames,
-                                    required_approvals=
-                                    toList(trigger.get('require-approval')))
+                                    required_approvals_any=(
+                                    toList(trigger.get('require-approval-any'))
+                                    + toList(trigger.get('require-approval'))),
+                                    required_approvals_all=
+                                    toList(trigger.get('require-approval-all'))
+                                    )
                     manager.event_filters.append(f)
             if 'timer' in conf_pipeline['trigger']:
                 for trigger in toList(conf_pipeline['trigger']['timer']):
@@ -338,8 +344,12 @@ class Scheduler(threading.Thread):
                     f = EventFilter(trigger=self.triggers['zuul'],
                                     types=toList(trigger['event']),
                                     pipelines=toList(trigger.get('pipeline')),
-                                    required_approvals=
-                                    toList(trigger.get('require-approval')))
+                                    required_approvals_any=(
+                                    toList(trigger.get('require-approval-any'))
+                                    + toList(trigger.get('require-approval'))),
+                                    required_approvals_all=
+                                    toList(trigger.get('require-approval-all'))
+                                    )
                     manager.event_filters.append(f)
 
         for project_template in data.get('project-templates', []):
