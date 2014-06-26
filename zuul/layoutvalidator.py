a6e15c2d695310ac3821f838e5d26395fa13a3f0
Joshua Hesketh <josh@nitrotech.org>
2014-06-26 15:30:08 +1000

    Add support for negative requirements
    This change adds support for false matching of requirements. To make
    this useful you can now require all approvals match a requirement or
    only one (ie any).
    
    Therefore depreciate require-approvals, replacing with
    require-approvals-any and a new require-approvals-all.
    
    Change-Id: I458e677315ccb90d64cd0c0e734951141324a9c3

--- zuul/layoutvalidator.py
+++ zuul/layoutvalidator.py
@@ -62,6 +62,8 @@ class LayoutSchema(object):
                       'branch': toList(str),
                       'ref': toList(str),
                       'approval': toList(variable_dict),
+                      'require-approval-all': toList(require_approval),
+                      'require-approval-any': toList(require_approval),
                       'require-approval': toList(require_approval),
                       }
 
@@ -85,7 +87,9 @@ class LayoutSchema(object):
                                },
                       }
 
-    require = {'approval': toList(require_approval),
+    require = {'approval-all': toList(require_approval),
+               'approval-any': toList(require_approval),
+               'approval': toList(require_approval),
                'open': bool,
                'current-patchset': bool,
                'status': toList(str)}
