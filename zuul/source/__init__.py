02becd15a29fcea29f99ae2b2016e882490bd8dd
Joshua Hesketh <josh@nitrotech.org>
2014-09-05 21:52:47 +1000

    Add base class for sources
    and test the all sources adhere to the set contract.
    
    Also standardise the source (triggers to come) class names
    to NameSource.
    
    This will make it easier to do more sources in the future and also
    add the possibility of loading sources dynamically.
    
    Change-Id: I15b32013904f60873601dd7cc8fce3c158787de4

---
+++ zuul/source/__init__.py
@@ -0,0 +1,55 @@
+# Copyright 2014 Rackspace Australia
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+class BaseSource(object):
+    """Base class for sources.
+
+    Defines the exact public methods that must be supplied."""
+
+    def __init__(self, config, sched):
+        raise NotImplementedError()
+
+    def getRefSha(self, project, ref):
+        raise NotImplementedError()
+
+    def waitForRefSha(self, project, ref, old_sha=''):
+        raise NotImplementedError()
+
+    def isMerged(self, change, head=None):
+        raise NotImplementedError()
+
+    def canMerge(self, change, allow_needs):
+        raise NotImplementedError()
+
+    def maintainCache(self, relevant):
+        pass
+
+    def postConfig(self):
+        pass
+
+    def getChange(self, event, project):
+        raise NotImplementedError()
+
+    def getProjectOpenChanges(self, project):
+        raise NotImplementedError()
+
+    def updateChange(self, change):
+        raise NotImplementedError()
+
+    def getGitUrl(self, project):
+        raise NotImplementedError()
+
+    def getGitwebUrl(self, project, sha=None):
+        raise NotImplementedError()
