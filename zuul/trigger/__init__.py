aba1900681885ad085570e3cab2b00128add00f8
Joshua Hesketh <josh@nitrotech.org>
2014-09-26 15:16:13 +1000

    Add support for 'connection' concept
    WIP
    
    Change-Id: Iac5083996a183d1d8a9b6cb8f70836f7c39ee910

--- zuul/trigger/__init__.py
+++ zuul/trigger/__init__.py
@@ -17,8 +17,8 @@ class BaseTrigger(object):
     """Base class for triggers.
 
     Defines the exact public methods that must be supplied."""
-    def __init__(self, *args, **kwargs):
-        raise NotImplementedError()
+    def __init__(self):
+        pass
 
     def stop(self):
         raise NotImplementedError()
@@ -34,3 +34,9 @@ class BaseTrigger(object):
 
     def onChangeEnqueued(self, change, pipeline):
         raise NotImplementedError()
+
+    def registerScheduler(self, sched):
+        self.sched = sched
+
+    def registerConnection(self, connection):
+        self.connection = connection
--- zuul/trigger/zuultrigger.py
+++ zuul/trigger/zuultrigger.py
@@ -22,9 +22,7 @@ class ZuulTrigger(BaseTrigger):
     name = 'zuul'
     log = logging.getLogger("zuul.ZuulTrigger")
 
-    def __init__(self, config, sched):
-        self.sched = sched
-        self.config = config
+    def __init__(self):
         self._handle_parent_change_enqueued_events = False
         self._handle_project_change_merged_events = False
 
