7f7d83e9021f5db029cb6b867f3200e00202ab41
Davanum Srinivas <dims@linux.vnet.ibm.com>
2014-11-18 13:26:52 -0500

    Add gerrit owner information to status.json
    gerrit.query does return the owner information, we can save that
    in the Change object and serialize it to status.json
    
    Change-Id: Ib505ef8703bc60058d2775f522fcc3d02910b05f

--- zuul/model.py
+++ zuul/model.py
@@ -709,6 +709,7 @@ class QueueItem(object):
         ret['project'] = changeish.project.name
         ret['enqueue_time'] = int(self.enqueue_time * 1000)
         ret['jobs'] = []
+        ret['owner'] = changeish.owner
         max_remaining = 0
         for job in self.pipeline.getJobs(changeish):
             now = time.time()
@@ -857,6 +858,7 @@ class Change(Changeish):
         self.approvals = []
         self.open = None
         self.status = None
+        self.owner = None
 
     def _id(self):
         return '%s,%s' % (self.number, self.patchset)
