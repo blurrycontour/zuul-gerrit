599198e200a58da4c74e336020d0889f194306c3
Dmitry Teselkin <dteselkin@mirantis.com>
2014-11-20 18:18:53 +0300

    Process event multiple times.
    Recently I faced a situation when zuul fails to query event information from
    gerrit after receiving a 'patchset-created' event from that gerrit.
    Because of this zuul ignores that event until a comment with 'recheck' is added.
    
    The reason is that the event received from gerrit is lost after first exception.
    
    So I propose to return such events back to the queue, and decrease their TTL
    to avoid infinite loops.
    
    Change-Id: If561f02f0c073c1cfab1804042677a6a626411c2

--- zuul/trigger/gerrit.py
+++ zuul/trigger/gerrit.py
@@ -31,6 +31,7 @@ class GerritEventConnector(threading.Thread):
         self.gerrit = gerrit
         self.sched = sched
         self.trigger = trigger
+        self._event = None
         self._stopped = False
 
     def stop(self):
@@ -38,7 +39,10 @@ class GerritEventConnector(threading.Thread):
         self.gerrit.addEvent(None)
 
     def _handleEvent(self):
-        data = self.gerrit.getEvent()
+        self._event = self.gerrit.getEvent()
+        if 'zuulTimeToLive' not in self._event.keys():
+            self._event['zuulTimeToLive'] = 5
+        data = self._event
         if self._stopped:
             return
         event = TriggerEvent()
@@ -102,6 +106,11 @@ class GerritEventConnector(threading.Thread):
                 self._handleEvent()
             except:
                 self.log.exception("Exception moving Gerrit event:")
+                if self._event['zuulTimeToLive'] > 0:
+                    self.log.debug("Adding event back to the queue.")
+                    self._event['zuulTimeToLive'] -= 1
+                    self.gerrit.addEvent(self._event)
+            self._event = None
 
 
 class Gerrit(object):
