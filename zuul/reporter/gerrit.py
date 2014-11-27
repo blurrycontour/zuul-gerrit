7c1bd640d8018ad1315bf4791f0fc77ae0e929da
Joshua Hesketh <josh@nitrotech.org>
2014-11-27 11:33:58 +1100

    Add support for 'connection' concept
    This uncovered a bug with test_cloner where the builds were not
    necessarily done in the order expected in the tests. To make sure
    they are in order wait after each gerrit event before continuing.
    
    This is a large refactor and as small as I could feasibly make it
    while keeping the tests working. I'll do the documentation and
    touch ups in the next commit to make digesting easier.
    
    Change-Id: Iac5083996a183d1d8a9b6cb8f70836f7c39ee910

--- zuul/reporter/gerrit.py
+++ zuul/reporter/gerrit.py
@@ -24,11 +24,6 @@ class GerritReporter(BaseReporter):
     name = 'gerrit'
     log = logging.getLogger("zuul.reporter.gerrit.Reporter")
 
-    def __init__(self, gerrit):
-        """Set up the reporter."""
-        # TODO: make default_gerrit come from a connection
-        self.default_gerrit = gerrit
-
     def report(self, source, change, message, params):
         """Send a message to gerrit."""
         self.log.debug("Report change %s, params %s, message: %s" %
@@ -36,8 +31,9 @@ class GerritReporter(BaseReporter):
         changeid = '%s,%s' % (change.number, change.patchset)
         change._ref_sha = source.getRefSha(change.project.name,
                                            'refs/heads/' + change.branch)
-        return self.default_gerrit.review(
-            change.project.name, changeid, message, params)
+
+        return self.connection.review(change.project.name, changeid, message,
+                                      params)
 
     def getSubmitAllowNeeds(self, params):
         """Get a list of code review labels that are allowed to be
