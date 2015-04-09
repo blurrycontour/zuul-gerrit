8a6fd4dab8df4fffc43a11777e32bd529849f59b
Timo Tijhof <krinklemail@gmail.com>
2015-04-10 00:33:50 +0100

    Status: Don't raise "abort" as error to the user
    If the request was aborted, the failure should not be presented
    to the user as nothing actually went wrong.
    
    This happens when a request is not finished when next 'update'
    starts. This is amplified by the page visibility events that
    will start an 'update' loop prematurely.
    
    Without this, switching between tabs often causes the user to
    see a "status.json: abort" message when they come back to the
    dashboard.
    
    Change-Id: I834384535f8d04d1274336bc993c20035be01d60

--- etc/status/public_html/jquery.zuul.js
+++ etc/status/public_html/jquery.zuul.js
@@ -683,6 +683,9 @@
                         );
                     })
                     .fail(function (err, jqXHR, errMsg) {
+                        if (err.textStatus === 'abort') {
+                            return;
+                        }
                         $msg.text(options.source + ': ' + errMsg)
                             .addClass('alert-danger')
                             .removeClass('zuul-msg-wrap-off')
