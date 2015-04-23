7906a4aeb0de4044d596c5624e17c0fbc72f3a5d
Gregory Haynes <greg@greghaynes.net>
2015-04-22 20:38:06 -0700

    Support GitHub PR webhooks
    Change-Id: I2713c5d19326213539689e9d822831a393b2bf19
    Co-Authored-By: Wayne Warren <waynr+launchpad@sdf.org>
    Co-Authored-By: Jan Hruban <jan.hruban@gooddata.com>
    Co-Authored-By: Jesse Keating <omgjlk@us.ibm.com>

--- zuul/lib/connections.py
+++ zuul/lib/connections.py
@@ -17,6 +17,7 @@ import re
 import zuul.driver.zuul
 import zuul.driver.gerrit
 import zuul.driver.git
+import zuul.driver.github
 import zuul.driver.smtp
 import zuul.driver.timer
 from zuul.connection import BaseConnection
@@ -36,6 +37,7 @@ class ConnectionRegistry(object):
         self.registerDriver(zuul.driver.zuul.ZuulDriver())
         self.registerDriver(zuul.driver.gerrit.GerritDriver())
         self.registerDriver(zuul.driver.git.GitDriver())
+        self.registerDriver(zuul.driver.git.GitHubDriver())
         self.registerDriver(zuul.driver.smtp.SMTPDriver())
         self.registerDriver(zuul.driver.timer.TimerDriver())
 
