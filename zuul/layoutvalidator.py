b88308335bd40d4820ddf283968ebe51027a4a6c
Wayne <wayne@puppetlabs.com>
2015-04-22 20:38:06 -0700

    (WIP) Support GitHub PR webhooks
    Co-Authored-By: Wayne Warren (waynr+launchpad@sdf.org)
    
    Change-Id: I2713c5d19326213539689e9d822831a393b2bf19

--- zuul/layoutvalidator.py
+++ zuul/layoutvalidator.py
@@ -17,6 +17,7 @@
 
 import voluptuous as v
 import string
+import logging
 
 
 # Several forms accept either a single item or a list, this makes
@@ -26,6 +27,8 @@ def toList(x):
 
 
 class LayoutSchema(object):
+    log = logging.getLogger("zuul.layoutvalidator.LayoutSchema")
+
     include = {'python-file': str}
     includes = [include]
 
@@ -157,6 +160,7 @@ class LayoutSchema(object):
         connection_drivers = {
             'trigger': {
                 'gerrit': 'zuul.trigger.gerrit',
+                'github': 'zuul.trigger.github',
             },
             'reporter': {
                 'gerrit': 'zuul.reporter.gerrit',
@@ -273,6 +277,8 @@ class LayoutSchema(object):
 
 
 class LayoutValidator(object):
+    log = logging.getLogger("zuul.layoutvalidator.LayoutValidator")
+
     def checkDuplicateNames(self, data, path):
         items = []
         for i, item in enumerate(data):
@@ -320,6 +326,7 @@ class LayoutValidator(object):
 
     def validate(self, data, connections=None):
         schema = LayoutSchema().getSchema(data, connections)
+        self.log.debug(str(schema))
         schema(data)
         self.checkDuplicateNames(data['pipelines'], ['pipelines'])
         if 'jobs' in data:
