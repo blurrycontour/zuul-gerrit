c67d556603ae3878a3e6b34769af9c427dab9acd
Jan Hruban <jan.hruban@gooddata.com>
2015-08-21 14:00:54 +0200

    Allow using webapp from connections
    Allow connections to register their own handlers for HTTP URIs inside
    the zuul's webapp HTTP server. That way, connections can listen for
    events comming through HTTP.
    
    Change-Id: Ic5887d00ff302f67469df5154e9df10b99f1cfcd

--- tests/base.py
+++ tests/base.py
@@ -968,6 +968,7 @@ class ZuulTestCase(BaseTestCase):
         self.useFixture(fixtures.MonkeyPatch('swiftclient.client.Connection',
                                              FakeSwiftClientConnection))
         self.swift = zuul.lib.swift.Swift(self.config)
+        self.webapp = zuul.webapp.WebApp(self.sched, port=0)
 
         self.event_queues = [
             self.sched.result_event_queue,
@@ -975,7 +976,7 @@ class ZuulTestCase(BaseTestCase):
         ]
 
         self.configure_connections()
-        self.sched.registerConnections(self.connections)
+        self.sched.registerConnections(self.connections, self.webapp)
 
         def URLOpenerFactory(*args, **kw):
             if isinstance(args[0], urllib.request.Request):
@@ -997,7 +998,6 @@ class ZuulTestCase(BaseTestCase):
         self.sched.setLauncher(self.launcher)
         self.sched.setMerger(self.merge_client)
 
-        self.webapp = zuul.webapp.WebApp(self.sched, port=0)
         self.rpc = zuul.rpclistener.RPCListener(self.config, self.sched)
 
         self.sched.start()
