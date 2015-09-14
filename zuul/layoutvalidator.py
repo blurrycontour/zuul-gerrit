b29f889fe22a00f6ef36ffb8516f47fb40ab6250
Joshua Hesketh <josh@nitrotech.org>
2015-09-14 16:56:34 -0600

    Add support for sqlalchemy reporter
    This will allow us to enter results from all jobs for
    use with the openstack-health dashboard.
    
    Depends-On: I08dbbb64b3daba915a94e455f75eef61ab392852
    Change-Id: I28056d84a3f6abcd8d9038a91a6c9a3902142f90

--- zuul/layoutvalidator.py
+++ zuul/layoutvalidator.py
@@ -166,6 +166,7 @@ class LayoutSchema(object):
             'reporter': {
                 'gerrit': 'zuul.reporter.gerrit',
                 'smtp': 'zuul.reporter.smtp',
+                'sql': 'zuul.reporter.sql',
             },
         }
         standard_drivers = {
