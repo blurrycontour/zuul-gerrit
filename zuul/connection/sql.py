006b6daae21d278391018b7d6ce0a60f92c12ffb
Joshua Hesketh <josh@nitrotech.org>
2015-09-14 16:56:34 -0600

    Add support for sqlalchemy reporter
    This will allow us to enter results from all jobs for
    use with the openstack-health dashboard.
    
    Depends-On: I08dbbb64b3daba915a94e455f75eef61ab392852
    Change-Id: I28056d84a3f6abcd8d9038a91a6c9a3902142f90

---
+++ zuul/connection/sql.py
@@ -0,0 +1,106 @@
+# Copyright 2014 Rackspace Australia
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+
+import alembic
+import alembic.config
+import sqlalchemy
+import voluptuous as v
+
+from zuul.connection import BaseConnection
+
+
+class SQLConnection(BaseConnection):
+    driver_name = 'sql'
+    log = logging.getLogger("connection.sql")
+
+    def __init__(self, connection_name, connection_config):
+
+        super(SQLConnection, self).__init__(connection_name, connection_config)
+
+        self.dburi = None
+        self.engine = None
+        self.connection = None
+        self.tables_established = False
+        self._build_table = self.connection_config.get('build_table',
+                                                       'zuul_build')
+        self._build_metadata_table = self._build_table + '_metadata'
+        try:
+            self.dburi = self.connection_config.get('dburi')
+            self.engine = sqlalchemy.create_engine(self.dburi)
+            self._migrate()
+            self._setup_tables()
+            self.tables_established = True
+        except sqlalchemy.exc.NoSuchModuleError:
+            self.log.exception(
+                "The required module for the dburi dialect isn't available. "
+                "SQL connection %s will be unavailable." % connection_name)
+        except sqlalchemy.exc.OperationalError:
+            self.log.exception(
+                "Unable to connect to the database or establish the required "
+                "tables. Reporter %s is disabled" % self)
+
+    def _migrate(self):
+        """Perform the alembic migrations for this connection"""
+        with self.engine.begin() as conn:
+            context = alembic.migration.MigrationContext.configure(conn)
+            current_rev = context.get_current_revision()
+            self.log.debug('Current migration revision: %s' % current_rev)
+
+            config = alembic.config.Config()
+            config.set_main_option("script_location",
+                                   "zuul:alembic/sql_reporter")
+            config.set_main_option("sqlalchemy.url",
+                                   self.connection_config.get('dburi'))
+            config.set_main_option("zuul.build_table",
+                                   self._build_table)
+
+            alembic.command.upgrade(config, 'head')
+
+    def _setup_tables(self):
+        metadata = sqlalchemy.MetaData()
+
+        self.zuul_build = sqlalchemy.Table(
+            self._build_table, metadata,
+            sqlalchemy.Column('uuid', sqlalchemy.String(255)),
+            sqlalchemy.Column('job_name', sqlalchemy.String(255)),
+            sqlalchemy.Column('score', sqlalchemy.Integer),
+            sqlalchemy.Column('result', sqlalchemy.String(255)),
+            sqlalchemy.Column('start_time', sqlalchemy.Integer),
+            sqlalchemy.Column('end_time', sqlalchemy.Integer),
+            sqlalchemy.Column('message', sqlalchemy.String(255)),
+            sqlalchemy.UniqueConstraint('uuid', 'job_name',
+                                        name='zuul_build_uuid_job_name'),
+        )
+
+        self.zuul_build_metadata = sqlalchemy.Table(
+            self._build_metadata_table, metadata,
+            sqlalchemy.Column(
+                'build_uuid', sqlalchemy.String(255),
+                sqlalchemy.ForeignKey(self._build_table + '.uuid')),
+            sqlalchemy.Column(
+                'build_job_name', sqlalchemy.String(255),
+                sqlalchemy.ForeignKey(self._build_table + '.job_name')),
+            sqlalchemy.Column('key', sqlalchemy.String(255)),
+            sqlalchemy.Column('value', sqlalchemy.String(255)),
+            sqlalchemy.UniqueConstraint(
+                'build_uuid', 'build_job_name', 'key',
+                name='zuul_build_metadata_build_uuid_build_job_name_key'),
+        )
+
+
+def getSchema():
+    sql_connection = v.Any(str, v.Schema({}, extra=True))
+    return sql_connection
