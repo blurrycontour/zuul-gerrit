9c0c12b8b2b3627adc5f20e20a78bd926733bd09
Joshua Hesketh <josh@nitrotech.org>
2015-09-14 16:56:34 -0600

    Add support for sqlalchemy reporter
    This will allow us to enter results from all jobs for
    use with the openstack-health dashboard.
    
    Depends-On: I08dbbb64b3daba915a94e455f75eef61ab392852
    Change-Id: I28056d84a3f6abcd8d9038a91a6c9a3902142f90

---
+++ zuul/alembic/sql_reporter/versions/4d3ebd7f06b9_set_up_initial_reporter_tables.py
@@ -0,0 +1,45 @@
+"""Set up initial reporter tables
+
+Revision ID: 4d3ebd7f06b9
+Revises:
+Create Date: 2015-12-06 15:27:38.080020
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '4d3ebd7f06b9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+BUILD_TABLE = 'zuul_build'
+BUILD_METADATA_TABLE = BUILD_TABLE + '_metadata'
+
+
+def upgrade():
+    op.create_table(
+        BUILD_TABLE,
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('uuid', sa.String(36)),
+        sa.Column('job_name', sa.String(255)),
+        sa.Column('score', sa.Integer),
+        sa.Column('result', sa.String(255)),
+        sa.Column('start_time', sa.Integer),
+        sa.Column('end_time', sa.Integer),
+        sa.Column('message', sa.String(255)),
+    )
+
+    op.create_table(
+        BUILD_METADATA_TABLE,
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('build_id', sa.Integer, sa.ForeignKey(BUILD_TABLE + ".id")),
+        sa.Column('key', sa.String(255)),
+        sa.Column('value', sa.String(255)),
+    )
+
+
+def downgrade():
+    raise Exception("Downgrades not supported")
