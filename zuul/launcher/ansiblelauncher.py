145d49dec6ec056b61bd5ab990a78d4865a71e3c
James E. Blair <jeblair@linux.vnet.ibm.com>
2016-01-11 14:38:13 -0800

    Add ansible as a dependency
    Note that ansible is licensed under the GPL which has implications
    for the distribution of Zuul as a whole.
    
    The dependency is on the 2.0 branch which should be released *very*
    soon at which point it can change to a normal pypi dep.
    
    Actually use ansible to launch a hello galaxy test.
    
    Change-Id: I30f7ed2cadbb7d2381ec477e229d19c9c6e41743

--- zuul/launcher/ansiblelauncher.py
+++ zuul/launcher/ansiblelauncher.py
@@ -20,6 +20,12 @@ import tempfile
 import threading
 import traceback
 
+from ansible.parsing.dataloader import DataLoader
+from ansible.vars import VariableManager
+from ansible.inventory import Inventory
+from ansible.playbook.play import Play
+from ansible.executor.task_queue_manager import TaskQueueManager
+
 import gear
 
 import zuul.merger
@@ -222,6 +228,71 @@ class LaunchServer(object):
             commit = merger.mergeChanges(args['items'])  # noqa
             # TODOv3: Ansible the ansible thing here.
 
+            # From: http://docs.ansible.com/ansible/developing_api.html
+
+            Options = collections.namedtuple(
+                'Options',
+                ['connection','module_path', 'forks', 'remote_user',
+                 'private_key_file', 'ssh_common_args', 'ssh_extra_args',
+                 'sftp_extra_args', 'scp_extra_args', 'become',
+                 'become_method', 'become_user', 'verbosity', 'check'])
+
+            # initialize needed objects
+            variable_manager = VariableManager()
+            loader = DataLoader()
+            options = Options(connection='local',
+                              module_path='/path/to/mymodules',
+                              forks=100,
+                              remote_user=None,
+                              private_key_file=None,
+                              ssh_common_args=None,
+                              ssh_extra_args=None,
+                              sftp_extra_args=None,
+                              scp_extra_args=None,
+                              become=None,
+                              become_method=None,
+                              become_user=None,
+                              verbosity=None,
+                              check=False)
+            passwords = dict(vault_pass='secret')
+
+            host_list = self.getHostList()
+
+            # create inventory and pass to var manager
+            inventory = Inventory(loader=loader,
+                                  variable_manager=variable_manager,
+                                  host_list=host_list)
+            variable_manager.set_inventory(inventory)
+
+            # create play with tasks
+            # TODOv3: this should come from the job definition
+            play_source =  dict(
+                name = "Ansible Play",
+                hosts = 'localhost',
+                gather_facts = 'no',
+                tasks = [ dict(action=dict(module='debug',
+                                           args=dict(msg='Hello Galaxy!'))) ]
+            )
+            play = Play().load(play_source,
+                               variable_manager=variable_manager,
+                               loader=loader)
+
+            # actually run it
+            tqm = None
+            try:
+                tqm = TaskQueueManager(
+                    inventory=inventory,
+                    variable_manager=variable_manager,
+                    loader=loader,
+                    options=options,
+                    passwords=passwords,
+                    stdout_callback='default',
+                )
+                result = tqm.run(play)
+            finally:
+                if tqm is not None:
+                    tqm.cleanup()
+
             data = {
                 'url': 'https://server/job',
                 'number': 1
@@ -231,6 +302,11 @@ class LaunchServer(object):
 
             result = dict(result='SUCCESS')
             job.sendWorkComplete(json.dumps(result))
+
+    def getHostList(self):
+        # TODOv3: This should get the appropriate nodes from nodepool,
+        # or in the unit tests, be overriden to return localhost.
+        return ['localhost']
 
     def cat(self, job):
         args = json.loads(job.arguments)
