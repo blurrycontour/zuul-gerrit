332e6b4138a9fa3e312ecf20075eed776ecde06f
Jan Hruban <jan.hruban@gooddata.com>
2016-02-03 13:10:22 +0100

    Better merge message for GitHub pull reqeusts
    * Preserve pull reqeust title in the commit message.
    * Add information about the user who triggered the merge.
    
    Change-Id: Ibe0bb5ee273a6ae22fde2e0d71fd10b600dee17f

--- tests/base.py
+++ tests/base.py
@@ -562,6 +562,7 @@ class FakeGithubPullRequest(object):
         self.updated_at = None
         self.head_sha = None
         self.is_merged = False
+        self.merge_message = None
         self._createPRRef()
         self._addCommitToRepo()
         self._updateTimeStamp()
@@ -606,6 +607,9 @@ class FakeGithubPullRequest(object):
             },
             'repository': {
                 'full_name': self.project
+            },
+            'sender': {
+                'login': 'ghuser'
             }
         }
         return (name, data)
@@ -628,6 +632,7 @@ class FakeGithubPullRequest(object):
             'action': action,
             'pull_request': {
                 'number': self.number,
+                'title': self.subject,
                 'updated_at': self.updated_at,
                 'base': {
                     'ref': self.branch,
@@ -641,6 +646,9 @@ class FakeGithubPullRequest(object):
             },
             'label': {
                 'name': label
+            },
+            'sender': {
+                'login': 'ghuser'
             }
         }
         return (name, data)
@@ -707,6 +715,7 @@ class FakeGithubPullRequest(object):
             'number': self.number,
             'pull_request': {
                 'number': self.number,
+                'title': self.subject,
                 'updated_at': self.updated_at,
                 'base': {
                     'ref': self.branch,
@@ -717,6 +726,9 @@ class FakeGithubPullRequest(object):
                 'head': {
                     'sha': self.head_sha
                 }
+            },
+            'sender': {
+                'login': 'ghuser'
             }
         }
         return (name, data)
@@ -789,6 +801,7 @@ class FakeGithubConnection(githubconnection.GithubConnection):
         pr = self.pull_requests[number - 1]
         data = {
             'number': number,
+            'title': pr.subject,
             'updated_at': pr.updated_at,
             'base': {
                 'repo': {
@@ -799,6 +812,14 @@ class FakeGithubConnection(githubconnection.GithubConnection):
             'head': {
                 'sha': pr.head_sha
             }
+        }
+        return data
+
+    def getUser(self, login):
+        data = {
+            'username': login,
+            'name': 'Github User',
+            'email': 'github.user@example.com'
         }
         return data
 
@@ -818,7 +839,7 @@ class FakeGithubConnection(githubconnection.GithubConnection):
         pull_request = self.pull_requests[pr_number - 1]
         pull_request.addComment(message)
 
-    def mergePull(self, project, pr_number, sha=None):
+    def mergePull(self, project, pr_number, commit_message='', sha=None):
         pull_request = self.pull_requests[pr_number - 1]
         if self.merge_failure:
             raise Exception('Pull request was not merged')
@@ -827,6 +848,7 @@ class FakeGithubConnection(githubconnection.GithubConnection):
             raise MergeFailure('Merge was not successful due to mergeability'
                                ' conflict')
         pull_request.is_merged = True
+        pull_request.merge_message = commit_message
 
     def setCommitStatus(self, project, sha, state,
                         url='', description='', context=''):
--- zuul/model.py
+++ zuul/model.py
@@ -1758,6 +1758,8 @@ class Change(Ref):
         self.status = None
         self.owner = None
 
+        self.source_event = None
+
     def _id(self):
         return '%s,%s' % (self.number, self.patchset)
 
@@ -1808,6 +1810,7 @@ class PullRequest(Change):
     def __init__(self, project):
         super(PullRequest, self).__init__(project)
         self.updated_at = None
+        self.title = None
 
     def isUpdateOf(self, other):
         if (hasattr(other, 'number') and self.number == other.number and
@@ -1878,6 +1881,10 @@ class TriggerEvent(object):
 
 
 class GithubTriggerEvent(TriggerEvent):
+
+    def __init__(self):
+        super(GithubTriggerEvent, self).__init__()
+        self.title = None
 
     def isPatchsetCreated(self):
         return self.type in ['pr-open', 'pr-change']
