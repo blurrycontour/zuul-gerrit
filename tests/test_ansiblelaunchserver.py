4794f5fa86701b82414c27dc34139cfb9203e839
Joshua Hesketh <josh@nitrotech.org>
2016-05-18 00:02:04 +1000

    Start fleshing out ansible-launcher tests
    Some basic tests and checks.
    
    Will need to flesh out the fakeAnsibleLauncher to allow us
    to introspect running jobs and results etc.
    
    Change-Id: Ibe2b1d8f6ced2d4af7820b96fd78396fc42e04bb

--- tests/test_ansiblelaunchserver.py
+++ tests/test_ansiblelaunchserver.py
@@ -19,8 +19,6 @@ from tests.base import (
 
 
 class TestAnsibleLaunchServer(BaseTestCase):
-    def test_jjb_functions_list(self):
-        pass
 
     def test_console_log_is_captured(self):
         pass
@@ -42,11 +40,34 @@ class TestAnsibleLaunchServer(BaseTestCase):
 
 
 class TestAnsibleLaunchServerScenario(ZuulAnsibleLauncherTestCase):
-    def test_get_nodes(self):
-        pass
+    def test_jjb_functions_list(self):
+        "Check the jobs were loaded from jjb"
+        self.assertEqual(1, len(self.ansible_launcher.jobs))
+        self.assertEqual(['hello-world'], self.ansible_launcher.jobs.keys())
 
-    def test_build_functions_register_with_scheduler(self):
-        pass
+    def test_node_assign(self):
+        "Check localhost was assigned as a node to this ansible_launcher"
+        self.assertEqual(1, len(self.ansible_launcher.node_workers))
+        self.assertEqual(['node-local'],
+                         self.ansible_launcher.node_workers.keys())
+
+    def test_build_functions_and_worker_register_with_scheduler(self):
+        "Check the job has registered and that there is 1 worker available"
+        self.assertIn('build:hello-world', self.gearman_server.functions)
+        self.assertEqual(
+            [0, 0, 1],
+            self.gearman_server._getFunctionStats()['build:hello-world'])
 
     def test_basic_job_runs(self):
-        pass
+        return
+        self.config.set('zuul', 'layout_config',
+                        'tests/fixtures/layout-ansible-launcher.yaml')
+        self.sched.reconfigure(self.config)
+
+        A = self.fake_gerrit.addFakeChange('org/project', 'master', 'A')
+        A.addApproval('CRVW', 2)
+        self.fake_gerrit.addEvent(A.addApproval('APRV', 1))
+
+        # todo...
+        # Wait for job to finish
+        # Check results
