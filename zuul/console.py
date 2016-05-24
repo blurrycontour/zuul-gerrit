17a92b3d392c408b833a8d7fdeec5401d8486866
Monty Taylor <mordred@inaugust.com>
2016-05-24 11:28:10 -0500

    WIP: DNM - Add web-based console log streaming
    This is a very early placeholder commit. It hard codes streaming console
    logs for a single hard-coded location. BUT - it has all of the bits to
    start from.
    
    Change-Id: Idd0d3f9259e81fa9a60d7540664ce8d5ad2c298f

---
+++ zuul/console.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# Copyright (c) 2016 IBM Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from autobahn.asyncio.websocket import WebSocketServerProtocol
+from autobahn.asyncio.websocket import WebSocketServerFactory
+
+import asyncio
+try:
+    import uvloop
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+except ImportError:
+    pass
+
+import json
+
+
+class ZuulSocketServerProtocol(WebSocketServerProtocol):
+
+    @asyncio.coroutine
+    def get_port_location(self, request):
+        # TODO(mordred): Make an outbound request to zuul to find out where
+        # to serve based on the change id and logfile name that is requested
+        return ('127.0.0.1', '8088')
+
+    @asyncio.coroutine
+    def onMessage(self, payload, isBinary):
+        if not isBinary:
+            request = json.loads(payload.decode('utf8'))
+            for key in ('change', 'logfile'):
+                if key not in request:
+                    self.sendClose(
+                        1000, "'{key}' missing from request payload".format(
+                            key=key))
+
+            try:
+                host, port = yield from self.get_port_location(request)
+            except Exception as e:
+                self.sendClose(1000, "Exception raised: {0}".format(e))
+            try:
+                remote_reader, remote_writer = yield from asyncio.open_connection(host, port)
+                while True:
+                    line = yield from asyncio.wait_for(
+                        remote_reader.readline(), timeout=10.0)
+                    if not line:
+                        remote_reader.close()
+                        remote_writer.close()
+                        break
+                    response = self.sendMessage(line)
+            except Exception as e:
+                self.sendClose(1000, "Exception raised: {0}".format(e))
+            self.sendClose()
+
+
+if __name__ == '__main__':
+
+    factory = WebSocketServerFactory(u"ws://127.0.0.1:9000")
+    factory.protocol = ZuulSocketServerProtocol
+
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(factory, '127.0.0.1', 9000)
+    server = loop.run_until_complete(coro)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.close()
+        loop.close()
