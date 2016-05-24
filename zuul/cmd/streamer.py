79ad3aa667d11db1f690127659c9a04d2302f323
Monty Taylor <mordred@inaugust.com>
2016-05-24 11:28:10 -0500

    WIP: Add web-based console log streaming
    zuul now provides socket-based console streaming, which is super cool.
    In order to have jenkins parity with web streaming, we need to provide a
    websocket (javascript in browsers can't really connect to random ports
    on servers)
    
    After surveying the existing python websocket options, basically all of
    them are based around twisted, eventlet, gevent or asyncio. It's not
    just a thing we can easily deal with from our current webob/paste
    structure, because it is a change to the fundamental HTTP handling.
    While we could write our own websocket server implementation that was
    threaded like the rest of zuul, that's a pretty giant amount of work.
    
    Instead, we can run an async-based server that's just for the
    websockets, so that we're not all of a sudden putting async code into
    the rest of zuul and winding up frankensteined. Since this is new code,
    using asyncio and python3 seems like an excellent starting place.
    
    The autobahn framework is the most mature websocket implementation for
    python and has twisted and asyncio implementations. This also has
    support for optional installation of uvloop, which is an accelerated
    loop that plugs into asyncio's pluggable event loop.
    
    In order to keep us from being an open socket relay, we'll expect two
    parameters as the first message on the websocket - what's the zuul build
    uuid, and what log file do we want to stream. (the second thing,
    multiple log files, isn't supported yet by the rest of zuul, but one can
    imagine a future where we'd like to support that too, so it's in the
    protocol) The websocket server will then ask zuul over gearman for the
    IP and port associated with the build and logfile and will start
    streaming it to the socket.
    
    Ultimately we'll want the status page to make links of the form:
    
      /console.html?uuid=<uuid>&logfile=console.log
    
    and we'll want to have apache map the websocket server to something like
    /console.
    
    Change-Id: Idd0d3f9259e81fa9a60d7540664ce8d5ad2c298f

---
+++ zuul/cmd/streamer.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# Copyright 2017 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import argparse
+import extras
+import lockfile.pidlockfile
+import logging
+import os
+import signal
+import sys
+
+import zuul.cmd
+import zuul.streamer
+
+# as of python-daemon 1.6 it doesn't bundle pidlockfile anymore
+# instead it depends on lockfile-0.9.1 which uses pidfile.
+pid_file_module = extras.try_imports(['daemon.pidlockfile', 'daemon.pidfile'])
+
+
+class WebStreamer(zuul.cmd.ZuulApp):
+
+    def parse_arguments(self):
+        parser = argparse.ArgumentParser(description='Zuul Web Log Streamer.')
+        parser.add_argument('-c', dest='config',
+                            help='specify the config file')
+        parser.add_argument('-d', dest='nodaemon', action='store_true',
+                            help='do not run as a daemon')
+        parser.add_argument('--version', dest='version', action='version',
+                            version=self._get_version(),
+                            help='show zuul version')
+        self.args = parser.parse_args()
+
+    def exit_handler(self, signum, frame):
+        self.streamer.stop()
+
+    def _main(self):
+        signal.signal(signal.SIGUSR1, self.exit_handler)
+        signal.signal(signal.SIGTERM, self.exit_handler)
+        signal.signal(signal.SIGINT, self.exit_handler)  # Ctrl+C
+
+        params = dict()
+
+        if self.config.has_option('streamer', 'gearman_server'):
+            params['gear_server'] = self.config.get('streamer',
+                                                    'gearman_server')
+
+        if self.config.has_option('streamer', 'gearman_port'):
+            params['gear_port'] = self.config.get('streamer', 'gearman_port')
+
+        if self.config.has_option('streamer', 'listen_address'):
+            params['listen_address'] = self.config.get('streamer',
+                                                       'listen_address')
+        if self.config.has_option('streamer', 'port'):
+            params['listen_port'] = self.config.get('streamer', 'port')
+
+        try:
+            self.streamer = zuul.streamer.ZuulStreamer(**params)
+        except Exception as e:
+            self.log.exception("Error creating ZuulStreamer:")
+            sys.exit(1)
+
+        self.log.info('Starting Zuul Web Streamer')
+
+        self.streamer.start()
+        # try:
+        #     self.streamer.start()
+        # except KeyboardInterrupt:
+        #     print("Ctrl + C: asking streamer to exit nicely...\n")
+        #     self.exit_handler(None, None)
+
+        self.log.info("Zuul Web Streamer quitting")
+
+    def main(self):
+        self.setup_logging('streamer', 'log_config')
+        self.log = logging.getLogger("zuul.WebStreamer")
+
+        try:
+            self._main()
+        except Exception:
+            self.log.exception("Exception from WebStreamer:")
+
+
+def main():
+    server = WebStreamer()
+    server.parse_arguments()
+    server.read_config()
+
+    if server.config.has_option('streamer', 'pidfile'):
+        pid_fn = os.path.expanduser(server.config.get('streamer', 'pidfile'))
+    else:
+        pid_fn = '/var/run/zuul-web-streamer/zuul-web-streamer.pid'
+
+    if server.args.nodaemon:
+        server.main()
+    else:
+        # NOTE(Shrews): The python-daemon library, normally used for the
+        # Zuul daemons, doesn't seem to play nicely with either the autobahn
+        # library or asyncio itself.
+        child_pid = os.fork()
+        if child_pid == 0:
+            pidfile = lockfile.pidlockfile.PIDLockFile(pid_fn)
+            pidfile.acquire()
+            server.main()
+            pidfile.release()
+        else:
+            print("Started process %s" % child_pid)
+
+
+if __name__ == "__main__":
+    sys.path.insert(0, '.')
+    main()
