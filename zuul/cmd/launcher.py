9c078148571258f952fdfd91cfe774ee2d2d042f
Morgan Fainberg <morgan.fainberg@gmail.com>
2016-05-27 08:42:17 -0700

    Python 3 Fixes: Use print() not print
    For Python 3 fixes, use print() instead of the python 2 "print"
    bareword syntax.
    
    Change-Id: Ib77544d111aab6a1bd52555bcbd1dad9955d0074

--- zuul/cmd/launcher.py
+++ zuul/cmd/launcher.py
@@ -87,7 +87,7 @@ class Launcher(zuul.cmd.ZuulApp):
             try:
                 signal.pause()
             except KeyboardInterrupt:
-                print "Ctrl + C: asking launcher to exit nicely...\n"
+                print("Ctrl + C: asking launcher to exit nicely...\n")
                 self.exit_handler(signal.SIGINT, None)
                 sys.exit(0)
 
