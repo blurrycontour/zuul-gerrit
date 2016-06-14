7a8d5561ed7f4badbe811a5bc874b6a57aff0c77
James E. Blair <jeblair@redhat.com>
2016-06-14 10:12:29 -0700

    Ansible launcher: Remove unused plugins dir
    This was being set up in the jobdir, but not used.
    
    Change-Id: I44017b0f2bbcabe4d5fde98f68c299f5f0d7247f

--- zuul/launcher/ansiblelaunchserver.py
+++ zuul/launcher/ansiblelaunchserver.py
@@ -95,8 +95,6 @@ class JobDir(object):
         os.makedirs(self.logs)
         self.ansible_root = os.path.join(self.root, 'ansible')
         os.makedirs(self.ansible_root)
-        self.plugins_root = os.path.join(self.ansible_root, 'plugins')
-        os.makedirs(self.plugins_root)
         self.inventory = os.path.join(self.ansible_root, 'inventory')
         self.playbook = os.path.join(self.ansible_root, 'playbook')
         self.post_playbook = os.path.join(self.ansible_root, 'post_playbook')
