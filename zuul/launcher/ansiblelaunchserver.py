e50a1dff4c03e58ab64fe17e38a02e89ff50993b
Paul Belanger <pabelanger@redhat.com>
2016-06-18 12:47:58 -0400

    Remove unused timeout args for _makeBuilderTask
    Change-Id: If8c6aca0735ca2716fe8402d5665c10973593344
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- zuul/launcher/ansiblelaunchserver.py
+++ zuul/launcher/ansiblelaunchserver.py
@@ -1007,7 +1007,7 @@ class NodeWorker(object):
         tasks.append(task)
         return tasks
 
-    def _makeBuilderTask(self, jobdir, builder, parameters, timeout):
+    def _makeBuilderTask(self, jobdir, builder, parameters):
         tasks = []
         script_fn = '%s.sh' % str(uuid.uuid4().hex)
         script_path = os.path.join(jobdir.script_root, script_fn)
@@ -1126,8 +1126,7 @@ class NodeWorker(object):
             for builder in jjb_job.get('builders', []):
                 if 'shell' in builder:
                     main_block.extend(
-                        self._makeBuilderTask(jobdir, builder,
-                                              parameters, timeout))
+                        self._makeBuilderTask(jobdir, builder, parameters))
             task = dict(zuul_log=dict(msg="Job complete, result: SUCCESS"))
             main_block.append(task)
 
