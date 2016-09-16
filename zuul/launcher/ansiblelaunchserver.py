47255716af02d162232b335e3f1aefa6f8b65341
Monty Taylor <mordred@inaugust.com>
2016-09-16 13:15:28 -0400

    Split playbook into vars, pre-playbook and playbook
    In order to get to the point where playbooks that people write for tests
    are playbooks that they could conceivably also use outside of the zuul
    context, we need to remove the need for zuul-specific things in the main
    playbook.
    
    Add a pre-playbook that runs before the playbook and runs the things
    that are not tied to current JJB content - namely setting up the logger
    and prepping directories.
    
    Move the SUCCESS/FAILURE message to the post-playbook.
    
    Extract the injected variables into a variables file and add a
    -e@vars.yaml option to the playbook invocation. This provides variables
    in a known namespace. Obviously there is still an exercise in how a user
    might write a playbook that wants to consume those variables in some
    way.
    
    Change-Id: Ie5ec6ec65a03ceea9afc3ac59df73cb28f5ca4dd

--- zuul/launcher/ansiblelaunchserver.py
+++ zuul/launcher/ansiblelaunchserver.py
@@ -40,6 +40,7 @@ from zuul.lib import commandsocket
 
 ANSIBLE_WATCHDOG_GRACE = 5 * 60
 ANSIBLE_DEFAULT_TIMEOUT = 2 * 60 * 60
+ANSIBLE_DEFAULT_PRE_TIMEOUT = 10 * 60
 ANSIBLE_DEFAULT_POST_TIMEOUT = 10 * 60
 
 
@@ -150,6 +151,8 @@ class JobDir(object):
         os.makedirs(self.ansible_root)
         self.known_hosts = os.path.join(self.ansible_root, 'known_hosts')
         self.inventory = os.path.join(self.ansible_root, 'inventory')
+        self.vars = os.path.join(self.ansible_root, 'vars.yaml')
+        self.pre_playbook = os.path.join(self.ansible_root, 'pre_playbook')
         self.playbook = os.path.join(self.ansible_root, 'playbook')
         self.post_playbook = os.path.join(self.ansible_root, 'post_playbook')
         self.config = os.path.join(self.ansible_root, 'ansible.cfg')
@@ -637,6 +640,7 @@ class NodeWorker(object):
         self._aborted_job = False
         self._watchdog_timeout = False
         self._sent_complete_event = False
+        self.ansible_pre_proc = None
         self.ansible_job_proc = None
         self.ansible_post_proc = None
         self.workspace_root = config.get('launcher', 'workspace_root')
@@ -917,6 +921,7 @@ class NodeWorker(object):
                                'SUCCESS', {})
 
     def runJob(self, job, args):
+        self.ansible_pre_proc = None
         self.ansible_job_proc = None
         self.ansible_post_proc = None
         result = None
@@ -944,6 +949,12 @@ class NodeWorker(object):
 
             job.sendWorkData(json.dumps(data))
             job.sendWorkStatus(0, 100)
+
+            pre_status = self.runAnsiblePrePlaybook(jobdir)
+            if pre_status is None:
+                # These should really never fail, so return None and have
+                # zuul try again
+                return result
 
             job_status = self.runAnsiblePlaybook(jobdir, timeout)
             if job_status is None:
@@ -1034,7 +1045,7 @@ class NodeWorker(object):
                 syncargs['rsync_opts'] = rsync_opts
             task = dict(synchronize=syncargs)
             if not scpfile.get('copy-after-failure'):
-                task['when'] = 'success'
+                task['when'] = 'success|bool'
             task.update(self.retry_args)
             tasks.append(task)
 
@@ -1078,7 +1089,7 @@ class NodeWorker(object):
         task = dict(shell=shellargs,
                     delegate_to='127.0.0.1')
         if not scpfile.get('copy-after-failure'):
-            task['when'] = 'success'
+            task['when'] = 'success|bool'
 
         return task
 
@@ -1107,11 +1118,11 @@ class NodeWorker(object):
         if rsync_opts:
             syncargs['rsync_opts'] = rsync_opts
         task = dict(synchronize=syncargs,
-                    when='success')
+                    when='success|bool')
         task.update(self.retry_args)
         tasks.append(task)
         task = dict(shell='lftp -f %s' % ftpscript,
-                    when='success',
+                    when='success|bool',
                     delegate_to='127.0.0.1')
         ftpsource = ftpcontent
         if ftp.get('remove-prefix'):
@@ -1172,7 +1183,7 @@ class NodeWorker(object):
         if rsync_opts:
             syncargs['rsync_opts'] = rsync_opts
         task = dict(synchronize=syncargs,
-                    when='success')
+                    when='success|bool')
         task.update(self.retry_args)
         tasks.append(task)
 
@@ -1200,7 +1211,7 @@ class NodeWorker(object):
         # content at the root *and* at a tag location).
         task = dict(shell=find_pipe.format(path=afssource,
                                            file=src_markers_file),
-                    when='success',
+                    when='success|bool',
                     delegate_to='127.0.0.1')
         tasks.append(task)
 
@@ -1208,7 +1219,7 @@ class NodeWorker(object):
         # published site.
         task = dict(shell=find_pipe.format(path=afstarget,
                                            file=dst_markers_file),
-                    when='success',
+                    when='success|bool',
                     delegate_to='127.0.0.1')
         tasks.append(task)
 
@@ -1220,7 +1231,7 @@ class NodeWorker(object):
             dst=dst_markers_file,
             exclude=exclude_file)
         task = dict(shell=exclude_command,
-                    when='success',
+                    when='success|bool',
                     delegate_to='127.0.0.1')
         tasks.append(task)
 
@@ -1246,7 +1257,7 @@ class NodeWorker(object):
                        src=src_markers_file,
                        filter=filter_file))
         task = dict(shell=command,
-                    when='success',
+                    when='success|bool',
                     delegate_to='127.0.0.1')
         tasks.append(task)
 
@@ -1262,7 +1273,7 @@ class NodeWorker(object):
                        exclude=exclude_file,
                        filter=filter_file))
         task = dict(shell=command,
-                    when='success',
+                    when='success|bool',
                     delegate_to='127.0.0.1')
         tasks.append(task)
 
@@ -1280,7 +1291,7 @@ class NodeWorker(object):
                        exclude=exclude_file,
                        filter=filter_file))
         task = dict(shell=command,
-                    when='success',
+                    when='success|bool',
                     delegate_to='127.0.0.1')
         tasks.append(task)
 
@@ -1309,7 +1320,7 @@ class NodeWorker(object):
             keytab=site['keytab'])
 
         task = dict(shell=shellargs,
-                    when='success',
+                    when='success|bool',
                     delegate_to='127.0.0.1')
         tasks.append(task)
 
@@ -1321,9 +1332,8 @@ class NodeWorker(object):
         (executable, shell) = deal_with_shebang(builder['shell'])
 
         task = dict(shell=shell)
-        task['name'] = ('command with {{ timeout | int - elapsed_time }} '
-                        'second timeout')
-        task['environment'] = parameters
+        task['name'] = "shell task imported from JJB"
+        task['environment'] = "{{ zuul.environment }}"
         task['args'] = dict(chdir=parameters['WORKSPACE'])
         if executable:
             task['args']['executable'] = executable
@@ -1389,53 +1399,53 @@ class NodeWorker(object):
         if timeout_var:
             parameters[timeout_var] = str(timeout * 1000)
 
-        with open(jobdir.playbook, 'w') as playbook:
-            pre_tasks = []
-            tasks = []
-            main_block = []
-            error_block = []
-            variables = []
+        with open(jobdir.vars, 'w') as vars_yaml:
+            variables = dict(
+                timeout=timeout,
+                environment=parameters,
+            )
+            zuul_vars = dict(zuul=variables)
+            vars_yaml.write(
+                yaml.safe_dump(zuul_vars, default_flow_style=False))
+
+        with open(jobdir.pre_playbook, 'w') as pre_playbook:
 
             shellargs = "ssh-keyscan {{ ansible_host }} > %s" % (
                 jobdir.known_hosts)
-            pre_tasks.append(dict(shell=shellargs,
-                             delegate_to='127.0.0.1'))
-
-            tasks.append(dict(block=main_block,
-                              rescue=error_block))
+            tasks = []
+            tasks.append(dict(shell=shellargs, delegate_to='127.0.0.1'))
 
             task = dict(file=dict(path='/tmp/console.html', state='absent'))
-            main_block.append(task)
+            tasks.append(task)
 
             task = dict(zuul_console=dict(path='/tmp/console.html',
                                           port=19885))
-            main_block.append(task)
+            tasks.append(task)
 
             task = dict(file=dict(path=parameters['WORKSPACE'],
                                   state='directory'))
-            main_block.append(task)
+            tasks.append(task)
 
             msg = [
                 "Launched by %s" % self.manager_name,
                 "Building remotely on %s in workspace %s" % (
                     self.name, parameters['WORKSPACE'])]
             task = dict(zuul_log=dict(msg=msg))
-            main_block.append(task)
+            tasks.append(task)
+
+            play = dict(hosts='node', name='Job setup', tasks=tasks)
+            pre_playbook.write(
+                yaml.safe_dump([play], default_flow_style=False))
+
+        with open(jobdir.playbook, 'w') as playbook:
+            tasks = []
 
             for builder in jjb_job.get('builders', []):
                 if 'shell' in builder:
-                    main_block.extend(
+                    tasks.extend(
                         self._makeBuilderTask(jobdir, builder, parameters))
-            task = dict(zuul_log=dict(msg="Job complete, result: SUCCESS"))
-            main_block.append(task)
 
-            task = dict(zuul_log=dict(msg="Job complete, result: FAILURE"))
-            error_block.append(task)
-            error_block.append(dict(fail=dict(msg='FAILURE')))
-
-            variables.append(dict(timeout=timeout))
-            play = dict(hosts='node', name='Job body', vars=variables,
-                        pre_tasks=pre_tasks, tasks=tasks)
+            play = dict(hosts='node', name='Job body', tasks=tasks)
             playbook.write(yaml.safe_dump([play], default_flow_style=False))
 
         early_publishers, late_publishers = self._transformPublishers(jjb_job)
@@ -1461,6 +1471,14 @@ class NodeWorker(object):
             # we run the log publisher regardless of whether the rest
             # of the publishers succeed.
             tasks = []
+
+            task = dict(zuul_log=dict(msg="Job complete, result: SUCCESS"),
+                        when='success|bool')
+            blocks[0].insert(0, task)
+            task = dict(zuul_log=dict(msg="Job complete, result: FAILURE"),
+                        when='not success|bool')
+            blocks[0].insert(0, task)
+
             tasks.append(dict(block=blocks[0],
                               always=blocks[1]))
 
@@ -1499,6 +1517,46 @@ class NodeWorker(object):
         self.log.warning(msg)
         self.abortRunningProc(proc)
 
+    def runAnsiblePrePlaybook(self, jobdir):
+        # Set LOGNAME env variable so Ansible log_path log reports
+        # the correct user.
+        env_copy = os.environ.copy()
+        env_copy['LOGNAME'] = 'zuul'
+
+        if self.options['verbose']:
+            verbose = '-vvv'
+        else:
+            verbose = '-v'
+
+        cmd = ['ansible-playbook', jobdir.pre_playbook,
+               '-e@%s' % jobdir.vars, verbose]
+        self.log.debug("Ansible pre command: %s" % (cmd,))
+
+        self.ansible_pre_proc = subprocess.Popen(
+            cmd,
+            cwd=jobdir.ansible_root,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            preexec_fn=os.setsid,
+            env=env_copy,
+        )
+        ret = None
+        watchdog = Watchdog(ANSIBLE_DEFAULT_PRE_TIMEOUT,
+                            self._ansibleTimeout,
+                            (self.ansible_pre_proc,
+                             "Ansible pre timeout exceeded"))
+        watchdog.start()
+        try:
+            for line in iter(self.ansible_pre_proc.stdout.readline, b''):
+                line = line[:1024].rstrip()
+                self.log.debug("Ansible pre output: %s" % (line,))
+            ret = self.ansible_pre_proc.wait()
+        finally:
+            watchdog.stop()
+        self.log.debug("Ansible pre exit code: %s" % (ret,))
+        self.ansible_pre_proc = None
+        return ret == 0
+
     def runAnsiblePlaybook(self, jobdir, timeout):
         # Set LOGNAME env variable so Ansible log_path log reports
         # the correct user.
@@ -1510,7 +1568,8 @@ class NodeWorker(object):
         else:
             verbose = '-v'
 
-        cmd = ['ansible-playbook', jobdir.playbook, verbose]
+        cmd = ['ansible-playbook', jobdir.playbook, verbose,
+               '-e@%s' % jobdir.vars]
         self.log.debug("Ansible command: %s" % (cmd,))
 
         self.ansible_job_proc = subprocess.Popen(
@@ -1559,7 +1618,9 @@ class NodeWorker(object):
             verbose = '-v'
 
         cmd = ['ansible-playbook', jobdir.post_playbook,
-               '-e', 'success=%s' % success, verbose]
+               '-e', 'success=%s' % success,
+               '-e@%s' % jobdir.vars,
+               verbose]
         self.log.debug("Ansible post command: %s" % (cmd,))
 
         self.ansible_post_proc = subprocess.Popen(
