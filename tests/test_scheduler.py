cea053d90e2408453394cee7fdb74f5bab0b108a
Paul Belanger <pabelanger@redhat.com>
2016-11-03 15:24:16 -0400

    Enable test_new_patchset_check / test_abandoned_check
    Update tests twith new zuulv3 syntax for tenants.
    
    Change-Id: Icaca7c7a894e8b8f69705a1381d5ceb846e892c1
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1730,7 +1730,6 @@ jobs:
         self.assertEqual(D.reported, 2)
         self.assertEqual(len(self.history), 9)  # 3 each for A, B, D.
 
-    @skip("Disabled for early v3 development")
     def test_new_patchset_check(self):
         "Test a new patchset in check"
 
@@ -1738,7 +1737,8 @@ jobs:
 
         A = self.fake_gerrit.addFakeChange('org/project', 'master', 'A')
         B = self.fake_gerrit.addFakeChange('org/project', 'master', 'B')
-        check_pipeline = self.sched.layout.pipelines['check']
+        tenant = self.sched.abide.tenants.get('tenant-one')
+        check_pipeline = tenant.layout.pipelines['check']
 
         # Add two git-dependent changes
         B.setDependsOn(A, 1)
@@ -1851,7 +1851,6 @@ jobs:
         self.assertEqual(A.reported, 1,
                          "Abandoned gate change should report only start")
 
-    @skip("Disabled for early v3 development")
     def test_abandoned_check(self):
         "Test that an abandoned change is dequeued from check"
 
@@ -1859,7 +1858,8 @@ jobs:
 
         A = self.fake_gerrit.addFakeChange('org/project', 'master', 'A')
         B = self.fake_gerrit.addFakeChange('org/project', 'master', 'B')
-        check_pipeline = self.sched.layout.pipelines['check']
+        tenant = self.sched.abide.tenants.get('tenant-one')
+        check_pipeline = tenant.layout.pipelines['check']
 
         # Add two git-dependent changes
         B.setDependsOn(A, 1)
