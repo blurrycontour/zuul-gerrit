9c1c8c11dcd606f14cd3f27bcb9639ae504c289d
Adam Gandelman <adamg@ubuntu.com>
2016-11-04 13:20:27 -0700

    Re-enable test_new_patchset_dequeues_old* tests
    This re-enables four old tests that are fixed by ensuring the check
    pipeline has jobs defined in the config fixture:
    
     test_new_patchset_dequeues_old
     test_new_patchset_dequeues_old_on_head
     test_new_patchset_dequeues_old_without_dependents
     test_new_patchset_dequeues_old_independent_queue
    
    Change-Id: Ib50000aa7131996dea0f8e86a8bad57227bfff28

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1734,7 +1734,6 @@ jobs:
 
         self.assertEqual(len(self.history), 0)
 
-    @skip("Disabled for early v3 development")
     def test_new_patchset_dequeues_old(self):
         "Test that a new patchset causes the old to be dequeued"
         # D -> C (depends on B) -> B (depends on A) -> A -> M
@@ -2009,7 +2008,6 @@ jobs:
         self.launch_server.release()
         self.waitUntilSettled()
 
-    @skip("Disabled for early v3 development")
     def test_new_patchset_dequeues_old_on_head(self):
         "Test that a new patchset causes the old to be dequeued (at head)"
         # D -> C (depends on B) -> B (depends on A) -> A -> M
@@ -2053,7 +2051,6 @@ jobs:
         self.assertEqual(D.reported, 2)
         self.assertEqual(len(self.history), 7)
 
-    @skip("Disabled for early v3 development")
     def test_new_patchset_dequeues_old_without_dependents(self):
         "Test that a new patchset causes only the old to be dequeued"
         self.launch_server.hold_jobs_in_build = True
@@ -2085,7 +2082,6 @@ jobs:
         self.assertEqual(C.reported, 2)
         self.assertEqual(len(self.history), 9)
 
-    @skip("Disabled for early v3 development")
     def test_new_patchset_dequeues_old_independent_queue(self):
         "Test that a new patchset causes the old to be dequeued (independent)"
         self.launch_server.hold_jobs_in_build = True
