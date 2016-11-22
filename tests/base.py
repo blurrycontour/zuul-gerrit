a2248622dedb5b37f215925620ca36b1a6a2bf3c
Adam Gandelman <adamg@ubuntu.com>
2016-11-21 16:43:14 -0800

    Re-enable test_success_pattern as test_success_url
    This re-enables test_success_pattern.  This job parameter has been renamed to
    success-url and the test has been reworked and renamed accordingly.
    
    Change-Id: I4ef00923ed916d08e4edb3a24fcb2e25bca823b0
    Story: 2000773
    Task: 3380

--- tests/base.py
+++ tests/base.py
@@ -1011,6 +1011,7 @@ class ZuulTestCase(BaseTestCase):
         self.init_repo("org/noop-project")
         self.init_repo("org/experimental-project")
         self.init_repo("org/no-jobs-project")
+        self.init_repo("org/docs")
 
         self.statsd = FakeStatsd()
         # note, use 127.0.0.1 rather than localhost to avoid getting ipv6
