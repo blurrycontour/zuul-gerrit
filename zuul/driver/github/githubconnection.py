26204b0aeaf89766b43dc814e5eacb365c82c243
Jesse Keating <omgjlk@us.ibm.com>
2016-12-06 11:29:13 -0800

    Add support for github enterprise
    GitHub enterprise just requires setting a git_host to something other
    than the default of 'github.com'. If this host is set to something else,
    perform an enterprise_login() instead of a login(). All other activity
    appears to be the same.
    
    A more complete change would be allowing clone/push using the API key
    instead of the ssh key, which would reduce the amount of credentials one
    must manage on the zuul host.
    
    Change-Id: I4db7677cf5ff0bf798e20dcbc327e1dd874a2e7e

--- zuul/driver/github/githubconnection.py
+++ zuul/driver/github/githubconnection.py
@@ -238,7 +238,6 @@ class GithubConnection(BaseConnection):
     log = logging.getLogger("connection.github")
     payload_path = 'payload'
     git_user = 'git'
-    git_host = 'github.com'
 
     def __init__(self, driver, connection_name, connection_config):
         super(GithubConnection, self).__init__(
@@ -247,6 +246,7 @@ class GithubConnection(BaseConnection):
         self._change_cache = {}
         self.projects = {}
         self._git_ssh = bool(self.connection_config.get('sshkey', None))
+        self.git_host = self.connection_config.get('git_host', 'github.com')
 
     def onLoad(self):
 
@@ -261,7 +261,11 @@ class GithubConnection(BaseConnection):
     def _authenticateGithubAPI(self):
         token = self.connection_config.get('api_token', None)
         if token is not None:
-            self.github = github3.login(token=token)
+            if self.git_host != 'github.com':
+                url = 'https://%s/' % self.git_host
+                self.github = github3.enterprise_login(token=token, url=url)
+            else:
+                self.github = github3.login(token=token)
             self.log.info("Github API Authentication successful.")
         else:
             self.github = None
