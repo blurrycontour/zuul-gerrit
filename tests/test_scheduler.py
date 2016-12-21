236c8ed6f9c50c67c49d565c495c36d1a32750a1
Clint Byrum <clint@fewbar.com>
2016-12-21 12:21:07 -0800

    Re-enable test_dependent_behind_dequeue
    The number of pipelines and thus, jobs, that run is less in this new v3
    common configuration than it was in master. Notably, in v3, we
    only see 'check', 'gate', 'experimental', and 'post'. But in master we
    also have dup1, dup2, conflict, and unused.
    
    Meanwhile, it appears that the jobs that should abort, do, and the ones
    that should not, do not.
    
    Change-Id: Iefb257ea6477d504c78d40dd411ac9eaf3125ae5
    Story: 2000773
    Task: 3436

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -286,6 +286,7 @@ class TestScheduler(ZuulTestCase):
             dict(name='project1-project2-integration', changes='2,1'),
             dict(name='project-test1', changes='2,1 3,1'),
             dict(name='project-test2', changes='2,1 3,1'),
+            dict(name='project1-project2-integration', changes='2,1 3,1'),
         ])
 
         self.orderedRelease()
@@ -303,6 +304,9 @@ class TestScheduler(ZuulTestCase):
                 changes='2,1'),
             dict(name='project-test1', result='SUCCESS', changes='2,1 3,1'),
             dict(name='project-test2', result='SUCCESS', changes='2,1 3,1'),
+            dict(name='project1-project2-integration',
+                 result='SUCCESS',
+                 changes='2,1 3,1'),
         ])
 
         self.assertEqual(A.data['status'], 'MERGED')
@@ -1393,7 +1397,6 @@ class TestScheduler(ZuulTestCase):
         self.assertEqual(self.getJobFromHistory('project-test2').result,
                          'FAILURE')
 
-    @skip("Disabled for early v3 development")
     def test_dependent_behind_dequeue(self):
         "test that dependent changes behind dequeued changes work"
         # This complicated test is a reproduction of a real life bug
