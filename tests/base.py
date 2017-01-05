f07513c3b18ad5ddb4199f23a9a0e7b3dcdaf370
James E. Blair <jeblair@redhat.com>
2017-01-05 13:38:07 -0800

    Remove excess printing from stats test
    This isn't necessary and makes the output of this test noisy.
    
    Change-Id: I214d40fada9567ac6b9cee5ff9cdc748a472cbbb

--- tests/base.py
+++ tests/base.py
@@ -1616,7 +1616,6 @@ class ZuulTestCase(BaseTestCase):
         start = time.time()
         while time.time() < (start + 5):
             for stat in self.statsd.stats:
-                pprint.pprint(self.statsd.stats)
                 k, v = stat.split(':')
                 if key == k:
                     if value is None and kind is None:
@@ -1629,7 +1628,6 @@ class ZuulTestCase(BaseTestCase):
                             return
             time.sleep(0.1)
 
-        pprint.pprint(self.statsd.stats)
         raise Exception("Key %s not found in reported stats" % key)
 
     def assertBuilds(self, builds):
