d128b9f7af988d4c6120e97c891bc23f1cbfffa9
James E. Blair <jeblair@redhat.com>
2017-01-05 11:17:28 -0800

    Verify nodes and requests are not leaked
    Check that at the end of every test, there are no outstanding
    nodepool requests and no locked nodes.
    
    Fix some typos uncovered by this testing.
    
    Normalize the debug messages in nodepool.py.
    
    Change-Id: I32f6807ac95034fc2636993824f4a45ffe7c59d8

--- zuul/manager/__init__.py
+++ zuul/manager/__init__.py
@@ -405,7 +405,7 @@ class PipelineManager(object):
             if not was_running:
                 try:
                     nodeset = build.build_set.getJobNodeSet(build.job.name)
-                    self.nodepool.returnNodeset(nodeset)
+                    self.sched.nodepool.returnNodeset(nodeset)
                 except Exception:
                     self.log.exception("Unable to return nodeset %s for "
                                        "canceled build request %s" %
@@ -611,7 +611,7 @@ class PipelineManager(object):
 
         try:
             nodeset = build.build_set.getJobNodeSet(build.job.name)
-            self.nodepool.returnNodeset(nodeset)
+            self.sched.nodepool.returnNodeset(nodeset)
         except Exception:
             self.log.exception("Unable to return nodeset %s" % (nodeset,))
 
