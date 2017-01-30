03fce7694ca78dac09a8bfe1267044008585674c
Paul Belanger <pabelanger@redhat.com>
2017-01-30 08:46:55 -0500

    WIP: Add reporter for Federated Message Bus (fedmsg)
    Fedmsg is a bus that is heavily used by the Fedora project. By adding
    support for fedmsg, we can start to do cool things with the Fedora
    projects infrastructure.  As an example, fedmsg-notify could be used
    to provide desktop notifications.
    
    Change-Id: Id0fa74bd81c3f809a2ec4fb03e960e2e2691c675
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

---
+++ zuul/driver/fedmsg/fedmsgreporter.py
@@ -0,0 +1,71 @@
+# Copyright 2017 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+import voluptuous as v
+
+from zuul.reporter import BaseReporter
+
+
+class FedmsgReporter(BaseReporter):
+    """Publish messages to a topic via fedmsg."""
+
+    name = 'fedmsg'
+    log = logging.getLogger("zuul.reporter.fedmsg.Reporter")
+
+    def report(self, source, pipeline, item):
+        """Publish messages to a topic via fedmsg."""
+
+        self.log.debug("Report change %s, params %s" %
+                       (item.change, self.config))
+        message = {
+            'zuul_ref': item.current_build_set.ref,
+            'pipeline': item.pipeline.name,
+            'project': item.change.project.name,
+            'change': item.change.number,
+            'patchset': item.change.patchset,
+            'ref': item.change.refspec,
+            'message': self._formatItemReport(
+                pipeline, item),
+        }
+        self.connection.publish(self.config['topic'], message)
+
+        job_tree = pipeline.getJobTree(item.change.project)
+        for job in job_tree.getJobs():
+            build = item.current_build_set.getBuild(job.name)
+            if not build:
+                # build hasn't began. The sql reporter can only send back
+                # stats about builds. It doesn't understand how to store
+                # information about the change.
+                continue
+            (result, url) = item.formatJobResult(job)
+            message = {
+                'uuid': build.uuid,
+                'job_name': build.job.name,
+                'result': result,
+                'start_time': build.start_time,
+                'end_time': build.end_time,
+                'voting': build.job.voting,
+                'log_url': url,
+                'node_name': build.node_name,
+            }
+            topic = '%s.%s' % (self.config['topic'], 'job')
+            self.connection.publish(topic, message)
+
+
+def getSchema():
+    fedmsg_reporter = v.Schema({
+        'topic': str,
+    })
+    return fedmsg_reporter
