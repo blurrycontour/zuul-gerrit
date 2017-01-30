3c7bf333fd7c5c25aba43abe9c6ca6f1a26e1a90
Paul Belanger <pabelanger@redhat.com>
2017-01-30 08:46:55 -0500

    [WIP] Add reporter for Federated Message Bus (fedmsg)
    Fedmsg is a bus that is heavily used by the Fedora project. By adding
    support for fedmsg, we can start to do cool things with the Fedora
    projects infrastructure.  As an example, fedmsg-notify could be used
    to provide desktop notifications.
    
    Change-Id: Id0fa74bd81c3f809a2ec4fb03e960e2e2691c675
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- tools/nodepool-integration-setup.sh
+++ tools/nodepool-integration-setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -xe
 
+# Install bindep.txt for zuul
+/usr/local/jenkins/slave_scripts/install-distro-packages.sh
+
 /usr/zuul-env/bin/zuul-cloner --workspace /tmp --cache-dir /opt/git \
     git://git.openstack.org openstack-infra/nodepool
 
@@ -8,5 +11,9 @@ ln -s /tmp/nodepool/log $WORKSPACE/logs
 cd /tmp/openstack-infra/nodepool
 /usr/local/jenkins/slave_scripts/install-distro-packages.sh
 sudo pip install .
+
+# Launch fedmsg-tail in screen
+sudo pip install fedmsg[commands]
+screen -dm -S fedmsg-tail "fedmsg-tail --config-filename tools/fedmsg.d/config.py | tee $WORKSPACE/logs/fedmsg-tail.log"
 
 bash -xe ./tools/zuul-nodepool-integration/start.sh
