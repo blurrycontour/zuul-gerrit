8b1bf01f1130ed98567497fa146812f34a6ce53e
James E. Blair <jeblair@redhat.com>
2017-02-22 15:17:07 -0500

    Expose the jobdir git root as a variable
    This way the pre-playbook that pushes the git repo onto the worker
    knows where to find the git repos.
    
    Change-Id: Id7c924540add867e720a77a019e86a842e27457b

--- zuul/launcher/server.py
+++ zuul/launcher/server.py
@@ -776,7 +776,8 @@ class AnsibleJob(object):
                         self.jobdir.known_hosts))
 
         with open(self.jobdir.vars, 'w') as vars_yaml:
-            zuul_vars = dict(zuul=args['zuul'])
+            zuul_vars = dict(zuul=args['zuul'],
+            zuul_vars['zuul']['jobdir'] = dict(git_root=self.jobdir.git_root)
             vars_yaml.write(
                 yaml.safe_dump(zuul_vars, default_flow_style=False))
         self.writeAnsibleConfig(self.jobdir.config)
