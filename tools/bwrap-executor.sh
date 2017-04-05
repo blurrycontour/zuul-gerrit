83545be09101489ddfedacd0a188ea09ad25fd4f
Clint Byrum <clint@fewbar.com>
2017-04-05 13:02:59 -0700

    Create wrapper script for executor
    This will be the minimum "batteries included" bubblwrap script. It does
    not do any MAC configuration, since these vary by system. Operators
    may wish to wrap this in a MAC wrapper. It also does not setup seccomp,
    but it should at some point once we learn what is necessary for running
    playbooks.
    
    Change-Id: I52bb15d744f1ca7cba96be54a91d29b81b47b6b7

---
+++ tools/bwrap-executor.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# The intention here is to isolate anything the executor must run as much as
+# we can within reason.
+# Explanation from less obvious things follows:
+#
+# * Ansible and SSH both require access to /dev/null so we need --dev /dev
+# * Ansible wants to read /proc, so we need --proc /proc
+# * 65534 is the nobody/nogroup UID/GID
+# * work_dir is bind mounted and intentionally writable. It should contain
+#   any and all data that playbooks need, and will be used for temporary
+#   storage of artifacts between playbook phases.
+#
+
+work_dir=$1
+shift
+
+# We still run as ourselves
+GID=$(id -g)
+
+set -euo pipefail
+
+(exec bwrap --dir /tmp \
+      --tmpfs /tmp \
+      --dir /var \
+      --dir /var/tmp \
+      --dir /run/user/${UID} \
+      --ro-bind /usr /usr \
+      --ro-bind /lib /lib \
+      --ro-bind /lib64 /lib64 \
+      --ro-bind /bin /bin \
+      --ro-bind /sbin /sbin \
+      --ro-bind /etc/resolv.conf /etc/resolv.conf \
+      --dir ${work_dir} \
+      --bind ${work_dir} ${work_dir} \
+      --dev /dev \
+      --dir ${HOME} \
+      --chdir / \
+      --unshare-all \
+      --share-net \
+      --uid ${UID} \
+      --gid ${GID} \
+      --file 11 /etc/passwd \
+      --file 12 /etc/group \
+      "$@") \
+      11< <(getent passwd ${UID} 65534) \
+      12< <(getent group ${GID} 65534)
