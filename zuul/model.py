3e3b4de3f23e965273e4d39bdab00b34b929f172
James E. Blair <jeblair@redhat.com>
2017-04-05 11:27:11 -0700

    Remove source from pipelines (1/2)
    Now that sources are associated with projects, remove the source
    attribute from pipelines altogether.
    
    For ease of review, the test fixtures are updated in a subsequent
    commit.
    
    Story: 2000953
    Change-Id: Ie6b3b6be1b11b0d4462188902121d9cd29c91284

--- zuul/model.py
+++ zuul/model.py
@@ -97,11 +97,7 @@ def normalizeCategory(name):
 
 
 class Pipeline(object):
-    """A configuration that ties triggers, reporters, managers and sources.
-
-    Source
-        Where changes should come from. It is a named connection to
-        an external service defined in zuul.conf
+    """A configuration that ties together triggers, reporters and managers
 
     Trigger
         A description of which events should be processed
@@ -127,7 +123,6 @@ class Pipeline(object):
         self.manager = None
         self.queues = []
         self.precedence = PRECEDENCE_NORMAL
-        self.source = None
         self.triggers = []
         self.start_actions = []
         self.success_actions = []
