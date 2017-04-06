ad1d774dfb46c33f3ae2b87f8c2ee579a503d996
James E. Blair <jeblair@redhat.com>
2017-04-06 16:11:44 -0700

    Add some lookup plugin tests
    This is not complete, but it's a start.
    
    Change-Id: Ic84d8265e6fd7e15a0e5d66c781409a087d761d7

--- tests/unit/test_v3.py
+++ tests/unit/test_v3.py
@@ -671,6 +671,45 @@ class TestAnsible(AnsibleZuulTestCase):
             self.assertIn(fail.format("timeout", build_timeout.uuid), msg)
             self.assertIn(fail.format("failpost", build_failpost.uuid), msg)
 
+    def _add_job(self, job):
+        conf = textwrap.dedent(
+            """
+            - job:
+                name: %s
+
+            - project:
+                name: org/lookup-project
+                check:
+                  jobs:
+                    - %s
+            """ % (job, job))
+
+        file_dict = {'.zuul.yaml': conf}
+        A = self.fake_gerrit.addFakeChange('org/lookup-project', 'master', 'A',
+                                           files=file_dict)
+        self.fake_gerrit.addEvent(A.getPatchsetCreatedEvent(1))
+        self.waitUntilSettled()
+
+    def test_lookups(self):
+        count = 0
+        lookups = [
+            ('passwd', 'FAILURE'),
+            ('cartesian', 'SUCCESS'),
+            ('consul_kv', 'FAILURE'),
+            ('credstash', 'FAILURE'),
+            ('csvfile_good', 'SUCCESS'),
+            ('csvfile_bad', 'FAILURE'),
+        ]
+        for job, result in lookups:
+            count += 1
+            self._add_job(job)
+            self.assertEqual(count, len(self.history))
+            build = self.history[-1]
+            self.assertEqual(build.result, result)
+
+        # TODOv3(jeblair): parse the ansible output and verify we're
+        # getting the exception we expect.
+
 
 class TestPrePlaybooks(AnsibleZuulTestCase):
     # A temporary class to hold new tests while others are disabled
