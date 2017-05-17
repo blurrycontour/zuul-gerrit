ebb54ef32294ebba6e6bddaabf1efb16ba8a0655
Tristan Cacqueray <tdecacqu@redhat.com>
2017-05-17 12:36:00 +0000

    sql-reporter: add support for Ref change
    This change fixes AttributeErrors when the sql reporter
    is used in post/periodic pipelines where the item.change is of
    type Ref without number/patchset/refspec members.
    
    Change-Id: I11696b41a0a71405dfb7845ce222ef302a96986b

--- zuul/driver/sql/sqlreporter.py
+++ zuul/driver/sql/sqlreporter.py
@@ -39,13 +39,25 @@ class SQLReporter(BaseReporter):
             return
 
         with self.connection.engine.begin() as conn:
+            if hasattr(item.change, 'number'):
+                change = item.change.number
+            else:
+                change = ''
+            if hasattr(item.change, 'patchset'):
+                patchset = item.change.patchset
+            else:
+                patchset = ''
+            if hasattr(item.change, 'refspec'):
+                refspec = item.change.refspec
+            else:
+                refspec = item.change.newrev
             buildset_ins = self.connection.zuul_buildset_table.insert().values(
                 zuul_ref=item.current_build_set.ref,
                 pipeline=item.pipeline.name,
                 project=item.change.project.name,
-                change=item.change.number,
-                patchset=item.change.patchset,
-                ref=item.change.refspec,
+                change=change,
+                patchset=patchset,
+                ref=refspec,
                 score=self.result_score,
                 message=self._formatItemReport(
                     pipeline, item, with_jobs=False),
