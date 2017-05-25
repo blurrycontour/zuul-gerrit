405b64654efe15ca8e7bf2e58a01d23c9382fd3c
Clint Byrum <clint@fewbar.com>
2017-05-25 16:11:13 -0700

    Make zuul ansible dir self-contained
    This way we can bind mount it in and set it as a plugin path in bwrap
    without needing all of zuul.
    
    Change-Id: Ibb81167895c73b64bb49809d007f4768013c7220

--- zuul/ansible/action/patch.py
+++ zuul/ansible/action/patch.py
@@ -14,7 +14,7 @@
 # along with this software.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from zuul.ansible import paths
+from . import paths
 patch = paths._import_ansible_action_plugin("patch")
 
 
