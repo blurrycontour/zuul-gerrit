1d97e5f0c7ccd7acf800cbb7d8bea7ab06f67689
Tristan Cacqueray <tdecacqu@redhat.com>
2017-05-28 00:33:55 +0000

    executor: add support for custom ansible_port
    This change adds 'ssh_port' to the Node class so that zuul-executor can
    use a custom ssh server when it is set by nodepool provider.
    
    Change-Id: Icdac6cd41dded0e46fba5d14c31f40810f73b74a

--- zuul/executor/server.py
+++ zuul/executor/server.py
@@ -905,15 +905,20 @@ class AnsibleJob(object):
             # results in the wrong thing being in interface_ip
             # TODO(jeblair): Move this notice to the docs.
             ip = node.get('interface_ip')
+            port = node.get('ssh_port', 22)
             host_vars = dict(
                 ansible_host=ip,
+                ansible_port=port,
                 nodepool_az=node.get('az'),
                 nodepool_provider=node.get('provider'),
                 nodepool_region=node.get('region'))
 
             host_keys = []
             for key in node.get('host_keys'):
-                host_keys.append("%s %s" % (ip, key))
+                if port != 22:
+                    host_keys.append("[%s]:%s %s" % (ip, port, key))
+                else:
+                    host_keys.append("%s %s" % (ip, key))
 
             hosts.append(dict(
                 name=node['name'],
