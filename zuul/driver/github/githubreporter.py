2c4dde8ad7f47d4ad0031d98f8decc35291790b3
K Jonathan Harker <Jonathan.Harker@ibm.com>
2017-06-02 11:31:34 -0700

    Add github reporter status-url config option
    Make it possible to set the github status link to something other than the
    zuul server status_url. This allows the github status to link to the log output
    for the jobs. If set, this url will be run through the same formatter
    as the url_pattern used for comment links. If unset, it will fall back to the
    previous behaviour of using the zuul server status_url.
    
    Change-Id: I5f2ff35bb38426ddcd7f65798b4f114256a54847

--- zuul/driver/github/githubreporter.py
+++ zuul/driver/github/githubreporter.py
@@ -71,7 +71,10 @@ class GithubReporter(BaseReporter):
         context = '%s/%s' % (pipeline.layout.tenant.name, pipeline.name)
         state = self._commit_status
         url = ''
-        if self.connection.sched.config.has_option('zuul', 'status_url'):
+        pipeline_url = self.config.get('status-url', '')
+        if pipeline_url:
+            url = item.formatUrlPattern(pipeline_url)
+        elif self.connection.sched.config.has_option('zuul', 'status_url'):
             base = self.connection.sched.config.get('zuul', 'status_url')
             url = '%s/#%s,%s' % (base,
                                  item.change.number,
@@ -157,6 +160,7 @@ class GithubReporter(BaseReporter):
 def getSchema():
     github_reporter = v.Schema({
         'status': v.Any('pending', 'success', 'failure'),
+        'status-url': scalar_or_list(str),
         'comment': bool,
         'merge': bool,
         'label': scalar_or_list(str),
