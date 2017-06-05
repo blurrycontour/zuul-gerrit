0f944c6ba31fd784e079ff05b30b469145672d78
David Shrewsbury <shrewsbury.dave@gmail.com>
2017-06-05 14:15:18 -0400

    WIP: add log streaming test
    Change-Id: Ia153e2d436855dcce256d5fc91d0fd4c47116042

--- tests/unit/test_log_streamer.py
+++ tests/unit/test_log_streamer.py
@@ -14,7 +14,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import logging
+import os
+import os.path
 import socket
 import tempfile
 
@@ -23,8 +24,6 @@ import tests.base
 
 
 class TestLogStreamer(tests.base.BaseTestCase):
-
-    log = logging.getLogger("zuul.test.cloner")
 
     def setUp(self):
         super(TestLogStreamer, self).setUp()
@@ -51,3 +50,51 @@ class TestLogStreamer(tests.base.BaseTestCase):
         self.addCleanup(s.close)
         self.assertNotEqual(0, s.connect_ex((self.host, port)))
         s.close()
+
+
+class TestStreaming(tests.base.AnsibleZuulTestCase):
+
+    tenant_config_file = 'config/ansible/main.yaml'
+
+    def setUp(self):
+        super(TestStreaming, self).setUp()
+        self.host = '0.0.0.0'
+
+    def startStreamer(self, port, root=None):
+        if not root:
+            root = tempfile.gettempdir()
+        return zuul.lib.log_streamer.LogStreamer(None, self.host, port, root)
+
+    def test_streaming(self):
+        # We don't want the job dir deleted
+        self.executor_server.hold_jobs_in_build = True
+
+        A = self.fake_gerrit.addFakeChange('org/project', 'master', 'A')
+        self.fake_gerrit.addEvent(A.getPatchsetCreatedEvent(1))
+        self.waitUntilSettled()
+
+        build = None
+        for b in self.builds:
+            if b.name == 'python27':
+                build = b
+                break
+        self.assertIsNotNone(build)
+
+        self.log.debug("DWS executor.jobdir_root: %s",
+                       self.executor_server.jobdir_root)
+        d = os.listdir(self.executor_server.jobdir_root)
+        self.log.debug("DWS jobdir_root listing: %s", d)
+
+        self.log.debug("DWS build.jobdir.log_root: %s",
+                       build.jobdir.log_root)
+        # This will fail because the path does not yet exist
+        d = os.listdir(build.jobdir.log_root)
+        self.log.debug("DWS log_root listing: %s", d)
+
+        ansible_log = os.path.join(build.jobdir.log_root, 'ansible_log.txt')
+        self.assertTrue(os.path.exists(ansible_log))
+
+        # Start the finger log streamer
+        port = 7901
+        streamer = self.startStreamer(port)
+        self.addCleanup(streamer.stop)
