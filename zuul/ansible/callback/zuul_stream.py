a07a48f6cfafcca0740f1976d8d00259132c85d5
Monty Taylor <mordred@inaugust.com>
2017-06-07 09:34:53 -0500

    WIP Make log streaming to executor per-task
    Change-Id: Ic5eb6c38af698f4ba8b4504aa69170834ec4036a

--- zuul/ansible/callback/zuul_stream.py
+++ zuul/ansible/callback/zuul_stream.py
@@ -13,12 +13,15 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import absolute_import
+
 import os
 import multiprocessing
 import socket
 import time
+import uuid
 
-from ansible.plugins.callback import default
+from zuul.ansible.callback import default
 
 LOG_STREAM_PORT = 19885
 
@@ -82,32 +85,37 @@ class CallbackModule(default.CallbackModule):
         super(CallbackModule, self).__init__()
         self._task = None
         self._daemon_running = False
-        self._daemon_stamp = 'daemon-stamp-%s'
         self._host_dict = {}
+        self._play = None
 
-    def _read_log(self, host, ip):
-        self._display.display("[%s] starting to log" % host)
+    def _read_log(self, host, ip, log_id):
+        self._display.vvv("[%s] starting to log" % host)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         while True:
             try:
                 s.connect((ip, LOG_STREAM_PORT))
             except Exception:
-                self._display.display("[%s] Waiting on logger" % host)
+                self._display.vvv("[%s] Waiting on logger" % host)
                 time.sleep(0.1)
                 continue
+            s.send(log_id + '\n')
             for line in linesplit(s):
-                self._display.display("[%s] %s " % (host, line.strip()))
+                if line.startswith("[Zuul] Task exit code"):
+                    return
+                else:
+                    self._display.display("[%s] %s " % (host, line.strip()))
 
     def v2_playbook_on_play_start(self, play):
         self._play = play
         super(CallbackModule, self).v2_playbook_on_play_start(play)
 
     def v2_playbook_on_task_start(self, task, is_conditional):
-        self._task = task
 
         if self._play.strategy != 'free':
             self._print_task_banner(task)
         if task.action == 'command':
+            log_id = uuid.uuid4().hex
+            task.args['zuul_log_id'] = log_id
             play_vars = self._play._variable_manager._hostvars
 
             hosts = self._play.hosts
@@ -120,23 +128,21 @@ class CallbackModule(default.CallbackModule):
 
             for host in hosts:
                 ip = play_vars[host]['ansible_host']
-                daemon_stamp = self._daemon_stamp % host
-                if not os.path.exists(daemon_stamp):
-                    self._host_dict[host] = ip
-                    # Touch stamp file
-                    open(daemon_stamp, 'w').close()
-                    p = multiprocessing.Process(
-                        target=self._read_log, args=(host, ip))
-                    p.daemon = True
-                    p.start()
+                self._host_dict[host] = ip
+                self._p = multiprocessing.Process(
+                    target=self._read_log, args=(host, ip, log_id))
+                self._p.daemon = True
+                self._p.start()
 
     def v2_runner_on_failed(self, result, ignore_errors=False):
+        self._p.join()
         if result._task.action in ('command', 'shell'):
             zuul_filter_result(result._result)
         super(CallbackModule, self).v2_runner_on_failed(
             result, ignore_errors=ignore_errors)
 
     def v2_runner_on_ok(self, result):
+        self._p.join()
         if result._task.action in ('command', 'shell'):
             zuul_filter_result(result._result)
         else:
