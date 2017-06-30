d5e31f026a56d214a81f972a7528b8936e6fefa3
Monty Taylor <mordred@inaugust.com>
2017-06-30 13:11:37 -0500

    Write secrets into their own file, not into inventory
    Publishing the inventory as part of publishing logs would be handy
    in debugging that everything is as one expects. However, secrets
    currently go into the inventory, which means publishing it is not safe.
    
    Write the secrets to their own file.
    
    Change-Id: If58b89882a817ff219ed5f8faf2bde31cc8e1a6a

--- zuul/executor/server.py
+++ zuul/executor/server.py
@@ -204,17 +204,19 @@ class JobDir(object):
         os.makedirs(self.src_root)
         self.log_root = os.path.join(self.work_root, 'logs')
         os.makedirs(self.log_root)
-        self.ansible_root = os.path.join(self.root, 'ansible')
-        os.makedirs(self.ansible_root)
-        self.trusted_root = os.path.join(self.root, 'trusted')
-        os.makedirs(self.trusted_root)
         ssh_dir = os.path.join(self.work_root, '.ssh')
         os.mkdir(ssh_dir, 0o700)
         self.result_data_file = os.path.join(self.work_root, 'results.json')
         with open(self.result_data_file, 'w'):
             pass
         self.known_hosts = os.path.join(ssh_dir, 'known_hosts')
+        self.ansible_root = os.path.join(self.root, 'ansible')
+        os.makedirs(self.ansible_root)
+        self.ansible_root = os.path.join(self.root, 'trusted')
+        os.makedirs(self.trusted_root)
         self.inventory = os.path.join(self.ansible_root, 'inventory.yaml')
+        self.secrets = os.path.join(self.ansible_root, 'secrets.yaml')
+        self.has_secrets = False
         self.playbooks = []  # The list of candidate playbooks
         self.playbook = None  # A pointer to the candidate we have chosen
         self.pre_playbooks = []
@@ -1192,6 +1194,13 @@ class AnsibleJob(object):
                 for key in node['host_keys']:
                     known_hosts.write('%s\n' % key)
 
+        secrets = dict(args['secrets'])
+        if secrets:
+            with open(self.jobdir_playbook.secrets, 'w') as secrets_yaml:
+                secrets_yaml.write(
+                    yaml.safe_dump(secrets, default_flow_style=False))
+            self.jobdir.has_secrets = True
+
     def writeAnsibleConfig(self, jobdir_playbook):
         trusted = jobdir_playbook.trusted
 
@@ -1372,6 +1381,8 @@ class AnsibleJob(object):
             verbose = '-v'
 
         cmd = ['ansible-playbook', verbose, playbook.path]
+        if self.jobdir.has_secrets:
+            cmd.extend(['-e', '@' + self.jobdir.secrets])
 
         if success is not None:
             cmd.extend(['-e', 'success=%s' % str(bool(success))])
