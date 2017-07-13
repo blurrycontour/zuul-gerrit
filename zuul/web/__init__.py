1063d264b12e1d721afe3429bdfb92237fc47222
Monty Taylor <mordred@inaugust.com>
2017-07-13 13:27:56 -0500

    Serve html statically
    This isn't complete - we need to be able to display the root URL to the
    zuul-web process which at the moment neither the executor nor the
    scheduler know. So we need to plumb something through, likely in config,
    because otherwise folks who are running zuul-web on a sub-url will have
    a hard time.
    
    The relative path should be fine though - ../console-stream is
    relative to static/stream.html no matter how the two are served.
    
    We'll also want to think about getting tenant names in here, and whether
    or not static routes want to be per-tenant or global (could imagine a
    per-tenant white-labelling if per-tenant, but also a ton of useless
    apache config to deal with one set of static content for a global
    install.
    
    Change-Id: I2da7979f448934abe3d41f3a5e50d09004fcccc2

--- zuul/web/__init__.py
+++ zuul/web/__init__.py
@@ -18,12 +18,15 @@
 import asyncio
 import json
 import logging
+import os
 import uvloop
 
 import aiohttp
 from aiohttp import web
 
 import zuul.rpcclient
+
+STATIC_DIR = os.path.join(os.path.dirname(__file__), 'static')
 
 
 class LogStreamingHandler(object):
@@ -147,102 +150,6 @@ class LogStreamingHandler(object):
         return ws
 
 
-class StaticStreamingPage(object):
-
-    DOC = """
-        <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-           "http://www.w3.org/TR/html4/strict.dtd">
-        <html>
-          <head>
-            <style type="text/css">
-
-              body {{
-                font-family: monospace;
-                background-color: black;
-                color: lightgrey;
-              }}
-
-              #overlay {{
-                position: fixed;
-                top: 5px;
-                right: 5px;
-                background-color: darkgrey;
-                color: black;
-              }}
-
-              pre {{
-                white-space: pre;
-                margin: 0px 10px;
-              }}
-            </style>
-
-            <script type="text/javascript">
-
-              window.onload = function() {{
-
-                pageUpdateInMS = 250;
-                var receiveBuffer = "";
-
-                setInterval(function() {{
-                  console.log("autoScroll");
-                  if (receiveBuffer != "") {{
-                    document.getElementById('pagecontent').innerHTML += receiveBuffer;
-                    receiveBuffer = "";
-                    if (document.getElementById('autoscroll').checked) {{
-                      window.scrollTo(0, document.body.scrollHeight);
-                    }}
-                  }}
-                }}, pageUpdateInMS);
-
-                var url = new URL(window.location)
-
-                document.getElementById('pagetitle').innerHTML = "{uuid} ({logfile})";
-
-                var ws = new WebSocket('/console-stream');
-
-                ws.onmessage = function(event) {{
-                  console.log("onmessage");
-                  receiveBuffer = receiveBuffer + event.data + "\n";
-                }};
-
-                ws.onopen = function(event) {{
-                  console.log("onopen");
-                  ws.send('{{"uuid": "{uuid}", "logfile": "{logfile}"}}');
-                }}
-
-                ws.onclose = function(event) {{
-                  console.log("onclose");
-                  receiveBuffer = receiveBuffer + "\n--- END OF STREAM ---\n";
-                }}
-
-              }};
-
-            </script>
-
-            <title id="pagetitle"></title>
-          </head>
-
-          <body>
-
-            <div id="overlay">
-              <form>
-                <input type="checkbox" id="autoscroll" checked> autoscroll
-              </form>
-            </div>
-
-            <pre id="pagecontent"></pre>
-
-          </body>
-        </html>
-    """  # noqa: E501
-
-    async def processRequest(self, request):
-        uuid = request.query.get('uuid')
-        logfile = request.query.get('logfile', 'console.log')
-        html = self.DOC.format(uuid=uuid, logfile=logfile)
-        return web.Response(text=html, content_type='text/html')
-
-
 class ZuulWeb(object):
 
     log = logging.getLogger("zuul.web.ZuulWeb")
@@ -281,7 +188,6 @@ class ZuulWeb(object):
         '''
         routes = [
             ('GET', '/console-stream', self._handleWebsocket),
-            ('GET', '/static/stream', self._handleHTTPStream),
         ]
 
         self.log.debug("ZuulWeb starting")
@@ -296,6 +202,7 @@ class ZuulWeb(object):
         app = web.Application()
         for method, path, handler in routes:
             app.router.add_route(method, path, handler)
+        app.router.add_static('/static', STATIC_PATH)
         handler = app.make_handler(loop=self.event_loop)
 
         # create the server
