f24794aee140c7611ec2f1945c0ea4581aa9ad82
Tristan Cacqueray <tdecacqu@redhat.com>
2017-07-17 06:14:07 +0000

    Add tenant column to the buildset reporter table
    This change adds a tenant column to the zuul_buildset sql table to be able
    to filter results based on tenant.
    
    Change-Id: I4eec8e9a32883c54da953b7279e0f8a8c86b943b

--- tests/unit/test_connection.py
+++ tests/unit/test_connection.py
@@ -108,6 +108,7 @@ class TestSQLConnection(ZuulDBTestCase):
         self.assertEqual(1, buildset0['patchset'])
         self.assertEqual(1, buildset0['score'])
         self.assertEqual('Build succeeded.', buildset0['message'])
+        self.assertEqual('tenant-one', buildset0['tenant'])
 
         buildset0_builds = conn.execute(
             sa.sql.select([reporter.connection.zuul_build_table]).
