af0eb6f4820687aae25bd43c0fd8faaacfb3c63f
Clint Byrum <clint@fewbar.com>
2017-07-20 16:36:27 -0700

    Monitor job root and kill over limit jobs
    If a job were to be pointed at an abnormally large git repository (or
    a maliciously large one), a clone would fill the disk. Or anything else
    that might happen that writes data onto the executor disk.
    
    We run a single thread periodically running du on the root of all jobs
    on this executor. This is called the DiskAccountant.
    
    We set a config item per executor of the limit per job. This won't
    actually save a server from a full disk if many thousands of concurrent
    changes are submitted, but this will prevent any accidental filling of
    the disk, and make malicious disk filling much harder.
    
    Change-Id: I415e5930cc3ebe2c7e1a84316e78578d6b9ecf30
    Story: 2000879
    Task: 3504

---
+++ tests/unit/test_disk_accountant.py
@@ -0,0 +1,48 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os
+import tempfile
+import time
+
+from tests.base import BaseTestCase
+
+from zuul.executor.server import DiskAccountant
+
+
+class FakeExecutor(object):
+    def __init__(self):
+        self.stopped_jobs = set()
+
+    def stopJobByJobDir(self, jobdir):
+        self.stopped_jobs.add(jobdir)
+
+
+class TestDiskAccountant(BaseTestCase):
+    def test_disk_accountant(self):
+        jobsdir = tempfile.mkdtemp()
+        executor_server = FakeExecutor()
+        da = DiskAccountant(jobsdir, 1, executor_server.stopJobByJobDir)
+        da.start()
+        testdir = os.path.join(jobsdir, '012345')
+        os.mkdir(testdir)
+
+        testfile = os.path.join(testdir, 'tfile')
+        with open(testfile, 'w') as tf:
+            tf.write(2 * 1024 * 1024 * '.')
+        # da should catch over-limit dir within 5 seconds
+        for i in range(0, 50):
+            if testdir in executor_server.stopped_jobs:
+                break
+            time.sleep(0.1)
+        self.assertEqual(set([testdir]), executor_server.stopped_jobs)
+        da.stop()
