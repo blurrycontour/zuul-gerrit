d2749f24a7bd6b15aae7c41fc4cac79f4711062d
Monty Taylor <mordred@inaugust.com>
2017-07-25 07:28:04 -0400

    Use yarn and webpack to manage status and streaming
    yarn drives package and dependency management. webpack handles
    bundling, minification and transpiling down to browser-acceptable
    javascript but allows for more modern javascript like import statements.
    
    There are some really neat things in the webpack dev server. CSS
    changes, for instance, get applied immediately without a refresh. Other
    things, like the jquery plugin do need a refresh, but it's handled just
    on a file changing.
    
    As a followup, we can also consider turning the majority of the status page
    into a webpack library that other people can depend on as a mechanism
    for direct use. Things like that haven't been touched because allowing
    folks to poke at the existing known status page without too many changes
    using the tools seems like a good way for people to learn/understand the
    stack.
    
    Move things so that the built content gets put
    into zuul/web/static so that the built-in static serving from zuul-web
    will/can serve the files.
    
    Update MANIFEST.in so that if npm run build:dist is run before the
    python setup.py sdist, the built html/javascript content will be
    included in the source tarball.
    
    Add a zuul job with a success url that contains a source_url
    pointing to the live v3 data.
    
    Depends-On: I9ae7dd3b33b2b607c1c5a17632e2aeb12e339d15
    Change-Id: I5864e1b2f9952fbc35b08ebdef348c4917564c37

---
+++ web/config/webpack.common.js
@@ -0,0 +1,120 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'dashboard': './web/dashboard.js',
+    'status': './web/status.js',
+    'stream': './web/stream.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    // path.resolve(__dirname winds up relative to the config dir
+    path: path.resolve(__dirname, '../../zuul/web/static'),
+    publicPath: ''
+  },
+  // Some folks prefer "cheaper" source-map for dev and one that is more
+  // expensive to build for prod. Debugging without the full source-map sucks,
+  // so define it here in common.
+  devtool: 'source-map',
+  plugins: [
+    new webpack.ProvidePlugin({
+        $: 'jquery/dist/jquery',
+        jQuery: 'jquery/dist/jquery',
+    }),
+    new CleanWebpackPlugin(
+        ['zuul/web/static'], { root: path.resolve(__dirname, '../..')}),
+    new HtmlWebpackPlugin({
+      chunks: ['status'],
+      filename: 'status.html',
+      template: 'web/templates/status.ejs',
+      title: 'Zuul Status'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Zuul Builds',
+      chunks: ['dashboard'],
+      template: 'web/templates/builds.ejs',
+      filename: 'builds.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Zuul Jobs',
+      chunks: ['dashboard'],
+      template: 'web/templates/jobs.ejs',
+      filename: 'jobs.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Zuul Tenants',
+      chunks: ['dashboard'],
+      template: 'web/templates/index.ejs',
+      filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Zuul Console Stream',
+      chunks: ['stream'],
+      template: 'web/templates/stream.ejs',
+      filename: 'stream.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+      // The majority of the rules below are all about getting bootstrap copied
+      // appropriately.
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimetype: 'application/font-woff'
+          }
+        }
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimetype: 'application/octet-stream'
+          }
+        }
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimetype: 'image/svg+xml'
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: ['raw-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        use: ['file-loader']
+      }
+    ]
+  }
+};
