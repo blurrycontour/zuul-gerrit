89e1d3f182d387f22c4bf7923641c6c3651bf0a6
Monty Taylor <mordred@inaugust.com>
2017-07-25 07:28:04 -0400

    WIP Use yarn and webpack to manage status javascript
    This is WIP unti post v3 rollout.
    
    yarn drives package and dependency management. webpack handles
    bundling, minification and transpiling down to browser-acceptable
    javascript but allows for more modern javascript like import statements.
    
    There are some really neat things in the webpack dev server. CSS
    changes, for instance, get applied immediately without a refresh. Other
    things, like the jquery plugin do need a refresh, but it's handled just
    on a file changing.
    
    If we decide we want to go down this road we should be able to combine
    this with the web console streaming and the upcoming console.json
    display. We may want to put it into its own repo so that we're not
    overlaying javascript/python tools. HOWEVER - there is nothing
    inherently blocking keeping it in tree either.
    
    Finally, we can also consider turning the majority of the status page
    into a webpack library that other people can depend on as a mechanism
    for direct use. Things like that haven't been touched because allowing
    folks to poke at the existing known status page without too many changes
    using the tools seems like a good way for people to learn/understand the
    stack.
    
    Change-Id: I5864e1b2f9952fbc35b08ebdef348c4917564c37

---
+++ etc/status/config/webpack.common.js
@@ -0,0 +1,89 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  // Some folks prefer "cheaper" source-map for dev and one that is more
+  // expensive to build for prod. Debugging without the full source-map sucks,
+  // so define it here in common.
+  devtool: 'source-map',
+  plugins: [
+    new webpack.ProvidePlugin({
+        $: 'jquery/dist/jquery',
+        jQuery: 'jquery/dist/jquery',
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Zuul Status'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+      // The majority of the rules below are all about getting bootstrap copied
+      // appropriately.
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimetype: 'application/font-woff'
+          }
+        }
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimetype: 'application/octet-stream'
+          }
+        }
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimetype: 'image/svg+xml'
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: ['raw-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        use: ['file-loader']
+      }
+    ]
+  }
+};
