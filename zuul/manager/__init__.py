938ee284b3554d292d72b24cccd27f674fd54660
James E. Blair <jeblair@redhat.com>
2017-07-27 15:02:29 -0700

    Ensure ref-updated jobs run with their ref
    We were incorrectly preparing the current state of the repo for
    ref updated (eg, post) jobs.  This ensures that we run with the
    actual supplied ref, even if the remote has moved on since then.
    
    Change-Id: I52f05406246e6e39805fd8365412f3cb77fe3a0a

--- zuul/manager/__init__.py
+++ zuul/manager/__init__.py
@@ -13,6 +13,7 @@
 import logging
 
 from zuul import exceptions
+from zuul import model
 
 
 class DynamicChangeQueueContextManager(object):
@@ -483,20 +484,18 @@ class PipelineManager(object):
     def scheduleMerge(self, item, files=None, dirs=None):
         build_set = item.current_build_set
 
-        if not hasattr(item.change, 'branch'):
-            self.log.debug("Change %s does not have an associated branch, "
-                           "not scheduling a merge job for item %s" %
-                           (item.change, item))
-            build_set.merge_state = build_set.COMPLETE
-            return True
-
         self.log.debug("Scheduling merge for item %s (files: %s, dirs: %s)" %
                        (item, files, dirs))
         build_set = item.current_build_set
         build_set.merge_state = build_set.PENDING
-        self.sched.merger.mergeChanges(build_set.merger_items,
-                                       item.current_build_set, files, dirs,
-                                       precedence=self.pipeline.precedence)
+        if isinstance(item.change, model.Change):
+            self.sched.merger.mergeChanges(build_set.merger_items,
+                                           item.current_build_set, files, dirs,
+                                           precedence=self.pipeline.precedence)
+        else:
+            self.sched.merger.getItemRepoState(build_set.merger_items,
+                                               item.current_build_set,
+                                               precedence=self.pipeline.precedence)
         return False
 
     def prepareItem(self, item):
