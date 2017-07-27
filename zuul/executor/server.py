6119391a2daefc74b33007797973722dbc083d5d
James E. Blair <jeblair@redhat.com>
2017-07-27 15:02:29 -0700

    Ensure ref-updated jobs run with their ref
    We were incorrectly preparing the current state of the repo for
    ref updated (eg, post) jobs.  This ensures that we run with the
    actual supplied ref, even if the remote has moved on since then.
    
    Change-Id: I52f05406246e6e39805fd8365412f3cb77fe3a0a

--- zuul/executor/server.py
+++ zuul/executor/server.py
@@ -537,6 +537,7 @@ class ExecutorServer(object):
                                               self.hostname)
         self.merger_worker.registerFunction("merger:merge")
         self.merger_worker.registerFunction("merger:cat")
+        self.merger_worker.registerFunction("merger:refstate")
 
     def stop(self):
         self.log.debug("Stopping")
@@ -632,6 +633,9 @@ class ExecutorServer(object):
                     elif job.name == 'merger:merge':
                         self.log.debug("Got merge job: %s" % job.unique)
                         self.merge(job)
+                    elif job.name == 'merger:refstate':
+                        self.log.debug("Got refstate job: %s" % job.unique)
+                        self.refstate(job)
                     else:
                         self.log.error("Unable to handle job %s" % job.name)
                         job.sendWorkFail()
@@ -703,6 +707,14 @@ class ExecutorServer(object):
         result = dict(updated=True,
                       files=files,
                       zuul_url=self.zuul_url)
+        job.sendWorkComplete(json.dumps(result))
+
+    def refstate(self, job):
+        args = json.loads(job.arguments)
+        with self.merger_lock:
+            success, repo_state = self.merger.getRepoState(args['items'])
+        result = dict(updated=success,
+                      repo_state=repo_state)
         job.sendWorkComplete(json.dumps(result))
 
     def merge(self, job):
@@ -861,6 +873,10 @@ class AnsibleJob(object):
                 # There was a merge conflict and we have already sent
                 # a work complete result, don't run any jobs
                 return
+
+        state_items = [i for i in args['items'] if not i.get('number')]
+        if state_items:
+            merger.setRepoState(state_items, args['repo_state'])
 
         for project in args['projects']:
             repo = repos[project['canonical_name']]
