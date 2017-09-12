59a92ec00fac1b565a39aad2e418f5eb2ea03173
Tristan Cacqueray <tdecacqu@redhat.com>
2017-09-12 21:25:00 +0000

    web: add /{source}/{project}.pem route
    This change adds key:get function to the scheduler gearman worker so
    that zuul-web can request project's public key.
    
    Change-Id: I7ae16f27ccd9efd68c8cf10df7ff0ff5b7b7aa13

--- zuul/web/__init__.py
+++ zuul/web/__init__.py
@@ -173,6 +173,7 @@ class GearmanHandler(object):
             'tenant': self.tenant_list,
             'status': self.status_get,
             'jobs': self.jobs_list,
+            'key': self.key_get,
         }
 
     def getClient(self):
@@ -202,6 +203,13 @@ class GearmanHandler(object):
         tenant = request.match_info["tenant"]
         job = rpc.submitJob('job:list', {'tenant': tenant})
         return web.json_response(json.loads(job.data[0]))
+
+    def key_get(self, rpc, request):
+        source = request.match_info["source"]
+        project = request.match_info["project"]
+        job = rpc.submitJob('key:get', {'source': source,
+                                        'project': project})
+        return web.Response(body=job.data[0])
 
     async def processRequest(self, request, action):
         try:
@@ -345,6 +353,9 @@ class ZuulWeb(object):
     async def _handleSqlRequest(self, request):
         return await self.sql_handler.processRequest(request)
 
+    async def _handleKeyRequest(self, request):
+        return await self.gearman_handler.processRequest(request, 'key')
+
     async def _handleStaticRequest(self, request):
         fp = None
         if request.path.endswith("tenants.html") or request.path.endswith("/"):
@@ -373,6 +384,7 @@ class ZuulWeb(object):
             ('GET', '/tenants.json', self._handleTenantsRequest),
             ('GET', '/{tenant}/status.json', self._handleStatusRequest),
             ('GET', '/{tenant}/jobs.json', self._handleJobsRequest),
+            ('GET', '/{source}/{project}.pem', self._handleKeyRequest),
             ('GET', '/{tenant}/status.html', self._handleStaticRequest),
             ('GET', '/{tenant}/jobs.html', self._handleStaticRequest),
             ('GET', '/tenants.html', self._handleStaticRequest),
