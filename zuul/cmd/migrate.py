35cdd3a5f9d1827d5136a2ab507b83d46a485f65
David Shrewsbury <shrewsbury.dave@gmail.com>
2017-09-12 13:11:53 -0600

    DNM Expand templates for project-specific matchers
    Change-Id: I6858ee5b2eaa3e53cffd208bef713b8c74465387

--- zuul/cmd/migrate.py
+++ zuul/cmd/migrate.py
@@ -454,6 +454,9 @@ class JobMapping:
         # Handle matchers
         for layout_job in self.layout.get('jobs', []):
             if re.search(layout_job['name'], new_job.orig):
+                # Matchers that can apply to templates must be processed first
+                # since project-specific matchers can cause the template to
+                # be expanded into a project.
                 if not layout_job.get('voting', True):
                     new_job.voting = False
                 if layout_job.get('branch'):
@@ -550,21 +553,92 @@ class ZuulMigrate:
 
         return new_template
 
+    def scanForProjectMatchers(self, project_name):
+        ''' Get list of job matchers that reference the given project name '''
+        job_matchers = []
+        for matcher in self.layout.get('jobs', []):
+            for skipper in matcher.get('skip-if', []):
+                if skipper.get('project'):
+                    if re.search(skipper['project'], project_name):
+                        job_matchers.append(matcher['name'])
+        return job_matchers
+
+
+    def findReferencedTemplateNames(self, job_matchers, project_name):
+        ''' Search templates in the layout file for matching jobs '''
+        template_names = []
+
+        def search_jobs(template):
+            def _search(job):
+                if isinstance(job, str):
+                    for matcher in job_matchers:
+                        if re.search(matcher, job.format(name=project_name)):
+                            template_names.append(template['name'])
+                            return True
+                elif isinstance(job, list):
+                    for i in job:
+                        if _search(i):
+                            return True
+                elif isinstance(job, dict):
+                    for k, v in job.items():
+                        if _search(k) or _search(v):
+                            return True
+                return False
+
+            for key, value in template.items():
+                if key == 'name':
+                    continue
+                for job in template[key]:
+                    if _search(job):
+                        return
+
+        for template in self.layout.get('project-templates', []):
+            search_jobs(template)
+        return template_names
+
+
+    def expandTemplateIntoProject(self, template_name, project):
+        self.log.debug("EXPAND template %s into project %s",
+                       template_name, project)
+
+
     def writeProject(self, project):
+        '''
+        Create a new v3 project definition.
+
+        As part of creating the project, scan for project-specific job matchers
+        referencing this project and remove the templates matching the job
+        regex for that matcher. Expand the matched template(s) into the project
+        so we can apply the project-specific matcher to the job(s).
+        '''
         new_project = collections.OrderedDict()
         if 'name' in project:
             new_project['name'] = project['name']
+
+        job_matchers = self.scanForProjectMatchers(project['name'])
+        if job_matchers:
+            exp_template_names = self.findReferencedTemplateNames(
+                job_matchers, project['name'])
+        else:
+            exp_template_names = []
+
         if 'template' in project:
             new_project['template'] = []
             for template in project['template']:
+                if template['name'] in exp_template_names:
+                    continue
                 new_project['template'].append(dict(
                     name=self.mapping.getNewTemplateName(template['name'])))
+
         for key, value in project.items():
             if key in ('name', 'template'):
                 continue
             else:
                 jobs = [job.toDict() for job in self.makeNewJobs(value)]
                 new_project[key] = dict(jobs=jobs)
+
+        for name in exp_template_names:
+            self.expandTemplateIntoProject(name, new_project)
 
         return new_project
 
