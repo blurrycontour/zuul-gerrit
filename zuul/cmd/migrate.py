6f95b898ac026325fc538a7bef1d756d13cbf384
Monty Taylor <mordred@inaugust.com>
2017-09-12 08:19:23 -0600

    Always strip {name} from name
    We have this protecte in the Job constructor but it needs to come
    out no matter what.
    
    Change-Id: I179f41df9ad01f8ebbdcb126f50ad347abcae11b

--- zuul/cmd/migrate.py
+++ zuul/cmd/migrate.py
@@ -282,8 +282,7 @@ class Job:
 
         if self.content and not self.name:
             self.name = get_single_key(content)
-        if not self.name:
-            self.name = self.orig.replace('-{name}', '').replace('{name}-', '')
+        self.name = self.orig.replace('-{name}', '').replace('{name}-', '')
         if self.orig.endswith('-nv'):
             self.voting = False
         if self.name.endswith('-nv'):
