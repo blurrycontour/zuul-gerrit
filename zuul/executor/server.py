41ea6c231e1a510fe52da3cb480933bbdf5c2f81
Monty Taylor <mordred@inaugust.com>
2017-09-14 10:00:22 -0600

    Add a message to the job log on unexpected exceptions
    We only get messages in the debug log on unexpected exceptions. Add
    a message to the job log so that people can know there was a problem.
    
    Change-Id: I4cb6c5d22724b55c265577dffa581a3c8e74ec25

--- zuul/executor/server.py
+++ zuul/executor/server.py
@@ -1620,6 +1620,15 @@ class AnsibleJob(object):
                     job_output.write("{now} | {line}\n".format(
                         now=datetime.datetime.now(),
                         line=line.decode('utf-8').rstrip()))
+        elif ret == 250:
+            # Unexpected error from ansible
+            with open(self.jobdir.job_output_file, 'a') as job_output:
+                job_output.write("{now} | UNEXPECTED ANSIBLE ERROR\n".format(
+                    now=datetime.datetime.now()))
+                for line in syntax_buffer:
+                    job_output.write("{now} | {line}\n".format(
+                        now=datetime.datetime.now(),
+                        line=line.decode('utf-8').rstrip()))
 
         return (self.RESULT_NORMAL, ret)
 
