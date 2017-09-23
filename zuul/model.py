18bad099533baaa8373b7e7d5f035a644643b4e8
Tristan Cacqueray <tdecacqu@redhat.com>
2017-09-23 13:45:24 +0000

    Add restricted-node-labels global limits
    This change adds a new global limits objects with a restricted-node-labels
    setting to limit a label to a set of jobs, for examples, to limit
    the 'hsm-worker' label to the 'sign-job' job of the common-config project:
    
      - limit:
          restricted-node-labels:
            hsm-worker:
              connection_name/common-config: [sign-job]
    
    Change-Id: Ia6562ff4bc4fae20e1f1a80316c9b26baddfd0a4

--- zuul/model.py
+++ zuul/model.py
@@ -2189,6 +2189,7 @@ class UnparsedAbideConfig(object):
 
     def __init__(self):
         self.tenants = []
+        self.limits = {}
 
     def extend(self, conf):
         if isinstance(conf, UnparsedAbideConfig):
@@ -2206,6 +2207,8 @@ class UnparsedAbideConfig(object):
             key, value = list(item.items())[0]
             if key == 'tenant':
                 self.tenants.append(value)
+            elif key == 'limit':
+                self.limits.update(value)
             else:
                 raise ConfigItemUnknownError()
 
@@ -2617,6 +2620,7 @@ class Tenant(object):
 class Abide(object):
     def __init__(self):
         self.tenants = OrderedDict()
+        self.limits = {}
 
 
 class JobTimeData(object):
