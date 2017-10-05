bc19e23f8df504ea72fd391b55df848c2965649f
James E. Blair <jeblair@redhat.com>
2017-10-05 14:56:35 -0700

    Fix early processing of merge-pending items on reconfig
    On a reconfiguration, when we re-enqueue an item, we could end
    up creating a job graph for an item before the merge for that
    item has returned.  Make sure we use the same methods that the
    main processing loop uses so that we maintain the state machine
    around waiting for merges and dynamic layouts.
    
    Change-Id: I2b99620cfe1f7666ee3e6297ce041b3f7a02e051

--- zuul/manager/__init__.py
+++ zuul/manager/__init__.py
@@ -228,20 +228,16 @@ class PipelineManager(object):
                                (item.change, change_queue))
                 change_queue.enqueueItem(item)
 
-                # Get an updated copy of the layout if necessary.
-                # This will return one of the following:
-                # 1) An existing layout from the item ahead or pipeline.
-                # 2) A newly created layout from the cached pipeline
-                #    layout config plus the previously returned
-                #    in-repo files stored in the buildset.
-                # 3) None in the case that a fetch of the files from
-                #    the merger is still pending.
-                item.layout = self.getLayout(item)
+                # Get an updated copy of the layout and update the job
+                # graph if necessary.  This resumes the buildset merge
+                # state machine.  If we have an up-to-date layout, it
+                # will go ahead and refresh the job graph if needed;
+                # or it will send a new merge job if necessary, or it
+                # will do nothing if we're waiting on a merge job.
 
-                # Rebuild the frozen job tree from the new layout, if
-                # we have one.  If not, it will be built later.
-                if item.layout:
-                    item.freezeJobGraph()
+                if item.active:
+                    if self.prepareItem(item):
+                        self.prepareJobs(item)
 
                 # Re-set build results in case any new jobs have been
                 # added to the tree.
@@ -550,13 +546,13 @@ class PipelineManager(object):
             self.log.info("Dequeuing change %s because "
                           "it can no longer merge" % item.change)
             self.cancelJobs(item)
-            self.dequeueItem(item)
             item.setDequeuedNeedingChange()
             if item.live:
                 try:
                     self.reportItem(item)
                 except exceptions.MergeFailure:
                     pass
+            self.dequeueItem(item)
             return (True, nnfi)
 
         actionable = change_queue.isActionable(item)
