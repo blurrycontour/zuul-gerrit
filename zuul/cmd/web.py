2d01eaff0f457fee58fd167fca80605e1f3a9a7c
Tristan Cacqueray <tdecacqu@redhat.com>
2017-11-29 05:37:46 +0000

    web: add /{tenant}/builds route
    This change adds a SqlHandler to query the sql reporter database from
    zuul-web through the /{tenant}/builds.json controller.
    
    This change also adds a /{tenant}/builds.html basic web interface.
    
    Change-Id: I423a37365316cc96ed07ad0895c7198d9cff8be5

--- zuul/cmd/web.py
+++ zuul/cmd/web.py
@@ -25,6 +25,7 @@ import threading
 import zuul.cmd
 import zuul.web
 
+from zuul.driver.sql import sqlconnection
 from zuul.lib.config import get_default
 
 # as of python-daemon 1.6 it doesn't bundle pidlockfile anymore
@@ -61,6 +62,30 @@ class WebServer(zuul.cmd.ZuulApp):
         params['ssl_cert'] = get_default(self.config, 'gearman', 'ssl_cert')
         params['ssl_ca'] = get_default(self.config, 'gearman', 'ssl_ca')
 
+        sql_conn_name = get_default(self.config, 'web',
+                                    'sql_connection_name')
+        sql_conn = None
+        if sql_conn_name:
+            # we want a specific sql connection
+            sql_conn = self.connections.connections.get(sql_conn_name)
+            if not sql_conn:
+                self.log.error("Couldn't find sql connection '%s'" %
+                               sql_conn_name)
+                sys.exit(1)
+        else:
+            # look for any sql connection
+            connections = [c for c in self.connections.connections.values()
+                           if isinstance(c, sqlconnection.SQLConnection)]
+            if len(connections) > 1:
+                self.log.error("Multiple sql connection found, "
+                               "set the sql_connection_name option "
+                               "in zuul.conf [web] section")
+                sys.exit(1)
+            if connections:
+                # use this sql connection by default
+                sql_conn = connections[0]
+        params['sql_connection'] = sql_conn
+
         try:
             self.web = zuul.web.ZuulWeb(**params)
         except Exception as e:
@@ -91,6 +116,8 @@ class WebServer(zuul.cmd.ZuulApp):
     def main(self):
         self.setup_logging('web', 'log_config')
         self.log = logging.getLogger("zuul.WebServer")
+
+        self.configure_connections()
 
         try:
             self._main()
