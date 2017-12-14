e1cf65a53cf4de4cec8008767020e56cc45513e1
Monty Taylor <mordred@inaugust.com>
2017-12-14 13:45:15 -0600

    Add StandardJS eslint config for linting
    JavaScript Standard Style is a code style similar to pep8/flake8. It's
    being added here not because of the pep8 part, but because the pyflakes
    equivalent can catch real errors. This uses the babel-eslint parser
    since we're using Babel to transpile already.
    
    This auto-formats the existing code with:
    
      npm run format
    
    Rather than using StandardJS directly through the 'standard' package,
    use the standardjs eslint plugin so that we can ignore the camelCase
    rule (and any other rule that might emerge in the future)
    
    Many of under_score/camelCase were fixed in a previous version of the patch.
    Since the prevailing zuul style is camelCase methods anyway, those fixes
    were left. That warning has now been disabled.
    
    Other things, such as == vs. === and ensuring template
    strings are in backticks are fixed.
    
    Ignore indentation errors for now - we'll fix them at the end of this
    stack and then remove the exclusion.
    
    Add a 'format' npm run target that will run the eslint command with
    --fix for ease of fixing reported issues.
    
    Add a 'lint' npm run target and a 'lint' environment that runs with
    linting turned to errors. The next patch makes the lint environment more
    broadly useful.
    
    Change-Id: Ie55ee36aad3744c063ffe9e5fe61d86e13231229

--- web/config/webpack.lint.js
+++ web/config/webpack.lint.js
@@ -5,12 +5,6 @@ const CommonConfig = require('./webpack.common.js');
 
 module.exports = Merge(CommonConfig, {
 
-  // Enable Hot Module Replacement for devServer
-  devServer: {
-    hot: true,
-    contentBase: path.resolve(__dirname, './zuul/web/static'),
-    publicPath: '/'
-  },
   module: {
     rules: [
       {
@@ -27,11 +21,5 @@ module.exports = Merge(CommonConfig, {
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(),
-    // We only need to bundle the demo files when we're running locally
-    new webpack.ProvidePlugin({
-        DemoStatusBasic: './status-basic.json',
-        DemoStatusOpenStack: './status-openstack.json',
-        DemoStatusTree: './status-tree.json'
-    }),
   ]
 })
