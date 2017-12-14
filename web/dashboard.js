dc64c77990a1d3ec617cdea81e767c52994c682e
Monty Taylor <mordred@inaugust.com>
2017-12-14 13:45:15 -0600

    Add StandardJS eslint config for linting
    JavaScript Standard Style is a code style similar to pep8/flake8. It's
    being added here not because of the pep8 part, but because the pyflakes
    equivalent can catch real errors. This uses the babel-eslint parser
    since we're using Babel to transpile already.
    
    This auto-formats the existing code with:
    
      npm run format
    
    Rather than using StandardJS directly through the 'standard' package,
    use the standardjs eslint plugin so that we can ignore the camelCase
    rule (and any other rule that might emerge in the future)
    
    Many of under_score/camelCase were fixed in a previous version of the patch.
    Since the prevailing zuul style is camelCase methods anyway, those fixes
    were left. That warning has now been disabled.
    
    Other things, such as == vs. === and ensuring template
    strings are in backticks are fixed.
    
    Ignore indentation errors for now - we'll fix them at the end of this
    stack and then remove the exclusion.
    
    Add a 'format' npm run target that will run the eslint command with
    --fix for ease of fixing reported issues.
    
    Add a 'lint' npm run target and a 'lint' environment that runs with
    linting turned to errors. The next patch makes the lint environment more
    broadly useful.
    
    Change-Id: Ie55ee36aad3744c063ffe9e5fe61d86e13231229

--- web/dashboard.js
+++ web/dashboard.js
@@ -18,34 +18,32 @@
 // @licend  The above is the entire license notice
 // for the JavaScript code in this page.
 
-import 'bootstrap/dist/css/bootstrap.css';
-import angular from 'angular';
+import 'bootstrap/dist/css/bootstrap.css'
+import angular from 'angular'
 
-import './styles/zuul.css';
-import './jquery.zuul';
+import './styles/zuul.css'
+import './jquery.zuul'
 
-
-function get_source_url(filename, $location) {
-    let query_args = $location.search();
-    if (query_args['source_url']) {
-        return query_args['source_url'] + '/' + filename;
-    } else {
-        return filename;
-    }
+function getSourceUrl (filename, $location) {
+  let queryArgs = $location.search()
+  if (queryArgs['source_url']) {
+    return queryArgs['source_url'] + '/' + filename
+  } else {
+    return filename
+  }
 }
 
 angular.module('zuulTenants', []).controller(
-    'mainController', function($scope, $http, $location)
-{
-    $scope.tenants = undefined;
-    $scope.tenants_fetch = function() {
-        $http.get(get_source_url("tenants", $location))
-            .then(function success(result) {
-                $scope.tenants = result.data;
-            });
-    }
-    $scope.tenants_fetch();
-});
+    'mainController', function ($scope, $http, $location) {
+      $scope.tenants = undefined
+      $scope.tenants_fetch = function () {
+        $http.get(getSourceUrl('tenants', $location))
+            .then(function success (result) {
+              $scope.tenants = result.data
+            })
+      }
+      $scope.tenants_fetch()
+    })
 
 angular.module('zuulJobs', [], function ($locationProvider) {
   $locationProvider.html5Mode({
@@ -53,72 +51,73 @@ angular.module('zuulJobs', [], function ($locationProvider) {
     requireBase: false
   })
 }).controller(
-    'mainController', function($scope, $http, $location)
-{
-    $scope.jobs = undefined;
-    $scope.jobs_fetch = function() {
-        $http.get(get_source_url("jobs", $location))
-            .then(function success(result) {
-                $scope.jobs = result.data;
-            });
-    }
-    $scope.jobs_fetch();
-});
+    'mainController', function ($scope, $http, $location) {
+      $scope.jobs = undefined
+      $scope.jobs_fetch = function () {
+        $http.get(getSourceUrl('jobs', $location))
+            .then(function success (result) {
+              $scope.jobs = result.data
+            })
+      }
+      $scope.jobs_fetch()
+    })
 
-angular.module('zuulBuilds', [], function($locationProvider) {
-    $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
-    });
-}).controller('mainController', function($scope, $http, $location)
-{
-    $scope.rowClass = function(build) {
-        if (build.result == "SUCCESS") {
-            return "success";
-        } else {
-            return "warning";
-        }
-    };
-    let query_args = $location.search();
-    let url = $location.url();
-    if (query_args['source_url']) {
-      $scope.tenant = undefined
+angular.module('zuulBuilds', [], function ($locationProvider) {
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  })
+}).controller('mainController', function ($scope, $http, $location) {
+  $scope.rowClass = function (build) {
+    if (build.result === 'SUCCESS') {
+      return 'success'
     } else {
-      let tenant_start = url.lastIndexOf(
-          '/', url.lastIndexOf('/builds.html') - 1) + 1;
-      let tenant_length = url.lastIndexOf('/builds.html') - tenant_start;
-      $scope.tenant = url.substr(tenant_start, tenant_length);
+      return 'warning'
     }
-    $scope.builds = undefined;
-    if (query_args["pipeline"]) {$scope.pipeline = query_args["pipeline"];
-    } else {$scope.pipeline = "";}
-    if (query_args["job_name"]) {$scope.job_name = query_args["job_name"];
-    } else {$scope.job_name = "";}
-    if (query_args["project"]) {$scope.project = query_args["project"];
-    } else {$scope.project = "";}
-    $scope.builds_fetch = function() {
-        let query_string = "";
-        if ($scope.tenant) {query_string += "&tenant="+$scope.tenant;}
-        if ($scope.pipeline) {query_string += "&pipeline="+$scope.pipeline;}
-        if ($scope.job_name) {query_string += "&job_name="+$scope.job_name;}
-        if ($scope.project) {query_string += "&project="+$scope.project;}
-        if (query_string != "") {query_string = "?" + query_string.substr(1);}
-        $http.get(get_source_url("builds", $location) + query_string)
-            .then(function success(result) {
-                for (build_pos = 0;
-                     build_pos < result.data.length;
-                     build_pos += 1) {
-                    build = result.data[build_pos]
-                    if (build.node_name == null) {
-                        build.node_name = 'master'
-                    }
-                    /* Fix incorect url for post_failure job */
-                    if (build.log_url == build.job_name) {
-                        build.log_url = undefined;
-                    }
+  }
+  let queryArgs = $location.search()
+  let url = $location.url()
+  if (queryArgs['source_url']) {
+    $scope.tenant = undefined
+  } else {
+    let tenantStart = url.lastIndexOf(
+      '/', url.lastIndexOf('/builds.html') - 1) + 1
+    let tenantLength = url.lastIndexOf('/builds.html') - tenantStart
+    $scope.tenant = url.substr(tenantStart, tenantLength)
+  }
+  $scope.builds = undefined
+  if (queryArgs['pipeline']) {
+    $scope.pipeline = queryArgs['pipeline']
+  } else { $scope.pipeline = '' }
+  if (queryArgs['job_name']) {
+    $scope.job_name = queryArgs['job_name']
+  } else { $scope.job_name = '' }
+  if (queryArgs['project']) {
+    $scope.project = queryArgs['project']
+  } else { $scope.project = '' }
+  $scope.builds_fetch = function () {
+    let queryString = ''
+    if ($scope.tenant) { queryString += '&tenant=' + $scope.tenant }
+    if ($scope.pipeline) { queryString += '&pipeline=' + $scope.pipeline }
+    if ($scope.job_name) { queryString += '&job_name=' + $scope.job_name }
+    if ($scope.project) { queryString += '&project=' + $scope.project }
+    if (queryString !== '') { queryString = '?' + queryString.substr(1) }
+    $http.get(getSourceUrl('builds', $location) + queryString)
+            .then(function success (result) {
+              for (let buildPos = 0;
+                     buildPos < result.data.length;
+                     buildPos += 1) {
+                let build = result.data[buildPos]
+                if (build.node_name == null) {
+                  build.node_name = 'master'
                 }
-                $scope.builds = result.data;
-            });
-    }
-    $scope.builds_fetch()
-});
+                /* Fix incorect url for post_failure job */
+                if (build.log_url === build.job_name) {
+                  build.log_url = undefined
+                }
+              }
+              $scope.builds = result.data
+            })
+  }
+  $scope.builds_fetch()
+})
