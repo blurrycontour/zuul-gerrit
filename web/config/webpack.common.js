f108dfa0a5670e7f8167090e066534194bd4dab1
Monty Taylor <mordred@inaugust.com>
2017-12-14 10:37:16 -0600

    Add babel transpiling enabling use of ES6 features
    ECMAScript6 has a bunch of nice things, like block scoped variables,
    const, template strings and classes. Babel is a javascript transpiler
    which webpack can use to allow us to write using modern javascript but
    the resulting code to still work on older browsers.
    
    Use the babel-plugin-angularjs-annotate so that angular's dependency
    injection doesn't get borked by babel's transpiling things (which causes
    variables to otherwise be renamed in a way that causes angular to not
    find them)
    
    While we're at it, replace our use of var with let (let is the new
    block-scoped version of var) and toss in some use of const and template
    strings for good measure.
    
    Change-Id: Ib18b3090d16b05e1d24c3b1de7abd3e82a8c084f

--- web/config/webpack.common.js
+++ web/config/webpack.common.js
@@ -60,6 +60,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      },
+      {
         test: /.css$/,
         use: [
           'style-loader',
