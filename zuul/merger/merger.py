cb65e06811f8dc86380d6aed7807e0877ff7b2ab
Tobias Henkel <tobias.henkel@bmw.de>
2017-12-20 11:36:19 +0100

    Set remote url on every getRepo in merger
    In case we run on github with apps we have an access token in the git
    url. This access token is only valid for a specific period of
    time. Currently the merger caches its repos and never changes the git
    url on subsequent merge or cat requests. After timeout the merger then
    faces an exception [1].
    
    The fix is to update the remote url on every getRepo call in the
    merger. This makes sure that we have a recent access token on every
    merge or cat call.
    
    Further the executor has the same problems and gets fixed by this too
    as it also uses getRepo from the merger.
    
    [1] Exception:
     2017-12-20 07:00:00,874 ERROR zuul.Merger: Unable to update github/sandbox/sandbox
     Traceback (most recent call last):
       File "/usr/lib/python3.6/site-packages/zuul/merger/merger.py", line 382, in updateRepo
         repo.reset()
       File "/usr/lib/python3.6/site-packages/zuul/merger/merger.py", line 143, in reset
         self.update()
       File "/usr/lib/python3.6/site-packages/zuul/merger/merger.py", line 293, in update
         self._git_fetch(repo, 'origin', tags=True)
       File "/usr/lib/python3.6/site-packages/zuul/merger/merger.py", line 133, in _git_fetch
         **kwargs)
       File "/usr/lib/python3.6/site-packages/git/cmd.py", line 551, in <lambda>
         return lambda *args, **kwargs: self._call_process(name, *args, **kwargs)
       File "/usr/lib/python3.6/site-packages/git/cmd.py", line 1010, in _call_process
         return self.execute(call, **exec_kwargs)
       File "/usr/lib/python3.6/site-packages/git/cmd.py", line 821, in execute
         raise GitCommandError(command, status, stderr_value, stdout_value)
     git.exc.GitCommandError: Cmd('git') failed due to: exit code(128)
       cmdline: git fetch --tags origin
       stderr: 'remote: Invalid username or password.
     fatal: Authentication failed for 'https://x-access-token:v1.c8ec09e233b16871b64843adeec5fb048a383fba@github.example.com/sandbox/sandbox/''
    
    Change-Id: If990dc48e6c10c24b6b32db5f5711fc3608bdfe4

--- zuul/merger/merger.py
+++ zuul/merger/merger.py
@@ -113,8 +113,7 @@ class Repo(object):
                 config_writer.set_value('user', 'name', self.username)
             config_writer.write()
         if rewrite_url:
-            with repo.remotes.origin.config_writer as config_writer:
-                config_writer.set('url', self.remote_url)
+            self._git_set_remote_url(repo, self.remote_url)
         self._initialized = True
 
     def isInitialized(self):
@@ -131,6 +130,10 @@ class Repo(object):
         with timeout_handler(self.local_path):
             repo.git.fetch(remote, ref, kill_after_timeout=self.git_timeout,
                            **kwargs)
+
+    def _git_set_remote_url(self, repo, url):
+        with repo.remotes.origin.config_writer as config_writer:
+            config_writer.set('url', url)
 
     def createRepoObject(self):
         self._ensure_cloned()
@@ -330,6 +333,13 @@ class Repo(object):
         repo = self.createRepoObject()
         repo.delete_remote(repo.remotes[remote])
 
+    def setRemoteUrl(self, url):
+        # NOTE: the remote_url does not necessarily reflect the remote url
+        # which is currently on disk so set that regardless.
+        self.log.debug("Set remote url to %s" % url)
+        self.remote_url = url
+        self._git_set_remote_url(self.createRepoObject(), self.remote_url)
+
 
 class Merger(object):
     def __init__(self, working_root, connections, email, username,
@@ -363,6 +373,7 @@ class Merger(object):
             repo = Repo(
                 url, path, self.email, self.username, self.speed_limit,
                 self.speed_time, sshkey, cache_path, self.logger)
+            repo.setRemoteUrl(url)
 
             self.repos[key] = repo
         except Exception:
@@ -377,7 +388,9 @@ class Merger(object):
         url = source.getGitUrl(project)
         key = '/'.join([hostname, project_name])
         if key in self.repos:
-            return self.repos[key]
+            repo = self.repos[key]
+            repo.setRemoteUrl(url)
+            return repo
         sshkey = self.connections.connections.get(connection_name).\
             connection_config.get('sshkey')
         if not url:
