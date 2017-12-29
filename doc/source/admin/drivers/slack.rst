d34dd66c1994c881efb95feb2ca896822589b41c
Clint Byrum <clint@fewbar.com>
2017-12-29 15:05:33 -0800

    Slack driver
    This adds a slack reporter. It should also be the beginnings of a basis for
    a slack trigger source.
    
    The goal is to have a nice thread that encompasses all the runs for a
    single queue item, rather than starting a new one each time. The cache
    is in-memory, so it could grow rather large. However the
    WeakKeyDictionary should cause it to remove cache entries as soon as
    items are garbage collected.
    
    Change-Id: I8a55be0f4c2e6a70fb5a98bada253dba2430e111

---
+++ doc/source/admin/drivers/slack.rst
@@ -0,0 +1,92 @@
+:title: Slack Driver
+
+Slack
+=====
+
+The Slack driver supports reporters only.  It is used to send chat messages
+when items report.
+
+Connection Configuration
+------------------------
+
+.. attr:: <slack connection>
+
+   .. attr:: driver
+      :required:
+
+      .. value:: slack
+
+         The connection must set ``driver=slack`` for Slack connections.
+
+   .. attr:: token
+
+      Slack API token to use. There are numerous ways to obtain one of these.
+      It's recommended to create a bot_ integration and invite the bot to the
+      listed channels.
+
+   .. attr:: subject
+
+      Default subject for messages from this connection.
+
+.. _bot: https://api.slack.com/bot-users
+
+Reporter Configuration
+----------------------
+
+A :ref:`connection<connections>` that uses the slack driver must be supplied to the
+reporter.
+
+Each pipeline can set the ``subject`` to use for the initial
+non-threaded message. The body of the report will be set as a reply
+in this thread to avoid vertically spamming channels.
+
+Each pipeline can also set specific project channel mappings. All
+messages will be sent to ``channel``, which can be a list of strings,
+or a single string.
+
+This example pipeline sends all messages to ``#post-merge`` and messages for
+the ``org/sandbox`` project to ``#sandbox``. It also sends messages for
+``org2/foo`` to both ``#org2`` and ``#foo-devs``.
+
+.. code-block:: yaml
+
+   - pipeline:
+       name: post-merge
+       success:
+         team_slack:
+           channel: '#post-merge'
+           project-channels:
+             org/sandbox: '#sandbox'
+             org2/foo:
+               - #org2
+               - #foo-devs
+       failure:
+         team_slack:
+           channel: '#post-merge'
+           project-channels:
+             org/sandbox: '#sandbox'
+             org2/foo:
+               - #org2
+               - #foo-devs
+
+.. attr:: pipeline.<reporter>.<slack source>
+
+   To report via slack, the dictionaries passed to any of the pipeline
+   :ref:`reporter<reporters>` attributes support the following
+   attributes:
+
+   .. attr:: channel
+
+      A channel or a list of channels to send all messages to.
+
+   .. attr:: project-channels
+
+      A mapping of project names to channels. The value can be a single string
+      or a list of strings indicating the channel or channels to send messages
+      to respectively.
+
+   .. attr:: subject
+
+      The Subject of the report message.
+
+      .. TODO: document subject string formatting.
---
+++ zuul/driver/slack/slackreporter.py
@@ -0,0 +1,81 @@
+# Copyright 2015 Rackspace Australia
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+import voluptuous as v
+
+from zuul.reporter import BaseReporter
+
+
+class SlackReporter(BaseReporter):
+    """Sends off reports to slack channels."""
+
+    name = 'slack'
+    log = logging.getLogger("zuul.SlackReporter")
+
+    def report(self, item):
+        """Send message to associated channels."""
+        message = self._formatItemReport(item)
+        subject = self.config.get('subject', self.connection.subject)
+        subject = subject.format(**vars(item))
+        channels = self.config.get('channel')
+        if isinstance(channels, str):
+            channels = [channels]
+        pchannels = self.config.get(
+            'project-channels', {}).get(item.change.project.name, [])
+        if isinstance(pchannels, str):
+            pchannels = [pchannels]
+        channels += pchannels
+        for channel in channels:
+            ts_cache = self.connection.thread_cache.get(item, {})
+            thread_ts = ts_cache.get(channel)
+            update_cache = thread_ts is None
+            self.log.debug("Reporting {change} to {channel}".format(
+                change=item.change, channel=channel))
+            api_kwargs = dict(
+                channel=channel,
+                as_user=True,
+                text=subject,
+            )
+            if thread_ts:
+                api_kwargs['thread_ts'] = thread_ts
+            initial_msg = self.connection.client.api_call(
+                "chat.postMessage", **api_kwargs)
+            if not initial_msg['ok']:
+                self.log.error(
+                    'Failed sending message for {change}: {error}'.format(
+                        change=item.change, error=initial_msg['error']))
+                continue
+            if update_cache:
+                thread_ts = initial_msg['ts']
+                ts_cache = self.connection.thread_cache.get(item, {})
+                ts_cache[channel] = thread_ts
+                self.connection.thread_cache[item] = ts_cache
+            self.connection.client.api_call(
+                "chat.postMessage",
+                channel=channel,
+                as_user=True,
+                text=message,
+                thread_ts=thread_ts)
+        self.log.info("Reported {change} to {channels}".format(
+            change=item.change, channels=channels))
+
+
+def getSchema():
+    slack_reporter = v.Schema({
+        v.Optional('subject'): str,
+        v.Optional('channel'): v.Any([str], str),
+        v.Optional('project-channels'): {str: v.Any([str], str)},
+    })
+    return slack_reporter
