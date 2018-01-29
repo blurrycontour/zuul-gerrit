cb07cce5ba371b598850ca39dcd5233d7b8f10f0
Markus Hosch <Markus.Hosch@bmw.de>
2018-01-29 10:50:40 +0100

    Add --validate-tenants option to zuul scheduler
    This option can be used to check whether a given tenant configuration
    results in a valid configuration without errors. This can be used for
    validating changes to the tenant config prior to merging and
    reconfiguring zuul..
    
    Change-Id: I9d27f8c2cc3c5a6286b643e8032a94dcd6bd5876
    Co-authored-by: Tobias Henkel <tobias.henkel@bmw.de>

--- tests/unit/test_v3.py
+++ tests/unit/test_v3.py
@@ -3142,7 +3142,7 @@ class TestBrokenConfig(ZuulTestCase):
 
     def test_broken_config_on_startup(self):
         # verify get the errors at tenant level.
-        tenant = self.scheds.first.sched.abide.tenants.get('tenant-one')
+        tenant = self.scheds.first.sched.abide.tenants.get('tenant-broken')
         loading_errors = tenant.layout.loading_errors
         self.assertEquals(
             len(tenant.layout.loading_errors), 2,
@@ -3179,7 +3179,7 @@ class TestBrokenConfig(ZuulTestCase):
 
     def test_dynamic_ignore(self):
         # Verify dynamic config behaviors inside a tenant broken config
-        tenant = self.scheds.first.sched.abide.tenants.get('tenant-one')
+        tenant = self.scheds.first.sched.abide.tenants.get('tenant-broken')
         # There is a configuration error
         self.assertEquals(
             len(tenant.layout.loading_errors), 2,
@@ -3211,7 +3211,7 @@ class TestBrokenConfig(ZuulTestCase):
 
     def test_dynamic_fail_unbroken(self):
         # Verify dynamic config behaviors inside a tenant broken config
-        tenant = self.scheds.first.sched.abide.tenants.get('tenant-one')
+        tenant = self.scheds.first.sched.abide.tenants.get('tenant-broken')
         # There is a configuration error
         self.assertEquals(
             len(tenant.layout.loading_errors), 2,
@@ -3245,7 +3245,7 @@ class TestBrokenConfig(ZuulTestCase):
 
     def test_dynamic_fail_broken(self):
         # Verify dynamic config behaviors inside a tenant broken config
-        tenant = self.scheds.first.sched.abide.tenants.get('tenant-one')
+        tenant = self.scheds.first.sched.abide.tenants.get('tenant-broken')
         # There is a configuration error
         self.assertEquals(
             len(tenant.layout.loading_errors), 2,
@@ -3279,7 +3279,7 @@ class TestBrokenConfig(ZuulTestCase):
 
     def test_dynamic_fix_broken(self):
         # Verify dynamic config behaviors inside a tenant broken config
-        tenant = self.scheds.first.sched.abide.tenants.get('tenant-one')
+        tenant = self.scheds.first.sched.abide.tenants.get('tenant-broken')
         # There is a configuration error
         self.assertEquals(
             len(tenant.layout.loading_errors), 2,
@@ -3311,7 +3311,7 @@ class TestBrokenConfig(ZuulTestCase):
 
     def test_dynamic_fail_cross_repo(self):
         # Verify dynamic config behaviors inside a tenant broken config
-        tenant = self.scheds.first.sched.abide.tenants.get('tenant-one')
+        tenant = self.scheds.first.sched.abide.tenants.get('tenant-broken')
         # There is a configuration error
         self.assertEquals(
             len(tenant.layout.loading_errors), 2,
@@ -3405,6 +3405,51 @@ class TestProjectKeys(ZuulTestCase):
 
         # Make sure it's the right length
         self.assertEqual(2048, ssh_key.get_bits())
+
+
+class TestValidateAllBroken(ZuulTestCase):
+    # Test we fail while validating all tenants with one broken tenant
+
+    validate_tenants = []
+    tenant_config_file = 'config/broken/main.yaml'
+
+    def setUp(self):
+        self.assertRaises(zuul.configloader.ConfigurationSyntaxError,
+                          super().setUp)
+
+    def test_validate_all_tenants_broken(self):
+        # If we reach this point we successfully catched the config exception.
+        # There is nothing more to test here.
+        pass
+
+
+class TestValidateBroken(ZuulTestCase):
+    # Test we fail while validating a broken tenant
+
+    validate_tenants = ['tenant-broken']
+    tenant_config_file = 'config/broken/main.yaml'
+
+    def setUp(self):
+        self.assertRaises(zuul.configloader.ConfigurationSyntaxError,
+                          super().setUp)
+
+    def test_validate_tenant_broken(self):
+        # If we reach this point we successfully catched the config exception.
+        # There is nothing more to test here.
+        pass
+
+
+class TestValidateGood(ZuulTestCase):
+    # Test we don't fail while validating a good tenant in a multi tenant
+    # setup that contains a broken tenant.
+
+    validate_tenants = ['tenant-good']
+    tenant_config_file = 'config/broken/main.yaml'
+
+    def test_validate_tenant_good(self):
+        # If we reach this point we successfully validated the good tenant.
+        # There is nothing more to test here.
+        pass
 
 
 class RoleTestCase(ZuulTestCase):
