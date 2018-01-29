e9fcf6a64e00d83bcb3123b9b78019f1cba5ef97
Markus Hosch <Markus.Hosch@bmw.de>
2018-01-29 10:50:40 +0100

    Add --check-config option to zuul scheduler
    This option can be used to check whether a given configuration
    would result in a successful startup of the zuul scheduler. When
    using this feature, exit code will tell if the configuration was
    ok or not.
    
    Change-Id: I9d27f8c2cc3c5a6286b643e8032a94dcd6bd5876
    Co-authored-by: Tobias Henkel <tobias.henkel@bmw.de>

--- tests/base.py
+++ tests/base.py
@@ -2820,6 +2820,7 @@ class ZuulTestCase(BaseTestCase):
     use_ssl = False
     git_url_with_auth = False
     log_console_port = 19885
+    check_config = False
 
     def _startMerger(self):
         self.merge_server = zuul.merger.server.MergeServer(self.config,
@@ -2973,7 +2974,7 @@ class ZuulTestCase(BaseTestCase):
         self.addCleanup(self.shutdown)
         self.addCleanup(self.assertFinalState)
 
-        self.sched.reconfigure(self.config)
+        self.sched.reconfigure(self.config, check_config=self.check_config)
         self.sched.resume()
 
     def fullReconfigure(self):
