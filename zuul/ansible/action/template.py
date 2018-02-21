39a288a255a6f5889306f021d09e794b94fb3907
Tobias Henkel <tobias.henkel@bmw.de>
2018-02-21 17:26:32 +0100

    Move tmpdir into work root
    Ansible 2.4 changes the way the template module works. It now
    processes the template ans writes it into a temporary file in a newly
    created temporary dir. After that it reuses the copy plugin to copy
    this onto the node. This fails for untrusted jobs because the
    temporary file is created outside of the work root which fails the
    safe path validation of the copy plugin [1].
    
    There are two issues with this behavior. First ansible doesn't use the
    configured local_tmp dir for this temporary file. This can be fixed by
    setting the default tempdir to the configured local_tmp in the module.
    
    Second our current local_tmp setting is outside of the work dir so
    this needs to be moved into the work dir.
    
    [1] Failed log:
    TASK [gitlint : Ensure project has a fallback default config]
    node | ERROR
    node | {
    node |   "msg": "Accessing files from outside the working dir /tmp/54614d6f189a48968648c4e68c05bdba/work is prohibited",
    node |   "path": "/tmp/tmpssae4qfb/gitlint.j2"
    node | }
    
    Change-Id: Ie2c7518973fc81f51826fa16021b95590e08749e

--- zuul/ansible/action/template.py
+++ zuul/ansible/action/template.py
@@ -14,6 +14,8 @@
 # along with this software.  If not, see <http://www.gnu.org/licenses/>.
 
 
+import tempfile
+from ansible import constants as C
 from zuul.ansible import paths
 template = paths._import_ansible_action_plugin("template")
 
@@ -27,5 +29,16 @@ class ActionModule(template.ActionModule):
     def run(self, tmp=None, task_vars=None):
         if not paths._is_official_module(self):
             return paths._fail_module_dict(self._task.action)
+
+        # HACK(tobiash): Set the default dir to the local_tmp. This is a
+        # workaround for making template action respect local_tmp in
+        # ansible 2.4. Otherwise the template module is broken as starting with
+        # ansible 2.4 it defers the copying to the copy module which itself
+        # does the safe path check again with a temp file not residing in the
+        # work dir.
+        #
+        # For 2.5 this is fixed in
+        # https://github.com/ansible/ansible/pull/35005
+        tempfile.tempdir = C.DEFAULT_LOCAL_TMP
 
         return super(ActionModule, self).run(tmp, task_vars)
