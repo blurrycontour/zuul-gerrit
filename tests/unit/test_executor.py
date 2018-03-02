9fb7abe30f55559784bcc45851c54c461ff52261
Tobias Henkel <tobias.henkel@bmw.de>
2018-03-02 16:01:39 +0000

    Refactor load sensors into drivers
    The executor governor can be made much simpler and generic when
    abstracting the actual data gathering into sensors.
    
    Change-Id: Ieb41eef1991c04c58bfb41c39ea0d1010ef3e330

--- tests/unit/test_executor.py
+++ tests/unit/test_executor.py
@@ -23,6 +23,7 @@ import zuul.executor.server
 import zuul.model
 
 from tests.base import ZuulTestCase, simple_layout
+from zuul.executor.sensors.startingbuilds import StartingBuildsSensorDriver
 
 
 class TestExecutorRepos(ZuulTestCase):
@@ -486,9 +487,15 @@ class TestGovernor(ZuulTestCase):
             time.sleep(0.1)
 
     def test_slow_start(self):
+
+        def _set_starting_builds(min, max):
+            for sensor in self.executor_server.sensors:
+                if isinstance(sensor, StartingBuildsSensorDriver):
+                    sensor.min_starting_builds = min
+                    sensor.max_starting_builds = max
+
         self.executor_server.hold_jobs_in_build = True
-        self.executor_server.max_starting_builds = 1
-        self.executor_server.min_starting_builds = 1
+        _set_starting_builds(1, 1)
         self.executor_server.manageLoad()
         self.assertTrue(self.executor_server.accepting_work)
         A = self.fake_gerrit.addFakeChange('common-config', 'master', 'A')
@@ -500,7 +507,7 @@ class TestGovernor(ZuulTestCase):
         self.assertFalse(self.executor_server.accepting_work)
         self.assertEqual(len(self.executor_server.job_workers), 1)
         # Allow enough starting builds for the test to complete.
-        self.executor_server.max_starting_builds = 3
+        _set_starting_builds(1, 3)
         build1.release()
         self.waitForWorkerCompletion(build1)
         self.executor_server.manageLoad()
