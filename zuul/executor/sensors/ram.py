1f99d627198793ca1b5568801a9d45b5e3df166b
Tobias Henkel <tobias.henkel@bmw.de>
2018-03-03 21:30:09 +0000

    WIP: Add cgroup support to ram sensor
    When running within k8s the system memory statistics are useless as
    soon there are configured limits (which is strongly advised). In this
    case we additionally need to check the cgroups.
    
    Change-Id: Idebe5d7e60dc862e89d012594ab362a19f18708d

--- zuul/executor/sensors/ram.py
+++ zuul/executor/sensors/ram.py
@@ -13,15 +13,51 @@
 # under the License.
 
 import logging
+import math
 import psutil
 
 from zuul.executor.sensors import SensorInterface
 from zuul.lib.config import get_default
 
+CGROUP_LIMIT_FILE = '/sys/fs/cgroup/memory/memory.limit_in_bytes'
+CGROUP_USAGE_FILE = '/sys/fs/cgroup/memory/memory.usage_in_bytes'
+
 
 def get_avail_mem_pct():
     avail_mem_pct = 100.0 - psutil.virtual_memory().percent
     return avail_mem_pct
+
+
+def get_avail_mem_pct_cgroup():
+    limit = get_cgroup_value(CGROUP_LIMIT_FILE)
+    usage = get_cgroup_value(CGROUP_USAGE_FILE)
+
+    if math.inf(limit) or math.inf(usage):
+        # pretend we have all memory available if we got infs
+        return 100
+
+    return 100.0 - usage / limit * 100
+
+
+def get_cgroup_value(file):
+    try:
+        with open(file) as f:
+            # If we have no limit we get a really large number back so
+            # compare that with the total system memory. If it's lower
+            # than the total system memory we have a cgroup limit we should
+            # check.
+            return int(f.read().strip())
+    except Exception:
+        return math.inf
+
+
+def get_cgroup_limit():
+    limit = get_cgroup_value(CGROUP_LIMIT_FILE)
+    mem_total = psutil.virtual_memory().total
+    if limit < mem_total:
+        return limit
+    else:
+        return math.inf
 
 
 class RAMSensor(SensorInterface):
@@ -31,6 +67,8 @@ class RAMSensor(SensorInterface):
         self.min_avail_mem = float(get_default(config, 'executor',
                                                'min_avail_mem', '5.0'))
 
+        self.cgroup_limit = get_cgroup_limit()
+
     def isOk(self):
         avail_mem_pct = get_avail_mem_pct()
 
@@ -38,10 +76,27 @@ class RAMSensor(SensorInterface):
             return False, "low memory {:3.1f}% < {}".format(
                 avail_mem_pct, self.min_avail_mem)
 
-        return True, "{:3.1f}% <= {}".format(avail_mem_pct, self.min_avail_mem)
+        if math.isinf(self.cgroup_limit):
+            # we have no cgroup defined limit so we're done now
+            return True, "{:3.1f}% <= {}".format(
+                avail_mem_pct, self.min_avail_mem)
+
+        avail_mem_pct_cgroup = get_avail_mem_pct_cgroup()
+        if avail_mem_pct_cgroup < self.min_avail_mem:
+            return False, "low memory cgroup {:3.1f}% < {}".format(
+                avail_mem_pct_cgroup, self.min_avail_mem)
+
+        return True, "{:3.1f}% <= {}, {:3.1f}% <= {}".format(
+            avail_mem_pct, self.min_avail_mem,
+            avail_mem_pct_cgroup, self.min_avail_mem)
 
     def reportStats(self, statsd, base_key):
         avail_mem_pct = get_avail_mem_pct()
 
         statsd.gauge(base_key + '.pct_used_ram',
                      int((100.0 - avail_mem_pct) * 100))
+
+        if math.isfinite(self.cgroup_limit):
+            avail_mem_pct_cgroup = get_avail_mem_pct_cgroup()
+            statsd.gauge(base_key + '.pct_used_ram_cgroup',
+                         int((100.0 - avail_mem_pct_cgroup) * 100))
