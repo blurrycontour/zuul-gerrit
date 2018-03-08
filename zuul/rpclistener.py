1c665870885bd4e5c896486c61e41ec1bae51b1a
Monty Taylor <mordred@inaugust.com>
2018-03-08 12:57:27 -0600

    web: add /{tenant}/projects and /{tenant}/project/{project} routes
    This change adds the projects_list and project_get gearman function to the
    scheduler gear to expose projects configuration.
    
    Change-Id: Idca04815027b75445961c77fa963b4e601a4d7d7

--- zuul/rpclistener.py
+++ zuul/rpclistener.py
@@ -62,6 +62,8 @@ class RPCListener(object):
         self.worker.registerFunction("zuul:status_get")
         self.worker.registerFunction("zuul:job_get")
         self.worker.registerFunction("zuul:job_list")
+        self.worker.registerFunction("zuul:project_get")
+        self.worker.registerFunction("zuul:project_list")
         self.worker.registerFunction("zuul:key_get")
 
     def getFunctions(self):
@@ -359,6 +361,54 @@ class RPCListener(object):
             output.append({"name": job_name,
                            "description": desc})
         job.sendWorkComplete(json.dumps(output))
+
+    def handle_project_get(self, gear_job):
+        args = json.loads(gear_job.arguments)
+        tenant = self.sched.abide.tenants.get(args["tenant"])
+        trusted, project = tenant.getProject(args["project"])
+        if not project:
+            gear_job.sendWorkComplete(json.dumps({}))
+            return
+        result = {"canonical_name": project.canonical_name}
+        config = tenant.layout.getAllProjectConfigs(project.canonical_name)
+        if config:
+            config = config[0]
+            result["default_branch"] = config.default_branch,
+            result["merge_mode"] = config.merge_mode
+            pipelines = []
+            for pipeline_name, pipeline_config in sorted(
+                    config.pipelines.items()):
+                pipeline_jobs = []
+                for jobs in pipeline_config.job_list.jobs.values():
+                    job = jobs[0]
+                    job_deps = []
+                    for job_dep in job.dependencies:
+                        job_deps.append(job_dep)
+                    pipeline_jobs.append({
+                        'name': job.name,
+                        'dependencies': job_deps,
+                        'hold_following_changes': job.hold_following_changes,
+                        'voting': job.voting,
+                    })
+                pipelines.append({
+                    "name": pipeline_name,
+                    "queue_name": pipeline_config.queue_name,
+                    "jobs": pipeline_jobs,
+                })
+            result["pipelines"] = pipelines
+
+        gear_job.sendWorkComplete(json.dumps(result))
+
+    def handle_project_list(self, job):
+        args = json.loads(job.arguments)
+        tenant = self.sched.abide.tenants.get(args.get("tenant"))
+        output = []
+        for project in tenant.config_projects:
+            output.append({"name": project.name, "type": "config"})
+        for project in tenant.untrusted_projects:
+            output.append({"name": project.name, "type": "untrusted"})
+        job.sendWorkComplete(json.dumps(
+            sorted(output, key=lambda project: project["name"])))
 
     def handle_key_get(self, job):
         args = json.loads(job.arguments)
