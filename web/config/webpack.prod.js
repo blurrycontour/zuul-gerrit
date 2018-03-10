2ae8850d84425f98b00cf0bc813f27e51c4eaaf3
Monty Taylor <mordred@inaugust.com>
2018-03-10 09:19:28 -0600

    Upgrade to webpack 4
    The webpack community just released webpack v4. Upgrade to it.
    
    Amongst the relevant upstream changes, the uglify plugin and
    commonschunk plugins are no longer needed as their behavior is now just
    builtin.
    
    Change-Id: I0359f69861211231e4875abf9817316d9e8878a1

--- web/config/webpack.prod.js
+++ web/config/webpack.prod.js
@@ -5,6 +5,7 @@ const CommonConfig = require('./webpack.common.js');
 const ArchivePlugin = require('webpack-archive-plugin');
 
 module.exports = Merge(CommonConfig, {
+  mode: 'production',
   output: {
     filename: '[name].[chunkhash].js',
     // path.resolve(__dirname winds up relative to the config dir
@@ -23,21 +24,6 @@ module.exports = Merge(CommonConfig, {
     }),
     // Keeps the vendor bundle from changing needlessly.
     new webpack.HashedModuleIdsPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      warningsFilter: function(filename) {
-        return ! /node_modules/.test(filename);
-      },
-      beautify: false,
-      mangle: {
-        screw_ie8: true,
-        keep_fnames: true
-      },
-      compress: {
-        screw_ie8: true
-      },
-      sourceMap: true,
-      comments: false
-    }),
     new ArchivePlugin({
       output: path.resolve(__dirname, '../../zuul-web'),
       format: [
