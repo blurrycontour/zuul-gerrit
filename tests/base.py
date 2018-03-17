6c39c686a9ecbcf333247fd9f7c597b382d1cdcb
Tristan Cacqueray <tdecacqu@redhat.com>
2018-03-17 11:46:49 +0000

    web: add /{tenant}/labels route
    This change adds a zk client to the ZuulWeb service to implement a
    labels route for returning the available labels.
    
    Change-Id: Iecf2948e4895829da3f090734c93fcd0c8a497b5

--- tests/base.py
+++ tests/base.py
@@ -1610,6 +1610,7 @@ class FakeSMTP(object):
 class FakeNodepool(object):
     REQUEST_ROOT = '/nodepool/requests'
     NODE_ROOT = '/nodepool/nodes'
+    LAUNCHER_ROOT = '/nodepool/launchers'
 
     log = logging.getLogger("zuul.test.FakeNodepool")
 
@@ -1618,6 +1619,7 @@ class FakeNodepool(object):
         self.client = kazoo.client.KazooClient(
             hosts='%s:%s%s' % (host, port, chroot))
         self.client.start()
+        self.registerLauncher()
         self._running = True
         self.paused = False
         self.thread = threading.Thread(target=self.run)
@@ -1645,6 +1647,12 @@ class FakeNodepool(object):
             return
         for req in self.getNodeRequests():
             self.fulfillRequest(req)
+
+    def registerLauncher(self, labels=["label1"], id="FakeLauncher"):
+        path = os.path.join(self.LAUNCHER_ROOT, id)
+        data = {'id': id, 'supported_labels': labels}
+        self.client.create(
+            path, json.dumps(data).encode('utf8'), makepath=True)
 
     def getNodeRequests(self):
         try:
@@ -1898,7 +1906,7 @@ class WebProxyFixture(fixtures.Fixture):
 
 
 class ZuulWebFixture(fixtures.Fixture):
-    def __init__(self, gearman_server_port, config, info=None):
+    def __init__(self, gearman_server_port, config, info=None, zk_hosts=None):
         super(ZuulWebFixture, self).__init__()
         self.gearman_server_port = gearman_server_port
         self.connections = zuul.lib.connections.ConnectionRegistry()
@@ -1910,6 +1918,7 @@ class ZuulWebFixture(fixtures.Fixture):
             self.info = zuul.model.WebInfo()
         else:
             self.info = info
+        self.zk_hosts = zk_hosts
 
     def _setUp(self):
         # Start the web server
@@ -1917,7 +1926,8 @@ class ZuulWebFixture(fixtures.Fixture):
             listen_address='::', listen_port=0,
             gear_server='127.0.0.1', gear_port=self.gearman_server_port,
             info=self.info,
-            connections=self.connections)
+            connections=self.connections,
+            zk_hosts=self.zk_hosts)
         self.web.start()
         self.addCleanup(self.stop)
 
