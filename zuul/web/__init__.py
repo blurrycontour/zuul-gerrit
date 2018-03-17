5893fef32fcaddfeb5f8aa2202191a99fc0b9b7e
Tristan Cacqueray <tdecacqu@redhat.com>
2018-03-17 11:46:49 +0000

    web: add /{tenant}/labels route
    This change adds a zk client to the ZuulWeb service to implement a
    labels route for returning the available labels.
    
    Change-Id: Iecf2948e4895829da3f090734c93fcd0c8a497b5

--- zuul/web/__init__.py
+++ zuul/web/__init__.py
@@ -30,6 +30,7 @@ import threading
 
 import zuul.model
 import zuul.rpcclient
+import zuul.zk
 
 STATIC_DIR = os.path.join(os.path.dirname(__file__), 'static')
 cherrypy.tools.websocket = WebSocketTool()
@@ -194,6 +195,7 @@ class ZuulWebAPI(object):
 
     def __init__(self, zuulweb):
         self.rpc = zuulweb.rpc
+        self.zk = zuulweb.zk
         self.zuulweb = zuulweb
         self.cache = {}
         self.cache_time = {}
@@ -333,6 +335,19 @@ class ZuulWebAPI(object):
     def pipelines(self, tenant):
         job = self.rpc.submitJob('zuul:pipeline_list', {'tenant': tenant})
         ret = json.loads(job.data[0])
+        resp = cherrypy.response
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return ret
+
+    @cherrypy.expose
+    @cherrypy.tools.save_params()
+    @cherrypy.tools.json_out(content_type='application/json; charset=utf-8')
+    def labels(self, tenant):
+        labels = set()
+        for launcher in self.zk.getRegisteredLaunchers():
+            for label in launcher.supported_labels:
+                labels.add(label)
+        ret = [{'name': label} for label in sorted(labels)]
         resp = cherrypy.response
         resp.headers['Access-Control-Allow-Origin'] = '*'
         return ret
@@ -488,7 +503,8 @@ class ZuulWeb(object):
                  static_cache_expiry=3600,
                  connections=None,
                  info=None,
-                 static_path=None):
+                 static_path=None,
+                 zk_hosts=None):
         self.start_time = time.time()
         self.listen_address = listen_address
         self.listen_port = listen_port
@@ -501,6 +517,8 @@ class ZuulWeb(object):
         # instanciate handlers
         self.rpc = zuul.rpcclient.RPCClient(gear_server, gear_port,
                                             ssl_key, ssl_cert, ssl_ca)
+        self.zk = zuul.zk.ZooKeeper()
+        self.zk.connect(hosts=zk_hosts, read_only=True)
         self.connections = connections
         self.stream_manager = StreamManager()
 
@@ -528,6 +546,8 @@ class ZuulWeb(object):
                           controller=api, action='project')
         route_map.connect('api', '/api/tenant/{tenant}/pipelines',
                           controller=api, action='pipelines')
+        route_map.connect('api', '/api/tenant/{tenant}/labels',
+                          controller=api, action='labels')
         route_map.connect('api', '/api/tenant/{tenant}/key/{project:.*}.pub',
                           controller=api, action='key')
         route_map.connect('api', '/api/tenant/{tenant}/console-stream',
