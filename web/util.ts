c7329288f985828ad088ef2ac247dc54c24becc8
Monty Taylor <mordred@inaugust.com>
2018-03-23 15:17:14 -0500

    Upgrade from angularjs (v1) to angular (v5)
    Since we got started in all of this angular business back in the good
    old storyboard days of yore, the angular folks cut a major release
    (ok, 4 major releases). The old v1 angular is known as angularjs now, and
    starting at v2 the new codebase is just 'angular'. While angularjs is
    still supported for now, angularjs vs. angular seems to be more like
    zuulv2 vs. zuulv3 - the developers really want people to
    be on the >=v2 series, and they spent a good deal of time fixing issues
    from the original angularjs.
    
    The notable differences are the angular is a bit more explicit/verbose,
    and that it uses typescript instead of plain javscript. The increased
    verbosity wasn't the most popular with some fans of the original angularjs,
    but for those of us who aren't breathing it every day the verbosity is
    helpful.
    
    There is a recommended code organization structure which has been used.
    
    For zuul, there are notable changes to how the http client and location
    service work, so the code related to those has been reworked.
    
    $http has been reworked to use HttpClient - which defaults to grabbing
    the remote json and which can do so in a typesafe way.
    
    $location has been reworked to use the angular-routing module, which allows us
    to pull both URL and Query String parameters in a structured manner. We
    can similary pass query parameters to our output http requests.
    
    Since routing is the new solution for $location, extract the navigation
    bar into a re-usable component.
    
    Add tslint config for the typescript. Keep running eslint on our
    remaining plain javascript files, at least until we've got them all
    transitioned over. Use the angular tslint config as a base, but also
    adopt the rule from standardjs that says to not use semicolons since
    they are not actually needed.
    
    The main.ejs file is a webpack template, not an angular template. Move
    it to web/config with the other webpack files to make that clear.
    
    Add a job that builds the zuul dashboard with the ZUUL_API_URL set to
    point to software factory. This should allow us to see a live test with
    a multi-tenant scheme.
    
    Change-Id: Ida959da05df358994f4d11bb6f40f094d39a9541

--- web/util.ts
+++ web/util.ts
@@ -1,7 +1,3 @@
-/* global URL, ZUUL_API_URL */
-// @licstart  The following is the entire license notice for the
-// JavaScript code in this page.
-//
 // Copyright 2017 Red Hat
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -15,31 +11,20 @@
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 // License for the specific language governing permissions and limitations
 // under the License.
-//
-// @licend  The above is the entire license notice
-// for the JavaScript code in this page.
-
-// TODO(mordred) This is a temporary hack until we're on @angular/router
-function extractTenant (url) {
-  if (url.includes('/t/')) {
-    // This is a multi-tenant deploy, find the tenant
-    const tenantStart = url.lastIndexOf('/t/') + 3
-    const tenantEnd = url.indexOf('/', tenantStart)
-    return url.slice(tenantStart, tenantEnd)
-  } else {
-    return null
-  }
-}
 
 // TODO(mordred) This should be encapsulated in an Angular Service singleton
 // that fetches the other things from the info endpoint.
-export function getSourceUrl (filename, $location) {
+
+import * as url from 'url'
+
+declare var ZUUL_API_URL: string
+declare var ZUUL_BASE_HREF: string
+
+function getSourceUrl (filename: string, tenant?: string): string {
   if (typeof ZUUL_API_URL !== 'undefined') {
     return `${ZUUL_API_URL}/api/${filename}`
   } else {
-    const currentUrl = new URL(window.location)
-    const tenant = extractTenant(currentUrl.href)
-    const baseHref = getBaseHrefFromPath(currentUrl.pathname)
+    const baseHref = getBaseHrefFromPath(window.location.pathname)
     if (tenant) {
       // Multi-tenant deploy. This is at t/a-tenant/x.html
       return `${baseHref}api/tenant/${tenant}/${filename}`
@@ -50,11 +35,33 @@ export function getSourceUrl (filename, $location) {
     }
   }
 }
+export default getSourceUrl
 
-function getBaseHrefFromPath (path) {
+export function getWebsocketUrl (filename: string, tenant?: string): string {
+  let apiBase: string
+  if (typeof ZUUL_API_URL !== 'undefined') {
+    apiBase = ZUUL_API_URL
+  } else {
+    apiBase = window.location.href
+  }
+
+  return url
+    .resolve(apiBase, getSourceUrl(filename, tenant))
+    .replace(/(http)(s)?\:\/\//, 'ws$2://')
+}
+
+function getBaseHrefFromPath (path: string) {
   if (path.includes('/t/')) {
     return path.slice(0, path.lastIndexOf('/t/') + 1)
   } else {
     return path.split('/').slice(0, -1).join('/') + '/'
+  }
+}
+
+export function getBaseHref (): string {
+  if (typeof ZUUL_BASE_HREF !== 'undefined') {
+    return ZUUL_BASE_HREF
+  } else {
+    return getBaseHrefFromPath(window.location.pathname)
   }
 }
