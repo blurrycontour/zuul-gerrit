bc699275eed934c8c30939349530625843856a1c
Monty Taylor <mordred@inaugust.com>
2018-04-03 15:04:06 -0500

    Shift log streaming code into StreamComponent
    Rather than an external function, make zuulStartStream a method on
    StreamComponent. This puts us in a good position to refactor it to use
    data binding.
    
    The only functional changes made in this patch other than re-arranging
    where the code is are making zuulStartStream not take this.route as a
    parameter but instead use this.route which it has access to now.
    
    Change-Id: I0da5ac3f4c345d49e0f385d7c8e89c78a976a9de

--- web/stream/stream.component.ts
+++ web/stream/stream.component.ts
@@ -11,6 +11,7 @@
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 // License for the specific language governing permissions and limitations
 // under the License.
+declare var BuiltinConfig: object
 
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
