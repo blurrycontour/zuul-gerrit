7ce1deb1d065ead853b624dbc8fc0d570355bfe7
Tristan Cacqueray <tdecacqu@redhat.com>
2018-05-27 06:14:08 +0000

    executor: add support for generic build resource
    This change adds support for generic build resources to the executor server.
    Using the connection_port, access information such as service account and
    cluster endpoint are extracted from the node list and injected in a
    resources variable file. Currently this change supports 'kubectl' resource
    for containers that behave like a machine, and 'project'/'namespace' resource
    for the native container workflow.
    
    This change also adds initial support for automatic node's secret setup to
    configure the .kube/config file. This can be extended to also setup
    ssh keys if/when nodepool will manage ssh access for zuul executor.
    
    Depends-On: https://review.openstack.org/570667
    Depends-On: https://review.openstack.org/535557
    Change-Id: Icdb9e800177dc770c58f65b02456a6b904be0666

--- zuul/ansible/callback/zuul_stream.py
+++ zuul/ansible/callback/zuul_stream.py
@@ -227,6 +227,10 @@ class CallbackModule(default.CallbackModule):
                 if ip in ('localhost', '127.0.0.1'):
                     # Don't try to stream from localhost
                     continue
+                connection = play_vars[host].get('ansible_connection')
+                if connection in ('kubectl', ):
+                    # Don't try to stream from kubectl connection
+                    continue
                 log_id = "%s-%s" % (
                     task._uuid, paths._sanitize_filename(inventory_hostname))
                 streamer = threading.Thread(
