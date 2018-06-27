3ebf3cde0effeaff20dec995c25766da952337a2
Monty Taylor <mordred@inaugust.com>
2018-06-27 08:35:08 -0500

    Consolidate navigation functions
    We had getBaseHref and getBaseHrefFromPath in two places. We need
    getBaseHref to be a function so we can all it in the app.module
    provider. Make the ZuulService class use it. Also, make sure
    ZUUL_API_URL is always honored.
    
    Change-Id: I7457a7b160a312ea3eec74b8a4f3380a1f95d8e1

--- web/zuul/zuul.service.ts
+++ web/zuul/zuul.service.ts
@@ -15,66 +15,43 @@
 import { Injectable } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 
-import { getBaseHrefFromPath } from '../util'
-import * as url from 'url'
+import { getBaseHref } from '../util'
 
 declare var ZUUL_API_URL: string
-declare var ZUUL_BASE_HREF: string
 
 @Injectable()
 class ZuulService {
-  private baseHref: string
+  public baseHref: string
 
   constructor() {
+    this.baseHref = getBaseHref()
+  }
+
+  getApiUrl (): string {
     if (typeof ZUUL_API_URL !== 'undefined') {
-      this.baseHref = ZUUL_API_URL
+      return ZUUL_API_URL
     } else {
-      this.baseHref = getBaseHrefFromPath(window.location.href)
-    }
-    if (this.baseHref.endsWith('/')) {
-      this.baseHref = this.baseHref.slice(0, 1)
+      return this.baseHref
     }
   }
 
   getSourceUrl (filename: string, tenant?: string): string {
+    let apiUrl: string = this.getApiUrl()
     if (tenant) {
       // Multi-tenant deploy. This is at t/a-tenant/x.html
-      return `${this.baseHref}/api/tenant/${tenant}/${filename}`
+      return `${apiUrl}/api/tenant/${tenant}/${filename}`
     } else {
       // Whitelabel deploy or tenants list, such as /status.html,
       // /tenants.html or /zuul/status.html or /zuul/tenants.html
-      return `${this.baseHref}/api/${filename}`
+      return `${apiUrl}/api/${filename}`
     }
   }
 
   getWebsocketUrl (filename: string, tenant?: string): string {
-    let apiBase: string
-    if (typeof ZUUL_API_URL !== 'undefined') {
-      apiBase = ZUUL_API_URL
-    } else {
-      apiBase = window.location.href
-    }
-
-    return url
-      .resolve(apiBase, this.getSourceUrl(filename, tenant))
+    return this.getSourceUrl(filename, tenant)
       .replace(/(http)(s)?\:\/\//, 'ws$2://')
   }
 
-  getBaseHrefFromPath (path: string) {
-    if (path.includes('/t/')) {
-      return path.slice(0, path.lastIndexOf('/t/') + 1)
-    } else {
-      return path.split('/').slice(0, -1).join('/') + '/'
-    }
-  }
-
-  getBaseHref (): string {
-    if (typeof ZUUL_BASE_HREF !== 'undefined') {
-      return ZUUL_BASE_HREF
-    } else {
-      return this.getBaseHrefFromPath(window.location.pathname)
-    }
-  }
 }
 
 export default ZuulService
