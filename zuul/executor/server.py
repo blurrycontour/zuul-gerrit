be70b643bd53bcd85b31de61fb398629e4f5b0c2
Ian Wienand <iwienand@redhat.com>
2018-06-27 10:38:46 +1000

    Export inventory_file from zuul.executor
    For localhost only, ansible does not populate inventory_file (see
    [1]).  This makes it difficult to always gather this for debugging
    purposes in zuul-jobs.  Export the inventory file from zuul so we can
    always copy it.
    
    [1] http://docs.ansible.com/ansible/latest/porting_guide_2.4.html#inventory
    
    Change-Id: I239a7f2b32fbe950556ae4f8701fd2a7447504b7

--- zuul/executor/server.py
+++ zuul/executor/server.py
@@ -1349,6 +1349,7 @@ class AnsibleJob(object):
             src_root=self.jobdir.src_root,
             log_root=self.jobdir.log_root,
             work_root=self.jobdir.work_root,
+            inventory_file=self.inventory,
             result_data_file=self.jobdir.result_data_file)
 
         nodes = self.getHostList(args)
