217069336b6f9311f2502cd5715b077440dc3167
Matthieu Huin <mhuin@redhat.com>
2018-07-11 07:13:26 -0700

    Add a dequeue command to zuul client
    Add the ability for an operator to dequeue a change from a pipeline.
    
    Change-Id: I4524291807c8b97b62cfaa31fb5d46dc48adbac9
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- zuul/rpcclient.py
+++ zuul/rpcclient.py
@@ -89,6 +89,13 @@ class RPCClient(object):
                 }
         return not self.submitJob('zuul:enqueue_ref', data).failure
 
+    def dequeue(self, tenant, pipeline, change_ids):
+        data = {'tenant': tenant,
+                'pipeline': pipeline,
+                'change_ids': change_ids,
+                }
+        return not self.submitJob('zuul:dequeue', data).failure
+
     def promote(self, tenant, pipeline, change_ids):
         data = {'tenant': tenant,
                 'pipeline': pipeline,
