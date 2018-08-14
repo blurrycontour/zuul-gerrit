9672c7f72d3a85cf7108d9fc7fdce7a26cb9e5a8
Tristan Cacqueray <tdecacqu@redhat.com>
2018-08-14 05:13:15 +0000

    [wip] web: rewrite interface in react
    This change is a prototype to test react in place of angular.
    
    TODO:
    * Add stream component
    * Add status change filters and expand by default button
    * Add status page periodic refresh
    * Add status page extra informations
    * Add build query filter
    
    Depends-On: https://review.openstack.org/591964
    Change-Id: Ic6c33102ac3da69ebd0b8e9c6c8b431d51f3cfd4

---
+++ web/src/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router }    from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './reducers'
+import App from './App'
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>,
+                  div)
+  ReactDOM.unmountComponentAtNode(div)
+})
---
+++ web/src/pages/jobs.jsx
@@ -0,0 +1,88 @@
+// Copyright 2018 Red Hat, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+import * as React from 'react'
+import { Table } from 'patternfly-react'
+import { Link } from 'react-router-dom'
+import { store } from '../reducers'
+
+import { fetchJobs } from '../api'
+
+class JobsPage extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      jobs: [],
+    }
+  }
+
+  componentDidMount() {
+    fetchJobs().then(response => {
+      this.setState({jobs: response.data})
+    })
+  }
+  render() {
+    const { jobs } = this.state
+    if (jobs.length === 0) {
+      return (<p>Loading...</p>)
+    }
+    let to
+    const state = store.getState()
+    if (state.info.tenant) {
+      to = "/builds"
+    } else {
+      to = "/t/" + state.tenant + "/builds"
+    }
+
+    const headerFormat = value => <Table.Heading>{value}</Table.Heading>
+    const cellFormat = (value, { rowData }) => (
+      <Table.Cell>{value}</Table.Cell>)
+    const cellBuildFormat = (value, { rowData }) => (
+      <Table.Cell>
+        <Link to={to + "?job_name=" + value}>builds</Link>
+      </Table.Cell>)
+    const columns = []
+    const myColumns = ['name', 'description', 'Last builds']
+    myColumns.forEach(column => {
+      let formatter = cellFormat
+      let prop = column
+      if (column === 'Last builds') {
+        prop = 'name'
+        formatter = cellBuildFormat
+      }
+      columns.push({
+        header: {label: column,
+                 formatters: [headerFormat]},
+        property: prop,
+        cell: {formatters: [formatter]},
+      })
+    })
+    return (
+      <Table.PfProvider
+        striped
+        bordered
+        hover
+        columns={columns}
+        >
+        <Table.Header/>
+        <Table.Body
+          rows={jobs}
+          rowKey="name"
+          />
+      </Table.PfProvider>)
+  }
+}
+
+export default JobsPage
