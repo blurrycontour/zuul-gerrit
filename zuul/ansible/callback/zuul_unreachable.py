a35d26fbbb05fdde534a66ad4988a8c590866f3e
Tobias Henkel <tobias.henkel@bmw.de>
2018-09-15 15:26:01 +0200

    Fix unreachable nodes detection
    According to the docs ansible is expected to return 3 if there were
    unreachable nodes. Zuul reacts on this and retries the job. However
    ansible really returns 4 in this case [1] which actually is the code
    for a parse error (which doesn't make sense to retry).
    
    To work around that add a simple callback plugin that gets notified if
    there are unreachable nodes and write the information about which
    nodes failed to a special file beneath the job-output.txt. The
    executor can detect this and react properly then. Further we have the
    information about which nodes failed in this file which gets uploaded
    to the log server too if it exists.
    
    [1] https://github.com/ansible/ansible/issues/19720
    
    Change-Id: I6d609835dba18b50dcbe883d01c6229ce4e38d91

---
+++ zuul/ansible/callback/zuul_unreachable.py
@@ -0,0 +1,50 @@
+# Copyright 2018 BMW Carit GmbH
+#
+# Zuul is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Zuul is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+# This is not needed in python3 - but it is needed in python2 because there
+# is a json module in ansible.plugins.callback and python2 gets confused.
+# Easy local testing with ansible-playbook is handy when hacking on zuul_stream
+# so just put in the __future__ statement.
+from __future__ import absolute_import
+
+import os
+
+from ansible.plugins.callback import default
+from zuul.ansible import logconfig
+
+
+class CallbackModule(default.CallbackModule):
+
+    CALLBACK_VERSION = 2.0
+    # aggregate means we can be loaded and not be the stdout plugin
+    CALLBACK_TYPE = 'aggregate'
+    CALLBACK_NAME = 'zuul_unreachable'
+
+    def __init__(self):
+        super(CallbackModule, self).__init__()
+        logging_config = logconfig.load_job_config(
+            os.environ['ZUUL_JOB_LOG_CONFIG'])
+
+        self.output_path = os.path.splitext(
+            logging_config.job_output_file)[0] + '.unreachable'
+
+        self.unreachable_hosts = set()
+
+    def v2_runner_on_unreachable(self, result):
+        host = result._host.get_name()
+        if host not in self.unreachable_hosts:
+            self.unreachable_hosts.add(host)
+            with open(self.output_path, 'a') as f:
+                f.write('%s\n' % host)
