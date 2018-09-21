cc8f30dfd8fe01f3fdbc569528f99a22a341f168
Fabien Boucher <fboucher@redhat.com>
2018-09-21 16:13:21 +0200

    WIP - Pagure driver
    EARLY WIP - don't take care of it at the moment
    
    Status:
    - Basic event handle - New PR/Commented PR (recheck)/Updated PR (issue with pagure webhook)
    - Job run at New PR event
    - Job result reported as comment on Pagure
    - Job result reported as CI flag on Pagure
    
    Todo:
    - Attempt gate pipeline trigger from message containing :thumbsup:
    - ... a bunch of stuff
    
    RFEs on pagure:
    - https://pagure.io/pagure/issue/3684
    - https://pagure.io/pagure/issue/3948
    - https://pagure.io/pagure/issue/3962
    - https://pagure.io/pagure/issue/3985
    
    Change-Id: I1711653355ae26a3fff3bb6de3c6fca7113cdd01

---
+++ zuul/driver/pagure/paguresource.py
@@ -0,0 +1,121 @@
+import logging
+import voluptuous as v
+
+from zuul.source import BaseSource
+from zuul.model import Project
+
+from zuul.driver.util import scalar_or_list
+
+
+class PagureSource(BaseSource):
+    name = 'pagure'
+    log = logging.getLogger("zuul.source.PagureSource")
+
+    def __init__(self, driver, connection, config=None):
+        hostname = connection.canonical_hostname
+        super(PagureSource, self).__init__(driver, connection,
+                                           hostname, config)
+
+    def getRefSha(self, project, ref):
+        """Return a sha for a given project ref."""
+        raise NotImplementedError()
+
+    def waitForRefSha(self, project, ref, old_sha=''):
+        """Block until a ref shows up in a given project."""
+        raise NotImplementedError()
+
+    def isMerged(self, change, head=None):
+        """Determine if change is merged."""
+        raise NotImplementedError()
+
+    def canMerge(self, change, allow_needs):
+        """Determine if change can merge."""
+        if not change.number:
+            # Not a pull request, considering merged.
+            return True
+        return self.connection.canMerge(change, allow_needs)
+
+    def postConfig(self):
+        """Called after configuration has been processed."""
+        raise NotImplementedError()
+
+    def getChange(self, event, refresh=False):
+        return self.connection.getChange(event, refresh)
+
+    def getChangeByURL(self, url):
+        raise NotImplementedError()
+
+    def getChangesDependingOn(self, change, projects, tenant):
+        raise NotImplementedError()
+
+    def getCachedChanges(self):
+        return self.connection._change_cache.values()
+
+    def getProject(self, name):
+        p = self.connection.getProject(name)
+        if not p:
+            p = Project(name, self)
+            self.connection.addProject(p)
+        return p
+
+    def getProjectBranches(self, project, tenant):
+        return self.connection.getProjectBranches(project, tenant)
+
+    def getProjectOpenChanges(self, project):
+        """Get the open changes for a project."""
+        raise NotImplementedError()
+
+    def updateChange(self, change, history=None):
+        """Update information for a change."""
+        raise NotImplementedError()
+
+    def getGitUrl(self, project):
+        """Get the git url for a project."""
+        return self.connection.getGitUrl(project)
+
+    def getGitwebUrl(self, project, sha=None):
+        """Get the git-web url for a project."""
+        raise NotImplementedError()
+
+    def _ghTimestampToDate(self, timestamp):
+        raise NotImplementedError()
+
+    def getRequireFilters(self, config):
+        raise NotImplementedError()
+
+    def getRejectFilters(self, config):
+        raise NotImplementedError()
+
+    def getRefForChange(self, change):
+        raise NotImplementedError()
+
+
+# Require model
+review = v.Schema({'username': str,
+                   'email': str,
+                   'older-than': str,
+                   'newer-than': str,
+                   'type': str,
+                   'permission': v.Any('read', 'write', 'admin'),
+                   })
+
+
+# Require model
+def getRequireSchema():
+    require = {'status': scalar_or_list(str),
+               'review': scalar_or_list(review),
+               'open': bool,
+               'merged': bool,
+               'current-patchset': bool,
+               'label': scalar_or_list(str)}
+    return require
+
+
+def getRejectSchema():
+    reject = {'status': scalar_or_list(str),
+              'review': scalar_or_list(review),
+              'open': bool,
+              'merged': bool,
+              'current-patchset': bool,
+              'label': scalar_or_list(str)}
+    return reject
---
+++ zuul/driver/pagure/paguretrigger.py
@@ -0,0 +1,49 @@
+import logging
+import voluptuous as v
+from zuul.trigger import BaseTrigger
+from zuul.driver.pagure.paguremodel import PagureEventFilter
+from zuul.driver.util import scalar_or_list, to_list
+
+
+class PagureTrigger(BaseTrigger):
+    name = 'pagure'
+    log = logging.getLogger("zuul.trigger.PagureTrigger")
+
+    def getEventFilters(self, trigger_config):
+        efilters = []
+        for trigger in to_list(trigger_config):
+            f = PagureEventFilter(
+                trigger=self,
+                types=to_list(trigger['event']),
+                actions=to_list(trigger.get('action')),
+                branches=to_list(trigger.get('branch')),
+                refs=to_list(trigger.get('ref')),
+                comments=to_list(trigger.get('comment')),
+                states=to_list(trigger.get('state')),
+                statuses=to_list(trigger.get('status')),
+                required_statuses=to_list(trigger.get('require-status'))
+            )
+            efilters.append(f)
+
+        return efilters
+
+    def onPullRequest(self, payload):
+        pass
+
+
+def getSchema():
+    pagure_trigger = {
+        v.Required('event'):
+            scalar_or_list(v.Any('pg_pull_request',
+                                 'pull_request_review',
+                                 'push')),
+        'action': scalar_or_list(str),
+        'branch': scalar_or_list(str),
+        'ref': scalar_or_list(str),
+        'comment': scalar_or_list(str),
+        'state': scalar_or_list(str),
+        'require-status': scalar_or_list(str),
+        'status': scalar_or_list(str)
+    }
+
+    return pagure_trigger
