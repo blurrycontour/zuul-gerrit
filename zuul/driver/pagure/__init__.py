b34e7e5730bae7df9f71f693696f103460246c24
Fabien Boucher <fboucher@redhat.com>
2018-09-21 16:13:21 +0200

    WIP - Pagure driver
    First Pagure PR gated by Zuul : https://pagure.io/test-zuul/pull-request/22
    
    Status:
    
    - Event handling
    ** New PR [OK]
    ** Commented PR (recheck) [OK]
    ** Updated PR [OK]
    ** CI flagged added [OK]
    ** Human review (:thumbsup:/:thumbsdown:)
    
    - Job results reported as comment on Pagure
    - Job results reported as CI flag on Pagure
    
    - PR merged by Zuul
    
    - Support of trigger and require filters
    
    Todo:
    - Fix workaround regarding issues open on Pagure tracker (see below)
    - Refs updated (post/release style pipelines)
    - Reversed depends-on check (no lucene like index in pagure)
    - Unittests
    
    RFEs on pagure:
    - https://pagure.io/pagure/issue/3948 (blocker)
    - https://pagure.io/pagure/issue/3985 (can be mitigated)
    - https://pagure.io/pagure/issue/4002 (blocker)
    
    Change-Id: I1711653355ae26a3fff3bb6de3c6fca7113cdd01

---
+++ zuul/driver/pagure/__init__.py
@@ -0,0 +1,36 @@
+from zuul.driver import Driver, ConnectionInterface, TriggerInterface
+from zuul.driver import SourceInterface, ReporterInterface
+from zuul.driver.pagure import pagureconnection
+from zuul.driver.pagure import paguresource
+from zuul.driver.pagure import pagurereporter
+from zuul.driver.pagure import paguretrigger
+
+
+class PagureDriver(Driver, ConnectionInterface, TriggerInterface,
+                   SourceInterface, ReporterInterface):
+    name = 'pagure'
+
+    def getConnection(self, name, config):
+        return pagureconnection.PagureConnection(self, name, config)
+
+    def getTrigger(self, connection, config=None):
+        return paguretrigger.PagureTrigger(self, connection, config)
+
+    def getSource(self, connection):
+        return paguresource.PagureSource(self, connection)
+
+    def getReporter(self, connection, pipeline, config=None):
+        return pagurereporter.PagureReporter(
+            self, connection, pipeline, config)
+
+    def getTriggerSchema(self):
+        return paguretrigger.getSchema()
+
+    def getReporterSchema(self):
+        return pagurereporter.getSchema()
+
+    def getRequireSchema(self):
+        return paguresource.getRequireSchema()
+
+    def getRejectSchema(self):
+        return paguresource.getRejectSchema()
