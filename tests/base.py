fbc3eee53294c14aa1fd92736bf63762ff5d927c
Tobias Henkel <tobias.henkel@bmw.de>
2018-11-07 22:01:14 +0100

    Report tenant and project specific resource usage stats
    We currently lack means to support resource accounting of tenants or
    projects. Together with an addition to nodepool that adds resource
    metadata to nodes we can emit statsd statistics per tenant and per
    project.
    
    The following statistics are emitted:
    * zuul.nodepool.resources.tenant.{tenant}.{resource}.current
      Gauge with the currently used resources by tenant
    
    * zuul.nodepool.resources.tenant.{project}.{resource}.current
      Gauge with the currently used resources by project
    
    * zuul.nodepool.resources.tenant.{tenant}.{resource}.counter
      Counter with the summed usage by tenant. e.g. cpu seconds
    
    * zuul.nodepool.resources.project.{project}.{resource}.counter
      Counter with the summed usage	by project. e.g. cpu seconds
    
    Depends-On: https://review.openstack.org/616262
    Change-Id: I68ea68128287bf52d107959e1c343dfce98f1fc8

--- tests/base.py
+++ tests/base.py
@@ -1748,6 +1748,7 @@ class FakeNodepool(object):
         self.thread.start()
         self.fail_requests = set()
         self.remote_ansible = False
+        self.resources = None
 
     def stop(self):
         self._running = False
@@ -1833,6 +1834,8 @@ class FakeNodepool(object):
                     host_keys=host_keys,
                     executor='fake-nodepool',
                     hold_expiration=None)
+        if self.resources:
+            data['resources'] = self.resources
         if self.remote_ansible:
             data['connection_type'] = 'ssh'
         if 'fakeuser' in node_type:
