3deb9558f01b6bc833b499b22dfed01696312a28
Benedikt Loeffler <benedikt.loeffler@bmw.de>
2019-01-23 14:56:56 +0100

    Report retried builds in a build set via mqtt.
    If a build must be retried, the previous build information get lost.
    To be informed that a build was retried, the retry builds are now part
    of the mqtt message.
    
    Change-Id: I8c93376f844c3d1c55c89a250384a7f835763677

--- zuul/model.py
+++ zuul/model.py
@@ -1808,6 +1808,7 @@ class BuildSet(object):
     def __init__(self, item):
         self.item = item
         self.builds = {}
+        self.retry_builds = {}
         self.result = None
         self.uuid = None
         self.commit = None
@@ -1868,6 +1869,9 @@ class BuildSet(object):
             self.tries[build.job.name] = 1
         build.build_set = self
 
+    def addRetryBuild(self, build):
+        self.retry_builds.setdefault(build.job.name, []).append(build)
+
     def removeBuild(self, build):
         if build.job.name not in self.builds:
             return
@@ -1881,6 +1885,9 @@ class BuildSet(object):
         keys = list(self.builds.keys())
         keys.sort()
         return [self.builds.get(x) for x in keys]
+
+    def getRetryBuildsForJob(self, job_name):
+        return self.retry_builds.get(job_name, [])
 
     def getJobNodeSet(self, job_name):
         # Return None if not provisioned; empty NodeSet if no nodes
@@ -1987,6 +1994,9 @@ class QueueItem(object):
 
     def addBuild(self, build):
         self.current_build_set.addBuild(build)
+
+    def addRetryBuild(self, build):
+        self.current_build_set.addRetryBuild(build)
 
     def removeBuild(self, build):
         self.current_build_set.removeBuild(build)
@@ -2272,6 +2282,7 @@ class QueueItem(object):
 
     def setResult(self, build):
         if build.retry:
+            self.addRetryBuild(build)
             self.removeBuild(build)
             return
 
@@ -2373,8 +2384,9 @@ class QueueItem(object):
 
         return url
 
-    def formatJobResult(self, job):
-        build = self.current_build_set.getBuild(job.name)
+    def formatJobResult(self, job, build=None):
+        if build is None:
+            build = self.current_build_set.getBuild(job.name)
         result = build.result
         pattern = None
         if result == 'SUCCESS':
