bcd39128652045bcc1e77029ada7be310020914e
Benedikt Loeffler <benedikt.loeffler@bmw.de>
2019-01-28 14:38:15 +0100

    Report retried builds via sql reporter.
    Since we added those to the MQTT reporter, we should also store them in
    the SQL database. They are stored in the zuul_build table and can be
    identified via the new "final" column which is set to False for those
    builds (and True for all others).
    
    To not spoil the build results page in zuul web with those RETRY
    results, we filter them out by default (final=false). Although, if
    interested one could retrieve them via the web API by setting the
    "final" parameter to true. If no final parameter is provided in the web
    api, all builds are returned.
    
    For the migration of the SQL database, all existing builds will be set
    to final.
    
    As I know already some people who are interested in seeing those builds
    also in the UI, we could add e.g. a checkbox next to the search filter
    saying "Show retried builds".
    
    Change-Id: I5960df8a997c7ab81a07b9bd8631c14dbe22b8ab

--- zuul/driver/sql/sqlconnection.py
+++ zuul/driver/sql/sqlconnection.py
@@ -60,7 +60,7 @@ class DatabaseSession(object):
                   change=None, branch=None, patchset=None, ref=None,
                   newrev=None, event_id=None, uuid=None, job_name=None,
                   voting=None, node_name=None, result=None, provides=None,
-                  limit=50, offset=0):
+                  final=None, limit=50, offset=0):
 
         build_table = self.connection.zuul_build_table
         buildset_table = self.connection.zuul_buildset_table
@@ -102,6 +102,7 @@ class DatabaseSession(object):
         q = self.listFilter(q, build_table.c.voting, voting)
         q = self.listFilter(q, build_table.c.node_name, node_name)
         q = self.listFilter(q, build_table.c.result, result)
+        q = self.listFilter(q, build_table.c.final, final)
         q = self.listFilter(q, provides_table.c.name, provides)
 
         q = q.order_by(build_table.c.id.desc()).\
@@ -300,6 +301,7 @@ class SQLConnection(BaseConnection):
             voting = sa.Column(sa.Boolean)
             log_url = sa.Column(sa.String(255))
             node_name = sa.Column(sa.String(255))
+            final = sa.Column(sa.Boolean)
             buildset = orm.relationship(BuildSetModel, backref="builds")
 
             def createArtifact(self, *args, **kw):
