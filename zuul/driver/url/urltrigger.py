1473e96190abeea2ce3aefb68fd73e9f0f5491ca
Fabien Boucher <fboucher@redhat.com>
2019-02-07 17:46:05 +0100

    URLTrigger driver time based - artifact change jobs triggering driver
    The aims of the driver is to provide a solution for triggering
    jobs based on an artifact state change.
    
    Given an URL and an attribute (an HTTP header field name) the
    URLTrigger is able to discover that the external resource has
    evolved since the last check. The delay attribute is used
    to set the check state rate.
    
    Response headers such as 'Last-Modified' or 'Etag' could be used
    with that driver to trigger jobs based on the URL content change.
    
    This driver is a proposal to bring a solution similar to
    the URLTrigger plugin of Jenkins.
    
    - pipeline:
        name: foo-artifact-changed
        post-review: true
        description: Jobs in this queue are triggered when the foo artifact has changed.
        manager: independent
        precedence: low
        trigger:
          url:
            - delay: '*/10 * * * *'
              url: https://domain.com/artifacts/foo/2.x/foo.tgz
              attribute: ETag
        success:
          sqlreporter:
        failure:
          sqlreporter:
    
    - project:
        name: myproject
        foo-artifact-changed:
          jobs:
            - functional-test-1
            - functional-test-2
    
    Change-Id: I58ed1fb4bc42575400110399dadb26e797a7f4d8

---
+++ zuul/driver/url/urltrigger.py
@@ -0,0 +1,46 @@
+# Copyright 2012 Hewlett-Packard Development Company, L.P.
+# Copyright 2013 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import voluptuous as v
+
+from zuul.trigger import BaseTrigger
+from zuul.driver.url.urlmodel import URLEventFilter
+from zuul.driver.util import to_list
+
+
+class URLTrigger(BaseTrigger):
+    name = 'url'
+
+    def getEventFilters(self, trigger_conf):
+        efilters = []
+        for trigger in to_list(trigger_conf):
+            f = URLEventFilter(trigger=self,
+                               delay=trigger['delay'],
+                               url=trigger['url'],
+                               attribute=trigger['attribute'],
+                               )
+
+            efilters.append(f)
+
+        return efilters
+
+
+def getSchema():
+    url_trigger = {
+        v.Required('delay'): str,
+        v.Required('url'): str,
+        v.Required('attribute'): str,
+    }
+    return url_trigger
