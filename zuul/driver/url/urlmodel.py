18128f7204cc550a16b4c6edf474bb52bd60ded7
Fabien Boucher <fboucher@redhat.com>
2019-02-07 17:46:05 +0100

    URLTrigger driver time based - artifact change jobs triggering driver
    The aims of the driver is to provide a solution for triggering
    jobs based on an artifact state change.
    
    Given an URL and an attribute (an HTTP header field name) the
    URLTrigger is able to discover that the external resource has
    evolved since the last check. The delay attribute is used
    to set the check state rate.
    
    Response headers such as 'Last-Modified' or 'Etag' could be used
    with that driver to trigger jobs based on the URL content change.
    
    This driver is a proposal to bring a solution similar to
    the URLTrigger plugin of Jenkins.
    
    Change-Id: I58ed1fb4bc42575400110399dadb26e797a7f4d8

---
+++ zuul/driver/url/urlmodel.py
@@ -0,0 +1,41 @@
+# Copyright 2017 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from zuul.model import EventFilter, TriggerEvent
+
+
+class URLEventFilter(EventFilter):
+    def __init__(self, trigger, delay, url, attribute):
+        EventFilter.__init__(self, trigger)
+
+        self.url = url
+        self.attribute = attribute
+        self.delay = delay
+
+    def __repr__(self):
+        ret = '<URLEventFilter'
+        ret += ' url: %s,' % self.url
+        ret += ' attribute: %s' % self.attribute
+        ret += '>'
+        return ret
+
+    def matches(self, event, change):
+        return True
+
+
+class URLTriggerEvent(TriggerEvent):
+    def __init__(self):
+        super(URLTriggerEvent, self).__init__()
+        self.url = None
+        self.attribute = None
