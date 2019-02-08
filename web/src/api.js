cb4b697ec11ed978d0e3435f4e5f0bee41f3256b
Tristan Cacqueray <tdecacqu@redhat.com>
2019-02-08 05:42:11 +0000

    web: add build button to trigger job
    This change updates the project info page to enable triggering
    individual job through the web trigger driver.
    
    TODO: do not show build button when user is not authenticated
    
    Change-Id: Icc05a5c32bcf90bd5c45fdb56fe4c4426218f361

--- web/src/api.js
+++ web/src/api.js
@@ -135,6 +135,9 @@ function fetchBuildsets (apiPrefix, queryString) {
   }
   return Axios.get(apiUrl + apiPrefix + path)
 }
+function fetchPipelines (apiPrefix) {
+  return Axios.get(apiUrl + apiPrefix + 'pipelines')
+}
 function fetchProject (apiPrefix, projectName) {
   return Axios.get(apiUrl + apiPrefix + 'project/' + projectName)
 }
@@ -153,6 +156,9 @@ function fetchLabels (apiPrefix) {
 function fetchNodes (apiPrefix) {
   return Axios.get(apiUrl + apiPrefix + 'nodes')
 }
+function triggerJobs (apiPrefix, projectName, inputs) {
+  return Axios.post(apiUrl + apiPrefix + 'trigger/' + projectName, inputs)
+}
 
 export {
   getHomepageUrl,
@@ -163,6 +169,7 @@ export {
   fetchBuild,
   fetchBuilds,
   fetchBuildsets,
+  fetchPipelines,
   fetchProject,
   fetchProjects,
   fetchJob,
@@ -170,5 +177,6 @@ export {
   fetchLabels,
   fetchNodes,
   fetchTenants,
-  fetchInfo
+  fetchInfo,
+  triggerJobs,
 }
--- web/src/containers/project/ProjectVariant.jsx
+++ web/src/containers/project/ProjectVariant.jsx
@@ -14,18 +14,64 @@
 
 import * as React from 'react'
 import PropTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import {
+  Button,
+  Checkbox,
+  Form,
+  FormGroup,
+} from 'patternfly-react'
+
+import BuildModal from '../build/BuildModal'
 
 
 class ProjectVariant extends React.Component {
   static propTypes = {
+    projectName: PropTypes.string,
     tenant: PropTypes.object,
-    variant: PropTypes.object.isRequired
+    pipelines: PropTypes.array,
+    variant: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired,
+  }
+
+  constructor (props) {
+    super(props)
+    this.pipelinesRef = {}
+    this.pipelinesJob = {}
+  }
+
+  handleCheckBox = (target, pipeline, job) => {
+    if (!target.checked) {
+      // TODO: add auto unchecked of un-needed auto-clicked job
+      return
+    }
+    if (job.dependencies) {
+      job.dependencies.forEach(dep => {
+        const inputRef = this.pipelinesRef[pipeline][dep.name]
+        if (inputRef) {
+          inputRef.checked = true
+          // Call the handler recursively to pick dependend's dependencies
+          this.handleCheckBox(inputRef, pipeline, this.pipelinesJob[pipeline][dep.name])
+        }
+      })
+    }
+  }
+
+  executeJobs = (pipeline) => {
+    const jobs = []
+    // Collect checked jobs
+    Object.entries(this.pipelinesRef[pipeline])
+      .filter(([, inputRef]) => inputRef.checked)
+      .forEach(([job,]) => {
+        jobs.push(job)
+      })
+    this.modalRef.show(pipeline, jobs)
   }
 
   render () {
-    const { tenant, variant } = this.props
+    const { pipelines, tenant, variant } = this.props
     const rows = []
 
     rows.push({label: 'Merge mode', value: variant.merge_mode})
@@ -40,29 +86,71 @@ class ProjectVariant extends React.Component {
       rows.push({label: 'Templates', value: templateList})
     }
 
+    const pipelineWebTrigger = []
+    pipelines.forEach(pipeline => {
+      pipeline.triggers.forEach(trigger => {
+        if (trigger.name === 'web') {
+          pipelineWebTrigger.push(pipeline.name)
+        }
+      })
+    })
+
     variant.pipelines.forEach(pipeline => {
       // TODO: either adds job link anchor to load the right variant
       // and/or show the job variant config in a modal?
+      this.pipelinesRef[pipeline.name] = {}
+      this.pipelinesJob[pipeline.name] = {}
       const jobList = (
         <React.Fragment>
           {pipeline.queue_name && (
             <p><strong>Queue: </strong> {pipeline.queue_name} </p>)}
-          <ul className='list-group'>
-            {pipeline.jobs.map((item, idx) => (
-              <li className='list-group-item' key={idx}>
-                <Link to={tenant.linkPrefix + '/job/' + item[0].name}>
-                  {item[0].name}
-                </Link>
-              </li>
-            ))}
-          </ul>
+          {(pipelineWebTrigger.indexOf(pipeline.name) !== -1) ? (
+              <Form>
+                <ul className='list-group'>
+                  <FormGroup controlId='project-branch' key='project-branch'>
+                    {pipeline.jobs.map((item, idx) => {
+                      this.pipelinesJob[pipeline.name][item[0].name] = item[0]
+                      return (
+                        <li className='list-group-item' key={idx}>
+                          <Checkbox
+                            className="pull-left"
+                            style={{'marginTop': 0, 'marginRight': '5px'}}
+                            onChange={(e) => this.handleCheckBox(e.target, pipeline.name, item[0])}
+                            inputRef={(ref) => {this.pipelinesRef[pipeline.name][item[0].name]= ref}} />
+                          <Link to={tenant.linkPrefix + '/job/' + item[0].name}>
+                            {item[0].name}
+                          </Link>
+                        </li>)
+                    })}
+                  </FormGroup>
+                </ul>
+                <Button
+                  bsStyle='primary'
+                  onClick={() => {this.executeJobs(pipeline.name)}}>
+                  Execute job(s)
+                </Button>
+              </Form>) : (
+                <ul className='list-group'>
+                  {pipeline.jobs.map((item, idx) => (
+
+                    <li className='list-group-item' key={idx}>
+                      <Link to={tenant.linkPrefix + '/job/' + item[0].name}>
+                        {item[0].name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              )}
         </React.Fragment>
       )
       rows.push({label: pipeline.name + ' jobs', value: jobList})
     })
-
     return (
       <div>
+        <BuildModal
+          onRef={e => this.modalRef = e}
+          projectName={this.props.projectName}
+        />
         <table className='table table-striped table-bordered'>
           <tbody>
             {rows.map(item => (
@@ -78,4 +166,6 @@ class ProjectVariant extends React.Component {
   }
 }
 
-export default connect(state => ({tenant: state.tenant}))(ProjectVariant)
+export default withRouter(connect(state => ({
+  tenant: state.tenant
+}))(ProjectVariant))
