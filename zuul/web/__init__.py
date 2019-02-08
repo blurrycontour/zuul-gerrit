7ee05a51d823f12fe2f992b32be01ea158b6fcc8
Felix Schmidt <felix.fs.schmidt@bmw.de>
2019-02-08 07:14:11 +0100

    Retrieve full list of jobs with details per tenant via API
    To retrieve the jobs in Zubbi directly from Zuul, we need an endpoint
    that returns the list of jobs per tenant with all their data.
    Otherwise, we would have to retrieve the data for each job
    independently via /api/tenants/{tenant_name}/jobs/{job_name}
    which would result in a lot of requests being sent to the Zuul API.
    
    Change-Id: I3040e9bacb2348b26ba125a50300db59e4582a8b

--- zuul/web/__init__.py
+++ zuul/web/__init__.py
@@ -275,8 +275,9 @@ class ZuulWebAPI(object):
     @cherrypy.expose
     @cherrypy.tools.save_params()
     @cherrypy.tools.json_out(content_type='application/json; charset=utf-8')
-    def jobs(self, tenant):
-        job = self.rpc.submitJob('zuul:job_list', {'tenant': tenant})
+    def jobs(self, tenant, full=None):
+        full = str(full).lower() == 'true'
+        job = self.rpc.submitJob('zuul:job_list', {'tenant': tenant, 'full': full})
         ret = json.loads(job.data[0])
         if ret is None:
             raise cherrypy.HTTPError(404, 'Tenant %s does not exist.' % tenant)
