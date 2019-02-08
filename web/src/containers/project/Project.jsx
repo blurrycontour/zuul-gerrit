834c8e33ffee59a8d8d34a5d1a1ac6daea5ca90b
Tristan Cacqueray <tdecacqu@redhat.com>
2019-02-08 05:42:11 +0000

    web: add build button to trigger job
    This change updates the project info page to enable triggering
    individual job through the web trigger driver.
    
    TODO: do not show build button when user is not authenticated
    
    Depends-On: https://review.openstack.org/637670
    Change-Id: Icc05a5c32bcf90bd5c45fdb56fe4c4426218f361

--- web/src/containers/project/Project.jsx
+++ web/src/containers/project/Project.jsx
@@ -28,6 +28,7 @@ import ProjectVariant from './ProjectVariant'
 class Project extends React.Component {
   static propTypes = {
     project: PropTypes.object.isRequired,
+    pipelines: PropTypes.array.isRequired,
   }
 
   state = {
@@ -43,7 +44,7 @@ class Project extends React.Component {
   }
 
   render () {
-    const { project } = this.props
+    const { pipelines, project } = this.props
     const { variantIdx } = this.state
 
     return (
@@ -65,7 +66,10 @@ class Project extends React.Component {
             <TabContent>
               <TabPane>
                 {project.configs[variantIdx] && (
-                  <ProjectVariant variant={project.configs[variantIdx]} />
+                  <ProjectVariant
+                    projectName={project.name}
+                    pipelines={pipelines}
+                    variant={project.configs[variantIdx]} />
                 )}
               </TabPane>
             </TabContent>
