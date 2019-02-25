4d287d3934058ca60cc2b9ffe0c9c1fc69db331e
Tristan Cacqueray <tdecacqu@redhat.com>
2019-02-25 06:14:39 +0000

    web: add depends-on support to the freeze job API
    This change enable zuul-runner to PUT a list of depends on and
    get the list of items and merger items needed to run a
    speculative job.
    
    Change-Id: I4ef9d2121fd7a7b8b411097993c12086dc8bcb57

--- zuul/web/__init__.py
+++ zuul/web/__init__.py
@@ -1123,10 +1123,16 @@ class ZuulWebAPI(object):
 
     @cherrypy.expose
     @cherrypy.tools.save_params()
+    @cherrypy.tools.json_in()
     @cherrypy.tools.json_out(content_type='application/json; charset=utf-8')
     def project_freeze_job(self, tenant, pipeline, project, branch, job):
-        # TODO(jhesketh): Allow a canonical change/item to be passed in which
-        # would return the job with any in-change modifications.
+        dependson = []
+        try:
+            if cherrypy.request.method == "PUT" and cherrypy.request.body:
+                dependson = list(cherrypy.request.json)
+        except json.decoder.JSONDecodeError:
+            self.log.warning(
+                "Couldn't decode request body %s", cherrypy.request.body)
 
         job = self.rpc.submitJob(
             'zuul:project_freeze_job',
@@ -1135,7 +1141,8 @@ class ZuulWebAPI(object):
                 'project': project,
                 'pipeline': pipeline,
                 'branch': branch,
-                'job': job
+                'job': job,
+                'depends-on': dependson,
             }
         )
         ret = json.loads(job.data[0])
