f6d890e1ef80c216c4c1b9ec509a876d9bfc6e3f
Joshua Hesketh <josh@nitrotech.org>
2019-03-12 14:43:51 +1100

    Separate out executor server from runner
    This refactors code to have the executor server separate from the job
    class. This will make it clearer the parts used by running the job.
    
    This change largely lifts code out of server.py and into common.py.
    To help with reviewing, minimal changes are made.
    
    Change-Id: I6661fc33305ca006458fec62a1e0a1bdb45da3c2
    Co-Authored-By: Tristan Cacqueray <tdecacqu@redhat.com>

--- tests/base.py
+++ tests/base.py
@@ -2889,7 +2889,7 @@ class FakeBuild(object):
         return repos
 
 
-class RecordingAnsibleJob(zuul.executor.server.AnsibleJob):
+class RecordingAnsibleJob(zuul.executor.server.AnsibleJobGearman):
     result = None
 
     def doMergeChanges(self, merger, items, repo_state):
