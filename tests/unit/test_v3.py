71ac6bdf2156629480b46897863f2877a192f098
Joshua Hesketh <josh@nitrotech.org>
2019-03-29 16:56:56 +1100

    Separate out executor concerns from AnsibleJob
    Primarily this change is about decoupling (the previously)
    AnsilbeJobGearman from AnsibleJob.
    
    This does a couple of things:
     - It gives us an AnsibleJobContextManager that is responsible for managing
       the starting and stopping of a job.
     - It separates out the concerns of running a playbook vs the concerns of
       communicating with the executor server (eg, gearman, finger etc).
     - It gives us a unified point into an AnsibleJob, rather than arbitrarily
       extending it.
     - It resolves a few mistakes where some functions that were needed by
       AnsibleJob were in its Child class.
    
    Change-Id: I10cd4036cf5663a27c25e570e9450944d445a06f

--- tests/unit/test_v3.py
+++ tests/unit/test_v3.py
@@ -4056,7 +4056,7 @@ class TestDataReturn(AnsibleZuulTestCase):
 
         # Stop the job worker to simulate an executor restart
         for job_worker in self.executor_server.job_workers.values():
-            if job_worker.job.unique == paused_job.uuid:
+            if job_worker.unique == paused_job.uuid:
                 job_worker.stop()
         self.waitUntilSettled("stop job worker")
 
@@ -5672,7 +5672,7 @@ class TestJobPause(AnsibleZuulTestCase):
 
         # Stop the job worker of compile1 to simulate an executor restart
         for job_worker in self.executor_server.job_workers.values():
-            if job_worker.job.unique == compile1.unique:
+            if job_worker.unique == compile1.unique:
                 job_worker.stop()
         self.waitUntilSettled("Stop job")
 
@@ -5921,7 +5921,7 @@ class TestJobPause(AnsibleZuulTestCase):
         # Stop the job worker of test to simulate an executor restart
         job_test = self.builds[1]
         for job_worker in self.executor_server.job_workers.values():
-            if job_worker.job.unique == job_test.unique:
+            if job_worker.unique == job_test.unique:
                 job_worker.stop()
 
         self.executor_server.hold_jobs_in_build = False
