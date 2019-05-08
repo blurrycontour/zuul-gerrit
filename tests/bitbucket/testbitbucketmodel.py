1b5932001453065dcbbfe04f751806a9356f9e6f
Mark Meyer <mark.meyer@smaato.com>
2019-05-08 16:21:19 +0200

    Add initial Bitbucket Server functionality
    Change-Id: Ib702a12efe54f5e62e78744fd59cb1696562777e

---
+++ tests/bitbucket/testbitbucketmodel.py
@@ -0,0 +1,25 @@
+import unittest
+
+from zuul.driver.bitbucket.bitbucketmodel import PullRequest
+
+
+class TestBitbucketModel(unittest.TestCase):
+    def test_isUpdateOf(self):
+        pr_a = PullRequest('aaa')
+        pr_a.id = 101
+        pr_a.patchset = ['foo', 'bar']
+        pr_a.updatedDate = 1060
+
+        pr_b = PullRequest('aaa')
+        pr_b.id = 101
+        pr_b.patchset = ['foo', 'xyz']
+        pr_b.updatedDate = 2120
+
+        pr_c = PullRequest('ccc')
+        pr_c.id = 101
+        pr_c.patchset = ['foo', 'bar']
+        pr_c.updatedDate = 1060
+
+        self.assertFalse(pr_a.isUpdateOf(pr_b))
+        self.assertTrue(pr_b.isUpdateOf(pr_a))
+        self.assertFalse(pr_c.isUpdateOf(pr_a))
