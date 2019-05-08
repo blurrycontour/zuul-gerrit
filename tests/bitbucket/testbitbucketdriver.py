ca4789d2a9f1ac6556edf76ff5640f5be886aa32
Mark Meyer <mark.meyer@smaato.com>
2019-05-08 16:21:19 +0200

    Add initial Bitbucket Server functionality
    Change-Id: Ib702a12efe54f5e62e78744fd59cb1696562777e

---
+++ tests/bitbucket/testbitbucketdriver.py
@@ -0,0 +1,22 @@
+
+import unittest
+from zuul.driver.bitbucket import BitbucketDriver
+from zuul.driver.bitbucket.bitbucketsource import BitbucketSource
+from zuul.driver.bitbucket.bitbucketconnection import BitbucketConnection
+
+class TestBitbucketDriver(unittest.TestCase):
+	def test_getRequireSchema(self):
+		drv = BitbucketDriver()
+		self.assertEqual({}, drv.getRequireSchema())
+
+	def test_getRejectSchema(self):
+		drv = BitbucketDriver()
+		self.assertEqual({}, drv.getRejectSchema())
+
+	def test_getSource(self):
+		drv = BitbucketDriver()
+		self.assertIsInstance(drv.getSource(drv.getConnection('foo', {})), BitbucketSource)
+
+	def test_getConnection(self):
+		drv = BitbucketDriver()
+		self.assertIsInstance(drv.getConnection('foo', {}), BitbucketConnection)
---
+++ zuul/driver/bitbucket/bitbucketmodel.py
@@ -0,0 +1,25 @@
+from zuul.model import Change
+
+class PullRequest(Change):
+    def __init__(self, project):
+        super(PullRequest, self).__init__(project)
+        self.project = None
+        self.pr = None
+        self.updatedDate = None
+        self.title = None
+        self.reviews = []
+        self.files = []
+        self.labels = []
+
+    def __eq__(self, obj):
+    	return isinstance(obj, PullRequest) and self.project == obj.project \
+    	    and self.id == obj.id and self.updatedDate == obj.updatedDate
+
+    def isUpdateOf(self, other):
+        if (self.project == other.project and
+            hasattr(other, 'id') and self.id == other.id and
+            hasattr(other, 'patchset') and self.patchset != other.patchset and
+            hasattr(other, 'updatedDate') and
+            self.updatedDate > other.updatedDate):
+            return True
+        return False
