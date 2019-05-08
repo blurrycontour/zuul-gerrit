bd5e0742504798e498356b41cfa525ff6a011743
Mark Meyer <mark.meyer@smaato.com>
2019-05-08 20:26:01 +0200

    Add Bitbucket Server source functionality
    This includes a Bitbucket Server source as well as the accompanying
    documentation.
    
    Change-Id: Ib702a12efe54f5e62e78744fd59cb1696562777e

---
+++ tests/unit/bitbucket/testbitbucketdriver.py
@@ -0,0 +1,25 @@
+
+from zuul.driver.bitbucket import BitbucketDriver
+from zuul.driver.bitbucket.bitbucketsource import BitbucketSource
+from zuul.driver.bitbucket.bitbucketconnection import BitbucketConnection
+from tests.base import BaseTestCase
+
+
+class TestBitbucketDriver(BaseTestCase):
+    def test_getRequireSchema(self):
+        drv = BitbucketDriver()
+        self.assertEqual({}, drv.getRequireSchema())
+
+    def test_getRejectSchema(self):
+        drv = BitbucketDriver()
+        self.assertEqual({}, drv.getRejectSchema())
+
+    def test_getSource(self):
+        drv = BitbucketDriver()
+        self.assertIsInstance(drv.getSource(drv.getConnection('foo', {})),
+                              BitbucketSource)
+
+    def test_getConnection(self):
+        drv = BitbucketDriver()
+        self.assertIsInstance(drv.getConnection('foo', {}),
+                              BitbucketConnection)
---
+++ tests/unit/bitbucket/testbitbucketsource.py
@@ -0,0 +1,125 @@
+from unittest.mock import patch
+import unittest
+
+from zuul.driver.bitbucket.bitbucketmodel import PullRequest
+from tests.unit.bitbucket.mocks import BitbucketClientMock, CommonConnectionTest
+
+
+class TestBitbucketSource(CommonConnectionTest):
+
+    def _source(self):
+        c = self._connection()
+        return c.source
+
+    def test_getProject(self):
+        con = self._connection()
+        p = con.getProject(self.realProject)
+        self.assertEqual(p.name, self.realProject)
+
+    @patch('zuul.driver.bitbucket.bitbucketconnection.BitbucketClient',
+           new=BitbucketClientMock)
+    def test_getProjectBranches(self):
+        s = self._source()
+        p = s.getProject(self.realProject)
+        b = s.getProjectBranches(p, 'default')
+        self.assertEqual(len(b), 3)
+        self.assertTrue('master' in b)
+
+    @patch('zuul.driver.bitbucket.bitbucketconnection.BitbucketClient',
+           new=BitbucketClientMock)
+    def test_getProjectOpenChanges(self):
+        s = self._source()
+        project = s.getProject(self.realProject)
+        change = s.getChangeByURL('https://bitbucket.glumfun.test/rest/api/'
+                                  '1.0/projects/sys-ic/repos/foobar/'
+                                  'pull-requests/101')
+
+        changes = s.getProjectOpenChanges(project)
+
+        self.assertEqual(len(changes), 1)
+        self.assertTrue(change in changes)
+
+    def test_getGitUrl(self):
+        s = self._source()
+        proj = s.getProject(self.realProject)
+        url = s.getGitUrl(proj)
+        self.assertEqual(url, self.realGitUrl)
+
+    @unittest.skip
+    @patch('zuul.driver.bitbucket.bitbucketconnection.BitbucketClient',
+           new=BitbucketClientMock)
+    def test_getRefSha(self):
+        s = self._source()
+        p = s.getProject('foobar')
+        with self.assertRaises(NotImplementedError) as ctx:
+            s.getRefSha(p, 'xxxxx')
+
+    @unittest.skip
+    @patch('zuul.driver.bitbucket.bitbucketconnection.BitbucketClient',
+           new=BitbucketClientMock)
+    def test_waitForRefSha(self):
+        s = self._source()
+        p = s.getProject('foobar')
+        with self.assertRaises(NotImplementedError) as ctx:
+            s.waitForRefSha(p, 'xxxxx')
+
+    @patch('zuul.driver.bitbucket.bitbucketconnection.BitbucketClient',
+           new=BitbucketClientMock)
+    def test_getChangeByURL(self):
+        s = self._source()
+        project = s.getProject(self.realProject)
+        change = s.getChangeByURL('https://bitbucket.glumfun.test/rest/'
+                                  'api/1.0/projects/sys-ic/repos/foobar/'
+                                  'pull-requests/101')
+        self.assertIsInstance(change, PullRequest)
+        self.assertEqual(change.id, 101)
+        self.assertEqual(change.project, project)
+
+    @patch('zuul.driver.bitbucket.bitbucketconnection.BitbucketClient',
+           new=BitbucketClientMock)
+    def test_canMerge(self):
+        s = self._source()
+        change = s.getChangeByURL('https://bitbucket.glumfun.test/rest/'
+                                  'api/1.0/projects/sys-ic/repos/foobar/'
+                                  'pull-requests/101')
+        self.assertFalse(s.canMerge(change, False))
+
+    @patch('zuul.driver.bitbucket.bitbucketconnection.BitbucketClient',
+           new=BitbucketClientMock)
+    def test_isMerged(self):
+        s = self._source()
+        change = s.getChangeByURL('https://bitbucket.glumfun.test/rest/'
+                                  'api/1.0/projects/sys-ic/repos/foobar/'
+                                  'pull-requests/101')
+        self.assertFalse(s.isMerged(change, 'xyz'))
+
+    @unittest.skip
+    @patch('zuul.driver.bitbucket.bitbucketconnection.BitbucketClient',
+           new=BitbucketClientMock)
+    def test_getChange(self):
+        s = self._source()
+        with self.assertRaises(NotImplementedError) as ctx:
+            s.getChange('xxx')
+
+    @patch('zuul.driver.bitbucket.bitbucketconnection.BitbucketClient',
+           new=BitbucketClientMock)
+    def test_getChangesDependingOn(self):
+        s = self._source()
+        project = s.getProject(self.realProject)
+        change = s.getChangeByURL('https://bitbucket.glumfun.test/rest/'
+                                  'api/1.0/projects/sys-ic/repos/foobar/'
+                                  'pull-requests/101')
+        self.assertEqual([], s.getChangesDependingOn(change, [project],
+                                                     'default'))
+
+    @patch('zuul.driver.bitbucket.bitbucketconnection.BitbucketClient',
+           new=BitbucketClientMock)
+    def test_getRejectFilters(self):
+        s = self._source()
+        self.assertEqual(s.getRejectFilters(), [])
+
+    @patch('zuul.driver.bitbucket.bitbucketconnection.BitbucketClient',
+           new=BitbucketClientMock)
+    def test_getRequireFilters(self):
+        s = self._source()
+        self.assertEqual(s.getRequireFilters(), [])
