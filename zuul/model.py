0ff1e3d4b84d9d24c1b6690c72d90343df886559
Tobias Henkel <tobias.henkel@bmw.de>
2019-05-13 20:50:56 +0200

    WIP: Annotate builds with event id
    It's useful to be able to trace an event through the system including
    the builds.
    
    Change-Id: If852cbe8aecc4cf346dccc1b8fc34272c8ff483d

--- zuul/model.py
+++ zuul/model.py
@@ -1814,7 +1814,7 @@ class Build(object):
     Job (related builds are grouped together in a BuildSet).
     """
 
-    def __init__(self, job, uuid):
+    def __init__(self, job, uuid, zuul_event_id=None):
         self.job = job
         self.uuid = uuid
         self.url = None
@@ -1834,6 +1834,7 @@ class Build(object):
         self.node_labels = []
         self.node_name = None
         self.nodeset = None
+        self.zuul_event_id = zuul_event_id
 
     def __repr__(self):
         return ('<Build %s of %s voting:%s on %s>' %
