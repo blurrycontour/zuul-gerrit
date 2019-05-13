2791f06cfc85d2b84b1799e11de46c896771ac84
Mark Meyer <mark.meyer@smaato.com>
2019-05-13 16:57:02 +0200

    Create a basic Bitbucket build status reporter
    Change-Id: Ie633395cffc5b551e48e3a17f42caae5a998d7f0

--- zuul/driver/bitbucket/__init__.py
+++ zuul/driver/bitbucket/__init__.py
@@ -13,12 +13,14 @@
 # under the License.
 
 from zuul.driver import Driver, ConnectionInterface
-from zuul.driver import SourceInterface
+from zuul.driver import SourceInterface, ReporterInterface
 from zuul.driver.bitbucket import bitbucketconnection
 from zuul.driver.bitbucket import bitbucketsource
+from zuul.driver.bitbucket import bitbucketreporter
 
 
-class BitbucketDriver(Driver, ConnectionInterface, SourceInterface):
+class BitbucketDriver(Driver, ConnectionInterface, SourceInterface,
+                      ReporterInterface):
     name = 'bitbucket'
 
     def getConnection(self, name, config):
@@ -32,3 +34,12 @@ class BitbucketDriver(Driver, ConnectionInterface, SourceInterface):
 
     def getRejectSchema(self):
         return {}
+
+    def getReporter(self, connection, pipeline, config=None):
+        return bitbucketreporter.BitbucketReporter(
+            self, connection, pipeline, config)
+
+    def getReporterSchema(self):
+        return bitbucketreporter.getSchema()
+
+        pass
