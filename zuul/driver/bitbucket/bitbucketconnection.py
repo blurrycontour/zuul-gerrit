b593dc2f47249af6ef3c1e25376a1385f91fbc89
Mark Meyer <mark.meyer@smaato.com>
2019-05-15 21:56:01 +0200

    Create a basic Bitbucket event source
    Change-Id: I18172d8b86ac16c37a0612adb15c7c5621876876

--- zuul/driver/bitbucket/bitbucketconnection.py
+++ zuul/driver/bitbucket/bitbucketconnection.py
@@ -17,11 +17,115 @@ from zuul.model import Project
 from zuul.exceptions import MergeFailure
 
 from zuul.driver.bitbucket.bitbucketsource import BitbucketSource
+from zuul.driver.bitbucket.bitbucketmodel import PullRequest
+from zuul.driver.bitbucket.bitbucketmodel import BitbucketTriggerEvent
 
+import traceback
 import logging
 import requests
+import threading
+import time
 from requests.auth import HTTPBasicAuth
 from urllib.parse import urlparse
+
+
+class BitbucketWatcher(threading.Thread):
+    log = logging.getLogger("zuul.connection.bitbucket.watcher")
+
+    def __init__(self, bitbucket_con, poll_delay):
+        threading.Thread.__init__(self)
+
+        self.daemon = True
+        self.bitbucket_con = bitbucket_con
+        self.poll_delay = poll_delay
+        self.stopped = False
+        self.branches = self.bitbucket_con.branches
+        self.event_count = 0
+
+    def isNew(self, change):
+        projectname = change.project.name
+        prid = change.id
+
+        return (self.bitbucket_con.cachedPR(projectname, prid)
+                is None)
+
+    def supersedes(self, change):
+        projectname = change.project.name
+        prid = change.id
+
+        oldpr = self.bitbucket_con.cachedPR(projectname, prid)
+        if oldpr and change.isUpdateOf(oldpr):
+            return oldpr
+
+        return None
+
+    def _handleBasePR(self, change):
+        event = BitbucketTriggerEvent()
+        event.type = 'bb-pr'
+        event.title = change.title
+        event.project_name = change.project.name
+        event.change_id = change.id
+        event.updateDate = change.updatedDate
+        event.branch = change.branch
+        event.ref = change.patchset
+        event.project_hostname = self.bitbucket_con.canonical_hostname
+
+        return event
+
+    def _handleNewPR(self, change):
+        event = self._handleBasePR(change)
+
+        event.action = 'opened'
+
+        self.log.debug('New event: {}'.format(event))
+
+        self.bitbucket_con.sched.addEvent(event)
+
+        return event
+
+    def _handleUpdatePR(self, change, oldchange):
+        event = self._handleBasePR(change)
+
+        event.action = 'updated'
+
+        self.log.debug('New event {}'.format(event))
+
+        self.bitbucket_con.sched.addEvent(event)
+
+    def _run(self):
+        self.log.debug("Check for updates: {}"
+                       .format(self.bitbucket_con.connection_name))
+        try:
+            for p in self.bitbucket_con.projects:
+                project = self.bitbucket_con.getProject(p)
+                for change in self.bitbucket_con\
+                        .getProjectOpenChanges(project, False):
+                    if self.isNew(change):
+                        self.log.debug("New change: {} in {}".format(change, change.project))
+                        self._handleNewPR(change)
+                        return
+                    oldpr = self.supersedes(change)
+                    if oldpr:
+                        self._handleUpdatePR(change, oldpr)
+                        return
+
+        except Exception as e:
+            self.log.debug("Unexpected issue in _run loop: {}"
+                           .format(str(e)))
+            self.log.debug(traceback.format_exception(Exception, e))
+
+    # core event loop, no unittest
+    def run(self):
+        while not self.stopped:
+            if not self.bitbucket_con.pause_watcher:
+                self._run()
+            else:
+                self.log.debug("Watcher is paused")
+            time.sleep(self.poll_delay)
+
+    # core event loop, no unittest
+    def stop(self):
+        self.stopped = True
 
 
 class BitbucketConnectionError(BaseException):
@@ -49,16 +153,19 @@ class BitbucketClient():
         return r.json()
 
     def post(self, path, payload):
-        url = '{}:{}{}'.format(self.baseurl, path)
+        url = '{}{}'.format(self.baseurl, path)
         r = requests.post(url, auth=HTTPBasicAuth(self.user, self.pw),
-                          data=payload)
+                          json=payload)
 
-        if r.status_code != 200:
+        if r.status_code not in [200, 204]:
             raise BitbucketConnectionError(
                 "Connection to server returned status {} path {}"
                 .format(r.status_code, url))
 
-        return r.json()
+        if r.status_code == 200:
+            return r.json()
+        else:
+            return None
 
 
 class BitbucketConnection(BaseConnection):
@@ -69,6 +176,7 @@ class BitbucketConnection(BaseConnection):
         super(BitbucketConnection, self).__init__(
             driver, connection_name, connection_config)
         self.projects = {}
+        self.prs = {}
 
         self.base_url = self.connection_config.get('baseurl').rstrip('/')
         self.cloneurl = self.connection_config.get('cloneurl').rstrip('/')
@@ -82,6 +190,14 @@ class BitbucketConnection(BaseConnection):
 
         self.branches = {}
 
+        self.pause_watcher = False
+
+        self.poll_delay = 60
+
+        self.canonical_hostname = 'bitbucket.smaato.net'
+
+        self.watcher_thread = BitbucketWatcher(self, self.poll_delay)
+
     def _getBitbucketClient(self):
         # authenticate, return client
         client = BitbucketClient(self.base_url)
@@ -93,7 +209,22 @@ class BitbucketConnection(BaseConnection):
         return project, repo
 
     def onLoad(self):
-        pass
+        self.log.debug("Starting Bitbucket watcher")
+        self._start_watcher_thread()
+
+    def onStop(self):
+        self.log.debug("Stopping Bitbucket watcher")
+        self._stop_watcher_thread()
+
+    def _start_watcher_thread(self):
+        self.watcher_thread = BitbucketWatcher(
+            self, self.poll_delay)
+        self.watcher_thread.start()
+
+    def _stop_watcher_thread(self):
+        if self.watcher_thread:
+            self.watcher_thread.stop()
+            self.watcher_thread.join()
 
     def clearBranchCache(self):
         self.projects = {}
@@ -132,6 +263,41 @@ class BitbucketConnection(BaseConnection):
         return [item.get('displayId') for item in res.get('values')
                 if item.get('type') == 'BRANCH']
 
+    def buildPR(self, project, repo, id, cache=True):
+        pr = self.getPR(project, repo, id)
+
+        project = self.getProject('{}/{}'.format(project, repo))
+        pull = PullRequest(project.name)
+        pull.project = project
+        pull.id = id
+        pull.updatedDate = pr.get('updatedDate')
+        fromProj = '{}/{}'.format(pr.get('fromRef').get('repository')
+                                  .get('project').get('key'),
+                                  pr.get('fromRef').get('repository')
+                                  .get('slug'))
+        bslug = self.getBranchSlug(fromProj, pr.get('fromRef')
+                                   .get('id'))
+        pull.patchset = self.getBranchSha(fromProj, bslug)
+        b = pr.get('fromRef').get('id')
+        if b.startswith('refs/heads/'):
+            pull.branch = b[len('refs/heads/'):]
+        else:
+            pull.branch = b
+        pull.title = pr.get('title')
+        pull.message = pr.get('description')
+
+        if cache:
+            self.cachePR(pull)
+
+        return pull
+
+    def getProjectOpenChanges(self, project, cache=True):
+        bb_proj, repo = self._getProjectRepo(project.name)
+        prs = self.getPRs(bb_proj, repo)
+
+        return [self.buildPR(bb_proj, repo, pr.get('id'), cache)
+                for pr in prs.get('values')]
+
     def getPR(self, project, repo, id):
         return self._getBitbucketClient()\
             .get('/rest/api/1.0/projects/{}/repos/{}/pull-requests/{}'
@@ -141,6 +307,18 @@ class BitbucketConnection(BaseConnection):
         return self._getBitbucketClient()\
             .get('/rest/api/1.0/projects/{}/repos/{}/pull-requests'
                  .format(project, repo))
+
+    def cachePR(self, pr):
+        projecthash = self.prs.get(pr.project.name, {})
+        projecthash[pr.id] = pr
+        self.prs[pr.project.name] = projecthash
+
+    def cachedPR(self, projectname, prid):
+        if projectname in self.prs:
+            if prid in self.prs[projectname]:
+                return self.prs[projectname][prid]
+
+        return None
 
     def canMerge(self, change, allow_needs):
         bb_proj, repo = self._getProjectRepo(change.project.name)
---
+++ zuul/driver/bitbucket/bitbuckettrigger.py
@@ -0,0 +1,54 @@
+# Copyright 2019 Smaato, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+import voluptuous as v
+
+from zuul.trigger import BaseTrigger
+from zuul.driver.util import scalar_or_list, to_list
+
+from zuul.driver.bitbucket.bitbucketmodel import BitbucketEventFilter
+
+
+class BitbucketTrigger(BaseTrigger):
+    name = 'bitbucket'
+    log = logging.getLogger('zuul.BitbucketTrigger')
+
+    def getEventFilters(self, trigger_conf):
+        efilters = []
+        for trigger in to_list(trigger_conf):
+            f = BitbucketEventFilter(
+                trigger=self,
+                types=to_list(trigger['event']),
+                actions=to_list(trigger.get('action')),
+                refs=to_list(trigger.get('ref')),
+                comments=to_list(trigger.get('comment')),
+                statuses=to_list(trigger.get('status')),
+            )
+            efilters.append(f)
+
+        return efilters
+
+
+def getSchema():
+    bitbucket_trigger = {
+        v.Required('event'):
+            scalar_or_list(v.Any('bb-pr')),
+        'action': scalar_or_list(str),
+        'ref': scalar_or_list(str),
+        'comment': scalar_or_list(str),
+        'status': scalar_or_list(str),
+    }
+
+    return bitbucket_trigger
