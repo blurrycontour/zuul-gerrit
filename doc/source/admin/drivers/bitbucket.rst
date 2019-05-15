222cd0a9ec2ff0d9c96a41619b72ca1c4877309d
Mark Meyer <mark.meyer@smaato.com>
2019-05-15 11:04:17 +0200

    Create a basic Bitbucket build status reporter
    Change-Id: Ie633395cffc5b551e48e3a17f42caae5a998d7f0

--- doc/source/admin/drivers/bitbucket.rst
+++ doc/source/admin/drivers/bitbucket.rst
@@ -52,3 +52,39 @@ via a named user. The options for the ``zuul.conf`` connections are:
     :required:
 
     Password for the user to log in to the server.
+
+Reporter configuration
+------------------------
+
+Zuul can report back build status to Bitbucket, it will do this on a
+per pipeline basis. This feature is based on the Bitbucket build
+status API. Build status can be used to set conditions in Bitbucket
+like all other build information can be used. Reporting includes
+the pipeline name and the string "Zuul".
+
+
+.. attr:: pipeline.<reporter>.<bitbucket source>
+
+   To report to Bitbucket, the dictionaries passed to any of the pipeline
+   :ref:`reporter<reporters>` attributes support the following
+   attributes:
+
+   .. attr:: merge
+      :default: False
+
+      Boolean value that determines if the reporter should merge
+      the pull request.
+
+   .. attr:: label
+      :default: Zuul
+
+      String value for a label URL to set in the build status.
+      Build status will include the report text prefixed by this
+      label.
+
+    .. attr:: reportid
+      :default: zuul
+
+      String value that acts as a prefix to the not user visible
+      id of the build status. This can be useful if you use multiple
+      Zuul servers that report.
---
+++ zuul/driver/bitbucket/bitbucketreporter.py
@@ -0,0 +1,71 @@
+import logging
+import voluptuous as v
+import time
+
+from zuul.reporter import BaseReporter
+from zuul.exceptions import MergeFailure
+from zuul.driver.bitbucket.bitbucketsource import BitbucketSource
+
+
+class BitbucketReporter(BaseReporter):
+    """Sends reports to Bitbucket, based on CodeInsights API"""
+
+    name = 'bitbucket'
+    log = logging.getLogger("zuul.BitbucketReporter")
+
+    def __init__(self, driver, connection, pipeline, config=None):
+        super(BitbucketReporter, self).__init__(driver, connection, config)
+
+        self.context = "{}/{}".format(pipeline.tenant.name, pipeline.name)
+        self._merge = self.config.get('merge', False)
+        self._label = self.config.get('label', 'Zuul')
+        self._report_id = self.config.get('reportid', 'zuul')
+
+    def report(self, item):
+        if not isinstance(item.change.project.source, BitbucketSource):
+            return
+
+        if item.change.project.source.connection.server != \
+                self.connection.server:
+            return
+
+        if hasattr(item.change, 'id'):
+            self.setBuildStatus(item)
+            if self._merge:
+                self.mergePull(item)
+
+    def mergePull(self, item):
+        for i in [1, 2, 3, 4]:
+            try:
+                self.connection.mergePull(item.change.project, item.change.id)
+                item.change.is_merged = True
+                return
+            except MergeFailure:
+                self.log.exception(
+                    'Merge attempt of change {} {}/4 failed.'
+                    .format(item.change, i))
+                time.sleep(2)
+        self.log.warning(
+            'Merge of change {} failed after 4 attempts, giving up',
+            item.change)
+
+    def setBuildStatus(self, item, comment=None):
+        message = comment or self._formatItemReport(item)
+        state = 'FAILED'
+        if not item.areAllJobsComplete():
+            state = 'INPROGRESS'
+        if item.didAllJobsSucceed():
+            state = 'SUCCESSFUL'
+        status = {
+            'state': state,
+            'key': '{}-{}'.format(self.reportid, self.context),
+            'name': '{}: {}'.format(self._label, self.context),
+            'description': message
+        }
+        self.connection.setBuildStatus(item.change.patchset, status)
+
+
+def getSchema():
+    bitbucket_reporter = v.Schema({
+    })
+    return bitbucket_reporter
