1469111029f86222c66682332a7e3f6aee182837
Mark Meyer <mark.meyer@smaato.com>
2019-05-30 08:00:30 +0200

    Extend event reporting
    Change-Id: I93ba9b921826704f89f93dd3eb4dee39577feba0

--- zuul/driver/bitbucket/bitbucketmodel.py
+++ zuul/driver/bitbucket/bitbucketmodel.py
@@ -14,7 +14,7 @@
 
 import re2
 
-from zuul.model import Change, EventFilter, TriggerEvent
+from zuul.model import Change, EventFilter, TriggerEvent, RefFilter
 
 
 class PullRequest(Change):
@@ -45,6 +45,16 @@ class PullRequest(Change):
             return True
         return False
 
+    def isNewerThan(self, other):
+        if (self.project == other.project and
+            hasattr(other, 'id') and
+            self.id == other.id and
+            hasattr(other, 'updatedDate') and
+            self.updatedDate > other.updatedDate
+        ):
+            return True
+        return False
+
 
 class BitbucketTriggerEvent(TriggerEvent):
     def __init__(self):
@@ -55,21 +65,67 @@ class BitbucketTriggerEvent(TriggerEvent):
         self.status = None
 
 
+# taken almost verbatimly from PagureRefFilter
+class BitbucketChangeFilter(RefFilter):
+    def __init__(self, connection_name, open=None,
+                 closed=None, status=None, canMerge=None):
+        RefFilter.__init__(self, connection_name)
+        self.open = open
+        self.closed = closed
+        self.status = status
+        self.canMerge = canMerge
+
+    def __repr__(self):
+        ret = '<BitbucketChangeFilter connection_name: {} '\
+            .format(self.connection_name)
+        if self.open:
+            ret += ' open: {}'.format(self.open)
+        if self.closed:
+            ret += ' closed: {}'.format(self.closed)
+        if self.status:
+            ret += ' status: {}'.format(self.status)
+        if self.canMerge:
+            ret += ' canMerge: {}'.format(self.status)
+
+        ret += '>'
+        return ret
+
+    def matches(self, change):
+        if self.open is not None:
+            if change.open != self.open:
+                return False
+
+        if self.closed is not None:
+            if change.closed != self.closed:
+                return False
+
+        if self.status is not None:
+            if change.status != self.status:
+                return False
+
+        if self.canMerge is not None:
+            if change.canMerge != self.canMerge:
+                return False
+
+        return True
+
+
 # taken almost verbatimly from PagureEventFilter
 class BitbucketEventFilter(EventFilter):
-    def __init__(self, trigger, types=[], refs=[], statuses=[],
-                 comments=[], actions=[]):
+    def __init__(self, trigger, types=[], branches=[],
+                 comments=[], actions=[], refs=[]):
 
         EventFilter.__init__(self, trigger)
 
         self._types = types
-        self._refs = refs
+        self._branches = branches
         self._comments = comments
         self.types = [re2.compile(x) for x in types]
-        self.refs = [re2.compile(x) for x in refs]
+        self.branches = [re2.compile(x) for x in branches]
         self.comments = [re2.compile(x) for x in comments]
         self.actions = actions
-        self.statuses = statuses
+        self._refs = refs
+        self.refs = [re2.compile(x) for x in refs]
 
     def matches(self, event, change):
         matches_type = False
@@ -89,6 +145,15 @@ class BitbucketEventFilter(EventFilter):
         if self.refs and not matches_ref:
             return False
 
+        matches_branch = False
+        if event.branch is not None:
+            for ref in self.branches:
+                if ref.match(event.branch):
+                    matches_branch = True
+                    break
+        if self.branches and not matches_branch:
+            return False
+
         matches_comment_re = False
         for comment_re in self.comments:
             if event.comment is not None and comment_re.search(event.comment):
@@ -103,14 +168,6 @@ class BitbucketEventFilter(EventFilter):
                 matches_action = True
                 break
         if self.actions and not matches_action:
-            return False
-
-        matches_status = False
-        for status in self.statuses:
-            if event.status == status:
-                matches_status = True
-                break
-        if self.statuses and not matches_status:
             return False
 
         return True
