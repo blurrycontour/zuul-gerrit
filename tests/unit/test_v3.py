6c33bc72b8b3d307e65f826210a44eeac5d8c080
Jeremy Stanley <fungi@yuggoth.org>
2019-06-03 22:25:22 +0000

    Safely add Ansible password lookup plugin
    Ansible's password lookup plugin can be useful for generating random
    passwords, but has other uses which could allow a nefarious user to
    create new files or read the contents of existing files outside the
    workspace (as long as those paths are writeable or readable by the
    user under which Ansible is executing). To that end, short-circuit
    and return an error if an untrusted use attempts to escape the
    workspace in these ways.
    
    Change-Id: I0e372dda3a0f0f74d9e343c439514317dceb4d24

--- tests/unit/test_v3.py
+++ tests/unit/test_v3.py
@@ -2707,6 +2707,11 @@ class TestAnsible25(AnsibleZuulTestCase):
             ('file_local_good', 'SUCCESS'),
             ('file_local_bad', 'FAILURE'),
             ('zuul_return', 'SUCCESS'),
+            ('password_create_good', 'SUCCESS'),
+            ('password_null_good', 'SUCCESS'),
+            ('password_read_good', 'SUCCESS'),
+            ('password_create_bad', 'FAILURE'),
+            ('password_read_bad', 'FAILURE'),
         ]
         for job_name, result in plugin_tests:
             count += 1
