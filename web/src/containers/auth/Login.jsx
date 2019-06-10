86de3b1904b31010d6129c7cf8fea0eae7cface6
Matthieu Huin <mhuin@redhat.com>
2019-06-10 12:43:03 +0200

    [WIP] admin REST API: zuul-web integration
    Add a configuration file allowing deployers to set up an OIDC provider
    for authentication.
    Save the authenticated user's JWT, admin tenants to the redux store for
    consumption with the API.
    If authenticated and allowed, add the following buttons to the GUI:
    
    * autohold a failed build's resources on its next failure,
      from the builds page
    * dequeue a buildset from the status page
    * re-enqueue a buildset from the buildsets page
    
    A few subtleties in redux workflow, and a few additions to the REST API
    must be worked out before merging.
    
    Change-Id: I052c340b15b123ca387cff42aea5d09babe01404

---
+++ web/src/containers/auth/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { connect } from "react-redux"
+import { Button } from 'patternfly-react'
+import userManager from '../../userManager'
+
+class LoginButton extends React.Component {
+    onLoginButtonClick(event) {
+      event.preventDefault();
+      userManager.signinRedirect();
+    }
+
+    render () {
+        return (
+            <Button onClick={this.onLoginButtonClick}>Log in</Button>
+        )
+    }
+}
+
+export default connect()(LoginButton)
---
+++ web/src/reducers/auth.js
@@ -0,0 +1,42 @@
+import update from 'immutability-helper'
+
+import {
+  USER_EXPIRED,
+  USER_FOUND,
+  SILENT_RENEW_ERROR,
+  SESSION_TERMINATED,
+  LOADING_USER,
+  USER_SIGNED_OUT,
+  LOADING_USER_AUTHORIZATIONS,
+  USER_AUTHORIZATIONS_LOADED
+} from '../actions/auth'
+
+const initialState = {
+  user: null,
+  isLoadingUser: false,
+  // TODO: set to null, so we know if "adminTenants" needs to be fetched
+  adminTenants: []
+}
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case USER_EXPIRED:
+        return { user: null, isLoadingUser: false, adminTenants: [] }
+    case SILENT_RENEW_ERROR:
+        return { user: null, isLoadingUser: false, adminTenants: [] }
+    case SESSION_TERMINATED:
+    case USER_SIGNED_OUT:
+        return { user: null, isLoadingUser: false, adminTenants: [] }
+    case USER_FOUND:
+        return { user: action.payload, isLoadingUser: false, adminTenants: [] }
+    case LOADING_USER:
+        return { user: null, isLoadingUser: true, adminTenants: [] }
+    case LOADING_USER_AUTHORIZATIONS:
+        return update(state, { isLoadingUser: {$set: true}, adminTenants: {$set: []} })
+    case USER_AUTHORIZATIONS_LOADED:
+        console.log(action.adminTenants)
+        return update(state, { isLoadingUser: {$set: false}, adminTenants: {$set: action.adminTenants} })
+    default:
+        return state
+  }
+}
