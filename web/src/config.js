68dca9514937012aa2a31e4c18304abf382943a1
Matthieu Huin <mhuin@redhat.com>
2019-06-11 23:26:58 +0200

    [WIP] Docker compose example: add keycloak authentication
    Add a keycloak container to the quickstart's compose. The IdP comes
    preconfigured with a realm, users and roles. A sample zuul config
    with two tenants and role-based access rules, and a zuul-web config
    are also provided.
    
    The full compose requires parent commits to be merged first, but the
    keycloak service can be used for local tests.
    
    TODO: add oidc support in gerrit, but not before provisioning the
    admin user.
    
    Change-Id: I918fc0eafb8edd08429933b01e8d634f5816b0af

--- web/src/config.js
+++ web/src/config.js
@@ -1,16 +1,15 @@
 // TODO: what's the best practice for config in deployments?
 
-const userManagerConfig = null
-
-//const userManagerConfig = {
-//  client_id: 'zuul',
-//  redirect_uri: 'https://sfrdotestinstance.org/zuul/auth_callback',
-//  response_type: 'token id_token',
-//  scope: 'openid profile',
-//  authority: 'https://keycloak.sfrdotestinstance.org/auth/realms/sf',
-//  automaticSilentRenew: false,
-//  filterProtocolClaims: true,
-//  loadUserInfo: true,
-//}
+const userManagerConfig = {
+  client_id: 'zuul-web',
+  redirect_uri: `http://localhost:9000/auth_callback`,
+  response_type: 'token id_token',
+  scope: 'openid profile',
+  authority: 'http://localhost:8282/auth/realms/zuul-demo/',
+//  silent_redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/silent_renew.html`,
+  automaticSilentRenew: false,
+  filterProtocolClaims: true,
+  loadUserInfo: true,
+}
 
 export default userManagerConfig
