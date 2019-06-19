78f02d7490c52adb9f9bde4757896ec06ba59ef3
Matthieu Huin <mhuin@redhat.com>
2019-06-19 12:08:38 +0200

    Dequeue a change by its buildset UUID
    Change-Id: I1d9c03ba03c153225c6f0e872adb8e37bf27a944

--- web/src/containers/status/ChangePanel.jsx
+++ web/src/containers/status/ChangePanel.jsx
@@ -161,6 +161,14 @@ class ChangePanel extends React.Component {
     )
   }
 
+  renderBuildSetUUID (change) {
+      return (
+        <React.Fragment>
+            <small>Buildset ID: {change.buildset_uuid}</small>
+        </React.Fragment>
+      )
+  }
+
   renderTimer (change) {
     let remainingTime
     if (change.remaining_time === null) {
@@ -310,6 +318,9 @@ class ChangePanel extends React.Component {
               <div className='row'>
                 <div className='col-xs-4'>
                   {this.renderChangeLink(change)}
+                </div>
+                <div className='col-xs-4'>
+                  {this.renderBuildSetUUID(change)}
                 </div>
                 <div className='col-xs-8'>
                   {this.renderProgressBar(change)}
