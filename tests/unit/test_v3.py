cc689f7d5a5397089a435e9eb91997e76d0409f7
Paul Belanger <pabelanger@redhat.com>
2019-08-06 11:46:33 -0400

    WIP: Support Ansible 2.9
    Ansible has released 2.9 and now Zuul also supports it.
    
    Change-Id: Iabf2d6278ba8d88e17403a4adae5521eb3e7019b
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- tests/unit/test_v3.py
+++ tests/unit/test_v3.py
@@ -2772,6 +2772,10 @@ class TestAnsible28(TestAnsible27):
     ansible_version = '2.8'
 
 
+class TestAnsible29(TestAnsible28):
+    ansible_version = '2.9'
+
+
 class TestPrePlaybooks(AnsibleZuulTestCase):
     # A temporary class to hold new tests while others are disabled
 
@@ -6185,6 +6189,7 @@ class TestAnsibleVersion(AnsibleZuulTestCase):
             dict(name='ansible-26', result='SUCCESS', changes='1,1'),
             dict(name='ansible-27', result='SUCCESS', changes='1,1'),
             dict(name='ansible-28', result='SUCCESS', changes='1,1'),
+            dict(name='ansible-29', result='SUCCESS', changes='1,1'),
         ], ordered=False)
 
 
@@ -6206,4 +6211,5 @@ class TestDefaultAnsibleVersion(AnsibleZuulTestCase):
             dict(name='ansible-26', result='SUCCESS', changes='1,1'),
             dict(name='ansible-27', result='SUCCESS', changes='1,1'),
             dict(name='ansible-28', result='SUCCESS', changes='1,1'),
+            dict(name='ansible-29', result='SUCCESS', changes='1,1'),
         ], ordered=False)
