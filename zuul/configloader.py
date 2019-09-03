83b2dd4e13739695a5cf730d40e2acc2478b818e
Simon Westphahl <simon.westphahl@bmw.de>
2019-09-03 10:10:16 +0200

    Add optional support for circular dependencies
    Allow Zuul to process circular dependencies between changes. Gating of
    circular dependencies must be explicitly enabled on a per tenant or
    project basis.
    
    In case Zuul detects a dependency cycle it will make sure that every
    change also include all other changes that are part of the cycle. However
    each change will still be a normal item in the queue with its own jobs.
    When it comes to reporting, all items in the cycle are treated as one
    unit that determines the success/failure of those changes.
    
    Changes with cross-repo circular dependencies are required to share the
    same change queue.
    
    Depends-On: https://review.opendev.org/#/c/643309/
    Change-Id: Ic121b2d8d057a7dc4448ae70045853347f265c6c

--- zuul/configloader.py
+++ zuul/configloader.py
@@ -1442,6 +1442,7 @@ class TenantParser(object):
         'exclude-unprotected-branches': bool,
         'extra-config-paths': to_list(str),
         'load-branch': str,
+        'allow-circular-dependencies': bool,
     }}
 
     project = vs.Any(str, project_dict)
@@ -1482,6 +1483,7 @@ class TenantParser(object):
                   'allowed-reporters': to_list(str),
                   'allowed-labels': to_list(str),
                   'disallowed-labels': to_list(str),
+                  'allow-circular-dependencies': bool,
                   'default-parent': str,
                   'default-ansible-version': vs.Any(str, float),
                   'admin-rules': to_list(str),
@@ -1504,6 +1506,9 @@ class TenantParser(object):
             tenant.authorization_rules = conf['admin-rules']
         if conf.get('report-build-page') is not None:
             tenant.report_build_page = conf['report-build-page']
+        if conf.get('allow-circular-dependencies') is not None:
+            tenant.allow_circular_dependencies = \
+                conf['allow-circular-dependencies']
         tenant.web_root = conf.get('web-root', self.scheduler.web_root)
         if tenant.web_root and not tenant.web_root.endswith('/'):
             tenant.web_root += '/'
@@ -1655,6 +1660,7 @@ class TenantParser(object):
             shadow_projects = []
             project_exclude_unprotected_branches = None
             project_load_branch = None
+            project_allow_circular_dependencies = None
         else:
             project_name = list(conf.keys())[0]
             project = source.getProject(project_name)
@@ -1681,6 +1687,8 @@ class TenantParser(object):
                                            if x.endswith('/')])
             project_load_branch = conf[project_name].get(
                 'load-branch', None)
+            project_allow_circular_dependencies = conf[project_name].get(
+                'allow-circular-dependencies', None)
 
         tenant_project_config = model.TenantProjectConfig(project)
         tenant_project_config.load_classes = frozenset(project_include)
@@ -1690,6 +1698,8 @@ class TenantParser(object):
         tenant_project_config.extra_config_files = extra_config_files
         tenant_project_config.extra_config_dirs = extra_config_dirs
         tenant_project_config.load_branch = project_load_branch
+        tenant_project_config.allow_circular_dependencies = \
+            project_allow_circular_dependencies
 
         return tenant_project_config
 
