61adf89e98d2c7308f78a959681b322853b68fce
Simon Westphahl <simon.westphahl@bmw.de>
2019-09-03 10:10:16 +0200

    Add optional support for circular dependencies
    Allow Zuul to process circular dependencies between changes. Gating of
    circular dependencies must be explicitly enabled on a per tenant or
    project basis.
    
    In case Zuul detects a dependency cycle it will make sure that every
    change also include all other changes that are part of the cycle. However
    each change will still be a normal item in the queue with its own jobs.
    When it comes to reporting, all items in the cycle are treated as one
    unit that determines the success/failure of those changes.
    
    Changes with cross-repo circular dependencies are required to share the
    same change queue.
    
    Depends-On: https://review.opendev.org/#/c/643309/
    Change-Id: Ic121b2d8d057a7dc4448ae70045853347f265c6c

--- zuul/scheduler.py
+++ zuul/scheduler.py
@@ -1733,7 +1733,8 @@ class Scheduler(threading.Thread):
                     if nodeset:
                         self.nodepool.returnNodeSet(
                             nodeset, build=build, zuul_event_id=item.event)
-                build.result = 'CANCELED'
+                if build.result is None:
+                    build.result = 'CANCELED'
             else:
                 nodeset = buildset.getJobNodeSet(job_name)
                 if nodeset:
