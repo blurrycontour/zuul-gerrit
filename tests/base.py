94309d7c76801e577819dad93b590a3bc5addc9a
Felix Schmidt <felix.fs.schmidt@bmw.de>
2019-10-25 09:29:39 +0200

    Make reporting asynchronous
    Reporting is currently done synchronously in the main loop. In a busy
    system reporting items can slow down the main loop considerably.
    Especially merge operations can stall zuul for multiple seconds. This
    gets worse when there are retries involved in the merges which pause
    additionally a few seconds.
    
    Changing the reporting to be asynchronous like merge operations makes
    it possible to continue in the event loop while zuul waits for the
    results of reporting which reduces overall stalls.
    
    Change-Id: I01a8de81a6a270e666009b8be27392bd44ca8c9d

--- tests/base.py
+++ tests/base.py
@@ -4564,6 +4564,22 @@ class ZuulTestCase(BaseTestCase):
         for event_queue in self.additional_event_queues:
             event_queue.join()
 
+    def __areAllReporterQueuesEmpty(self, matcher)\
+            -> Generator[bool, None, None]:
+        for app in self.scheds.filter(matcher):
+            connections = app.sched.connections.connections.values()
+            yield all(
+                con.report_queue.empty()
+                for con in connections
+                if con.report_queue
+            )
+
+    def __reporterQueuesJoin(self, matcher) -> None:
+        for app in self.scheds.filter(matcher):
+            for connection in app.sched.connections.connections.values():
+                if connection.report_queue:
+                    connection.report_queue.join()
+
     def waitUntilSettled(self, msg="", matcher=None) -> None:
         self.log.debug("Waiting until settled... (%s)", msg)
         start = time.time()
@@ -4595,13 +4611,29 @@ class ZuulTestCase(BaseTestCase):
                 # Join ensures that the queue is empty _and_ events have been
                 # processed
                 self.__eventQueuesJoin(matcher)
+                # As the reporting is asynchronous, we need another run of
+                # the scheduler main loop to pick up the reporting result and
+                # dequeue the item. Otherwise the tests end up in non-empty
+                # pipeline queues.
+                # To ensure this, we must wait for the event queue to finish
+                # processing and afterwards acquire the scheduler lock so the
+                # scheduler stops processing items.
+                # While holding the scheduler lock we wait for the reporter
+                # queues to finish and afterwards let the scheduler continue.
+                # As the reporter puts an event in the scheduler's
+                # result_event_queue to synchronize the report events in the
+                # scheduler main loop, the next iteration of the
+                # waitUntilSettled() loop will wait for (join) the event queues
+                # a second time to finish.
                 self.scheds.execute(
                     lambda app: app.sched.run_handler_lock.acquire())
+                self.__reporterQueuesJoin(matcher)
                 if (self.__areAllMergeJobsWaiting(matcher) and
                     self.__haveAllBuildsReported(matcher) and
                     self.__areAllBuildsWaiting(matcher) and
                     self.__areAllNodeRequestsComplete(matcher) and
-                    all(self.__eventQueuesEmpty(matcher))):
+                    all(self.__eventQueuesEmpty(matcher)) and
+                    all(self.__areAllReporterQueuesEmpty(matcher))):
                     # The queue empty check is placed at the end to
                     # ensure that if a component adds an event between
                     # when locked the run handler and checked that the
