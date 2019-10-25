71ad13515afcb3a1f7a017318dbcdf5ef1d558f2
Felix Schmidt <felix.fs.schmidt@bmw.de>
2019-10-25 09:29:39 +0200

    Make reporting asynchronous
    Reporting is currently done synchronously in the main loop. In a busy
    system reporting items can slow down the main loop considerably.
    Especially merge operations can stall zuul for multiple seconds. This
    gets worse when there are retries involved in the merges which pause
    additionally a few seconds.
    
    Changing the reporting to be asynchronous like merge operations makes
    it possible to continue in the event loop while zuul waits for the
    results of reporting which reduces overall stalls.
    
    Change-Id: I01a8de81a6a270e666009b8be27392bd44ca8c9d

--- zuul/driver/gerrit/gerritconnection.py
+++ zuul/driver/gerrit/gerritconnection.py
@@ -1502,6 +1502,7 @@ class GerritConnection(BaseConnection):
 
     def onLoad(self):
         self.log.debug("Starting Gerrit Connection/Watchers")
+        super().onLoad()
         try:
             if self.session:
                 self._getRemoteVersion()
@@ -1521,6 +1522,7 @@ class GerritConnection(BaseConnection):
 
     def onStop(self):
         self.log.debug("Stopping Gerrit Connection/Watchers")
+        super().onStop()
         self._stop_watcher_thread()
         self._stop_poller_thread()
         self._stop_ref_watcher_thread()
