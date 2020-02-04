e157d2ead331b0991d8e2e31ef314593b1b53dba
Jan Kubovy <jan.kubovy@bmw.de>
2020-02-04 16:46:44 +0100

    WIP: Store unparsed branch config in Zookeeper
    We need to store the global configuration in zookeeper. However
    zookeeper is not designed as a database with a large amount of data we
    should store as little as possible in zookeeper. Thus we only store
    the per project-branch unparsed config in zookeeper. From this every
    part of zuul like the scheduler or also zuul-web can quickly
    recalculate the layout of each tenant and keep it up to date by
    watching for changes in the unparsed project-branch-config. We will
    lock the complete global config with one lock and maintain a checkpoint
    version of it. This way each component can watch the config version
    number and react accordingly. Although we lock the complete global
    config we still should store the actual config in distinct nodes per
    project and branch. This is needed because of the 1MB limit per znode
    in zookeeper. It further makes it less expensive to cache the global
    config in each component as this cache will be updated incrementally.
    
    The configs will be stored in the path
    ``/zuul/config/<project>/<branch>`` per branch segmented in
    ``/zuul/config/<project>/<branch>/<path-to-config>/<shard>``. The
    ``shard`` is a sequence number and will be used to store larger than
    1MB files due to the limitation mentioned above.
    
    Manual reconfiguration on one instance will reload the config files and
    update zookeeper in case of differences. Other instances will be
    notified about those changes via zookeeper watchers. During start
    zookeeper will be used as primary store.
    
    Change-Id: I519c4e6af1f21efdeed2b919575bcad9c50a9f18
    Story: 2007192
    Task: 38620

--- zuul/manager/__init__.py
+++ zuul/manager/__init__.py
@@ -18,6 +18,7 @@ from zuul import exceptions
 from zuul import model
 from zuul.lib.dependson import find_dependency_headers
 from zuul.lib.logutil import get_annotated_logger
+from zuul.model import QueueItem
 
 
 class DynamicChangeQueueContextManager(object):
@@ -519,14 +520,19 @@ class PipelineManager(object):
                 relevant_errors.append(err)
         return relevant_errors
 
-    def _loadDynamicLayout(self, item):
+    def _loadDynamicLayout(self, item: QueueItem):
         # Load layout
         # Late import to break an import loop
         import zuul.configloader
         loader = zuul.configloader.ConfigLoader(
-            self.sched.connections, self.sched, None, None)
+            self.sched.connections, self.sched, None, None,
+            self.sched.zk, use_zk=True)
 
         self.log.debug("Loading dynamic layout")
+
+        # TODO JK: Check global layout in item.pipeline.tenant.layout
+        # item.pipeline.tenant.layout.zk_hash
+        # Reload global layout
 
         (trusted_updates, untrusted_updates) = item.includesConfigUpdates()
         build_set = item.current_build_set
@@ -771,7 +777,7 @@ class PipelineManager(object):
         # a change ahead, a newly generated layout for this change, or
         # None if there was an error that makes the layout unusable.
         # In the last case, it will have set the config_errors on this
-        # item, which may be picked up by the next itme.
+        # item, which may be picked up by the next item.
         if not item.layout:
             item.layout = self.getLayout(item)
         if not item.layout:
