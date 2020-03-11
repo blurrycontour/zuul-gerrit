9dd9e4571a4d5676ff2839408de2e07927822863
James E. Blair <jeblair@redhat.com>
2020-03-11 11:31:51 -0700

    WIP: ZK TLS
    This adds a script to generate TLS certs for zookeeper.
    
    Change-Id: Icd976cc32dfd9f75f8cfb1c9ad11e08af31723d6

---
+++ doc/source/howtos/zookeeper.rst
@@ -0,0 +1,102 @@
+ZooKeeper Administration
+========================
+
+This section will cover some basic tasks and recommendations when
+setting up ZooKeeper for use with Zuul.  A complete tutorial for
+ZooKeeper is out of scope for this documentation.
+
+Configuration
+-------------
+
+The following general configuration setting in
+``/etc/zookeeper/zoo.cfg`` is recommended:
+
+.. code-block::
+
+   autopurge.purgeInterval=6
+
+This instructs ZooKeeper to purge old snapshots every 6 hours.  This
+will avoid filling the disk.
+
+Authentication
+--------------
+
+ZooKeeper supports password authentication via SASL.  The following settings in
+``/etc/zookeeper/zoo.cfg`` will enable this:
+
+.. code-block::
+
+   requireClientAuthScheme=sasl
+   authProvider.1=org.apache.zookeeper.server.auth.SASLAuthenticationProvider
+
+Add the following to ``/etc/default/zookeeper``:
+
+.. code-block::
+
+   JAVA_OPTS="-Djava.security.auth.login.config=/etc/zookeeper/auth.conf"
+
+And create a file at ``/etc/zookeeper/auth.conf`` with contents
+similar to the following:
+
+.. code-block::
+
+   Client {
+      org.apache.zookeeper.server.auth.DigestLoginModule required
+        username="super"
+        password="changeme";
+   };
+   Server {
+      org.apache.zookeeper.server.auth.DigestLoginModule required
+        user_super="changeme";
+   };
+
+Adjust the password, ``changeme`` as appropriate, then configure Zuul
+to connect to Zookeeper with the username and password values supplied
+here.
+
+Encrypted Connections
+---------------------
+
+ZooKeeper version 3.5.1 or greater is required for TLS support.
+ZooKeeper performs hostname and validation, therefore each member of
+the ZooKeeper cluster should have its own certificate.  The
+``tools/zk-ca.sh`` script in the Zuul source code repository can be
+used to quickly and easily generate self-signed certificates for all
+ZooKeeper cluster members and clients.
+
+Make a directory for it to store the certificates and CA data, and run
+it once for each client:
+
+.. code-block::
+
+   mkdir /etc/zookeeper/ca
+   tools/zk-ca.sh /etc/zookeeper/ca zookeeper1.example.com
+   tools/zk-ca.sh /etc/zookeeper/ca zookeeper2.example.com
+
+Add the following to ``/etc/zookeeper/zoo.cfg``:
+
+.. code-block::
+
+   # Client TLS configuration
+   secureClientPort=2281
+   ssl.keyStore.location=/etc/zookeeper/ca/keystores/zookeeper1.example.com.jks
+   ssl.keyStore.password=changeit
+   ssl.trustStore.location=/etc/zookeeper/ca/certs/cacert.pem
+
+   # Server TLS configuration
+   sslQuorum=true
+   serverCnxnFactory=org.apache.zookeeper.server.NettyServerCnxnFactory
+   ssl.quorum.keyStore.location=/etc/zookeeper/ca/keystores/zookeeper1.example.com.jks
+   ssl.quorum.keyStore.password=changeit
+   ssl.quorum.trustStore.location=/etc/zookeeper/ca/certs/cacert.pem
+
+Change the name of the certificate filenames as appropriate for the
+host (e.g., ``zookeeper1.example.com.jks``).  Note that the keystore
+password ``changeit`` does not need to be changed unless you want to.
+
+TODO: update Zuul's configuration to specify port 2281, and the path
+to the client cert and key:
+
+    keyfile='/etc/zookeeper/ca/keys/clientkey.pem',
+    certfile='/etc/zookeeper/ca/certs/client.pem',
+    ca='/etc/zookeeper/ca/certs/cacert.pem',
---
+++ tools/zk-ca.sh
@@ -0,0 +1,115 @@
+#!/bin/sh
+
+# Copyright 2020 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# Manage a CA for Zookeeper
+
+CAROOT=$1
+SERVER=$2
+KEYSTORE_PASSWORD=changeit
+
+EXPORT_PASSWORD=foobar
+SUBJECT='/C=US/ST=California/L=Oakland/O=Company Name/OU=Org'
+
+make_ca() {
+    mkdir $CAROOT/demoCA
+    mkdir $CAROOT/demoCA/reqs
+    mkdir $CAROOT/demoCA/newcerts
+    mkdir $CAROOT/demoCA/crl
+    mkdir $CAROOT/demoCA/private
+    chmod 700 $CAROOT/demoCA/private
+    touch $CAROOT/demoCA/index.txt
+    touch $CAROOT/demoCA/index.txt.attr
+    mkdir $CAROOT/certs
+    mkdir $CAROOT/keys
+    mkdir $CAROOT/keystores
+    chmod 700 $CAROOT/keys
+    chmod 700 $CAROOT/keystores
+
+    openssl req -new -nodes -subj "$SUBJECT/CN=caroot" \
+	    -keyout $CAROOT/demoCA/private/cakey.pem \
+	    -out $CAROOT/demoCA/reqs/careq.pem
+    openssl ca -create_serial -days 3560 -batch -selfsign -extensions v3_ca \
+	    -out $CAROOT/demoCA/cacert.pem \
+	    -keyfile $CAROOT/demoCA/private/cakey.pem \
+	    -infiles $CAROOT/demoCA/reqs/careq.pem
+    keytool -import -noprompt -alias caroot \
+	    -keystore $CAROOT/keystores/truststore.jks \
+	    -file $CAROOT/demoCA/cacert.pem \
+	    -deststorepass "$KEYSTORE_PASSWORD"
+    cp $CAROOT/demoCA/cacert.pem $CAROOT/certs
+}
+
+make_client() {
+    openssl req -new -nodes -subj "$SUBJECT/CN=client" \
+	    -keyout $CAROOT/keys/clientkey.pem \
+	    -out $CAROOT/demoCA/reqs/clientreq.pem
+    openssl ca -batch -policy policy_anything -days 3560 \
+	    -out $CAROOT/certs/client.pem \
+	    -infiles $CAROOT/demoCA/reqs/clientreq.pem
+}
+
+make_server() {
+    openssl req -new -nodes -subj "$SUBJECT/CN=$SERVER" \
+	    -keyout $CAROOT/keys/${SERVER}key.pem \
+	    -out $CAROOT/demoCA/reqs/${SERVER}req.pem
+    openssl ca -batch -policy policy_anything -days 3560 \
+	    -out $CAROOT/certs/$SERVER.pem \
+	    -infiles $CAROOT/demoCA/reqs/${SERVER}req.pem
+    openssl pkcs12 -export -name $SERVER \
+	    -in $CAROOT/certs/$SERVER.pem \
+	    -inkey $CAROOT/keys/${SERVER}key.pem \
+	    -passout "pass:$EXPORT_PASSWORD" \
+	    > $CAROOT/keystores/$SERVER.p12
+    keytool -importkeystore -srcstoretype pkcs12 -alias $SERVER \
+	    -destkeystore $CAROOT/keystores/$SERVER.jks \
+	    -srckeystore $CAROOT/keystores/$SERVER.p12 \
+	    -deststorepass "$KEYSTORE_PASSWORD" \
+	    -srcstorepass "$EXPORT_PASSWORD"
+    rm -f $CAROOT/keystores/$SERVER.p12
+}
+
+help() {
+    echo "$0 CAROOT [SERVER]"
+    echo
+    echo "  CAROOT is the path to a directory in which to store the CA"
+    echo "         and certificates."
+    echo "  SERVER is the FQDN of a server for which a certificate should"
+    echo "         be generated"
+}
+
+if [ ! -d "$CAROOT" ]; then
+    echo "CAROOT must be a directory"
+    help
+    exit 1
+fi
+
+cd $CAROOT
+
+if [ ! -d "$CAROOT/demoCA" ]; then
+    echo 'Generate CA'
+    make_ca
+    echo 'Generate client certificate'
+    make_client
+fi
+
+if [ -f "$CAROOT/certs/$SERVER.pem" ]; then
+    echo "Certificate for $SERVER already exists"
+    exit 0
+fi
+
+if [ "$SERVER" != "" ]; then
+    make_server
+fi
