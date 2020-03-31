eab766a979a829a38851d11a9aa2998f40c894dd
Sorin Sbarnea <ssbarnea@redhat.com>
2020-03-31 12:25:05 +0100

    Enable ANSI rendering via react-ansi
    Enables ANSI rendering using react-ansi, which emulates a terminal
    and also gives line numbering for free, even being able to colorize
    output from some other commands.
    
    Easy to test using build b4655eb004d143379d424fd968c2e196
    https://sbarnea.com/ss/Screen-Shot-2020-08-25-16-23-03.45.png
    
    Change-Id: If65641216e43865735cee6e8fb932c41c0cb5f21

--- web/src/containers/build/Console.jsx
+++ web/src/containers/build/Console.jsx
@@ -15,6 +15,7 @@
 import * as moment from 'moment'
 import 'moment-duration-format'
 import * as React from 'react'
+import ReactAnsi from 'react-ansi'
 import PropTypes from 'prop-types'
 import ReactJson from 'react-json-view'
 import {
@@ -91,7 +92,7 @@ class TaskOutput extends React.Component {
     } else if (typeof(value) === 'string') {
       ret = (
         <pre>
-          {value}
+          <ReactAnsi log={value}/>
         </pre>
       )
     } else if (typeof(value) === 'object') {
