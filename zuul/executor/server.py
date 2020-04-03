cc16a2bee5d62e0ed594eb6fecae047629572565
Albin Vass <albin.vass@volvocars.com>
2020-04-03 10:59:00 +0200

    WIP: Add support for ansible callbacks
    Change-Id: Ida7b84795d922b85ec9cc6161ab1203fb82da825

--- zuul/executor/server.py
+++ zuul/executor/server.py
@@ -861,6 +861,17 @@ class AnsibleJob(object):
         self.lookup_dir = os.path.join(plugin_dir, 'lookup')
         self.filter_dir = os.path.join(plugin_dir, 'filter')
 
+        self.ansible_callbacks = {}
+        for section_name in self.executor_server.config.sections():
+            cb_match = re.match(r'^ansible_callback ([\'\"]?)(.*)(\1)$',
+                                section_name, re.I)
+            if not cb_match:
+                continue
+            cb_name = cb_match.group(2)
+            self.ansible_callbacks[cb_name] = dict(
+                self.executor_server.config.items(section_name)
+            )
+
     def run(self):
         self.running = True
         self.thread = threading.Thread(target=self.execute,
@@ -2041,6 +2052,7 @@ class AnsibleJob(object):
             config.write('stdout_callback = zuul_stream\n')
             config.write('filter_plugins = %s\n'
                          % self.filter_dir)
+
             config.write('nocows = True\n')  # save useless stat() calls
             # bump the timeout because busy nodes may take more than
             # 10s to respond
@@ -2075,6 +2087,11 @@ class AnsibleJob(object):
             # and reduces CPU load of the ansible process.
             config.write('internal_poll_interval = 0.01\n')
 
+            if self.ansible_callbacks:
+                config.write('callback_whitelist =\n')
+                for callback in self.ansible_callbacks.keys():
+                    config.write('    %s\n' % callback)
+
             config.write('[ssh_connection]\n')
             # NOTE(pabelanger): Try up to 3 times to run a task on a host, this
             # helps to mitigate UNREACHABLE host errors with SSH.
@@ -2093,6 +2110,13 @@ class AnsibleJob(object):
                 "-o ServerAliveInterval=60 " \
                 "-o UserKnownHostsFile=%s" % self.jobdir.known_hosts
             config.write('ssh_args = %s\n' % ssh_args)
+
+            if self.ansible_callbacks:
+                for cb_name, cb_config in self.ansible_callbacks.items():
+                    config.write("[callback_%s]\n" % cb_name)
+                    {config.write("%s = %s\n" % (k, cb_config[k]))
+                     for k in cb_config.keys()
+                     if k != "callback_config_section"}
 
     def _ansibleTimeout(self, msg):
         self.log.warning(msg)
