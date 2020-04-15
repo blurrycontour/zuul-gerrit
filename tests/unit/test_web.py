c987c4216d40ab9c17169723638cb35e1535311d
Tobias Henkel <tobias.henkel@bmw.de>
2020-04-15 12:39:08 +0200

    Move queue from pipeline to project
    Referring to a queue makes more sense in the project where it has more
    use such as allowing circular dependencies later. This makes it
    possible to refer to the queue on project level which takes precedence
    over referring queues in the pipeline config. We'll also deprecate
    referring to the queue in the pipeline config and remove this in a
    later release.
    
    Change-Id: I14533ae57cfe688b55c26a1e17ab38e133180b28

--- tests/unit/test_web.py
+++ tests/unit/test_web.py
@@ -712,6 +712,7 @@ class TestWeb(BaseTestWeb):
                 'configs': [{
                     'templates': [],
                     'default_branch': 'master',
+                    'queue_name': 'integrated',
                     'merge_mode': 'merge-resolve',
                     'pipelines': [{
                         'name': 'check',
@@ -719,7 +720,7 @@ class TestWeb(BaseTestWeb):
                         'jobs': jobs,
                     }, {
                         'name': 'gate',
-                        'queue_name': 'integrated',
+                        'queue_name': 'integrated-overridden',
                         'jobs': jobs,
                     }, {'name': 'post',
                         'queue_name': None,
