b27684784b4c84089719c264b97eb00b3e42b894
Jan Kubovy <jan.kubovy@bmw.de>
2020-04-16 14:25:32 +0200

    Prepare Zookeeper for scale-out scheduler
    This change is a common root for other
    Zookeeper related changed regarding
    scale-out-scheduler.
    
    Since the ZooKeeper class is expected to grow
    significantly (ZooKeeper is becoming a central part
    of Zuul) a split of the ZooKeeper class (zk.py) into
    zk module is done here to avoid the current god-class.
    
    Change-Id: I714c06052b5e17269a6964892ad53b48cf65db19
    Story: 2007192

---
+++ zuul/zk/zuul.py
@@ -0,0 +1,27 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+from zuul.zk.base import ZooKeeperBase
+
+
+class ZooKeeperZuulMixin(ZooKeeperBase):
+    ZUUL_CONFIG_ROOT = "/zuul"
+    # Node content max size: keep ~100kB as a reserve form the 1MB limit
+    ZUUL_CONFIG_MAX_SIZE = 1024 * 1024 - 100 * 1024
+
+    def _getZuulNodePath(self, *args: str) -> str:
+        return "/".join(filter(lambda s: s is not None and s != '',
+                               [self.ZUUL_CONFIG_ROOT] + list(args)))
+
+    def _getConfigPartContent(self, parent, child) -> str:
+        node = "%s/%s" % (parent, child)
+        return self.client.get(node)[0].decode(encoding='UTF-8') \
+            if self.client and self.client.exists(node) else ''
