bcce51d1c45f3525b386ba055b667a6f73fec5f6
Jan Kubovy <jan.kubovy@bmw.de>
2020-04-23 12:05:34 +0200

    Connect merger to Zookeeper
    Part of point 5 in https://etherpad.openstack.org/p/zuulv4
    
    Connection is idle for now.
    
    Change-Id: I97a97f61940fab2a555c3651e78fa7a929e8ebfb

--- zuul/lib/config.py
+++ zuul/lib/config.py
@@ -9,8 +9,9 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
+import configparser
 import os
+import zuul.zk
 
 
 def get_default(config, section, option, default=None, expand_user=False):
@@ -28,3 +29,22 @@ def get_default(config, section, option, default=None, expand_user=False):
     if expand_user and value:
         return os.path.expanduser(value)
     return value
+
+
+def connect_zookeeper(config: configparser.ConfigParser) -> zuul.zk.ZooKeeper:
+    zookeeper = zuul.zk.ZooKeeper(enable_cache=True)
+    zookeeper_hosts = get_default(config, 'zookeeper', 'hosts', None)
+    if not zookeeper_hosts:
+        raise Exception("The zookeeper hosts config value is required")
+    zookeeper_tls_key = get_default(config, 'zookeeper', 'tls_key')
+    zookeeper_tls_cert = get_default(config, 'zookeeper', 'tls_cert')
+    zookeeper_tls_ca = get_default(config, 'zookeeper', 'tls_ca')
+    zookeeper_timeout = float(get_default(config, 'zookeeper',
+                                          'session_timeout', 10.0))
+    zookeeper.connect(
+        hosts=zookeeper_hosts,
+        timeout=zookeeper_timeout,
+        tls_cert=zookeeper_tls_cert,
+        tls_key=zookeeper_tls_key,
+        tls_ca=zookeeper_tls_ca)
+    return zookeeper
