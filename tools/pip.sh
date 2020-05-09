da561c93e351e9c904a8ddc5177edb13673bc318
Monty Taylor <mordred@inaugust.com>
2020-05-09 13:27:30 -0500

    Update node to v14 and update to new jobs
    v14 is the latest lts. Let's use it.
    
    Also - rename the jobs to make it clear what they're doing, and
    add a dashboard job that points at opendev's multi-tenat api too.
    
    There are new jobs that default to latest node LTS and auto-detect
    yarn vs npm. Update to use them.
    
    Depends-On: https://review.opendev.org/728097
    Depends-On: https://review.opendev.org/726547
    Change-Id: I5717edea2cd09acc5bce673c38bbe7fa8057a376

--- tools/pip.sh
+++ tools/pip.sh
@@ -22,8 +22,7 @@ if [[ ! $(command -v yarn) ]]
 then
     pip install nodeenv
     # Initialize nodeenv and tell it to re-use the currently active virtualenv
-    # TODO(jeblair): remove node version pin.  upath 1.0.4 objects to node >9.
-    nodeenv --python-virtualenv -n 10.16.0
+    nodeenv --python-virtualenv
     # Use -g because inside of the virtualenv '-g' means 'install into the'
     # virtualenv - as opposed to installing into the local node_modules.
     # Avoid writing a package-lock.json file since we don't use it.
