b49ae9714aa56bf00bbb57dffdd464b16d858c36
Guillaume Chauvel <guillaume.chauvel@gmail.com>
2020-05-13 22:01:00 +0200

    WIP: Add tutorial tests
    Change-Id: I7e09cb5189c96fcd7eb77a2c0c5a8e8415b69a48

---
+++ doc/source/examples/test1-job-dependencies/goodbye_hello.py
@@ -0,0 +1,10 @@
+class Hello():
+    def run(self):
+        return hello()
+
+    def bye(self):
+        return goodbye()
+
+
+if __name__ == "__main__":
+    print(Hello().run())
---
+++ doc/source/examples/test1-use-zuul-jobs/hello/hello.py
@@ -0,0 +1,7 @@
+class Hello():
+    def run(self):
+        return "Hello Zuul"
+
+
+if __name__ == "__main__":
+    print(Hello().run())
