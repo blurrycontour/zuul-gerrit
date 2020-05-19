4ba7991986e6a0cea5e6bad1b48b82f6fe63df10
Matthieu Huin <mhuin@redhat.com>
2020-05-19 17:50:51 +0200

    [WIP] web UI: add Autoholds Requests page
    Dedicate a page to autohold requests.
    
    Change-Id: I650d0c5c85cdbd6968527e0a8bc324f9cefc720e

--- web/src/api.js
+++ web/src/api.js
@@ -122,6 +122,13 @@ function fetchStatus (apiPrefix) {
 function fetchChangeStatus (apiPrefix, changeId) {
   return Axios.get(apiUrl + apiPrefix + 'status/change/' + changeId)
 }
+function fetchAutoholds (apiPrefix, project) {
+  let path = 'autohold'
+  if (project) {
+    path += '?project=' + project
+  }
+  return Axios.get(apiUrl + apiPrefix + path)
+}
 function fetchBuild (apiPrefix, buildId) {
   return Axios.get(apiUrl + apiPrefix + 'build/' + buildId)
 }
@@ -168,6 +175,7 @@ export {
   fetchChangeStatus,
   fetchConfigErrors,
   fetchStatus,
+  fetchAutoholds,
   fetchBuild,
   fetchBuilds,
   fetchBuildset,
