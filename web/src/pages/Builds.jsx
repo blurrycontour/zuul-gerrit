2bf2f2a1219859d084b04bf907b528c4a46126b0
Matthieu Huin <mhuin@redhat.com>
2020-05-19 15:50:49 +0200

    [WIP] Web UI: add "held" information, filtering in Builds page
    Builds that triggered a autohold request are displayed in the "danger"
    style.
    It is possible to filter jobs by "held only".
    
    Change-Id: I40b06c83ed069e0756c7f8b00430d36a36230bfa

--- web/src/pages/Builds.jsx
+++ web/src/pages/Builds.jsx
@@ -44,7 +44,15 @@ class BuildsPage extends TableFilters {
   updateData = (filters) => {
     let queryString = ''
     if (filters) {
-      filters.forEach(item => queryString += '&' + item.key + '=' + item.value)
+      filters.forEach(item => {
+          if (item.key === 'held') {
+              if (item.value === true) {
+                  queryString += '&' + item.key + '=' + item.value
+              }
+          } else {
+              queryString += '&' + item.key + '=' + item.value
+          }
+      })
     }
     this.setState({builds: null})
     fetchBuilds(this.props.tenant.apiPrefix, queryString).then(response => {
@@ -118,7 +126,7 @@ class BuildsPage extends TableFilters {
         cell: {formatters: [formatter]}
       })
       if (prop !== 'start_time' && prop !== 'ref_url' && prop !== 'duration'
-          && prop !== 'log_url' && prop !== 'uuid') {
+          && prop !== 'log_url' && prop !== 'uuid' && prop !== 'held') {
         this.filterTypes.push({
           id: prop,
           title: label,
@@ -133,6 +141,13 @@ class BuildsPage extends TableFilters {
       title: 'Build',
       placeholder: 'Filter by Build UUID',
       filterType: 'text',
+    })
+    this.filterTypes.push({
+      id: 'held',
+      title: 'Held Status',
+      placeholder: 'Show only builds that triggered a autohold request',
+      filterType: 'select',
+      filterValues: [true, null],
     })
   }
 
@@ -152,7 +167,12 @@ class BuildsPage extends TableFilters {
               case 'SUCCESS':
                 return { className: 'success' }
               default:
-                return { className: 'warning' }
+                switch (row.held) {
+                  case true:
+                    return { className: 'danger' }
+                  default:
+                    return { className: 'warning' }
+                }
             }
           }} />
       </Table.PfProvider>)
