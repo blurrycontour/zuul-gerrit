0737ee1facb993f30be79143ea290d0adedadcbc
Tristan Cacqueray <tdecacqu@redhat.com>
2020-05-27 12:59:12 +0000

    executor: add extra kubectl port-forward debug and releasenotes
    This change documents how the kubectl was not working and it adds
    a log statement to help debug future issue.
    
    Change-Id: Iaf6ca030365d9e4e768bca716568cb2d4289665d

--- zuul/executor/server.py
+++ zuul/executor/server.py
@@ -355,6 +355,7 @@ class KubeFwd(object):
             self.port = m.group(1)
         else:
             try:
+                self.log.error("Coul not find the forwarded port: %s", line)
                 fwd.kill()
             except Exception:
                 pass
