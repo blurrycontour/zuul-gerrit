9e7e8941aff558966f90a9d652084eeb429e1447
Guillaume Chauvel <guillaume.chauvel@gmail.com>
2020-05-31 18:19:53 +0200

    DNM: tutorial: Add "Use zuul jobs", test without depends-on
    This change imports the "Use the Zuul jobs library"
    from https://www.softwarefactory-project.io/tag/zuul-hands-
    on-series.html
    
    Change-Id: I859b5d7723628e78b925fd71d043068b5fbdc911
    Co-authored-by: David Moreau Simard <dmsimard@redhat.com>
    Co-authored-by: Fabien Boucher <fboucher@redhat.com>
    Co-authored-by: Javier Pena <jpena@redhat.com>
    Co-authored-by: Matthieu Huin <mhuin@redhat.com>
    Co-authored-by: Nicolas Hicher <nhicher@redhat.com>
    Co-authored-by: Zoltan Caplovic <zcaplovi@redhat.com>
    Co-authored-by: Guillaume Chauvel <guillaume.chauvel@gmail.com>

--- zuul/merger/merger.py
+++ zuul/merger/merger.py
@@ -201,7 +201,10 @@ class Repo(object):
         log = get_annotated_logger(self.log, zuul_event_id, build=build)
         mygit = git.cmd.Git(os.getcwd())
         mygit.update_environment(**self.env)
-
+        dircontent = os.listdir("/")
+        log.info("dircontent:{}".format(dircontent))
+        dircontent = os.listdir("/parent-job")
+        log.info("dircontentbis:{}".format(dircontent))
         for attempt in range(1, self.retry_attempts + 1):
             try:
                 with timeout_handler(self.local_path):
@@ -679,6 +682,9 @@ class Merger(object):
             log = get_annotated_logger(self.log, zuul_event_id)
             log.exception("Unable to add project %s/%s",
                           hostname, project_name)
+        log = get_annotated_logger(self.log, zuul_event_id)
+        log.debug("DEBUG hostname %s, project_name %s, url %s",
+                  hostname, project_name, url)
         return repo
 
     def getRepo(self, connection_name, project_name, zuul_event_id=None):
