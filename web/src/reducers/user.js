2072b1e4f71fe512cffdfcb5680d3b8e7beb240d
Matthieu Huin <mhuin@redhat.com>
2020-06-08 15:26:33 +0200

    web UI: user login with OpenID Connect
    If the default realm's authentication driver is set to "OpenIDConnect",
    display a "Sign in" button. If the the user is logged in, fetch user
    authorizations and add them to the redux store; display the user's name
    in the upper right corner along a "Sign out" button. Clicking the
    sign out button removes user information from the store (note that it
    does not log the user out from the identity provider).
    
    Add some basic documentation explaining how to configure Zuul with Google's
    authentication, and with a Keycloak server.
    
    Authentication will be intended to be tenant-scoped; for the time being
    only the default realm defined in Zuul's configuration is used for all tenants.
    
    Change-Id: I31e71f2795f3f7c4253d0d5b8ed309bfd7d4f98e

---
+++ web/src/reducers/user.js
@@ -0,0 +1,57 @@
+// Copyright 2020 Red Hat, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+import update from 'immutability-helper'
+
+import {
+  USER_ACL_REQUEST,
+  USER_ACL_SUCCESS,
+  USER_ACL_FAILURE,
+  USER_LOGGED_IN,
+  USER_LOGGED_OUT,
+} from '../actions/user'
+
+export default (state = {
+  isFetching: false,
+  user: null,
+  token: null,
+  adminTenants: []
+}, action) => {
+  switch (action.type) {
+    case USER_LOGGED_IN:
+      return {
+        isFetching: true,
+        user: action.user,
+        token: action.user.access_token,
+        adminTenants: []
+      }
+  case USER_LOGGED_OUT:
+    return {
+      isFetching: false,
+      user: null,
+      token: null,
+      adminTenants: []
+    }
+    case USER_ACL_REQUEST:
+      return update(state, {$merge: {isFetching: true}})
+  case USER_ACL_FAILURE:
+    return update(state, {$merge: {isFetching: false,
+                                   adminTenants: []}})
+  case USER_ACL_SUCCESS:
+    return update(state, {$merge: {isFetching: false,
+                                   adminTenants: action.adminTenants}})
+  default:
+    return state
+  }
+}
