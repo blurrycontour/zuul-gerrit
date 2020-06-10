4e44c57c935b851546a3f3835de38c937b133308
Sorin Sbarnea <ssbarnea@redhat.com>
2020-06-10 14:18:30 +0100

    Enable display of failures inside zuul console
    Current failures returned by modules like package/dnf are hidden
    from default zuul UI.
    
    Also adds title to output blocks in order to indicate the nature of
    the blocks displayed. (accessibility)
    
    Change-Id: Ia2fdb1a5abc7873a555bdc6049935aae0347f5a5
    Related: https://github.com/ansible/ansible/issues/70003

--- web/src/containers/build/Console.jsx
+++ web/src/containers/build/Console.jsx
@@ -38,7 +38,7 @@ import {
   taskPathMatches,
 } from '../../actions/build'
 
-const INTERESTING_KEYS = ['msg', 'stdout', 'stderr']
+const INTERESTING_KEYS = ['msg', 'stdout', 'stderr', 'failures']
 
 
 class TaskOutput extends React.Component {
