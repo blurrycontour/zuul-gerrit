9bea25701f57fc7f7f5f10e4e60ce2b183193339
Felix Edel <felix.edel@bmw.de>
2020-06-24 08:47:54 +0200

    PF4: Update buildset result page (new layout and styling)
    This updates the buildset result page with Patternfly 4 components and
    a new design based on inspirations from other CI systems, Patternfly 4
    demos and my own ideas.
    
    I chose the buildset result page because it doesn't contain too much
    elements und therefore is a good starting point. If you like the
    design, I will continue with that also for other pages like the build
    result pages or the various "list" pages like builds and buildsets.
    
    Change-Id: I978ee448ddf6e22e4a6ec8211204f694932eaa4e

--- web/src/pages/Buildset.jsx
+++ web/src/pages/Buildset.jsx
@@ -15,12 +15,25 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import { PageSection, PageSectionVariants } from '@patternfly/react-core'
+import {
+  EmptyState,
+  EmptyStateIcon,
+  EmptyStateVariant,
+  PageSection,
+  PageSectionVariants,
+  Tab,
+  Tabs,
+  TabTitleIcon,
+  TabTitleText,
+  Title,
+} from '@patternfly/react-core'
+import { BuildIcon } from '@patternfly/react-icons'
 
 import { fetchBuildsetIfNeeded } from '../actions/build'
-import { Fetchable } from '../containers/Fetching'
+import { EmptyPage } from '../containers/Errors'
+import { Fetchable, Fetching } from '../containers/Fetching'
+import BuildList from '../containers/build/BuildList'
 import Buildset from '../containers/build/Buildset'
-
 
 class BuildsetPage extends React.Component {
   static propTypes = {
@@ -30,43 +43,114 @@ class BuildsetPage extends React.Component {
     dispatch: PropTypes.func,
   }
 
-  updateData = (force) => {
-    this.props.dispatch(fetchBuildsetIfNeeded(
-      this.props.tenant, this.props.match.params.buildsetId, force))
+  constructor() {
+    super()
+    this.state = {
+      activeTabKey: 0,
+    }
   }
 
-  componentDidMount () {
+  handleTabClick = (event, tabIndex) => {
+    this.setState({
+      activeTabKey: tabIndex,
+    })
+  }
+
+  updateData = (force) => {
+    this.props.dispatch(
+      fetchBuildsetIfNeeded(
+        this.props.tenant,
+        this.props.match.params.buildsetId,
+        force
+      )
+    )
+  }
+
+  componentDidMount() {
     document.title = 'Zuul Buildset'
     if (this.props.tenant.name) {
       this.updateData()
     }
   }
 
-  componentDidUpdate (prevProps) {
+  componentDidUpdate(prevProps) {
     if (this.props.tenant.name !== prevProps.tenant.name) {
       this.updateData()
     }
   }
 
-  render () {
-    const { remoteData } = this.props
-
+  render() {
+    const { remoteData, tenant } = this.props
+    const { activeTabKey } = this.state
     const buildset = remoteData.buildsets[this.props.match.params.buildsetId]
+
+    // Initial page load
+    if (!buildset && remoteData.isFetching) {
+      return <Fetching />
+    }
+
+    // Fetching finished, but no buildset found
+    if (!buildset) {
+      // TODO (felix): Provide some generic error (404?) page. Can we somehow
+      // identify the error here?
+      return (
+        <EmptyPage
+          title="This buildset does not exist"
+          icon={BuildIcon}
+          linkTarget={`${tenant.linkPrefix}/buildsets`}
+          linkText="Show all buildsets"
+        />
+      )
+    }
+
+    // Return the build list or an empty state if no builds are part of the
+    // buildset.
+    const buildsTabContent = buildset.builds ? (
+      <BuildList builds={buildset.builds} />
+    ) : (
+      <EmptyState variant={EmptyStateVariant.small}>
+        <EmptyStateIcon icon={BuildIcon} />
+        <Title headingLevel="h4" size="lg">
+          This buildset does not contain any builds
+        </Title>
+      </EmptyState>
+    )
+
+    const fetchable = (
+      <Fetchable
+        isFetching={remoteData.isFetching}
+        fetchCallback={this.updateData}
+      />
+    )
+
     return (
-      <PageSection variant={PageSectionVariants.light}>
-        <PageSection style={{paddingRight: '5px'}}>
-          <Fetchable
-            isFetching={remoteData.isFetching}
-            fetchCallback={this.updateData}
-          />
+      <>
+        <PageSection variant={PageSectionVariants.light}>
+          <Buildset buildset={buildset} fetchable={fetchable} />
         </PageSection>
-        {buildset && <Buildset buildset={buildset}/>}
-      </PageSection>
+        <PageSection variant={PageSectionVariants.light}>
+          <Tabs activeKey={activeTabKey} onSelect={this.handleTabClick}>
+            <Tab
+              eventKey={0}
+              title={
+                <>
+                  <TabTitleIcon>
+                    <BuildIcon />
+                  </TabTitleIcon>
+                  <TabTitleText>Builds</TabTitleText>
+                </>
+              }
+            >
+              {buildsTabContent}
+            </Tab>
+          </Tabs>
+        </PageSection>
+      </>
     )
   }
 }
 
-export default connect(state => ({
+export default connect((state) => ({
   tenant: state.tenant,
   remoteData: state.build,
 }))(BuildsetPage)
