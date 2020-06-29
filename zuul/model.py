619c0f319fc118d17de36eb4a5370eb0287b4467
Jan Kubovy <jan.kubovy@bmw.de>
2020-06-29 10:41:20 +0200

    WIP: Store unparsed branch config in Zookeeper
    We need to store the global configuration in zookeeper. However
    zookeeper is not designed as a database with a large amount of data we
    should store as little as possible in zookeeper. Thus we only store
    the per project-branch unparsed config in zookeeper. From this every
    part of zuul like the scheduler or also zuul-web can quickly
    recalculate the layout of each tenant and keep it up to date by
    watching for changes in the unparsed project-branch-config. We will
    lock the complete global config with one lock and maintain a checkpoint
    version of it. This way each component can watch the config version
    number and react accordingly. Although we lock the complete global
    config we still should store the actual config in distinct nodes per
    project and branch. This is needed because of the 1MB limit per znode
    in zookeeper. It further makes it less expensive to cache the global
    config in each component as this cache will be updated incrementally.
    
    The configs will be stored in the path
    ``/zuul/config/<tenant>/<project>/<branch>`` per branch segmented in
    ``/zuul/config/<tenant>/<project>/<branch>/<path-to-config>/<shard>``. The
    ``shard`` is a sequence number and will be used to store larger than
    1MB files due to the limitation mentioned above.
    
    Manual reconfiguration on one instance will reload the config files and
    update zookeeper in case of differences. Other instances will be
    notified about those changes via zookeeper watchers. During start
    zookeeper will be used as primary store.
    
    Change-Id: I519c4e6af1f21efdeed2b919575bcad9c50a9f18
    Story: 2007192
    Task: 38620

--- zuul/model.py
+++ zuul/model.py
@@ -926,7 +926,8 @@ class SourceContext(ConfigObject):
     Jobs and playbooks reference this to keep track of where they
     originate."""
 
-    def __init__(self, project, branch, path, trusted):
+    def __init__(self, project: Project, branch: str, path: str,
+                 trusted: bool):
         super(SourceContext, self).__init__()
         self.project = project
         self.branch = branch
@@ -3761,6 +3762,7 @@ class Layout(object):
 
     def __init__(self, tenant):
         self.uuid = uuid4().hex
+        self.zk_hash = None
         self.tenant = tenant
         self.project_configs = {}
         self.project_templates = {}
@@ -4363,6 +4365,7 @@ class Tenant(object):
         self.default_base_job = None
         self.report_build_page = False
         self.layout = None
+        self.layout_hash = None
         # The unparsed configuration from the main zuul config for
         # this tenant.
         self.unparsed_config = None
@@ -4391,6 +4394,11 @@ class Tenant(object):
         self.default_ansible_version = None
 
         self.authorization_rules = []
+        self.web_root = None
+        self.allowed_triggers = None
+        self.allowed_reporters = None
+        self.allowed_labels = None
+        self.disallowed_labels = None
 
     @property
     def all_projects(self):
@@ -4615,7 +4623,8 @@ class Abide(object):
         project_branch_cache[branch] = UnparsedBranchCache()
         return project_branch_cache[branch]
 
-    def clearUnparsedBranchCache(self, canonical_project_name, branch=None):
+    def clearUnparsedBranchCache(self, canonical_project_name: str,
+                                 branch: str=None) -> None:
         if canonical_project_name in self.unparsed_project_branch_cache:
             project_branch_cache = \
                 self.unparsed_project_branch_cache[canonical_project_name]
--- zuul/zk/zuul.py
+++ zuul/zk/zuul.py
@@ -17,13 +17,13 @@ from kazoo.recipe.lock import Lock
 
 
 class ZooKeeperZuulMixin:
-    ZUUL_CONFIG_ROOT = "/zuul"
+    CONFIG_ROOT = "/zuul"
     # Node content max size: keep ~100kB as a reserve form the 1MB limit
-    ZUUL_CONFIG_MAX_SIZE = 1024 * 1024 - 100 * 1024
+    CONFIG_MAX_SIZE = 1024 * 1024 - 100 * 1024
 
     def _getZuulNodePath(self, *args: str) -> str:
         return "/".join(filter(lambda s: s is not None and s != '',
-                               [self.ZUUL_CONFIG_ROOT] + list(args)))
+                               [self.CONFIG_ROOT] + list(args)))
 
     def _getConfigPartContent(self, parent, child) -> str:
         if TYPE_CHECKING:  # IDE type checking support
