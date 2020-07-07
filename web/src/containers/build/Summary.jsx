b12484e9de6db5f87ff168ab48f02454cdb5b77d
Felix Edel <felix.edel@bmw.de>
2020-07-07 13:31:34 +0200

    PF4: Update build result page layout
    Following up on the patterns used for the buildset result page this now
    updates the layout of the build result page in the same manner.
    
    Additionally, the old "Summary" part is now always shown above the tabs
    (Logs, Console, ...) and the first tab was changed to show the build
    results.
    
    The Logs and Console tabs are unchanged.
    
    Change-Id: If04b65596f189c343cda05a4a8fb3e1b740a0be2

--- web/src/containers/build/Summary.jsx
+++ web/src/containers/build/Summary.jsx
@@ -15,14 +15,9 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
 
 import ArtifactList from './Artifact'
 import BuildOutput from './BuildOutput'
-
-import * as moment from 'moment'
-import 'moment-duration-format'
-
 
 class Summary extends React.Component {
   static propTypes = {
@@ -31,101 +26,21 @@ class Summary extends React.Component {
     timezone: PropTypes.string,
   }
 
-  render () {
+  render() {
     const { build } = this.props
-    const rows = []
-    const myColumns = [
-      'job_name', 'result', 'buildset', 'voting',
-      'pipeline', 'start_time', 'end_time', 'duration',
-      'project', 'branch', 'change', 'patchset', 'oldrev', 'newrev',
-      'ref', 'new_rev', 'ref_url', 'log_url', 'event_id']
 
-    if (!build.buildset) {
-      // Safely handle missing buildset information
-      myColumns.splice(myColumns.indexOf('buildset'), 1)
-    }
-
-    myColumns.forEach(column => {
-      let label = column
-      let value = build[column]
-      if (column === 'job_name') {
-        label = 'job'
-        value = (
-          <React.Fragment>
-          <Link to={this.props.tenant.linkPrefix + '/job/' + value}>
-            {value}
-          </Link>
-          <span> &mdash; </span>
-          <Link to={this.props.tenant.linkPrefix + '/builds?job_name=' + value + '&project=' + build.project} title="See previous runs of this job inside current project.">
-            build history
-          </Link>
-          </React.Fragment>
-        )
-      }
-      if (column === 'buildset') {
-        value = (
-          <Link to={this.props.tenant.linkPrefix + '/buildset/' + value.uuid}>
-            {value.uuid}
-          </Link>
-        )
-      }
-      if (column === 'voting') {
-        if (value) {
-          value = 'true'
-        } else {
-          value = 'false'
-        }
-      }
-      if (column === 'start_time' || column === 'end_time') {
-        value = moment.utc(value).tz(this.props.timezone).format('YYYY-MM-DD HH:mm:ss')
-      }
-      if (column === 'duration') {
-          value = moment.duration(value, 'seconds')
-            .format('h [hr] m [min] s [sec]')
-      }
-      if (value && (column === 'log_url' || column === 'ref_url')) {
-        value = <a href={value}>{value}</a>
-      }
-      if (column === 'log_url') {
-        label = 'log url'
-        if (build.manifest && build.manifest.index_links) {
-          value = <a href={value + 'index.html'}>{value}</a>
-        } else {
-          value = <a href={value}>{value}</a>
-        }
-      }
-      if (column === 'ref_url') {
-        label = 'ref url'
-        value = <a href={value}>{value}</a>
-      }
-      if (column === 'event_id') {
-        label = 'event id'
-      }
-      if (value) {
-        rows.push({key: label, value: value})
-      }
-    })
     return (
       <React.Fragment>
-        <br/>
-        <table className="table table-striped table-bordered">
-          <tbody>
-            {rows.map(item => (
-              <tr key={item.key}>
-                <td>{item.key}</td>
-                <td>{item.value}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
         <h3>Artifacts</h3>
-        <ArtifactList build={build}/>
+        <ArtifactList build={build} />
         <h3>Results</h3>
-        {build.hosts && <BuildOutput output={build.hosts}/>}
+        {build.hosts && <BuildOutput output={build.hosts} />}
       </React.Fragment>
     )
   }
 }
 
-
-export default connect(state => ({tenant: state.tenant, timezone: state.timezone}))(Summary)
+export default connect((state) => ({
+  tenant: state.tenant,
+  timezone: state.timezone,
+}))(Summary)
--- web/src/pages/Build.jsx
+++ web/src/pages/Build.jsx
@@ -15,13 +15,14 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import { PageSection, PageSectionVariants } from '@patternfly/react-core'
+import {} from '@patternfly/react-core'
+import { BuildIcon } from '@patternfly/react-icons'
 
 import { fetchBuildIfNeeded } from '../actions/build'
+import { EmptyPage } from '../containers/Errors'
 import { Fetching } from '../containers/Fetching'
 import Build from '../containers/build/Build'
 import Summary from '../containers/build/Summary'
-
 
 class BuildPage extends React.Component {
   static propTypes = {
@@ -32,34 +33,49 @@ class BuildPage extends React.Component {
   }
 
   updateData = (force) => {
-    this.props.dispatch(fetchBuildIfNeeded(
-      this.props.tenant, this.props.match.params.buildId, null, force))
+    this.props.dispatch(
+      fetchBuildIfNeeded(
+        this.props.tenant,
+        this.props.match.params.buildId,
+        null,
+        force
+      )
+    )
   }
 
-  componentDidMount () {
+  componentDidMount() {
     document.title = 'Zuul Build'
     this.updateData()
   }
 
-  render () {
-    const { remoteData } = this.props
+  render() {
+    const { remoteData, tenant } = this.props
     if (remoteData.isFetching) {
       return <Fetching />
     }
 
     const build = remoteData.builds[this.props.match.params.buildId]
+
+    if (!build) {
+      return (
+        <EmptyPage
+          title="This build does not exist"
+          icon={BuildIcon}
+          linkTarget={`${tenant.linkPrefix}/builds`}
+          linkText="Show all builds"
+        />
+      )
+    }
+
     return (
-      <PageSection variant={PageSectionVariants.light}>
-      {build &&
-       <Build build={build} active='summary'>
-         <Summary build={build}/>
-       </Build>}
-      </PageSection>
+      <Build build={build} active="results">
+        <Summary build={build} />
+      </Build>
     )
   }
 }
 
-export default connect(state => ({
+export default connect((state) => ({
   tenant: state.tenant,
   remoteData: state.build,
 }))(BuildPage)
