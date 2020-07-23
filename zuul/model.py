b51806878db6eb4a579fdabe43f2cc7899cc6019
Guillaume Chauvel <guillaume.chauvel@gmail.com>
2020-07-23 22:43:21 +0200

    Tenant reconfiguration: Scheduler ref-updated create/delete
    branch_deleted reconfiguration
    -----------------------------
    During TenantParser fromYaml:
    - on initial loading
    - on reconfiguration
    for every (tenant,project,branch) tuple a cache entry is created
    (see configloader.py:1772 _cacheTenantYAML abide.getUnparsedBranchCache)
    
    So, on branch_deleted event, hasUnparsedBranchCache always return True, which
    triggers a reconfiguration, even if no config files are present
    
    hasUsefulBranchCache is introduced to return True only if the tenant/project/branch
    has valid zuul configuration elements, which is used to trigger or not a tenant
    reconfiguration
    
    branch_created reconfiguration
    ------------------------------
    When a branch is created, no cache entry exists for that branch, a
    reconfiguration could only start if the associated commit has valid
    zuul configuration elements
    
    for a Change object, 'files' is the list of files that are modified,
    added or removed for this change, an empty list means that no files were
    modified, so no reconfiguration is launched.
    A source driver should fill 'change.files' info if such data are available
    or can be easily fetched, otherwise 'change.files' is kept to the initialized
    'None' value to trigger a reconfiguration because we can't know if it touches
    the configuration or not.
    
    For Gerrit, it's possible to get the list of files associated to a commit
    using REST API 'projects/.../commits/.../files', only works if http session
    is available.
    
    on branch_created event:
    -ssh: only change info can be queried, commit files can't.
     'change.files' is kept to None, triggering a tenant reconfiguration
    -http(+ssh): files are queried using http REST API, change.files is a valid
     list
    
    Change-Id: I5628039758ae3357c0c54502f01cf5e2a58caf57

--- zuul/model.py
+++ zuul/model.py
@@ -3067,7 +3067,7 @@ class Ref(object):
         self.ref = None
         self.oldrev = None
         self.newrev = None
-        self.files = []
+        self.files = None
 
     def _id(self):
         return self.newrev
@@ -3169,6 +3169,7 @@ class Tag(Ref):
         super(Tag, self).__init__(project)
         self.tag = None
         self.containing_branches = []
+        self.files = []
 
 
 class Change(Branch):
@@ -4541,6 +4542,10 @@ class UnparsedBranchCache(object):
         self.extra_files_searched = set()
         self.extra_dirs_searched = set()
         self.files = {}
+        self.useful_conf = {}
+
+    def hasUsefulConf(self, tpc):
+        return self.useful_conf.get(tpc, False)
 
     def isValidFor(self, tpc):
         """Return True if this has valid cache results for the extra
@@ -4585,6 +4590,7 @@ class UnparsedBranchCache(object):
             data = self.files.get(fn)
             if data is not None:
                 ret.extend(data)
+                self.useful_conf[tpc] = True
         return ret
 
 
@@ -4602,6 +4608,16 @@ class Abide(object):
         if cache is None:
             return False
         return True
+
+    def hasUsefulBranchCache(self, canonical_project_name, branch, tenant):
+        project_branch_cache = self.unparsed_project_branch_cache.setdefault(
+            canonical_project_name, {})
+        cache = project_branch_cache.get(branch)
+        if cache is not None:
+            tpc = tenant.project_configs[canonical_project_name]
+            cache.get(tpc)
+            return cache.hasUsefulConf(tpc)
+        return False
 
     def getUnparsedBranchCache(self, canonical_project_name, branch):
         project_branch_cache = self.unparsed_project_branch_cache.setdefault(
