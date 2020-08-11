b8a77fd07b17343a6d714342d83c32988c9429ba
Felix Edel <felix.edel@bmw.de>
2020-08-11 14:47:50 +0200

    PF4: Update builds and buildsets tables + use newest patternfly release
    This updates the tables shown on the builds and buildsets pages with new
    PF4 components. While the data is loading, a fetching animation is shown
    and in case no results could be found the page shows an empty state.
    
    Each table row is clickable and points to the build/buildset result page.
    The table uses the same hover effect as the build list on the buildset
    result page.
    
    This change also updates the used @patternfly/react-core version to
    match the current version of @patternfly/react-table [1]. Otherwise,
    some of the styles in the different @patternfly/react-core versions
    conflict with each other leading to wrong font-weights and wrong text
    colors in labels.
    
    [1] patternfly.org/v4/documentation/react/overview/release-notes#202010-release-notes-2020-08-17
    
    Change-Id: I0f5e0815c53d18e8ae3570dc94594c77fecb90ce

---
+++ web/src/containers/build/BuildsetTable.jsx
@@ -0,0 +1,201 @@
+// Copyright 2020 BMW Group
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import {
+  Button,
+  EmptyState,
+  EmptyStateBody,
+  EmptyStateIcon,
+  EmptyStateSecondaryActions,
+  Spinner,
+  Title,
+} from '@patternfly/react-core'
+import {
+  BuildIcon,
+  CodeBranchIcon,
+  CodeIcon,
+  CubeIcon,
+  PollIcon,
+  StreamIcon,
+} from '@patternfly/react-icons'
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableVariant,
+} from '@patternfly/react-table'
+
+import { BuildResult, BuildResultWithIcon, IconProperty } from './Misc'
+import { ExternalLink } from '../../Misc'
+
+function BuildsetTable(props) {
+  const { buildsets, fetching, onClearFilters, tenant } = props
+  const columns = [
+    {
+      title: <IconProperty icon={<CubeIcon />} value="Project" />,
+    },
+    {
+      title: <IconProperty icon={<CodeBranchIcon />} value="Branch" />,
+    },
+    {
+      title: <IconProperty icon={<StreamIcon />} value="Pipeline" />,
+    },
+    {
+      title: <IconProperty icon={<CodeIcon />} value="Change" />,
+    },
+    {
+      title: <IconProperty icon={<PollIcon />} value="Result" />,
+    },
+  ]
+
+  function createBuildsetRow(buildset) {
+    // This link will be defined on each cell of the current row as this is the
+    // only way to define a valid HTML link on a table row. Although we could
+    // simply define an onClick handler on the whole row and programatically
+    // switch to the buildresult page, this wouldn't provide the same
+    // look-and-feel as a plain HTML link.
+    const buildsetResultLink = (
+      <Link
+        to={`${tenant.linkPrefix}/buildset/${buildset.uuid}`}
+        className="zuul-stretched-link"
+      />
+    )
+    return {
+      cells: [
+        {
+          // To allow passing anything else than simple string values to a table
+          // cell, we must use the title attribute.
+          title: (
+            <>
+              {buildsetResultLink}
+              <BuildResultWithIcon result={buildset.result}>
+                {buildset.project}
+              </BuildResultWithIcon>
+            </>
+          ),
+        },
+        {
+          title: (
+            <>
+              {buildsetResultLink}
+              <span>{buildset.branch ? buildset.branch : buildset.ref}</span>
+            </>
+          ),
+        },
+        {
+          title: (
+            <>
+              {buildsetResultLink}
+              <span>{buildset.pipeline}</span>
+            </>
+          ),
+        },
+        {
+          title: (
+            <>
+              {buildsetResultLink}
+              {buildset.change && (
+                <span style={{ zIndex: 1, position: 'relative' }}>
+                  <ExternalLink target={buildset.ref_url}>
+                    {buildset.change},{buildset.patchset}
+                  </ExternalLink>
+                </span>
+              )}
+            </>
+          ),
+        },
+        {
+          title: (
+            <>
+              {buildsetResultLink}
+              <BuildResult result={buildset.result} />
+            </>
+          ),
+        },
+      ],
+    }
+  }
+
+  function createFetchingRow() {
+    const rows = [
+      {
+        heightAuto: true,
+        cells: [
+          {
+            props: { colSpan: 8 },
+            title: (
+              <center>
+                <Spinner size="xl" />
+              </center>
+            ),
+          },
+        ],
+      },
+    ]
+    return rows
+  }
+
+  let rows = []
+  if (fetching) {
+    rows = createFetchingRow()
+  } else {
+    rows = buildsets.map((buildset) => createBuildsetRow(buildset))
+  }
+
+  return (
+    <>
+      <Table
+        aria-label="Builds Table"
+        variant={TableVariant.compact}
+        cells={columns}
+        rows={rows}
+        className="zuul-build-table"
+      >
+        <TableHeader />
+        <TableBody />
+      </Table>
+
+      {/* Show an empty state in case we don't have any buildsets but are also
+          not fetching */}
+      {!fetching && buildsets.length === 0 && (
+        <EmptyState>
+          <EmptyStateIcon icon={BuildIcon} />
+          <Title headingLevel="h1">No buildsets found</Title>
+          <EmptyStateBody>
+            No buildsets match this filter criteria. Remove some filters or
+            clear all to show results.
+          </EmptyStateBody>
+          <EmptyStateSecondaryActions>
+            <Button variant="link" onClick={onClearFilters}>
+              Clear all filters
+            </Button>
+          </EmptyStateSecondaryActions>
+        </EmptyState>
+      )}
+    </>
+  )
+}
+
+BuildsetTable.propTypes = {
+  buildsets: PropTypes.array.isRequired,
+  fetching: PropTypes.bool.isRequired,
+  onClearFilters: PropTypes.func.isRequired,
+  tenant: PropTypes.object.isRequired,
+}
+
+export default connect((state) => ({ tenant: state.tenant }))(BuildsetTable)
