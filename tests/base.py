bd9fe5718e008fdde93a56b842cf3bf40c118a4e
Jan Kubovy <jan.kubovy@bmw.de>
2020-08-27 10:28:26 +0200

    Scheduler's pause/resume functionality
    In multi-scheduler operation a pause/resume functionality is needed.
    
    This implements both, pause/resume, public methods and commands.
    
    Change-Id: I5cbaece476cf62bfd76bf8772ebac64e977ffb9a
    Story: 2007192

--- tests/base.py
+++ tests/base.py
@@ -3653,7 +3653,8 @@ class SchedulerTestApp:
 
         self.sched.start()
         executor_client.gearman.waitForServer()
-        self.sched.reconfigure(self.config)
+        if self.sched.is_resumed:
+            self.sched.reconfigure(self.config)
         self.sched.wakeUp()
 
     def fullReconfigure(self):
@@ -4570,8 +4571,9 @@ class ZuulTestCase(BaseTestCase):
 
     def __eventQueuesJoin(self, matcher) -> None:
         for app in self.scheds.filter(matcher):
-            for event_queue in app.event_queues:
-                event_queue.join()
+            if app.sched.is_resumed:
+                for event_queue in app.event_queues:
+                    event_queue.join()
         for event_queue in self.additional_event_queues:
             event_queue.join()
 
--- tests/unit/test_scheduler.py
+++ tests/unit/test_scheduler.py
@@ -8367,6 +8367,137 @@ class TestSchedulerSmartReconfiguration(ZuulTestCase):
         self._test_smart_reconfiguration(command_socket=True)
 
 
+class TestPausedScheduler(ZuulTestCase):
+    tenant_config_file = 'config/single-tenant/main.yaml'
+    wait_timeout = 2
+
+    def setup_config(self, config_file: str):
+        config = super(TestPausedScheduler, self).setup_config(config_file)
+        config.set('scheduler', 'paused_on_start', 'true')
+        return config
+
+    def test_resume_on_start(self):
+        """Test resume scheduler paused on start"""
+
+        # Given
+        self.create_branch('org/project', 'stable')
+        self.fake_gerrit.addEvent(
+            self.fake_gerrit.getFakeBranchCreatedEvent(
+                'org/project', 'stable'))
+        self.assertRaises(Exception, self.waitUntilSettled, "paused")
+
+        A = self.fake_gerrit.addFakeChange('org/project', 'stable', 'A')
+        A.addApproval('Code-Review', 2)
+        self.fake_gerrit.addEvent(A.addApproval('Approved', 1))
+
+        # When: Resuming scheduler
+        self.wait_timeout = 90  # reset original value
+        self.scheds.first.sched.resume()
+        self.waitUntilSettled("resumed")
+
+        # Then: Work will resume normally
+        self.assertEqual(self.getJobFromHistory('project-test1').result,
+                         'SUCCESS')
+        self.assertEqual(self.getJobFromHistory('project-test2').result,
+                         'SUCCESS')
+        self.assertEqual(A.data['status'], 'MERGED')
+        self.assertEqual(A.reported, 2, "A should report start and success")
+        self.assertIn('gate', A.messages[1], "A should transit gate")
+        self.assertEqual(self.getJobFromHistory('project-test1').node,
+                         'label2')
+
+
+class TestPauseResumedScheduler(ZuulTestCase):
+    tenant_config_file = 'config/single-tenant/main.yaml'
+
+    def test_pause_resume(self):
+        """Test pause/resume"""
+
+        # Given: Paused scenario
+        self.create_branch('org/project', 'stable')
+        self.fake_gerrit.addEvent(
+            self.fake_gerrit.getFakeBranchCreatedEvent(
+                'org/project', 'stable'))
+        self.waitUntilSettled()
+        self.wait_timeout = 2  # just not to wait too long
+
+        # When: Pause scheduler and add some changes
+        self.scheds.first.sched.pause()
+        while not self.scheds.first.sched.is_paused:
+            time.sleep(0.1)
+
+        A = self.fake_gerrit.addFakeChange('org/project', 'stable', 'A')
+        A.addApproval('Code-Review', 2)
+        self.fake_gerrit.addEvent(A.addApproval('Approved', 1))
+
+        # Then: Nothing should happen, i.e. timeout
+        self.assertRaises(Exception, self.waitUntilSettled, "paused")
+
+        # Given: Resumed scenario
+        self.wait_timeout = 90  # reset original value
+
+        # When: Resuming scheduler
+        self.scheds.first.sched.resume()
+        self.waitUntilSettled("resumed")
+
+        # Then: Work will resume normally
+        self.assertEqual(self.getJobFromHistory('project-test1').result,
+                         'SUCCESS')
+        self.assertEqual(self.getJobFromHistory('project-test2').result,
+                         'SUCCESS')
+        self.assertEqual(A.data['status'], 'MERGED')
+        self.assertEqual(A.reported, 2, "A should report start and success")
+        self.assertIn('gate', A.messages[1], "A should transit gate")
+        self.assertEqual(self.getJobFromHistory('project-test1').node,
+                         'label2')
+
+    def test_pause_resume_using_commands(self):
+        """Test pause/resume using commands"""
+
+        # Given: Paused scenario
+        self.create_branch('org/project', 'stable')
+        self.fake_gerrit.addEvent(
+            self.fake_gerrit.getFakeBranchCreatedEvent(
+                'org/project', 'stable'))
+        self.waitUntilSettled()
+        self.wait_timeout = 2  # just not to wait too long
+        command_socket = self.config.get('scheduler', 'command_socket')
+
+        # When: Pause scheduler and add some changes
+        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
+            s.connect(command_socket)
+            s.sendall('pause\n'.encode('utf8'))
+        while not self.scheds.first.sched.is_paused:
+            time.sleep(0.1)
+
+        A = self.fake_gerrit.addFakeChange('org/project', 'stable', 'A')
+        A.addApproval('Code-Review', 2)
+        self.fake_gerrit.addEvent(A.addApproval('Approved', 1))
+
+        # Then: Nothing should happen, i.e. timeout
+        self.assertRaises(Exception, self.waitUntilSettled, "paused")
+
+        # Given: Resumed scenario
+        self.wait_timeout = 90  # reset original value
+
+        # When: Resuming scheduler
+        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
+            s.connect(command_socket)
+            s.sendall('resume\n'.encode('utf8'))
+        self.waitUntilSettled("resumed")
+
+        # Then: Work will resume normally
+        self.assertEqual(self.getJobFromHistory('project-test1').result,
+                         'SUCCESS')
+        self.assertEqual(self.getJobFromHistory('project-test2').result,
+                         'SUCCESS')
+        self.assertEqual(A.data['status'], 'MERGED')
+        self.assertEqual(A.reported, 2, "A should report start and success")
+        self.assertIn('gate', A.messages[1], "A should transit gate")
+        self.assertEqual(self.getJobFromHistory('project-test1').node,
+                         'label2')
+
+
 class TestReconfigureBranch(ZuulTestCase):
 
     def _setupTenantReconfigureTime(self):
