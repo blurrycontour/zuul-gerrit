20ff98ddc34d51596b6ddf55135ae687adedf492
Jan Kubovy <jan.kubovy@bmw.de>
2020-08-27 10:28:26 +0200

    Scheduler's pause/resume functionality
    In multi-scheduler operation a pause/resume functionality is needed.
    
    This implements both, pause/resume, public methods and commands.
    
    Change-Id: I5cbaece476cf62bfd76bf8772ebac64e977ffb9a
    Story: 2007192

--- tests/base.py
+++ tests/base.py
@@ -3641,7 +3641,8 @@ class SchedulerTestApp:
 
         self.sched.start()
         executor_client.gearman.waitForServer()
-        self.sched.reconfigure(self.config)
+        if self.sched.is_resumed:
+            self.sched.reconfigure(self.config)
         self.sched.wakeUp()
 
     def fullReconfigure(self):
@@ -4558,8 +4559,9 @@ class ZuulTestCase(BaseTestCase):
 
     def __eventQueuesJoin(self, matcher) -> None:
         for app in self.scheds.filter(matcher):
-            for event_queue in app.event_queues:
-                event_queue.join()
+            if app.sched.is_resumed:
+                for event_queue in app.event_queues:
+                    event_queue.join()
         for event_queue in self.additional_event_queues:
             event_queue.join()
 
