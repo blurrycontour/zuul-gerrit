9cad37063978d44d491ff0813d29091cd18f0d81
Ian Wienand <iwienand@redhat.com>
2020-09-11 11:07:27 +1000

    web: PF4 minor rework of log viewer page
    I think the log-viewer page could do with some PF4-ness.
    
    This moves the title section into a card from a bootstrap panel, and
    puts the filename you're looking at in the prominent location and the
    build-id in a less prominent location.
    
    The filters are moved from jammed-together links into a button
    toggle-group, which is a mutually-exclusive set perfect for this
    interface.  This component requires the latest release of PF4, which
    is why the packages have been updated.
    
    The filtering is slightly reworked so that clicking on a button sets
    the "severity" state variable, instead of the Link model setting props
    before.
    
    Because the filters are, I think, a bit of an OpenStacky thing
    (devstack type logs) and generally not constantly used, I put them in
    a drop-down box so they don't take up space when not relevant.
    
    Wrap the output in a PgaeSection that spaces things out a bit more
    nicely.
    
    Change-Id: Ibcbc2bd9497f1d8b75acd9e4979a289173d014b2

--- web/src/containers/logfile/LogFile.jsx
+++ web/src/containers/logfile/LogFile.jsx
@@ -15,9 +15,12 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Panel } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-
+import { Badge } from '@patternfly/react-core'
+import { Card, CardHeader, CardBody } from '@patternfly/react-core'
+import { Divider } from '@patternfly/react-core'
+import { ExpandableSection } from '@patternfly/react-core'
+import { PageSection } from '@patternfly/react-core'
+import { ToggleGroup, ToggleGroupItem } from '@patternfly/react-core'
 
 function updateSelection (event) {
   const lines = window.location.hash.substring(1).split('-').map(Number)
@@ -46,50 +49,112 @@ class LogFile extends React.Component {
     item: PropTypes.object,
     tenant: PropTypes.object,
     data: PropTypes.array,
-    severity: PropTypes.string
   }
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            redirect: null,
+            severity: '',
+            isExpanded: false
+        }
+        this.handleItemClick = (isSelected, event) => {
+            let id = event.currentTarget.id
+            if (id === 'all') {
+                id = ''
+            }
+            this.setState({ severity: id })
+        }
+        this.onToggle = (isExpanded) => {
+            this.setState({
+                isExpanded
+            })
+        }
+    }
+
   render () {
-    const { build, data, severity } = this.props
+    const { build, data } = this.props
+    const { severity, isExpanded } = this.state
+    const fileName = window.location.pathname.substr(window.location.pathname.lastIndexOf('/') + 1)
     return (
-      <React.Fragment>
-        <Panel>
-          <Panel.Heading>Build result {build.uuid}</Panel.Heading>
-          <Panel.Body>
-            <Link to="?">All</Link>&nbsp;
-            <Link to="?severity=1">Debug</Link>&nbsp;
-            <Link to="?severity=2">Info</Link>&nbsp;
-            <Link to="?severity=3">Warning</Link>&nbsp;
-            <Link to="?severity=4">Error</Link>&nbsp;
-            <Link to="?severity=5">Trace</Link>&nbsp;
-            <Link to="?severity=6">Audit</Link>&nbsp;
-            <Link to="?severity=7">Critical</Link>&nbsp;
-          </Panel.Body>
-        </Panel>
-        <pre className="zuul-log-output">
-          <table>
-            <tbody>
-              {data.map((line) => (
-                ((!severity || (line.severity >= severity)) &&
-                 <tr key={line.index} className={'ln-' + line.index}>
-                   <td className="line-number" onClick={updateSelection}>
-                     {line.index}
-                   </td>
-                   <td>
-                      <span
-                        className={`log-message zuul-log-sev-${
-                          line.severity || 0
-                        }`}
-                      >
-                       {line.text+'\n'}
-                     </span>
-                   </td>
-                 </tr>
-                )))}
-            </tbody>
-          </table>
-        </pre>
-      </React.Fragment>
+        <React.Fragment>
+            <Card>
+                <CardHeader>
+                    {fileName}
+                </CardHeader>
+                <CardBody>
+                    <Badge isRead>Build {build.uuid}</Badge>
+                </CardBody>
+            </Card>
+            <ExpandableSection
+                toggleText={isExpanded ? 'Hide Filters' : 'Show Filters'}
+                onToggle={this.onToggle} isExpanded={isExpanded}>
+                <ToggleGroup aria-label='Log line severity filter'>
+                    <ToggleGroupItem
+                        buttonId='all' isSelected={severity === ''}
+                        onChange={this.handleItemClick}>
+                        All
+                    </ToggleGroupItem>
+                    <ToggleGroupItem
+                        buttonId='1' isSelected={severity === '1'}
+                        onChange={this.handleItemClick}>
+                        Debug
+                    </ToggleGroupItem>
+                    <ToggleGroupItem
+                        buttonId='2' isSelected={severity === '2'}
+                        onChange={this.handleItemClick}>
+                        Info
+                    </ToggleGroupItem>
+                    <ToggleGroupItem
+                        buttonId='3' isSelected={severity === '3'}
+                        onChange={this.handleItemClick}>
+                        Warning
+                    </ToggleGroupItem>
+                    <ToggleGroupItem
+                        buttonId='4' isSelected={severity === '4'}
+                        onChange={this.handleItemClick}>
+                        Error
+                    </ToggleGroupItem>
+                    <ToggleGroupItem
+                        buttonId='5' isSelected={severity === '5'}
+                        onChange={this.handleItemClick}>
+                        Trace
+                    </ToggleGroupItem>
+                    <ToggleGroupItem
+                        buttonId='6' isSelected={severity === '6'}
+                        onChange={this.handleItemClick}>
+                        Audit
+                    </ToggleGroupItem>
+                    <ToggleGroupItem
+                        buttonId='7' isSelected={severity === '7'}
+                        onChange={this.handleItemClick}>
+                        Critical
+                    </ToggleGroupItem>
+                </ToggleGroup>
+            </ExpandableSection>
+            <Divider/>
+            <PageSection>
+                <pre className="zuul-log-output">
+                    <table>
+                        <tbody>
+                            {data.map((line) => (
+                                ((!severity || (line.severity >= severity)) &&
+                                 <tr key={line.index} className={'ln-' + line.index}>
+                                     <td className="line-number" onClick={updateSelection}>
+                                         {line.index}
+                                     </td>
+                                     <td>
+                                         <span className={`log-message zuul-log-sev-${line.severity || 0}`}>
+                                             {line.text+'\n'}
+                                         </span>
+                                     </td>
+                                 </tr>
+                                )))}
+                        </tbody>
+                    </table>
+                </pre>
+            </PageSection>
+        </React.Fragment>
     )
   }
 }
