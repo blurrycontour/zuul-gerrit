8d6dfb6bc611d9666205a16b66c46242de3b2680
Tobias Henkel <tobias.henkel@bmw.de>
2020-09-15 16:17:01 +0200

    Remove an unneeded api call when creating check_runs
    During the start reporting the check run is being created in status
    in_progress. This currently does two api requests due to the
    github3.py object model. The first one is to get the repository
    object, the second one to actually create the check run. However we
    already have all information available that is required to directly
    craft a post request to create that check run.
    
    This is a problem because this start reporting runs within the main
    loop of zuul and needs to be as fast as possible. In a very busy zuul
    api requests are becoming a bottleneck so we need to remove any
    request that is unneeded.
    
    Change-Id: Idd166c61a4d0081c284dee5677b4cc2e48932307

--- tests/fakegithub.py
+++ tests/fakegithub.py
@@ -17,6 +17,8 @@ import urllib
 from collections import defaultdict
 
 import datetime
+import random
+
 import github3.exceptions
 import re
 import time
@@ -86,11 +88,12 @@ class FakeStatus(object):
 
 
 class FakeCheckRun(object):
-    def __init__(self, name, details_url, output, status, conclusion,
+    def __init__(self, id, name, details_url, output, status, conclusion,
                  completed_at, external_id, actions, app):
         if actions is None:
             actions = []
 
+        self.id = id
         self.name = name
         self.details_url = details_url
         self.output = output
@@ -109,6 +112,7 @@ class FakeCheckRun(object):
 
     def as_dict(self):
         return {
+            'id': self.id,
             "name": self.name,
             "status": self.status,
             "output": self.output,
@@ -164,9 +168,10 @@ class FakeCommit(object):
         # the last status provided for a commit.
         self._statuses.insert(0, status)
 
-    def set_check_run(self, name, details_url, output, status, conclusion,
+    def set_check_run(self, id, name, details_url, output, status, conclusion,
                       completed_at, external_id, actions, app):
         check_run = FakeCheckRun(
+            id,
             name,
             details_url,
             output,
@@ -180,6 +185,7 @@ class FakeCommit(object):
         # Always insert a check_run to the front of the list to represent the
         # last check_run provided for a commit.
         self._check_runs.insert(0, check_run)
+        return check_run
 
     def get_url(self, path, params=None):
         if path == 'statuses':
@@ -302,6 +308,7 @@ class FakeRepository(object):
             commit = FakeCommit(head_sha)
             self._commits[head_sha] = commit
         commit.set_check_run(
+            random.randint(0, 32000),
             name,
             details_url,
             output,
@@ -584,8 +591,11 @@ class FakeResponse(object):
 
     def raise_for_status(self):
         if 400 <= self.status_code < 600:
-            raise HTTPError('{} Something went wrong'.format(self.status_code),
-                            response=self)
+            if isinstance(self.data, str):
+                text = '{} {}'.format(self.status_code, self.data)
+            else:
+                text = '{} Something went wrong'.format(self.status_code)
+            raise HTTPError(text, response=self)
 
 
 class FakeGithubSession(object):
@@ -651,6 +661,38 @@ class FakeGithubSession(object):
                 'expires_at': expiry,
             }
             return FakeResponse(data, 201)
+
+        # Handle check run creation
+        match = re.match(r'.*/repos/(.*)/check-runs$', url)
+        if match:
+            org, reponame = match.groups()[0].split('/', 1)
+            repo = self.client._data.repos.get((org, reponame))
+
+            if repo._permissions.get("checks") is False:
+                # To create a proper github3 exception, we need to mock a
+                # response object
+                return FakeResponse(
+                    "Resource not accessible by integration", 403)
+
+            head_sha = json.get('head_sha')
+            commit = repo._commits.get(head_sha, None)
+            if commit is None:
+                commit = FakeCommit(head_sha)
+                repo._commits[head_sha] = commit
+            check_run = commit.set_check_run(
+                random.randint(0, 32000),
+                json['name'],
+                json['details_url'],
+                json['output'],
+                json.get('status'),
+                json.get('conclusion'),
+                json.get('completed_at'),
+                json['external_id'],
+                json['actions'],
+                json.get('app', 'zuul'),
+            )
+
+            return FakeResponse(check_run.as_dict(), 201)
 
         return FakeResponse(None, 404)
 
