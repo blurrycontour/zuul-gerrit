418bf6b462db1286e856cb562c9c8ffcd118d080
Tobias Henkel <tobias.henkel@bmw.de>
2020-09-15 16:17:01 +0200

    Remove an unneeded api call when creating check_runs
    During the start reporting the check run is being created in status
    in_progress. This currently does two api requests due to the
    github3.py object model. The first one is to get the repository
    object, the second one to actually create the check run. However we
    already have all information available that is required to directly
    craft a post request to create that check run.
    
    This is a problem because this start reporting runs within the main
    loop of zuul and needs to be as fast as possible. In a very busy zuul
    api requests are becoming a bottleneck so we need to remove any
    request that is unneeded.
    
    Change-Id: Idd166c61a4d0081c284dee5677b4cc2e48932307

--- zuul/driver/github/githubconnection.py
+++ zuul/driver/github/githubconnection.py
@@ -1993,8 +1993,8 @@ class GithubConnection(BaseConnection):
                     zuul_event_id=None):
         log = get_annotated_logger(self.log, zuul_event_id)
         github = self.getGithubClient(project, zuul_event_id=zuul_event_id)
+        self._append_accept_header(github, PREVIEW_CHECKS_ACCEPT)
         owner, proj = project.split("/")
-        repository = github.repository(owner, proj)
 
         # Always provide an empty list of actions by default
         actions = []
@@ -2045,6 +2045,8 @@ class GithubConnection(BaseConnection):
             # change (e.g. the check_run creation failed), there should already
             # be a check_run available. If not we will create one.
             check_runs = []
+
+            repository = github.repository(owner, proj)
             try:
                 check_runs = [
                     c for c in repository.commit(sha).check_runs()
@@ -2132,7 +2134,7 @@ class GithubConnection(BaseConnection):
 
             # Report the start of a check run
             try:
-                check_run = repository.create_check_run(
+                data = dict(
                     name=context,
                     head_sha=sha,
                     status=status,
@@ -2141,8 +2143,13 @@ class GithubConnection(BaseConnection):
                     external_id=external_id,
                     actions=actions,
                 )
-            except github3.exceptions.GitHubException as exc:
-                # TODO (felix): Should we retry the check run creation?
+
+                url = github.session.build_url('repos', project,
+                                               'check-runs')
+                resp = github.session.post(url, json=data)
+                resp.raise_for_status()
+
+            except requests.exceptions.HTTPError as exc:
                 log.error(
                     "Failed to create check run %s for %s#%s on sha %s: %s",
                     context, project, pr_number, sha, str(exc),
