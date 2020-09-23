d617335c8a3942fe12ea2e8b6564dedda7960909
Jan Kubovy <jan.kubovy@bmw.de>
2020-09-23 11:11:03 +0200

    Improve typings in context of 744416
    To improve navigation in the code in context
    of change: https://review.opendev.org/c/744416/
    and not to pollute that change with non-functional
    changes.
    
    Especially "usage search" or "execution path"
    benefits from this change. Especially, commonly
    used methods and properties, e.g., "start()",
    "stop()", "run()", "job", will be matched only
    to relevant object if typings are used. Parameters
    using common names and properties, e.g., "build",
    "job", get clear type. But also elsewhere, when
    same names are used.
    
    Since Python 3.6 is now the minimum requirement
    PEP 526 is used here (https://www.python.org/dev/peps/pep-0526/)
    
    This change does only adds typings relevant to above
    change, contains improvements from past
    https://review.opendev.org/c/715247 change and
    attempts iteratively improve the code.
    
    Change-Id: Iac23946dc61496b7199aca1719a23069c4f4e5de

--- zuul/driver/gerrit/gerritconnection.py
+++ zuul/driver/gerrit/gerritconnection.py
@@ -957,7 +957,7 @@ class GerritConnection(BaseConnection):
         return False
 
     def getRefSha(self, project: Project, ref: str) -> str:
-        refs = {}  # type: Dict[str, str]
+        refs: Dict[str, str] = {}
         try:
             refs = self.getInfoRefs(project)
         except Exception:
@@ -986,7 +986,7 @@ class GerritConnection(BaseConnection):
         self.log.debug("Running query %s to get project open changes" %
                        (query,))
         data = self.simpleQuery(query)
-        changes = []  # type: List[GerritChange]
+        changes: List[GerritChange] = []
         for record in data:
             try:
                 changes.append(
