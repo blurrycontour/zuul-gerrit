69aff3f7aa95ac6ceeec432033480f70617b60e4
Jan Kubovy <jan.kubovy@bmw.de>
2020-09-25 13:59:25 +0200

    Merge Zookeeper connection methods
    Zookeeper connection is done using the "connect_zookeeper"
    helper function and using "ZooKeeper#connect" method.
    
    This change merges both into one to consolidate usage.
    
    Additionally a "ZookeeperConnection" is added to allow
    save disconnecting using a "with" block.
    
    Last, in test, connection counter is checking if all
    Zookeeper connection were closed.
    
    Change-Id: I5fa4ddab5f85c658291f1262ee0392a60086846e

--- zuul/web/__init__.py
+++ zuul/web/__init__.py
@@ -42,6 +42,7 @@ from zuul.zk.nodepool import ZooKeeperNodepool
 if TYPE_CHECKING:
     from zuul.lib.connections import ConnectionRegistry
 
+from zuul.zk import ZooKeeperConnection
 
 STATIC_DIR = os.path.join(os.path.dirname(__file__), 'static')
 cherrypy.tools.websocket = WebSocketTool()
@@ -1209,6 +1210,7 @@ class StreamManager(object):
 
 class ZuulWeb(object):
     log = logging.getLogger("zuul.web.ZuulWeb")
+    _zk_connection_class = ZooKeeperConnection
 
     def __init__(self, listen_address: str, listen_port: int,
                  gear_server: str, gear_port: int,
@@ -1234,10 +1236,10 @@ class ZuulWeb(object):
         self.rpc: RPCClient = RPCClient(gear_server, gear_port,
                                         ssl_key, ssl_cert, ssl_ca,
                                         client_id='Zuul Web Server')
-        self.zk_client: ZooKeeperClient = ZooKeeperClient()
-        self.zk_client.connect(hosts=zk_hosts, read_only=True,
-                               timeout=zk_timeout, tls_cert=zk_tls_cert,
-                               tls_key=zk_tls_key, tls_ca=zk_tls_ca)
+        self.zk_client: ZooKeeperClient = self._zk_connection_class(
+            hosts=zk_hosts, read_only=True, timeout=zk_timeout,
+            tls_cert=zk_tls_cert, tls_key=zk_tls_key, tls_ca=zk_tls_ca
+        ).connect()
 
         self.connections: ConnectionRegistry = connections
         self.authenticators: AuthenticatorRegistry = authenticators
