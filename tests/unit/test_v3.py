2677a239028bc33133774eb0c60dca488a6b049b
Jan Kubovy <jan.kubovy@bmw.de>
2020-09-28 08:22:46 +0200

    Recover from cat job failures during startup
    In case zuul cat job fails during startup
    scheduler raises an exception and fails to start.
    
    This change reports the failure using "loading_errors"
    and prevents scheduler to fail completely.
    
    Change-Id: I2ec45bf109ba5b6fdd6b22e7046afe332c39d10d

--- tests/unit/test_v3.py
+++ tests/unit/test_v3.py
@@ -3248,20 +3248,25 @@ class TestBrokenConfig(ZuulTestCase):
     # Test we can deal with a broken config
 
     tenant_config_file = 'config/broken/main.yaml'
+    fail_cat_job = {"org/project4": True}
 
     def test_broken_config_on_startup(self):
         # verify get the errors at tenant level.
         tenant = self.scheds.first.sched.abide.tenants.get('tenant-one')
         loading_errors = tenant.layout.loading_errors
         self.assertEquals(
-            len(tenant.layout.loading_errors), 2,
+            len(tenant.layout.loading_errors), 3,
             "An error should have been stored")
         self.assertIn(
             "Zuul encountered an error while accessing the repo org/project3",
             str(loading_errors[0].error))
         self.assertIn(
-            "Zuul encountered a syntax error",
+            "Zuul encountered a failure of \"merger:cat\" job in\n"
+            "review.example.com/org/project4@master repository.",
             str(loading_errors[1].error))
+        self.assertIn(
+            "Zuul encountered a syntax error",
+            str(loading_errors[2].error))
 
     @simple_layout('layouts/broken-template.yaml')
     def test_broken_config_on_startup_template(self):
@@ -3291,7 +3296,7 @@ class TestBrokenConfig(ZuulTestCase):
         tenant = self.scheds.first.sched.abide.tenants.get('tenant-one')
         # There is a configuration error
         self.assertEquals(
-            len(tenant.layout.loading_errors), 2,
+            len(tenant.layout.loading_errors), 3,
             "An error should have been stored")
 
         # Inside a broken tenant configuration environment,
@@ -3323,7 +3328,7 @@ class TestBrokenConfig(ZuulTestCase):
         tenant = self.scheds.first.sched.abide.tenants.get('tenant-one')
         # There is a configuration error
         self.assertEquals(
-            len(tenant.layout.loading_errors), 2,
+            len(tenant.layout.loading_errors), 3,
             "An error should have been stored")
 
         # Inside a broken tenant configuration environment,
@@ -3357,7 +3362,7 @@ class TestBrokenConfig(ZuulTestCase):
         tenant = self.scheds.first.sched.abide.tenants.get('tenant-one')
         # There is a configuration error
         self.assertEquals(
-            len(tenant.layout.loading_errors), 2,
+            len(tenant.layout.loading_errors), 3,
             "An error should have been stored")
 
         # Inside a broken tenant configuration environment,
@@ -3391,7 +3396,7 @@ class TestBrokenConfig(ZuulTestCase):
         tenant = self.scheds.first.sched.abide.tenants.get('tenant-one')
         # There is a configuration error
         self.assertEquals(
-            len(tenant.layout.loading_errors), 2,
+            len(tenant.layout.loading_errors), 3,
             "An error should have been stored")
 
         # Inside a broken tenant configuration environment,
@@ -3423,7 +3428,7 @@ class TestBrokenConfig(ZuulTestCase):
         tenant = self.scheds.first.sched.abide.tenants.get('tenant-one')
         # There is a configuration error
         self.assertEquals(
-            len(tenant.layout.loading_errors), 2,
+            len(tenant.layout.loading_errors), 3,
             "An error should have been stored")
 
         # Inside a broken tenant configuration environment, remove a
--- zuul/configloader.py
+++ zuul/configloader.py
@@ -54,6 +54,19 @@ class ConfigurationSyntaxError(Exception):
     pass
 
 
+class ConfigFetchError(Exception):
+    def __init__(self, job):
+        message = textwrap.dedent("""\
+        Zuul encountered a failure of "{name}" job in
+        {repo}@{branch} repository.""")
+        message = textwrap.fill(message.format(
+            name=job.name,
+            repo=job.source_context.project.canonical_name,
+            branch=job.source_context.branch,
+        ))
+        super().__init__(message)
+
+
 class NodeFromGroupNotFoundError(Exception):
     def __init__(self, nodeset, node, group):
         message = textwrap.dedent("""\
@@ -1807,7 +1820,11 @@ class TenantParser(object):
             self.log.debug("Waiting for cat job %s" % (job,))
             job.wait(self.merger.git_timeout)
             if not job.updated:
-                raise Exception("Cat job %s failed" % (job,))
+                source_context = job.source_context.copy()
+                e = ConfigFetchError(job)
+                loading_errors.addError(source_context, None, e)
+                self.log.error("Cat job %s failed", job)
+                continue
             self.log.debug("Cat job %s got files %s" %
                            (job, job.files.keys()))
             loaded = False
