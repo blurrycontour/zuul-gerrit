3df546fb896181f7ecf4e8d48d3dfb3baf4a21d6
Andy Ladjadj <andy.ladjadj@adevinta.com>
2020-10-03 14:57:55 +0200

    [web][config] move timezone component to preferences
     - move timezone in modal
     - move state in preference
     - use patterfly component
     - update all pages
    
    Change-Id: I60bc1665c0660b86489e375c6720ad18ed940c53

--- web/src/containers/build/BuildTable.jsx
+++ web/src/containers/build/BuildTable.jsx
@@ -50,8 +50,8 @@ function BuildTable({
   builds,
   fetching,
   onClearFilters,
+  preferences,
   tenant,
-  timezone,
   history,
 }) {
   const columns = [
@@ -135,7 +135,7 @@ function BuildTable({
         {
           title: moment
             .utc(build.start_time)
-            .tz(timezone)
+            .tz(preferences.timezone)
             .format('YYYY-MM-DD HH:mm:ss'),
         },
         {
@@ -237,12 +237,12 @@ BuildTable.propTypes = {
   builds: PropTypes.array.isRequired,
   fetching: PropTypes.bool.isRequired,
   onClearFilters: PropTypes.func.isRequired,
+  preferences: PropTypes.string.isRequired,
   tenant: PropTypes.object.isRequired,
-  timezone: PropTypes.string.isRequired,
   history: PropTypes.object.isRequired,
 }
 
 export default connect((state) => ({
+  preferences: state.preferences,
   tenant: state.tenant,
-  timezone: state.timezone,
 }))(BuildTable)
