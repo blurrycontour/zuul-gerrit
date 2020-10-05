fe60d8c5bb2f448f71fd3025a41d21a63e2757e2
Ian Wienand <iwienand@redhat.com>
2020-10-05 12:52:16 +1100

    Run jobs automatically on playbook changes
    This adds all playbooks for a job, explicit and inherited, to a list
    that is checked in the files matcher.  This means that jobs that have
    restriced their run to specific files with "files:" will still run
    when the playbooks they use are modified.
    
    This is similar in spirit to 7fba932e5dfbabe80b9a1faadf0b0927f7ad01ec
    which runs jobs when their config changes.  While you can of course
    add playbook files explicitly, you can see plenty of examples where it
    is forgotten.  You also need to manually follow the inheritance chain
    and parent playbooks, which is error-prone and difficult to maintain.
    
    Change-Id: I6f0715b60bfc80e465ba87a73d278abe746747d9

--- zuul/model.py
+++ zuul/model.py
@@ -1230,6 +1230,8 @@ class Job(ConfigObject):
             protected_origin=None,
             secrets=(),  # secrets aren't inheritable
             queued=False,
+            playbook_files=(),
+            playbook_matcher=None,
         )
 
         self.attributes = {}
@@ -1676,6 +1678,22 @@ class Job(ConfigObject):
         elif other._get('allowed_projects') is not None:
             self.allowed_projects = other.allowed_projects
 
+        # This matcher covers all playbook files for the job, and
+        # those inherited from the same project.  This ensures jobs
+        # with file-filters run if their dependent playbooks change,
+        # without having to list all the playbooks explicitly as
+        # separate file-filters in the job definition.
+        if self.source_context.project.canonical_name == \
+           other.source_context.project.canonical_name:
+            if other._get('playbook_files'):
+                self.playbook_files = self.playbook_files + \
+                    other._get('playbook_files')
+        playbook_matchers = []
+        for playbook_fn in self.playbook_files:
+            playbook_matchers.append(
+                change_matcher.FileMatcher(playbook_fn))
+        self.playbook_matcher = change_matcher.MatchAnyFiles(playbook_matchers)
+
         for k in self.context_attributes:
             if (other._get(k) is not None and
                 k not in set(['tags', 'requires', 'provides'])):
@@ -1703,6 +1721,9 @@ class Job(ConfigObject):
         return True
 
     def changeMatchesFiles(self, change):
+        if self.playbook_matcher and self.playbook_matcher.matches(change):
+            return True
+
         if self.file_matcher and not self.file_matcher.matches(change):
             return False
 
