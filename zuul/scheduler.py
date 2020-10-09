b8cce50f0aa6ab06435bd9570fc4bfb98f530716
Jan Kubovy <jan.kubovy@bmw.de>
2020-10-09 07:35:10 +0200

    Improve typings in context of lock nodes on executor
    This improves the navigation and overall readability of the code.
    It's kept as a separate change but mainly touches the parts that are
    somehow relevant for the lock/unlock nodes on executor change that
    follows later on.
    
    Change-Id: Ic52e7c911d7b81f3ba94df6b5e00ed1cc42f3335

--- zuul/scheduler.py
+++ zuul/scheduler.py
@@ -1500,7 +1500,7 @@ class Scheduler(threading.Thread):
             return
         pipeline.manager.onBuildPaused(event.build)
 
-    def _handleExpiredHoldRequest(self, request):
+    def _handleExpiredHoldRequest(self, request: HoldRequest) -> bool:
         '''
         Check if a hold request is expired and delete it if it is.
 
