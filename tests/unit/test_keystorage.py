aba90b06bf26fbeb278c50f1bc4ddd5d4fbfac29
Simon Westphahl <simon.westphahl@bmw.de>
2020-10-20 09:33:01 +0200

    Store secrets keys and SSH keys in Zookeeper
    As a preparation for the HA scheduler project secrets keys and SSH keys
    will now be stored in Zookeeper. All private data will be encrypted at
    rest.
    
    Existing keys on the filesysem will be automatically imported into
    Zookeeper.
    
    Change-Id: I2a7d1a555f1db1f2178d3bb2f06756ecc8bc7a81

--- tests/unit/test_keystorage.py
+++ tests/unit/test_keystorage.py
@@ -15,12 +15,14 @@
 import os
 import fixtures
 
+import zuul.zk
+from zuul.lib import encryption
 from zuul.lib import keystorage
 
-from tests.base import BaseTestCase
+from tests.base import BaseTestCase, ChrootedKazooFixture
 
 
-class TestKeyStorage(BaseTestCase):
+class TestFileKeyStorage(BaseTestCase):
 
     def _setup_keys(self, root, connection_name, project_name):
         cn = os.path.join(root, connection_name)
@@ -50,7 +52,7 @@ class TestKeyStorage(BaseTestCase):
     def test_key_storage(self):
         root = self.useFixture(fixtures.TempDir()).path
         self._setup_keys(root, 'gerrit', 'org/example')
-        keystorage.KeyStorage(root)
+        keystorage.FileKeyStorage(root)
         self.assertFile(root, '.version', '1')
         self.assertPaths(root, [
             '.version',
@@ -65,4 +67,78 @@ class TestKeyStorage(BaseTestCase):
             'ssh/tenant',
         ])
         # It shouldn't need to upgrade this time
-        keystorage.KeyStorage(root)
+        keystorage.FileKeyStorage(root)
+
+
+class TestZooKeeperKeyStorage(BaseTestCase):
+
+    def setUp(self):
+        super().setUp()
+
+        self.zk_chroot_fixture = self.useFixture(
+            ChrootedKazooFixture(self.id()))
+        self.zk_config = '%s:%s%s' % (
+            self.zk_chroot_fixture.zookeeper_host,
+            self.zk_chroot_fixture.zookeeper_port,
+            self.zk_chroot_fixture.zookeeper_chroot)
+
+        self.zk = zuul.zk.ZooKeeper()
+        self.addCleanup(self.zk.disconnect)
+        self.zk.connect(self.zk_config)
+
+    def test_fallback(self):
+        root = self.useFixture(fixtures.TempDir()).path
+        fallback = keystorage.FileKeyStorage(root)
+        key_store = keystorage.ZooKeeperKeyStorage(
+            self.zk.client, password="DEADBEEF", fallback=fallback
+        )
+
+        # Create keys in the fallback keystore
+        fallback_secrets_pk = encryption.serialize_rsa_private_key(
+            fallback.getProjectSecretsKeys("github", "org/project")[0]
+        )
+        fallback_ssh_keys = fallback.getProjectSSHKeys("github", "org/project")
+
+        self.assertEqual(
+            encryption.serialize_rsa_private_key(
+                key_store.getProjectSecretsKeys("github", "org/project")[0]
+            ),
+            fallback_secrets_pk
+        )
+        self.assertEqual(
+            key_store.getProjectSSHKeys("github", "org/project"),
+            fallback_ssh_keys
+        )
+
+        self.assertIsNotNone(
+            key_store.getProjectSecretsKeys("github", "org/project1")
+        )
+        self.assertIsNotNone(
+            key_store.getProjectSSHKeys("github", "org/project1")
+        )
+
+        # New keys should not end up in the fallback key store
+        self.assertFalse(
+            fallback.hasProjectSecretsKeys("github", "org/project1")
+        )
+        self.assertFalse(fallback.hasProjectSSHKeys("github", "org/project1"))
+
+    def test_without_fallback(self):
+        key_store = keystorage.ZooKeeperKeyStorage(
+            self.zk.client, password="DECAFBAD"
+        )
+        secrets_pk = encryption.serialize_rsa_private_key(
+            key_store.getProjectSecretsKeys("github", "org/project")[0]
+        )
+        ssh_keys = key_store.getProjectSSHKeys("github", "org/project")
+
+        self.assertEqual(
+            encryption.serialize_rsa_private_key(
+                key_store.getProjectSecretsKeys("github", "org/project")[0]
+            ),
+            secrets_pk
+        )
+        self.assertEqual(
+            key_store.getProjectSSHKeys("github", "org/project"),
+            ssh_keys
+        )
