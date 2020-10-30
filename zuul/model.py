1adbb385238eeae9a2a759f44dcf0ec305ae06d6
Simon Westphahl <simon.westphahl@bmw.de>
2020-10-30 14:08:37 +0100

    Allow (de-)serialization of result events
    Since result events will be stored in Zookeeper we need a way to
    (de-)serialze them from/to dictionaries.
    
    Change-Id: I5b77aee6ea72e9fe0db4e71a508e0b5a87dae87f

--- zuul/model.py
+++ zuul/model.py
@@ -3570,10 +3570,12 @@ class EnqueueEvent(ManagementEvent):
         )
 
 
-class ResultEvent:
+class ResultEvent(AbstractEvent):
     """An event that needs to modify the pipeline state due to a
     result from an external system."""
-    pass
+
+    def updateFromDict(self, d: Dict[str, Any]) -> None:
+        pass
 
 
 class BuildStartedEvent(ResultEvent):
@@ -3585,6 +3587,19 @@ class BuildStartedEvent(ResultEvent):
     def __init__(self, build):
         self.build = build
 
+    def toDict(self) -> Dict[str, Any]:
+        return {
+            "build": self.build,
+        }
+
+    @classmethod
+    def fromDict(
+        cls: Type["BuildStartedEvent"], data: Dict[str, Any]
+    ) -> "BuildStartedEvent":
+        return cls(
+            data.get("build"),
+        )
+
 
 class BuildPausedEvent(ResultEvent):
     """A build has been paused.
@@ -3594,6 +3609,19 @@ class BuildPausedEvent(ResultEvent):
 
     def __init__(self, build):
         self.build = build
+
+    def toDict(self) -> Dict[str, Any]:
+        return {
+            "build": self.build,
+        }
+
+    @classmethod
+    def fromDict(
+        cls: Type["BuildPausedEvent"], data: Dict[str, Any]
+    ) -> "BuildPausedEvent":
+        return cls(
+            data.get("build"),
+        )
 
 
 class BuildCompletedEvent(ResultEvent):
@@ -3605,6 +3633,21 @@ class BuildCompletedEvent(ResultEvent):
     def __init__(self, build, result):
         self.build = build
         self.result = result
+
+    def toDict(self) -> Dict[str, Any]:
+        return {
+            "build": self.build,
+            "result": self.result,
+        }
+
+    @classmethod
+    def fromDict(
+        cls: Type["BuildCompletedEvent"], data: Dict[str, Any]
+    ) -> "BuildCompletedEvent":
+        return cls(
+            data.get("build"),
+            data.get("result"),
+        )
 
 
 class MergeCompletedEvent(ResultEvent):
@@ -3629,6 +3672,31 @@ class MergeCompletedEvent(ResultEvent):
         self.repo_state = repo_state
         self.item_in_branches = item_in_branches
 
+    def toDict(self) -> Dict[str, Any]:
+        return {
+            "build_set": self.build_set,
+            "merged": self.merged,
+            "updated": self.updated,
+            "commit": self.commit,
+            "files": list(self.files),
+            "repo_state": dict(self.repo_state),
+            "item_in_branches": list(self.item_in_branches),
+        }
+
+    @classmethod
+    def fromDict(
+        cls: Type["MergeCompletedEvent"], data: Dict[str, Any]
+    ) -> "MergeCompletedEvent":
+        return cls(
+            data.get("build_set"),
+            data.get("merged"),
+            data.get("updated"),
+            data.get("commit"),
+            list(data.get("files", [])),
+            dict(data.get("repo_state", {})),
+            list(data.get("item_in_branches", [])),
+        )
+
 
 class FilesChangesCompletedEvent(ResultEvent):
     """A remote fileschanges operation has completed
@@ -3641,6 +3709,21 @@ class FilesChangesCompletedEvent(ResultEvent):
         self.build_set = build_set
         self.files = files
 
+    def toDict(self) -> Dict[str, Any]:
+        return {
+            "build_set": self.build_set,
+            "files": list(self.files),
+        }
+
+    @classmethod
+    def fromDict(
+        cls: Type["FilesChangesCompletedEvent"], data: Dict[str, Any]
+    ) -> "FilesChangesCompletedEvent":
+        return cls(
+            data.get("build_set"),
+            list(data.get("files", [])),
+        )
+
 
 class NodesProvisionedEvent(ResultEvent):
     """Nodes have been provisioned for a build_set
@@ -3652,6 +3735,19 @@ class NodesProvisionedEvent(ResultEvent):
     def __init__(self, request):
         self.request = request
         self.request_id = request.id
+
+    def toDict(self) -> Dict[str, Any]:
+        return {
+            "request": self.request,
+        }
+
+    @classmethod
+    def fromDict(
+        cls: Type["NodesProvisionedEvent"], data: Dict[str, Any]
+    ) -> "NodesProvisionedEvent":
+        return cls(
+            data.get("request"),
+        )
 
 
 class TriggerEvent(object):
