bd44d68b3ae137ddaa2c01d3757a76c10ae79fef
Felix Edel <felix.edel@bmw.de>
2020-11-02 12:00:33 +0100

    Configure json-server as mock API for development
    This configures json-server to run as mock API in development mode.
    Using a mock API allows for faster development without having to rely
    on a real API. As we already have the possibility to switch between
    different APIs, this provides the advantage of another API that
    provides the same results every time.
    
    This will allow us to develop and test new UI components or features
    without relying on the backend providing the required functionality
    (e.g. new API endpoints). In the current state this is often very
    difficult, as we would have to wait for the backend changes to be
    merged and deployed to make the UI preview build work.
    
    With a mock API we could define a new job which serves the Zuul
    dashboard running against this mock API rather than a live Zuul
    instance.
    
    When starting the development server via
      $ yarn start
    the json-server is started on port 3001 together with the react
    development server on port 3000.
    By now json-server should provide the following endpoints:
      - http://localhost:3001 -> json-server landing page
      - http://localhost:3001/api/info
      - http://localhost:3001/api/tenants
      - http://localhost:3001/api/config-errors
      - http://localhost:3001/api/builds
      - http://localhost:3001/api/build/<uuid>
      - http://localhost:3001/api/buildsets
      - http://localhost:3001/api/buildset/<uuid>
    
    To make the Zuul dashboard work with the json-server instance, we set
    the REACT_APP_ZUUL_API envvar accordingly.
    
    The data provided by json-server is defined in web/tools/db.json which
    is automatically generated during startup based on the data in
    web/tools/mockData.js. By defining our mock data directly in JavaScript
    all developers could use the same data for development and we could even
    use this data in our unit tests later on.
    
    By now we only configure a minimal set of data to make the Zuul
    dashboard start with the mock API. This includes the central info data,
    a single "opendev" tenant, an empty list of config-errors and a bunch of
    builds and buildsets. Everything is taken from zuul.opendendev.org.
    When requried we could simply extend the data with whatever we need
    (projects, build(set)s with different results, nodes, ...).
    
    Change-Id: Ie6ee29cc25ef2e824d06cef25b953d955ca0c2fe

---
+++ web/tools/apiServer.js
@@ -0,0 +1,72 @@
+// Copyright 2020 BMW Group
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+// This file allows us to configure JSON server to our needs. Since we use the
+// API mainly to get the data from Zuul, but don't provide full-featured CRUD
+// operations, we only tweak a few settings. For a list of possible extras take
+// a look at https://github.com/typicode/json-server#extras.
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const path = require('path')
+// Make json-server look up the generated db.json file from the tools directory.
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+
+// Can pass a limited number of options to this to override (some) defaults.
+// See https://github.com/typicode/json-server#api
+const middlewares = jsonServer.defaults({
+  // Display json-server's built in homepage when json-server starts.
+  static: 'node_modules/json-server/public',
+})
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+// Change this value to simulate delay on all requests (milliseconds)
+server.use(function (req, res, next) {
+  setTimeout(next, 0)
+})
+
+server.use(
+  jsonServer.rewriter({
+    // This will prefix the path to all JSON server resources with "api", e.g.
+    // /api/builds. This is also were Zuul expects the data to be served.
+    '/api/*': '/$1',
+    // The Zuul API serves single resources under a different path e.g. build/
+    // instead of builds/
+    '/build/:id': '/builds/:id',
+    '/buildset/:id': '/buildsets/:id',
+  })
+)
+
+// Tell json server to use the uuid field as key for it's database. By default,
+// json server requires each object to provide an id field which will be used
+// to look up a single resource. Unfortunately, Zuul only provides a uuid field
+// for each object.
+// NOTE (felix): When using json-server via comment line, this can easly be done
+// by specyfing the --id parameter like this:
+// $ json-server --id uuid
+// Unfortunately, I didn't find a proper way when using the json-server API to
+// create a custom server instance and the following line is also what
+// json-server does when evaluating the command line argument:
+// https://github.com/typicode/json-server/blob/4f315b090f6a504c261bab83894d3eb85eabe686/src/cli/run.js#L73
+router.db._.id = 'uuid'
+server.use(router)
+
+// By default, JSON server is running on port 3000, which is already used by our
+// react app. Thus, change the port to 3001 and start the server.
+const port = 3001
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`)
+})
