edabb4e65f28f501c7212fa9982bf96f61ba4af2
Simon Westphahl <simon.westphahl@bmw.de>
2020-11-19 11:57:55 +0100

    Increase default test wait timeout to 120s
    Without the increased timeout we are seeing random test failures that
    could be related to thread starvation.
    
    Change-Id: Ie54fc16488ab8cbc15f97d003f36c12b8a648ed4

--- tests/base.py
+++ tests/base.py
@@ -3860,7 +3860,7 @@ class TestScheduler(zuul.scheduler.Scheduler):
 
 class BaseTestCase(testtools.TestCase):
     log = logging.getLogger("zuul.test")
-    wait_timeout = 90
+    wait_timeout = 120
 
     def attachLogs(self, *args):
         def reader():
