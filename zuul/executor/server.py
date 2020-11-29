4a7fd981ffb72421efb50dba63c7e621309f0f44
Albin Vass <albin.vass@gmail.com>
2020-11-29 10:37:42 +0100

    Throw a warning if the executors ssh-key can't be loaded
    No ssh-keys are needed if only kubernetes drivers or other
    non-ssh connection drivers are used so don't error if there
    are no ssh-keys installed. Currently a dummy ssh-key needs to
    be created as a workaround for the executor to not error when
    running jobs.
    
    Change-Id: Ib583df2dce9e7a4b93228c8fa7b41b082263e4c8

--- zuul/executor/server.py
+++ zuul/executor/server.py
@@ -831,9 +831,9 @@ class AnsibleJob(object):
         self._resume_event = threading.Event()
         self.thread = None
         self.project_info = {}
-        self.private_key_file = get_default(self.executor_server.config,
-                                            'executor', 'private_key_file',
-                                            '~/.ssh/id_rsa')
+
+        self.private_key_file = self.executor_server.private_key_file
+
         self.winrm_key_file = get_default(self.executor_server.config,
                                           'executor', 'winrm_cert_key_file',
                                           '~/.winrm/winrm_client_cert.key')
@@ -934,7 +934,8 @@ class AnsibleJob(object):
             self.job.sendWorkData(json.dumps(self._base_job_data()))
 
             self.ssh_agent.start()
-            self.ssh_agent.add(self.private_key_file)
+            if self.private_key_file:
+                self.ssh_agent.add(self.private_key_file)
             for key in self.arguments.get('ssh_keys', []):
                 self.ssh_agent.addData(key['name'], key['key'])
             self.jobdir = JobDir(self.executor_server.jobdir_root,
@@ -2656,6 +2657,14 @@ class ExecutorServer(BaseMergeServer):
             RAMSensor(config),
             StartingBuildsSensor(self, cpu_sensor.max_load_avg, config)
         ]
+
+        self.private_key_file = get_default(self.config, 'executor',
+                                            'private_key_file',
+                                            '~/.ssh/id_rsa')
+        if not os.path.isfile(self.private_key_file):
+            self.log.warning('SSH Key %s does not exist',
+                             self.private_key_file)
+            self.private_key_file = None
 
         manage_ansible = get_default(
             self.config, 'executor', 'manage_ansible', True)
