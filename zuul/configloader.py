4e2ae9321c89a8bb49d82850541269eeb0cccafb
Simon Westphahl <simon.westphahl@bmw.de>
2020-12-04 12:36:29 +0100

    Store tenants in unparsed abide as dict
    Change-Id: I0afd8c814973199781598dbefd51498ec92d733a

--- zuul/configloader.py
+++ zuul/configloader.py
@@ -2272,7 +2272,7 @@ class ConfigLoader(object):
         for conf_admin_rule in unparsed_abide.admin_rules:
             admin_rule = self.admin_rule_parser.fromYaml(conf_admin_rule)
             abide.admin_rules[admin_rule.name] = admin_rule
-        for conf_tenant in unparsed_abide.tenants:
+        for conf_tenant in unparsed_abide.tenants.values():
             # When performing a full reload, do not use cached data.
             tenant = self.tenant_parser.fromYaml(
                 abide, conf_tenant, ansible_manager)
@@ -2309,8 +2309,7 @@ class ConfigLoader(object):
                 del new_abide.tenants[tenant.name]
                 return new_abide
 
-            unparsed_config = next(t for t in unparsed_abide.tenants
-                                   if t['name'] == tenant.name)
+            unparsed_config = unparsed_abide.tenants[tenant.name]
         else:
             unparsed_config = tenant.unparsed_config
 
