2386f9c0238a33de9936727b5c762d5db8d87d26
Simon Westphahl <simon.westphahl@bmw.de>
2020-12-04 08:19:11 +0100

    Tenant read/write lock in Zookeeper
    Schedulers need to hold a tenant read lock while processing pipelines.
    During  reconfiguration a scheduler will acquire the write lock for the
    proccessed tenant, which prevents other schedulers from processing any
    pipelines in that tenant during the reconfiguration.
    
    In case a scheduler can't acquire the read lock during pipeline
    processing it will continue with the next tenant.
    
    Change-Id: Id68eb4a4e668673fa11b0e7813301ccf93ad7818

---
+++ zuul/zk/locks.py
@@ -0,0 +1,63 @@
+# Copyright 2020 BMW Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+from contextlib import contextmanager
+from typing import Generator, List
+
+from kazoo.recipe.lock import Lock, ReadLock, WriteLock
+
+from zuul.zk import ZooKeeperClient
+
+TENANT_LOCK_ROOT = "/zuul/locks/tenant"
+
+
+class LockFailedError(RuntimeError):
+    pass
+
+
+@contextmanager
+def locked(
+    *locks: Lock, blocking=True, timeout=None, ephemeral=True
+) -> Generator[None, None, None]:
+    need_release: List[Lock] = []
+    try:
+        for lock in locks:
+            if not lock.acquire(
+                blocking=blocking, timeout=timeout, ephemeral=ephemeral
+            ):
+                raise LockFailedError("Failed to aquire lock {}".format(lock))
+            need_release.append(lock)
+        yield
+    finally:
+        for lock in need_release:
+            try:
+                lock.release()
+            except Exception:
+                log = logging.getLogger("zuul.zk.locks")
+                log.exception("Failed to release lock %s", lock)
+
+
+def tenant_read_lock(
+    client: ZooKeeperClient, tenant_name: str
+) -> ReadLock:
+    lock_path = "/".join((TENANT_LOCK_ROOT, tenant_name))
+    return client.kazoo_client.ReadLock(lock_path)
+
+
+def tenant_write_lock(
+    client: ZooKeeperClient, tenant_name: str
+) -> WriteLock:
+    lock_path = "/".join((TENANT_LOCK_ROOT, tenant_name))
+    return client.kazoo_client.WriteLock(lock_path)
