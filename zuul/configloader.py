3d50a49ca0ba8f4c566c3d12348ecadcc7355bcb
Simon Westphahl <simon.westphahl@bmw.de>
2020-12-16 11:10:49 +0100

    Support cross scheduler config loading
    - prime config on scheduler startup w/o reconfiguration
    - sync layout on all schedulers after reconfiguration
    
    Change-Id: I48b145cecec5890d212d751f68b3947f04ee1ed5

--- zuul/configloader.py
+++ zuul/configloader.py
@@ -1808,9 +1808,6 @@ class TenantParser(object):
             for branch in branches:
                 branch_cache = abide.getUnparsedBranchCache(
                     project.canonical_name, branch)
-                if branch_cache.isValidFor(tpc):
-                    # We already have this branch cached.
-                    continue
                 if not tpc.load_classes:
                     # If all config classes are excluded then do not
                     # request any getFiles jobs.
@@ -1840,6 +1837,10 @@ class TenantParser(object):
                         )
                         branch_cache.put(source_context.path, incdata)
                     branch_cache.setValidFor(tpc)
+                    continue
+
+                if branch_cache.isValidFor(tpc):
+                    # We already have this branch cached.
                     continue
 
                 cache_ltime = self.zk_client.zxid
