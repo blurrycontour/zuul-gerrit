aacc55617ae666c4e37d719eea0b55b62594ed9d
Matthieu Huin <mhuin@redhat.com>
2020-12-18 16:04:25 +0100

    web UI: user login with OpenID Connect
    If the default realm's authentication driver is set to "OpenIDConnect",
    display a "Sign in" button. If the the user is logged in, fetch user
    authorizations and add them to the redux store; display the user's
    preferred username in the upper right corner. Clicking on the user icon
    in the right corner displays a modal with user information, such as
    the user's authentication token (that can be used with zuul-client);
    and a sign out button. Clicking on the sign out button removes
    user information from the store (note that it
    does not log the user out from the identity provider).
    
    Add some basic documentation explaining how to configure Zuul with
    Google's authentication, and with a Keycloak server.
    
    Authentication is intended to be tenant-scoped, but at this time the
    REST API only provides one auth configuration for all tenants.
    A follow-up patch allows linking a specific realm (and thus
    authentication) to a tenant.
    
    Change-Id: I31e71f2795f3f7c4253d0d5b8ed309bfd7d4f98e

---
+++ web/src/reducers/user.js
@@ -0,0 +1,75 @@
+// Copyright 2020 Red Hat, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+import update from 'immutability-helper'
+
+import {
+  USER_ACL_REQUEST,
+  USER_ACL_SUCCESS,
+  USER_ACL_FAILURE,
+  USER_LOGGED_IN,
+  USER_IN_LOCALSTORAGE,
+  USER_LOGGED_OUT,
+} from '../actions/user'
+
+export default (state = {
+  isFetching: false,
+  user: null,
+  token: null,
+  scope: [],
+  isAdmin: false
+}, action) => {
+  switch (action.type) {
+    case USER_LOGGED_IN: {
+      let user_stringified = JSON.stringify(action.user)
+      localStorage.setItem('zuul_user', user_stringified)
+      return {
+        isFetching: true,
+        user: action.user,
+        token: action.token,
+        scope: [],
+        isAdmin: false
+      }
+    }
+    case USER_IN_LOCALSTORAGE:
+      return {
+        isFetching: true,
+        user: action.user,
+        token: action.token,
+        scope: [],
+        isAdmin: false
+      }
+    case USER_LOGGED_OUT:
+      localStorage.removeItem('zuul_user')
+      return {
+        isFetching: false,
+        user: null,
+        token: null,
+        scope: [],
+        isAdmin: false
+    }
+    case USER_ACL_REQUEST:
+      return update(state, {$merge: {isFetching: true}})
+  case USER_ACL_FAILURE:
+    return update(state, {$merge: {isFetching: false,
+                                   scope: [],
+                                   isAdmin: false}})
+  case USER_ACL_SUCCESS:
+    return update(state, {$merge: {isFetching: false,
+                                   scope: action.scope,
+                                   isAdmin: action.isAdmin}})
+  default:
+    return state
+  }
+}
