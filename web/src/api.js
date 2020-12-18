44a29a74c407424a575559fbc7f820fb5cbd8ff7
Matthieu Huin <mhuin@redhat.com>
2020-12-18 18:28:06 +0100

    web UI: allow a privileged user to dequeue a change
    Add a "power off" button next to a change in the status page, if the
    currently logged in user is an admin on the tenant. Clicking the button
    opens a modal, so that the user can confirm her decision.
    
    Change-Id: I7c97509d62ef167ee5904a816998049d88c6b3cb

--- web/src/api.js
+++ web/src/api.js
@@ -176,6 +176,35 @@ function fetchUserAuthorizations (apiPrefix, token) {
   return res
 }
 
+function dequeue (apiPrefix, projectName, pipeline, change, token) {
+  const instance = Axios.create({
+    baseURL: apiUrl
+  })
+  instance.defaults.headers.common['Authorization'] = 'Bearer ' + token
+  let res = instance.post(
+    apiPrefix + 'project/' + projectName + '/dequeue',
+    {
+      pipeline: pipeline,
+      change: change,
+    }
+  )
+  return res
+}
+function dequeue_ref (apiPrefix, projectName, pipeline, ref, token) {
+  const instance = Axios.create({
+    baseURL: apiUrl
+  })
+  instance.defaults.headers.common['Authorization'] = 'Bearer ' + token
+  let res = instance.post(
+    apiPrefix + 'project/' + projectName + '/dequeue',
+    {
+      pipeline: pipeline,
+      ref: ref,
+    }
+  )
+  return res
+}
+
 export {
   apiUrl,
   getHomepageUrl,
@@ -198,4 +227,6 @@ export {
   fetchInfo,
   fetchTenantInfo,
   fetchUserAuthorizations,
+  dequeue,
+  dequeue_ref,
 }
