f925089ba9b5624098f1756581280d72ebcadf54
Clément Mondion <clement.mondion@adevinta.com>
2020-12-21 17:17:43 +0100

    [UI][Nodes] use patternfly 4
    Change-Id: Ieefae32e29d182098277d22991642c624c660dbf

--- web/src/pages/Nodes.jsx
+++ web/src/pages/Nodes.jsx
@@ -15,15 +15,81 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Table } from 'patternfly-react'
+
+import {
+  Spinner,
+} from '@patternfly/react-core'
+
+import {
+  ClusterIcon,
+  InfrastructureIcon,
+  KeyIcon,
+  NetworkIcon,
+  OutlinedCalendarAltIcon,
+  OutlinedCommentsIcon,
+  RunningIcon,
+  TagIcon
+} from '@patternfly/react-icons'
+
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableVariant,
+  sortable,
+  SortByDirection,
+} from '@patternfly/react-table'
+
 import * as moment from 'moment'
-import { PageSection, PageSectionVariants } from '@patternfly/react-core'
 
-import { fetchNodesIfNeeded } from '../actions/nodes'
-import { Fetchable } from '../containers/Fetching'
-
+import {fetchNodesIfNeeded} from '../actions/nodes'
+import {PageSection, PageSectionVariants} from "@patternfly/react-core";
+import {IconProperty} from "../containers/build/Misc";
 
 class NodesPage extends React.Component {
+  constructor(props) {
+    super(props);
+    const nodes = props.remoteData.nodes
+
+    this.state = {
+      sortBy: {index: 1, direction: SortByDirection.asc},
+      columns: [
+        {
+          title: <IconProperty icon={<KeyIcon />} value="Id" />,
+        },
+        {
+          title: <IconProperty icon={<TagIcon />} value="Label" />,
+          transforms: [sortable]
+        },
+        {
+          title: <IconProperty icon={<NetworkIcon />} value="Connection" />,
+        },
+        {
+          title: <IconProperty icon={<ClusterIcon />} value="Server" />,
+          transforms: [sortable]
+        },
+        {
+          title: <IconProperty icon={<InfrastructureIcon />} value="Provider" />,
+          transforms: [sortable]
+        },
+        {
+          title: <IconProperty icon={<RunningIcon />} value="State" />,
+          transforms: [sortable]
+        },
+        {
+          title: <IconProperty icon={<OutlinedCalendarAltIcon />} value="Age" />,
+          transforms: [sortable]
+        },
+        {
+          title: <IconProperty icon={<OutlinedCommentsIcon />} value="Comment" />,
+        },
+      ],
+      rows: this.parseRows(nodes),
+      fetching: false,
+    }
+    this.onSort = this.onSort.bind(this);
+  }
+
   static propTypes = {
     tenant: PropTypes.object,
     remoteData: PropTypes.object,
@@ -34,81 +100,98 @@ class NodesPage extends React.Component {
     this.props.dispatch(fetchNodesIfNeeded(this.props.tenant, force))
   }
 
-  componentDidMount () {
+  parseRows = nodes => nodes.map(n => [
+    {title: n.id},
+    {title: n.type[0]},
+    {title: n.connection_type},
+    {title: n.external_id},
+    {title: n.provider},
+    {title: n.state},
+    {title: n.state_time, formatters: [date => (moment.unix(date).fromNow())]},
+    {title: n.comment}
+  ])
+
+
+  emptyRows = () => ([
+    {
+      heightAuto: true,
+      cells: [
+        {
+          props: {colSpan: 8},
+          title: (
+            <center>
+              <Spinner size="xl"/>
+            </center>
+          ),
+        },
+      ],
+    },
+  ])
+
+  componentDidMount() {
     document.title = 'Zuul Nodes'
     if (this.props.tenant.name) {
       this.updateData()
     }
   }
 
-  componentDidUpdate (prevProps) {
+  componentDidUpdate(prevProps) {
+    const {nodes, isFetching} = this.props.remoteData
+    const {isFetching: wasFetching} = prevProps.remoteData
+    if (isFetching !== wasFetching) {
+      if (isFetching) {
+        this.setState({rows: this.emptyRows()})
+      } else {
+        this.setState({
+          rows: this.parseRows(nodes).sort((a, b) => (a[1].title < b[1].title ? -1 : a[1].title > b[1].title ? 1 : 0))
+        })
+      }
+    }
     if (this.props.tenant.name !== prevProps.tenant.name) {
       this.updateData()
     }
   }
 
-  render () {
-    const { remoteData } = this.props
-    const nodes = remoteData.nodes
+  onSort(_event, index, direction) {
+    const sortedRows = this.state.rows.sort((a, b) => (a[index].title < b[index].title ? -1 : a[index].title > b[index].title ? 1 : 0));
+    this.setState({
+      sortBy: {
+        index,
+        direction
+      },
+      rows: direction === SortByDirection.asc ? sortedRows : sortedRows.reverse()
+    });
+  }
 
-    const headerFormat = value => <Table.Heading>{value}</Table.Heading>
-    const cellFormat = value => <Table.Cell>{value}</Table.Cell>
-    const cellLabelsFormat = value => <Table.Cell>{value.join(',')}</Table.Cell>
-    const cellPreFormat = value => (
-      <Table.Cell style={{fontFamily: 'Menlo,Monaco,Consolas,monospace'}}>
-        {value}
-      </Table.Cell>)
-    const cellAgeFormat = value => (
-      <Table.Cell style={{fontFamily: 'Menlo,Monaco,Consolas,monospace'}}>
-        {moment.unix(value).fromNow()}
-      </Table.Cell>)
+  render() {
+    const {sortBy, rows, columns} = this.state
+    const {nodes} = this.props.remoteData
+    const building = nodes.filter(n => n.state === "building").length;
+    const ready = nodes.filter(n => n.state === "ready").length;
+    const inUse = nodes.filter(n => n.state === "in-use").length;
+    const deleting = nodes.filter(n => n.state === "deleting").length;
 
-    const columns = []
-    const myColumns = [
-      'id', 'labels', 'connection', 'server', 'provider', 'state',
-      'age', 'comment'
-    ]
-    myColumns.forEach(column => {
-      let formatter = cellFormat
-      let prop = column
-      if (column === 'labels') {
-        prop = 'type'
-        formatter = cellLabelsFormat
-      } else if (column === 'connection') {
-        prop = 'connection_type'
-      } else if (column === 'server') {
-        prop = 'external_id'
-        formatter = cellPreFormat
-      } else if (column === 'age') {
-        prop = 'state_time'
-        formatter = cellAgeFormat
-      }
-      columns.push({
-        header: {label: column, formatters: [headerFormat]},
-        property: prop,
-        cell: {formatters: [formatter]}
-      })
-    })
     return (
       <PageSection variant={PageSectionVariants.light}>
+        <div style={{marginBottom: 10}}>
+          <div>
+            <b>Summary
+              : </b>{rows.length} total, {building} building, {ready} ready, {inUse} in-use, {deleting} deleting
+          </div>
+        </div>
         <PageSection style={{paddingRight: '5px'}}>
-          <Fetchable
-            isFetching={remoteData.isFetching}
-            fetchCallback={this.updateData}
-          />
         </PageSection>
-        <Table.PfProvider
-          striped
-          bordered
-          hover
-          columns={columns}
+        <Table
+          aria-label="Sortable Table"
+          variant={TableVariant.compact}
+          cells={columns}
+          rows={rows}
+          sortBy={sortBy}
+          onSort={this.onSort}
         >
-          <Table.Header/>
-          <Table.Body
-            rows={nodes}
-            rowKey="id"
-          />
-        </Table.PfProvider>
+          <TableHeader/>
+          <TableBody/>
+        </Table>
       </PageSection>
     )
   }
