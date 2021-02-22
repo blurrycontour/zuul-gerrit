2e35f3d7e6006a7c64c475dac493ce8b4f14982d
Simon Westphahl <simon.westphahl@bmw.de>
2021-02-22 09:27:02 +0100

    Implement ABC for caching changes in Zookeeper
    In order to make updates atomic the change data is stored separate from
    the cache entry. The data uses a random UUID znode that is then
    referenced from the actual cache entry.
    
    The change data is immutable, which means that an update of a cached
    item will result in a new data node. The cache entry will then be
    changed to reference the new data.
    
    This approach also allows us to check if a given change is up-to-date by
    comparing the referenced UUID in Zookeeper with the one in the local
    cache without loading the whole change data.
    
    Since local change objects should be singletons and to prevent race
    conditions between local and remote updates, we need to protect those
    updates by a per-change lock.
    
    The change data is stored in the following Zookeeper path:
    
        /zuul/cache/connection/<connection-name>/data/<uuid>
    
    The cache entries that reference the change data use the following path:
    
        /zuul/cache/connection/<connection-name>/cache/<key>
    
    Data nodes will not be directly removed when an entry is removed or
    updated in order to prevent race conditions with multiple consumers of
    the cache. The stale data nodes will be instead cleaned up in the
    cache's cleanup() method. This expected to happen periodically.
    
    An interface for cleaning up entries older than X will be added in a
    follow-up change.
    
    Change-Id: I9a020b65c2c2e2326d5eb8bbd16270de4f4c1eab

--- zuul/scheduler.py
+++ zuul/scheduler.py
@@ -2188,6 +2188,8 @@ class Scheduler(threading.Thread):
                   "change caches", change)
         for source in self.connections.getSources():
             for other_change in source.getCachedChanges():
+                if not isinstance(other_change, Change):
+                    continue
                 if other_change.commit_needs_changes is None:
                     continue
                 for connection_name, key in other_change.commit_needs_changes:
