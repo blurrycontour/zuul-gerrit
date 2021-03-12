81a64dd0dc1cdf054017e91f8f3e713a1a6f952a
Guillaume Chauvel <guillaume.chauvel@gmail.com>
2021-03-12 14:17:43 +0100

    [DNM] run TestSchedulerSSL test with pending gear change
    Depends-On: https://review.opendev.org/780422
    Change-Id: I8163ee9a0469016442e8aa47320194d166c9a232

--- tests/base.py
+++ tests/base.py
@@ -3961,7 +3961,7 @@ class BaseTestCase(testtools.TestCase):
         # from libraries that zuul depends on such as gear.
         log_defaults_from_env = os.environ.get(
             'OS_LOG_DEFAULTS',
-            'git.cmd=INFO,kazoo.client=WARNING,gear=WARNING')
+            'git.cmd=INFO,kazoo.client=WARNING,gear=DEBUG')
 
         if log_defaults_from_env:
             for default in log_defaults_from_env.split(','):
