b40722d6dbbe4b3be3890a9f8793bc484822634d
Simon Westphahl <simon.westphahl@bmw.de>
2021-03-17 14:28:46 +0100

    Unify handling of dequeue and enqueue events
    Cleanup the implementation of enqueue and dequeue events in order to
    simplify the event processing in the scheduler.
    
    This removes the associated trigger event from the enqueue event and
    introduces a common base class for de-/enqueue events.
    
    Change-Id: I9b3edd1e6c45ecb1ce5ab9251bafe807bbefbbf7

--- tests/unit/test_scheduler.py
+++ tests/unit/test_scheduler.py
@@ -4435,9 +4435,14 @@ class TestScheduler(ZuulTestCase):
         client = zuul.rpcclient.RPCClient('127.0.0.1',
                                           self.gearman_server.port)
         self.addCleanup(client.shutdown)
-        with testtools.ExpectedException(
-            zuul.rpcclient.RPCFailure,
-            'Change 2,1 does not belong to project "org/project1"'):
+        matcher = AfterPreprocessing(
+            str, MatchesRegex(
+                r'.*Change <Change \w+ org/project2 2,1> does not belong to '
+                r'project "org/project1"',
+                re.DOTALL
+            )
+        )
+        with testtools.ExpectedException(zuul.rpcclient.RPCFailure, matcher):
             r = client.enqueue(tenant='tenant-one',
                                pipeline='gate',
                                project='org/project1',
@@ -4704,8 +4709,10 @@ class TestScheduler(ZuulTestCase):
         client = zuul.rpcclient.RPCClient('127.0.0.1',
                                           self.gearman_server.port)
         self.addCleanup(client.shutdown)
-        with testtools.ExpectedException(zuul.rpcclient.RPCFailure,
-                                         "Invalid tenant"):
+        matcher = AfterPreprocessing(
+            str, MatchesRegex(r'.*Unknown tenant', re.DOTALL)
+        )
+        with testtools.ExpectedException(zuul.rpcclient.RPCFailure, matcher):
             r = client.enqueue(tenant='tenant-foo',
                                pipeline='gate',
                                project='org/project',
@@ -4713,8 +4720,10 @@ class TestScheduler(ZuulTestCase):
                                change='1,1')
             self.assertEqual(r, False)
 
-        with testtools.ExpectedException(zuul.rpcclient.RPCFailure,
-                                         "Invalid project"):
+        matcher = AfterPreprocessing(
+            str, MatchesRegex(r'.*Unknown project', re.DOTALL)
+        )
+        with testtools.ExpectedException(zuul.rpcclient.RPCFailure, matcher):
             r = client.enqueue(tenant='tenant-one',
                                pipeline='gate',
                                project='project-does-not-exist',
@@ -4722,8 +4731,10 @@ class TestScheduler(ZuulTestCase):
                                change='1,1')
             self.assertEqual(r, False)
 
-        with testtools.ExpectedException(zuul.rpcclient.RPCFailure,
-                                         "Invalid pipeline"):
+        matcher = AfterPreprocessing(
+            str, MatchesRegex(r'.*Unknown pipeline', re.DOTALL)
+        )
+        with testtools.ExpectedException(zuul.rpcclient.RPCFailure, matcher):
             r = client.enqueue(tenant='tenant-one',
                                pipeline='pipeline-does-not-exist',
                                project='org/project',
@@ -4731,8 +4742,10 @@ class TestScheduler(ZuulTestCase):
                                change='1,1')
             self.assertEqual(r, False)
 
-        with testtools.ExpectedException(zuul.rpcclient.RPCFailure,
-                                         "Invalid change"):
+        matcher = AfterPreprocessing(
+            str, MatchesRegex(r'.*Unknown change', re.DOTALL)
+        )
+        with testtools.ExpectedException(zuul.rpcclient.RPCFailure, matcher):
             r = client.enqueue(tenant='tenant-one',
                                pipeline='gate',
                                project='org/project',
