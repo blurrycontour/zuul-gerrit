b46713dc899e01428ce79f0d6abf1c284e79e044
Matthieu Huin <mhuin@redhat.com>
2021-03-19 19:31:25 +0100

    web UI: allow a privileged user to promote a change
    Add a "promote" button in the actions menu of a change, if the
    currently logged in user is an admin on the tenant and if the pipeline
    is dependent.
    Clicking the button opens a modal, so that the user can confirm her decision.
    
    Change-Id: I8262888aef9ba1a106e0b321cc4cf2e14465b90c

--- web/src/containers/status/Pipeline.jsx
+++ web/src/containers/status/Pipeline.jsx
@@ -218,7 +218,7 @@ class Pipeline extends React.Component {
             <ChangeQueue
               queue={changeQueue}
               expanded={expanded}
-              pipeline={pipeline.name}
+              pipeline={pipeline}
               key={changeQueue.uuid}
             />
           ))}
