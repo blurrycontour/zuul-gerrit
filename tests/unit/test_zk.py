f89998f8227b3673e3f2f44872a8e97b858cf03c
James E. Blair <jim@acmegating.com>
2021-04-23 16:55:29 -0700

    Change zone handling in ExecutorApi
    This changes how zones are handled in the ExecutorApi.
    
    This creates an unambiguous unzoned root, as well as a root for
    each zone.  Unzoned jobs are represented with None as a zone.
    This allows us to remove a complicated method of setting dynamic
    watches.
    
    This also adds a test which simulates both sides of the API
    (scheduler and executor) so that we can exercise the full lifecycle
    of a request easily and to show concisely how the API should be
    used.
    
    Change-Id: I624f23b47daa5273b69289a6c7a2187c7ba9ddde

--- tests/unit/test_zk.py
+++ tests/unit/test_zk.py
@@ -13,6 +13,7 @@
 # under the License.
 
 import json
+import queue
 
 import testtools
 
@@ -21,7 +22,7 @@ from zuul.model import BuildRequestState, HoldRequest
 from zuul.zk import ZooKeeperClient
 from zuul.zk.config_cache import UnparsedConfigCache
 from zuul.zk.exceptions import LockException
-from zuul.zk.executor import ExecutorApi
+from zuul.zk.executor import ExecutorApi, BuildRequestEvent
 from zuul.zk.nodepool import ZooKeeperNodepool
 from zuul.zk.sharding import (
     RawShardIO,
@@ -239,12 +240,93 @@ class TestUnparsedConfigCache(ZooKeeperBaseTestCase):
 
 
 class TestExecutorApi(ZooKeeperBaseTestCase):
+    def test_build_request(self):
+        # Test the lifecycle of a build request
+        request_queue = queue.Queue()
+        event_queue = queue.Queue()
+
+        # A callback closure for the request queue
+        def rq_put():
+            request_queue.put(None)
+
+        # and the event queue
+        def eq_put(br, e):
+            event_queue.put((br, e))
+
+        # Simulate the client side
+        client = ExecutorApi(self.zk_client)
+        # Simulate the server side
+        server = ExecutorApi(self.zk_client,
+                             build_request_callback=rq_put,
+                             build_event_callback=eq_put)
+
+        # Scheduler submits request
+        client.submit("A", "tenant", "pipeline", {}, None)
+        request_queue.get(timeout=30)
+
+        # Executor receives request
+        reqs = list(server.next())
+        self.assertEqual(len(reqs), 1)
+        a = reqs[0]
+        self.assertEqual(a.uuid, 'A')
+
+        # Executor locks request
+        self.assertTrue(server.lock(a, blocking=False))
+        a.state = BuildRequestState.RUNNING
+        server.update(a)
+        self.assertEqual(client.get(a.path).state, BuildRequestState.RUNNING)
+
+        # Executor should see no pending requests
+        reqs = list(server.next())
+        self.assertEqual(len(reqs), 0)
+
+        # Executor pauses build
+        a.state = BuildRequestState.PAUSED
+        server.update(a)
+        self.assertEqual(client.get(a.path).state, BuildRequestState.PAUSED)
+
+        # Scheduler resumes build
+        self.assertTrue(event_queue.empty())
+        sched_a = client.get(a.path)
+        client.requestResume(sched_a)
+        (build_request, event) = event_queue.get(timeout=30)
+        self.assertEqual(build_request, a)
+        self.assertEqual(event, BuildRequestEvent.RESUMED)
+
+        # Executor resumes build
+        a.state = BuildRequestState.RUNNING
+        server.update(a)
+        server.fulfillResume(a)
+        self.assertEqual(client.get(a.path).state, BuildRequestState.RUNNING)
+
+        # Scheduler cancels build
+        self.assertTrue(event_queue.empty())
+        sched_a = client.get(a.path)
+        client.requestCancel(sched_a)
+        (build_request, event) = event_queue.get(timeout=30)
+        self.assertEqual(build_request, a)
+        self.assertEqual(event, BuildRequestEvent.CANCELED)
+
+        # Executor aborts build
+        a.state = BuildRequestState.COMPLETED
+        server.update(a)
+        server.fulfillCancel(a)
+        server.unlock(a)
+        self.assertEqual(client.get(a.path).state, BuildRequestState.COMPLETED)
+
+        # Scheduler removes build request on completion
+        client.remove(sched_a)
+
+    # TODO: test scheduler forcibly removing buildrequest
+    # TODO: test hold process
 
     def test_lost_build_requests(self):
-        executor_api = ExecutorApi(self.zk_client, use_cache=True)
+        # Test that lostBuildRequests() returns unlocked running build
+        # requests
+        executor_api = ExecutorApi(self.zk_client)
 
         executor_api.submit("A", "tenant", "pipeline", {}, "zone")
-        path_b = executor_api.submit("B", "tenant", "pipeline", {}, "zone")
+        path_b = executor_api.submit("B", "tenant", "pipeline", {}, None)
         path_c = executor_api.submit("C", "tenant", "pipeline", {}, "zone")
         path_d = executor_api.submit("D", "tenant", "pipeline", {}, "zone")
         path_e = executor_api.submit("E", "tenant", "pipeline", {}, "zone")
@@ -258,8 +340,8 @@ class TestExecutorApi(ZooKeeperBaseTestCase):
         executor_api.update(b)
 
         c.state = BuildRequestState.RUNNING
-        executor_api.update(c)
         executor_api.lock(c)
+        executor_api.update(c)
 
         d.state = BuildRequestState.COMPLETED
         executor_api.update(d)
