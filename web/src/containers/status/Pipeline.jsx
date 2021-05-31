94efd426d7958750a03888c5e5b2056e79a66e60
Simon Westphahl <simon.westphahl@bmw.de>
2021-05-31 14:19:53 +0200

    Show pipeline event queue lengths in Zuul web
    With I876c3634b5616b6fc88f3ee529958b32f2ceda43 we have the length of the
    pipeline event queues in the status JSON and can display those in the
    web UI.
    
    Change-Id: I8a437a288736ce5b3c892129bca9bbee6fefb98a

--- web/src/containers/status/Pipeline.jsx
+++ web/src/containers/status/Pipeline.jsx
@@ -15,6 +15,7 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import { Badge } from 'patternfly-react'
+import { Tooltip } from '@patternfly/react-core';
 
 import ChangeQueue from './ChangeQueue'
 
@@ -125,6 +126,15 @@ class Pipeline extends React.Component {
             <small>
               <p>{pipeline.description.split(/\r?\n\r?\n/)}</p>
             </small>) : ''}
+          <Tooltip position="top"
+            content={<div>Unprocessed pipeline specific events</div>}>
+            <small tabIndex="0" className="pf-u-color-200">
+              Events:&nbsp;
+              {pipeline.trigger_events} trigger events,&nbsp;
+              {pipeline.management_events} management events,&nbsp;
+              {pipeline.result_events} results.
+            </small>
+          </Tooltip>
         </div>
         {pipeline.change_queues.filter(item => item.heads.length > 0)
           .filter(item => (!filter || (
