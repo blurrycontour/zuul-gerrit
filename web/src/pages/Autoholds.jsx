41d6008a3d681b5fc3783226014e1279a17f5f92
Matthieu Huin <mhuin@redhat.com>
2021-07-27 18:54:46 +0200

    Web UI: Add "Create Autohold Request" form, improve API error messages
    Add a "create autohold request" button on the autoholds page for admin
    users.
    Refactor autohold form modal so that it can be reused in autoholds and
    build pages.
    Improve error messages when using admin actions: if the API call returns
    an error description, display it in the toast. Otherwise return the HTTP
    error code.
    
    Change-Id: I9d1cc8a1d751e7b4ce32e7abecc2ad9f3e96f676

--- web/src/pages/Autoholds.jsx
+++ web/src/pages/Autoholds.jsx
@@ -15,47 +15,103 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { PageSection, PageSectionVariants } from '@patternfly/react-core'
+import { Flex, FlexItem, PageSection, PageSectionVariants } from '@patternfly/react-core'
 
 import { fetchAutoholdsIfNeeded } from '../actions/autoholds'
 import AutoholdTable from '../containers/autohold/AutoholdTable'
 
+import {
+  AddCircleOIcon,
+} from '@patternfly/react-icons'
+import AutoholdModal from '../containers/autohold/autoholdModal'
+import { IconProperty } from '../Misc'
 
 
 class AutoholdsPage extends React.Component {
   static propTypes = {
     tenant: PropTypes.object,
+    user: PropTypes.object,
     remoteData: PropTypes.object,
     dispatch: PropTypes.func
+  }
+
+  constructor() {
+    super()
+
+    this.state = {
+      showAutoholdModal: false,
+    }
+  }
+
+  setShowAutoholdModal = (value) => {
+    this.setState(() => ({ showAutoholdModal: value }))
+  }
+
+  renderAutoholdButton() {
+    const value = (<span style={{
+      cursor: 'pointer',
+      color: 'var(--pf-global--primary-color--100)'
+    }}
+      title="Create a new autohold request to hold nodes in case of a build failure"
+      onClick={(event) => {
+        event.preventDefault()
+        this.setShowAutoholdModal(true)
+      }}
+    >
+      Create Request
+    </span>)
+    return (
+      <IconProperty
+        icon={<AddCircleOIcon />}
+        value={value}
+      />
+
+    )
   }
 
   updateData = (force) => {
     this.props.dispatch(fetchAutoholdsIfNeeded(this.props.tenant, force))
   }
 
-  componentDidMount () {
+  componentDidMount() {
     document.title = 'Zuul Autoholds'
     if (this.props.tenant.name) {
       this.updateData()
     }
   }
 
-  componentDidUpdate (prevProps) {
+  componentDidUpdate(prevProps) {
     if (this.props.tenant.name !== prevProps.tenant.name) {
       this.updateData()
     }
   }
 
-  render () {
-    const { remoteData } = this.props
+  render() {
+    const { tenant, user, remoteData } = this.props
     const autoholds = remoteData.autoholds
+    const { showAutoholdModal } = this.state
 
     return (
-      <PageSection variant={PageSectionVariants.light}>
-        <AutoholdTable
-          autoholds={autoholds}
-          fetching={remoteData.isFetching} />
-      </PageSection>
+      <>
+        {(user.isAdmin && user.scope.indexOf(tenant.name) !== -1) && (
+          <PageSection>
+            <Flex flex={{ default: 'flex_1' }}>
+              <FlexItem>
+                {this.renderAutoholdButton()}
+              </FlexItem>
+            </Flex>
+          </PageSection>
+        )}
+        <PageSection variant={PageSectionVariants.light}>
+          <AutoholdTable
+            autoholds={autoholds}
+            fetching={remoteData.isFetching} />
+        </PageSection>
+        {<AutoholdModal
+          showAutoholdModal={showAutoholdModal}
+          setShowAutoholdModal={this.setShowAutoholdModal}
+        />}
+      </>
     )
   }
 }
@@ -63,4 +119,5 @@ class AutoholdsPage extends React.Component {
 export default connect(state => ({
   tenant: state.tenant,
   remoteData: state.autoholds,
+  user: state.user,
 }))(AutoholdsPage)
