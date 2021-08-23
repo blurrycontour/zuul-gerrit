bfcc45f516bb1e0e317055b7e2a40d506e08f4cb
Ian Wienand <iwienand@redhat.com>
2021-08-23 16:48:09 +1000

    web: Projects: use composable table
    This converts the projects overview page to a composable table.
    
    Sorting is enabled for relevant fields.  A filter search is also
    added.
    
    Similar to Ibcf260fda08486b489797ea8073e7a057d7040cd the sorting state
    is kept in redux.  However the filter is done via a selector, which I
    believe to be the correct pattern to use.
    
    Change-Id: I4b073fbd1a0ee36483158a74fbc8c99ef9842c43

--- web/src/reducers/projects.js
+++ web/src/reducers/projects.js
@@ -15,33 +15,97 @@
 import {
   PROJECTS_FETCH_FAIL,
   PROJECTS_FETCH_REQUEST,
-  PROJECTS_FETCH_SUCCESS
+  PROJECTS_FETCH_SUCCESS,
+  PROJECTS_SORT,
+  PROJECTS_FILTER
 } from '../actions/projects'
 
-import update from 'immutability-helper'
+/*
+ * List of fields returned from API to show
+ * as columns
+ */
+const table_columns = [
+  'name',
+  'connection_name',
+  'type',
+  'builds'
+]
+
+/*
+ * Pretty name of table_columns entry,
+ * and if this column is sortable
+ */
+const table_columns_descriptions = [
+  ['Project', true ],
+  ['Connection Type', true],
+  ['Project Type', true],
+  ['Recent Builds', false]
+]
+
+function sortRows(rows, index, direction) {
+  if (index === -1) {
+    return rows
+  }
+
+  return rows.slice().sort((a, b) => {
+    // table_columns[index] is the name of the field
+    // to sort on
+    const field = table_columns[index]
+
+    let av = a[field] ? a[field] : ''
+    let bv = b[field] ? b[field] : ''
+
+    return direction === 'asc' ?
+      av.localeCompare(bv) :
+      bv.localeCompare(av)
+  })
+}
 
 export default (state = {
   isFetching: false,
-  projects: {},
+  projects: [],
+  sortIndex: -1,
+  sortDirection: 'none',
+  filterTerms: {},
+  filterString: '',
+  table_columns: table_columns,
+  table_columns_descriptions: table_columns_descriptions
 }, action) => {
   switch (action.type) {
     case PROJECTS_FETCH_REQUEST:
-      return {
-        isFetching: true,
-        projects: state.projects,
-      }
-    case PROJECTS_FETCH_SUCCESS:
-      return {
-        isFetching: false,
-        projects: update(
-          state.projects, {$merge: {[action.tenant]: action.projects}}),
-      }
-    case PROJECTS_FETCH_FAIL:
-      return {
-        isFetching: false,
-        projects: state.projects,
-      }
-    default:
-      return state
+    return {
+      ...state,
+      isFetching: true,
+    }
+  case PROJECTS_FETCH_SUCCESS:
+    return {
+      ...state,
+      isFetching: false,
+      projects: sortRows(action.projects,
+                         state.sortIndex,
+                         state.sortDirection)
+    }
+  case PROJECTS_FETCH_FAIL:
+    return {
+      ...state,
+      isFetching: false
+    }
+  case PROJECTS_SORT:
+    return {
+      ...state,
+      projects: sortRows(state.projects,
+                         action.sortIndex,
+                         action.sortDirection),
+      sortIndex: action.sortIndex,
+      sortDirection: action.sortDirection
+    }
+  case PROJECTS_FILTER:
+    return {
+      ...state,
+      filterTerms: action.filterTerms,
+      filterString: action.filterString,
+    }
+  default:
+    return state
   }
 }
