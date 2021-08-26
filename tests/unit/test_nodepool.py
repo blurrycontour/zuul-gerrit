1222e547d89747149f508398cc6f9c267d9302d5
James E. Blair <jim@acmegating.com>
2021-08-26 14:33:31 -0700

    Make node requests persistent
    The original Nodepool protocol specified that node requests should
    be ephemeral, that way if the requestor crashed before accepting
    the nodes, the request would automatically be cleaned up and the
    nodes returned.  This doesn't comport with multiple schedulers, as
    we will soon expect schedulers to stop and start routinely while
    we want the node requests they spawn to persist and be handled by
    other schedulers.
    
    Fortunately, Nodepool doesn't really care if the request is
    ephemeral or not.  So we'll drop the "ephemeral" flag.
    
    But in the short term, we will be stopping the scheduler and that
    will leave orphan node requests.  And even in the long term, we
    may have a complete Zuul system shutdown or even a bug which may
    leak node requests, so we still need a way of deleting node requests
    which don't belong.  To handle that, we add a cleanup routine which
    we run immediately on startup and every hour that looks for node
    requests created by this Zuul system but don't correspond to any
    queue entries.  We create a new UUID to identify the Zuul system
    and store it in ZK (so that if Nodepool has any other users we
    don't delete their requests).
    
    We no longer need to resubmit requests on connection loss, so tests
    addressing that behavior are removed.
    
    Change-Id: Ie22e99ef71cbe6b31d40c25a21498c1e867ca777

--- tests/unit/test_nodepool.py
+++ tests/unit/test_nodepool.py
@@ -12,14 +12,12 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-
-import threading
 import time
 
 from zuul import model
 import zuul.nodepool
 
-from tests.base import BaseTestCase, FakeNodepool, iterate_timeout
+from tests.base import BaseTestCase, FakeNodepool
 from zuul.zk import ZooKeeperClient
 from zuul.zk.nodepool import ZooKeeperNodepool
 
@@ -103,24 +101,6 @@ class TestNodepool(TestNodepoolBase):
             self.assertIsNone(node.lock)
             self.assertEqual(node.state, 'used')
 
-    def test_node_request_disconnect(self):
-        # Test that node requests are re-submitted after disconnect
-
-        nodeset = model.NodeSet()
-        nodeset.addNode(model.Node(['controller'], 'ubuntu-xenial'))
-        nodeset.addNode(model.Node(['compute'], 'ubuntu-xenial'))
-        job = model.Job('testjob')
-        job.nodeset = nodeset
-        self.fake_nodepool.pause()
-        request = self.nodepool.requestNodes(
-            "test-uuid", job, "tenant", "pipeline", "provider", 0, 0)
-        self.zk_client.client.stop()
-        self.zk_client.client.start()
-        self.fake_nodepool.unpause()
-        self.waitForRequests()
-        self.assertEqual(len(self.provisioned_requests), 1)
-        self.assertEqual(request.state, 'fulfilled')
-
     def test_node_request_canceled(self):
         # Test that node requests can be canceled
 
@@ -136,60 +116,6 @@ class TestNodepool(TestNodepoolBase):
 
         self.waitForRequests()
         self.assertEqual(len(self.provisioned_requests), 0)
-
-    def test_accept_nodes_resubmitted(self):
-        # Test that a resubmitted request would not lock nodes
-
-        nodeset = model.NodeSet()
-        nodeset.addNode(model.Node(['controller'], 'ubuntu-xenial'))
-        nodeset.addNode(model.Node(['compute'], 'ubuntu-xenial'))
-        job = model.Job('testjob')
-        job.nodeset = nodeset
-        request = self.nodepool.requestNodes(
-            "test-uuid", job, "tenant", "pipeline", "provider", 0, 0)
-        self.waitForRequests()
-        self.assertEqual(len(self.provisioned_requests), 1)
-        self.assertEqual(request.state, 'fulfilled')
-
-        # Accept the nodes, passing a different ID
-        new_nodeset = self.nodepool.checkNodeRequest(
-            request, "invalid", nodeset)
-        self.assertIsNone(new_nodeset)
-        # Don't call acceptNodes here as the node request wasn't accepted.
-
-        # Nothing we have done has returned an updated nodeset with
-        # real node records, so we need to do that ourselves to verify
-        # they are still unused.
-        for node_id, node in zip(request.nodes, nodeset.getNodes()):
-            self.nodepool.zk_nodepool.updateNode(node, node_id)
-            self.assertIsNone(node.lock)
-            self.assertEqual(node.state, 'ready')
-
-    def test_accept_nodes_lost_request(self):
-        # Test that a lost request would not lock nodes
-
-        nodeset = model.NodeSet()
-        nodeset.addNode(model.Node(['controller'], 'ubuntu-xenial'))
-        nodeset.addNode(model.Node(['compute'], 'ubuntu-xenial'))
-        job = model.Job('testjob')
-        job.nodeset = nodeset
-        request = self.nodepool.requestNodes(
-            "test-uuid", job, "tenant", "pipeline", "provider", 0, 0)
-        self.waitForRequests()
-        self.assertEqual(len(self.provisioned_requests), 1)
-        self.assertEqual(request.state, 'fulfilled')
-
-        self.zk_nodepool.deleteNodeRequest(request)
-
-        # Accept the nodes
-        new_nodeset = self.nodepool.checkNodeRequest(
-            request, request.id, nodeset)
-        self.assertIsNone(new_nodeset)
-        # Don't call acceptNodes here as the node request wasn't accepted.
-
-        for node in nodeset.getNodes():
-            self.assertIsNone(node.lock)
-            self.assertEqual(node.state, 'unknown')
 
     def test_node_request_priority(self):
         # Test that requests are satisfied in priority order
@@ -210,57 +136,3 @@ class TestNodepool(TestNodepoolBase):
         self.assertEqual(request1.state, 'fulfilled')
         self.assertEqual(request2.state, 'fulfilled')
         self.assertTrue(request2.state_time < request1.state_time)
-
-
-class TestNodepoolResubmit(TestNodepoolBase):
-    def setUp(self):
-        super().setUp()
-        self.run_once = False
-        self.disconnect_event = threading.Event()
-
-    def onNodesProvisioned(self, request):
-        # This is a scheduler method that the nodepool class calls
-        # back when a request is provisioned.
-        d = request.toDict()
-        d['_oid'] = request.id
-        self.provisioned_requests.append(d)
-        if not self.run_once:
-            self.run_once = True
-            self.disconnect_event.set()
-
-    def _disconnect_thread(self):
-        self.disconnect_event.wait()
-        self.zk_client.client.stop()
-        self.zk_client.client.start()
-        self.nodepool.checkNodeRequest(
-            self.request, self.request.id, self.nodeset)
-
-    def test_node_request_disconnect_late(self):
-        # Test that node requests are re-submitted after a disconnect
-        # which happens right before we accept the node request.
-
-        disconnect_thread = threading.Thread(target=self._disconnect_thread)
-        disconnect_thread.daemon = True
-        disconnect_thread.start()
-
-        nodeset = model.NodeSet()
-        nodeset.addNode(model.Node(['controller'], 'ubuntu-xenial'))
-        nodeset.addNode(model.Node(['compute'], 'ubuntu-xenial'))
-        self.nodeset = nodeset
-        job = model.Job('testjob')
-        job.nodeset = nodeset
-        self.request = self.nodepool.requestNodes(
-            "test-uuid", job, "tenant", "pipeline", "provider", 0, 0)
-        for x in iterate_timeout(30, 'fulfill request'):
-            if len(self.provisioned_requests) == 2:
-                break
-        # Both requests should be fulfilled and have nodes.  The
-        # important thing here is that they both have the same number
-        # of nodes (and the second request did not append extra nodes
-        # to the first).
-        self.assertEqual(self.provisioned_requests[0]['state'], 'fulfilled')
-        self.assertEqual(self.provisioned_requests[1]['state'], 'fulfilled')
-        self.assertNotEqual(self.provisioned_requests[0]['_oid'],
-                            self.provisioned_requests[1]['_oid'])
-        self.assertEqual(len(self.provisioned_requests[0]['nodes']), 2)
-        self.assertEqual(len(self.provisioned_requests[1]['nodes']), 2)
