d6a15703b905b85bc7c143410a691011c890a731
Matthieu Huin <mhuin@redhat.com>
2021-09-09 11:12:28 +0200

    [web] Early support for pagination in builds, buildsets search
    Paginate results when searching for builds. For now it is only
    possible to change the amount of results returned to 10, 20, 50 or
    100 results. This change ensures compatibility with an upcoming,
    API-breaking change to support fully paginated searches.
    
    Change-Id: If2985d6bcb194e3b074a860dab8cd5958b2577ec

--- web/src/pages/Builds.jsx
+++ web/src/pages/Builds.jsx
@@ -16,7 +16,7 @@ import * as React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import 'moment-duration-format'
-import { PageSection, PageSectionVariants } from '@patternfly/react-core'
+import { PageSection, PageSectionVariants, Pagination } from '@patternfly/react-core'
 
 import { fetchBuilds } from '../api'
 import {
@@ -127,13 +127,27 @@ class BuildsPage extends React.Component {
       },
     ]
 
+    const _filters = getFiltersFromUrl(props.location, this.filterCategories)
+    const perPage = _filters.limit[0]
+      ? parseInt(_filters.limit[0])
+      : 50
+    const currentPage = _filters.skip[0]
+      ? Math.floor(parseInt(_filters.skip[0] / perPage)) + 1
+      : 1
+
     this.state = {
-      builds: [],
+      builds: {
+        builds: [],
+        offset: null,
+        total: null,
+      },
       fetching: false,
-      filters: getFiltersFromUrl(props.location, this.filterCategories),
+      filters: _filters,
       projectsFetched: false,
       pipelinesFetched: false,
       jobsFetched: false,
+      resultsPerPage: perPage,
+      currentPage: currentPage,
     }
   }
 
@@ -194,9 +208,17 @@ class BuildsPage extends React.Component {
     // fetching shows infinitely or can we catch this and show an erro state in
     // the table instead?
     fetchBuilds(this.props.tenant.apiPrefix, queryString).then((response) => {
+      let _builds = Array.isArray(response.data) ?
+        {
+          builds: response.data,
+          offset: 0,
+          total: response.data.length
+        } :
+        response.data
       this.setState({
-        builds: response.data,
+        builds: _builds,
         fetching: false,
+        currentPage: Math.floor(_builds.offset / this.state.resultsPerPage) + 1,
       })
     })
   }
@@ -241,15 +263,39 @@ class BuildsPage extends React.Component {
     this.handleFilterChange(filters)
   }
 
+  handlePerPageSelect = (event, perPage) => {
+    const { filters } = this.state
+    this.setState({ resultsPerPage: perPage })
+    const newFilters = { ...filters, limit: [perPage,] }
+    this.handleFilterChange(newFilters)
+  }
+
+  handleSetPage = (event, pageNumber) => {
+    const { filters, resultsPerPage } = this.state
+    this.setState({ currentPage: pageNumber })
+    let offset = resultsPerPage * (pageNumber - 1)
+    const newFilters = { ...filters, skip: [offset,] }
+    this.handleFilterChange(newFilters)
+  }
+
+
   render() {
     const { history } = this.props
-    const { builds, fetching, filters } = this.state
+    const { builds, fetching, filters, resultsPerPage, currentPage } = this.state
     return (
       <PageSection variant={PageSectionVariants.light}>
         <FilterToolbar
           filterCategories={this.filterCategories}
           onFilterChange={this.handleFilterChange}
           filters={filters}
+        />
+        <Pagination
+          itemCount={builds.total}
+          perPage={resultsPerPage}
+          page={currentPage}
+          widgetId="pagination-menu"
+          onPerPageSelect={this.handlePerPageSelect}
+          onSetPage={this.handleSetPage}
         />
         <BuildTable
           builds={builds}
