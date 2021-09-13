71923aac7b9d7f3c4eef3a13fed815f82f58bd88
James E. Blair <jim@acmegating.com>
2021-09-13 16:20:01 -0700

    Move time database to SQL
    To support multiple schedulers, move the time database into the
    SQL database.
    
    We could use the existing build table, however performance can
    be somewhat unpredictable (at least with postgres, we observed
    a range of 2ms to 400ms to perform the necessary query depending
    on the project/job selected and the contents of the database).
    So instead, we create a new table dedicated to this task, with
    one row per tenant-project-branch-job combination.  This row
    has 10 slots for times and rotates the values through accordingly.
    
    There is a small chance we may collide when updating this table;
    that's not important so we don't attempt to recover (we will just
    lose that time update).
    
    Nothing in the scheduler uses the state directory anymore, so clean
    up the docs around that.  The executor still has a state dir where
    it may install ansible-related files.
    
    Switch an error log message to exception level so if we can't
    connect to the db (or create tables) we can see the actual error.
    
    Change-Id: Id3c64be7a05c9edc849e698200411ad436a1334d

---
+++ zuul/driver/sql/alembic/versions/22c86b8fa818_time_table.py
@@ -0,0 +1,59 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""time_table
+
+Revision ID: 22c86b8fa818
+Revises: 40c49b6fc2e3
+Create Date: 2021-09-17 14:39:22.974763
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '22c86b8fa818'
+down_revision = '40c49b6fc2e3'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+TIME_TABLE = 'zuul_time'
+
+
+def upgrade(table_prefix=''):
+    op.create_table(
+        table_prefix + TIME_TABLE,
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('tenant', sa.String(255)),
+        sa.Column('project', sa.String(255)),
+        sa.Column('branch', sa.String(255)),
+        sa.Column('job_name', sa.String(255)),
+        sa.Column('last_updated', sa.DateTime),
+        sa.Column('t0', sa.Integer),
+        sa.Column('t1', sa.Integer),
+        sa.Column('t2', sa.Integer),
+        sa.Column('t3', sa.Integer),
+        sa.Column('t4', sa.Integer),
+        sa.Column('t5', sa.Integer),
+        sa.Column('t6', sa.Integer),
+        sa.Column('t7', sa.Integer),
+        sa.Column('t8', sa.Integer),
+        sa.Column('t9', sa.Integer),
+        sa.UniqueConstraint('tenant', 'project', 'branch', 'job_name',
+                            name='time_idx'),
+    )
+
+
+def downgrade():
+    raise Exception("Downgrades not supported")
