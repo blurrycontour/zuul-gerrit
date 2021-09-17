c833bfb35360fb706b80461e599ef2f4750ebe17
Felix Edel <felix.edel@bmw.de>
2021-09-17 08:17:23 +0200

    Fix wrong if condition in job request state watch
    This fixes a wrong if condition in the _watchState function of the
    job_request_queue which led to an AttributeError in case the event
    didn't provide any data.
    
    Traceback (most recent call last):
      File "/home/zuul/src/opendev.org/zuul/zuul/zuul/zk/vendor/watchers.py", line 177, in _log_func_exception
          result = self._func(data, stat, event)
      File "/home/zuul/src/opendev.org/zuul/zuul/zuul/zk/job_request_queue.py", line 126, in watch
          return self._watchState(path, data, stat, event)
      File "/home/zuul/src/opendev.org/zuul/zuul/zuul/zk/job_request_queue.py", line 133, in _watchState
          content = self._bytesToDict(data)
      File "/home/zuul/src/opendev.org/zuul/zuul/zuul/zk/job_request_queue.py", line 540, in _bytesToDict
          return json.loads(data.decode("utf-8"))
    AttributeError: 'NoneType' object has no attribute 'decode'
    
    Change-Id: Ib3f6273594b936c3c9d7b08d832783d09f6967e7

--- zuul/zk/job_request_queue.py
+++ zuul/zk/job_request_queue.py
@@ -127,7 +127,7 @@ class JobRequestQueue(ZooKeeperSimpleBase):
         return watch
 
     def _watchState(self, path, data, stat, event=None):
-        if not event or (event.type == EventType.CHANGED and data is not None):
+        if (not event or event.type == EventType.CHANGED) and data is not None:
             # As we already get the data and the stat value, we can directly
             # use it without asking ZooKeeper for the data again.
             content = self._bytesToDict(data)
@@ -158,7 +158,7 @@ class JobRequestQueue(ZooKeeperSimpleBase):
             ):
                 self.request_callback()
 
-        elif (event.type == EventType.DELETED or data is None):
+        elif ((event and event.type == EventType.DELETED) or data is None):
             request = self._cached_requests.get(path)
             with suppress(KeyError):
                 del self._cached_requests[path]
