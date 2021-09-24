6e9c87f833c612529a6580a2287c3aa1083ed8f4
Simon Westphahl <simon.westphahl@bmw.de>
2021-09-24 16:11:13 +0200

    Store change queues in Zookeeper
    The change queue state is store in the following Zookeeper path:
    
        /zuul/<tenant>/pipeline/<pipeline>/queue/<layout-uuid>/<queue-uuid>
    
    TODO:
    * cleanup leaked queues for old layouts
    
    Change-Id: I0a64bd9adc7b9f8f7a775280bb7a01ace22baac4

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -38,6 +38,12 @@ class Dummy(object):
             setattr(self, k, v)
 
 
+class DummyChangeQueue(model.ChangeQueue):
+
+    def _save(self, ctx, create=False):
+        pass
+
+
 class TestJob(BaseTestCase):
     def setUp(self):
         self._env_fixture = self.useFixture(
@@ -64,7 +70,7 @@ class TestJob(BaseTestCase):
         self.pipeline.source_context = self.context
         self.pipeline.manager = mock.Mock()
         self.layout.addPipeline(self.pipeline)
-        self.queue = model.ChangeQueue(self.pipeline)
+        self.queue = DummyChangeQueue.new(None, pipeline=self.pipeline)
         self.pcontext = configloader.ParseContext(
             self.connections, None, self.tenant, AnsibleManager())
 
@@ -163,7 +169,7 @@ class TestJob(BaseTestCase):
 
     @mock.patch("zuul.model.zkobject.ZKObject._save")
     def test_job_inheritance_job_tree(self, save_mock):
-        queue = model.ChangeQueue(self.pipeline)
+        queue = DummyChangeQueue.new(None, pipeline=self.pipeline)
 
         base = self.pcontext.job_parser.fromYaml({
             '_source_context': self.context,
@@ -234,7 +240,7 @@ class TestJob(BaseTestCase):
 
     @mock.patch("zuul.model.zkobject.ZKObject._save")
     def test_inheritance_keeps_matchers(self, save_mock):
-        queue = model.ChangeQueue(self.pipeline)
+        queue = DummyChangeQueue.new(None, pipeline=self.pipeline)
 
         base = self.pcontext.job_parser.fromYaml({
             '_source_context': self.context,
