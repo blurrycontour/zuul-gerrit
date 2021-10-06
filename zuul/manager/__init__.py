dded71720b0eb7e8efcda8400594ab77e26de8df
Simon Westphahl <simon.westphahl@bmw.de>
2021-10-06 14:34:59 +0200

    Store RepoFiles for a build set in Zookeeper
    RepoFiles are stored in the following Zookeeper path:
    
        /zuul/<tenant>/pipeline/<pipeline>/item/<item-uuid>/buildset/<buildset-uuid>/files
    
    Since the files can become very large the data will be sharded.
    
    Change-Id: Ic591b53308b8dd0dec5530a8f040c7d487026182

--- zuul/manager/__init__.py
+++ zuul/manager/__init__.py
@@ -1617,9 +1617,11 @@ class PipelineManager(metaclass=ABCMeta):
                 build_set.commit = event.commit
                 items_ahead = item.getNonLiveItemsAhead()
                 for index, item in enumerate(items_ahead):
-                    files = item.current_build_set.files
-                    files.setFiles(event.files[:index + 1])
-                build_set.files.setFiles(event.files)
+                    with item.current_build_set.activeContext(
+                            self.current_context):
+                        item.current_build_set.setFiles(
+                            event.files[:index + 1])
+                build_set.setFiles(event.files)
             elif event.updated:
                 build_set.commit = (item.change.newrev or
                                     '0000000000000000000000000000000000000000')
