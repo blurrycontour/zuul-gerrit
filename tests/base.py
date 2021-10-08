b10ec6c95ca7fae581734a9a493d8bc164d685bc
Artem Goncharov <Artem.goncharov@gmail.com>
2021-10-08 09:17:22 +0200

    Check blocking_discussions_resolved in gitlab driver
    In addition to `merge_status` attribute `blocking_discussions_resolved`
    should be checked to know whether it makes sense to attempt merging.
    In the project setting it is possible to enable "All discussions must be
    resolved" check box what will result in the attribute to be set to false
    once there are open discussions. With that (while merge_status is still
    can_be_merged) the merge request can not be merged.
    
    Sadly this is another badly documented case.
    
    Change-Id: Iba3c7b424fb8acb3134622776eb1518ffddd5374

--- tests/base.py
+++ tests/base.py
@@ -2109,6 +2109,7 @@ class FakeGitlabMergeRequest(object):
             self.gitlab.server, self.project, self.number)
         self.base_sha = base_sha
         self.approved = False
+        self.blocking_discussions_resolved = True
         self.mr_ref = self._createMRRef(base_sha=base_sha)
         self._addCommitInMR(files=files)
 
