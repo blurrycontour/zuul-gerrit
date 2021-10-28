c573df7b0a184e936a795077755570c78431d110
Simon Westphahl <simon.westphahl@bmw.de>
2021-10-28 10:12:09 +0200

    wip: Allow refreshing project branches
    The connection's project branch cache is normally cleared when a related
    event is processes. Since the event processing only happens on a single
    scheduler the project branch cache will be outdated on the other
    schedulers.
    
    This is a WIP fix attempt that refreshes the branch cache when
    (re)loading a tenant. However, this doesn't solve two other cases where
    we need the project branch cache to be up-to-date:
    
    * checking if a reconfig is needed when forwarding trigger events based
      on the list of cached branches
    * generating trigger events in the timer driver
    
    The usage in the timer driver could be change to use
    `tenant.getProjectBranches()` as this will be updated during a
    reconfiguration.
    
    Change-Id: I173b4b4c9a24a74373662570073b61081571c2d9

--- zuul/driver/pagure/paguresource.py
+++ zuul/driver/pagure/paguresource.py
@@ -105,8 +105,8 @@ class PagureSource(BaseSource):
             self.connection.addProject(p)
         return p
 
-    def getProjectBranches(self, project, tenant):
-        return self.connection.getProjectBranches(project, tenant)
+    def getProjectBranches(self, project, tenant, refresh=False):
+        return self.connection.getProjectBranches(project, tenant, refresh)
 
     def getProjectOpenChanges(self, project):
         """Get the open changes for a project."""
