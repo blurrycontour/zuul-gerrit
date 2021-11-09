bfc9a22606ed9e4f124c497364aff7e5f95d2be6
Simon Westphahl <simon.westphahl@bmw.de>
2021-11-09 11:35:14 +0100

    Use config errors directly from layout in zuul-web
    Change-Id: I47931f97c072029a03bb5c850a07e048a27a3ce7

--- zuul/rpclistener.py
+++ zuul/rpclistener.py
@@ -148,7 +148,6 @@ class RPCListener(RPCListenerBase):
         'job_list',
         'project_get',
         'project_list',
-        'config_errors_list',
     ]
 
     def start(self):
@@ -359,16 +358,3 @@ class RPCListener(RPCListenerBase):
             output.append(pobj)
         job.sendWorkComplete(json.dumps(
             sorted(output, key=lambda project: project["name"])))
-
-    def handle_config_errors_list(self, job):
-        args = json.loads(job.arguments)
-        tenant = self.sched.abide.tenants.get(args.get("tenant"))
-        output = []
-        if not tenant:
-            job.sendWorkComplete(json.dumps(None))
-            return
-        for err in tenant.layout.loading_errors.errors:
-            output.append({
-                'source_context': err.key.context.toDict(),
-                'error': err.error})
-        job.sendWorkComplete(json.dumps(output))
