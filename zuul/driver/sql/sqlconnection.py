44431a1bd001582d07c2b7c938cd49fd915d0f0b
James E. Blair <jim@acmegating.com>
2022-02-15 09:14:49 -0800

    Add buildset start/end db columns
    Add two columns to the buildset table in the database: the timestamp
    of the start of the first build and the end of the last build.  These
    are calculated from the builds in the webui buildset page, but they
    are not available in the buildset listing without performing
    a table join on the server side.
    
    To keep the buildset query simple and fast, this adds the columns to
    the buildset table (which is a minor data duplication).
    
    Return the new values in the rest api.
    
    Change-Id: Ie162e414ed5cf09e9dc8f5c92e07b80934592fdf

--- zuul/driver/sql/sqlconnection.py
+++ zuul/driver/sql/sqlconnection.py
@@ -61,8 +61,9 @@ class DatabaseSession(object):
 
     def getBuilds(self, tenant=None, project=None, pipeline=None,
                   change=None, branch=None, patchset=None, ref=None,
-                  newrev=None, event_id=None, event_timestamp=None, uuid=None,
-                  job_name=None, voting=None, nodeset=None,
+                  newrev=None, event_id=None, event_timestamp=None,
+                  first_build_start_time=None, last_build_end_time=None,
+                  uuid=None, job_name=None, voting=None, nodeset=None,
                   result=None, provides=None, final=None, held=None,
                   complete=None, sort_by_buildset=False, limit=50,
                   offset=0, idx_min=None, idx_max=None):
@@ -104,6 +105,10 @@ class DatabaseSession(object):
         q = self.listFilter(q, buildset_table.c.event_id, event_id)
         q = self.listFilter(
             q, buildset_table.c.event_timestamp, event_timestamp)
+        q = self.listFilter(
+            q, buildset_table.c.first_build_start_time, first_build_start_time)
+        q = self.listFilter(
+            q, buildset_table.c.last_build_end_time, last_build_end_time)
         q = self.listFilter(q, build_table.c.uuid, uuid)
         q = self.listFilter(q, build_table.c.job_name, job_name)
         q = self.listFilter(q, build_table.c.voting, voting)
@@ -342,6 +347,8 @@ class SQLConnection(BaseConnection):
             uuid = sa.Column(sa.String(36))
             event_id = sa.Column(sa.String(255), nullable=True)
             event_timestamp = sa.Column(sa.DateTime, nullable=True)
+            first_build_start_time = sa.Column(sa.DateTime, nullable=True)
+            last_build_end_time = sa.Column(sa.DateTime, nullable=True)
 
             sa.Index(self.table_prefix + 'project_pipeline_idx',
                      project, pipeline)
