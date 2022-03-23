3d8760689690fd550ca53bab8410b0fab3a45b0d
Dong Zhang <dong.zhang@bmw.de>
2022-03-23 10:47:45 +0100

    Fix bug in getting changed files
    The fix including 2 parts:
    1. For Gtihub, we use the base_sha instead of target branch to
       be passed as "tosha" parameter to get precise changed files
    2. In method getFilesChanges(), use diff() result to filter out
       those files that changed and reverted between commits.
    
    The reason we do not direcly use diff() is that for those
    drivers other than github, the "base_sha" is not available yet,
    using diff() may include unexpected files when target branch
    has diverged from the feature branch.
    
    This solution works for  99.9% of the caseses, it may still get
    incorrect list of changed files in following corner case:
    1. In non-github connection, whose base_sha is not implented, and
    2. Files changed and reverted between commits in the change, and
    3. The same file has also diverged in target branch.
    
    The above corner case can be fixed by making base_sha available in
    other drivers.
    
    Change-Id: Ifae7018a8078c16f2caf759ae675648d8b33c538

--- zuul/merger/merger.py
+++ zuul/merger/merger.py
@@ -24,6 +24,7 @@ import re
 import shutil
 import time
 from concurrent.futures.process import BrokenProcessPool
+import traceback
 
 import git
 import gitdb
@@ -683,6 +684,12 @@ class Repo(object):
         return ret
 
     def getFilesChanges(self, branch, tosha=None, zuul_event_id=None):
+
+        self.log.info("..............getFilesChanges() branch:%s, tosha: %s", branch, tosha)
+
+        for line in traceback.format_stack():
+            self.log.info(".......... %s", line.strip())
+
         repo = self.createRepoObject(zuul_event_id)
         self.fetch(branch, zuul_event_id=zuul_event_id)
         head = repo.commit(
@@ -690,9 +697,26 @@ class Repo(object):
         files = set()
 
         if tosha:
+            # When "tosha" is the target branch, the result of diff() correctly
+            # excluds the files whose changes are reverted between the commits.
+            # But it may also include the files that are not changed in the
+            # referenced commit(s). This can happen, e.g. if the target branch
+            # has diverged from the feature branch.
+            # The idea is to use this result to filter out the files whose
+            # changes are reverted between the commits.
+            diff_files = set()
+            head_commit = repo.commit(head.hexsha)
+            diff_index = head_commit.diff(tosha)
+            diff_files.update((item.a_path for item in diff_index))
+
+            self.log.info(str(len(diff_files)))
+            self.log.info(str(diff_files))
+
             commit_diff = "{}..{}".format(tosha, head.hexsha)
             for cmt in repo.iter_commits(commit_diff, no_merges=True):
-                files.update(cmt.stats.files.keys())
+                files.update(f for f in cmt.stats.files.keys()
+                             if f in diff_files)
+            self.log.info(str(files))
         else:
             files.update(head.stats.files.keys())
         return list(files)
