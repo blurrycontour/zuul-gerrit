69ccde00f88b0df17561d367cdab541d407348bc
Dong Zhang <dong.zhang@bmw.de>
2022-04-26 12:56:43 +0200

    Fix a bug that re-enqueue may skip soft dependent jobs
    Before this fix, a re-enqueue would do setResult for all builds,
    skipped builds are considered non "SUCCESS", and their soft
    dependent childs would be skipped, too.
    
    Change-Id: I194e77968376621998f417b96c27fce71512f542

--- zuul/model.py
+++ zuul/model.py
@@ -4838,7 +4838,7 @@ class QueueItem(zkobject.ZKObject):
                         skip, skip_soft=True)
                     skipped += to_skip
 
-        elif build.result != 'SUCCESS' and not build.paused:
+        elif build.result not in ('SUCCESS', 'SKIPPED') and not build.paused:
             to_skip = job_graph.getDependentJobsRecursively(
                 build.job.name)
             skipped += to_skip
