b784d53f88cb9e6c5a48b9750c08b6fa554dbff4
James E. Blair <jim@acmegating.com>
2022-06-09 08:11:12 -0700

    Clarify missing merge requirement message
    When Zuul dequeues a change because it no longer meets the code
    review systems merge requirements, it says it was dequeued
    "due to a missing requirement".  To clarify it's the code review
    system's merge requirements (as opposed to some kind of job,
    artifact, or other change), update the text to "due to a
    missing merge requirement".
    
    Change-Id: Ia76c0564726c0e1317302e76f3f2bdae2c1ff431

--- zuul/reporter/__init__.py
+++ zuul/reporter/__init__.py
@@ -182,7 +182,7 @@ class BaseReporter(object, metaclass=abc.ABCMeta):
             msg = 'This change depends on a change that failed to merge.\n'
         elif item.dequeued_missing_requirements:
             msg = ('This change is unable to merge '
-                   'due to a missing requirement.\n')
+                   'due to a missing merge requirement.\n')
         elif item.isBundleFailing():
             msg = 'This change is part of a bundle that failed.\n'
             if with_jobs:
