86acaa97186f289a866fb01b96bc5398a1d51f6a
Artem Goncharov <Artem.goncharov@gmail.com>
2022-06-13 16:09:51 +0200

    [WIP] Add Gitlab commit status reporting
    Gitlab support commit status reporting.
    
    Change-Id: I5a3837bf13d168c75def792b7726e862665be87a

--- zuul/driver/gitlab/gitlabreporter.py
+++ zuul/driver/gitlab/gitlabreporter.py
@@ -49,6 +49,7 @@ class GitlabReporter(BaseReporter):
         self._unlabels = self.config.get('unlabel', [])
         if not isinstance(self._unlabels, list):
             self._unlabels = [self._unlabels]
+        self._commit_status = self.config.get('status', False)
 
     def report(self, item):
         """Report on an event."""
@@ -66,6 +67,8 @@ class GitlabReporter(BaseReporter):
                 self.setApproval(item)
             if self._labels or self._unlabels:
                 self.setLabels(item)
+            if self._commit_status:
+                self.setCommitStatus(item)
             if self._merge:
                 self.mergeMR(item)
                 if not item.change.is_merged:
@@ -75,12 +78,14 @@ class GitlabReporter(BaseReporter):
     def addMRComment(self, item, comment=None):
         log = get_annotated_logger(self.log, item.event)
         message = comment or self._formatItemReport(item)
-        project = item.change.project.name
-        mr_number = item.change.number
-        log.debug('Reporting change %s, params %s, message: %s',
-                  item.change, self.config, message)
-        self.connection.commentMR(project, mr_number, message,
-                                  event=item.event)
+        if message:
+            # We are sure we have message to post
+            project = item.change.project.name
+            mr_number = item.change.number
+            log.debug('Reporting change %s, params %s, message: %s',
+                      item.change, self.config, message)
+            self.connection.commentMR(project, mr_number, message,
+                                      event=item.event)
 
     def setApproval(self, item):
         log = get_annotated_logger(self.log, item.event)
@@ -133,12 +138,52 @@ class GitlabReporter(BaseReporter):
     def getSubmitAllowNeeds(self):
         return []
 
+    def setCommitStatus(self, item):
+        log = get_annotated_logger(self.log, item.event)
+        project = item.change.project.name
+        if hasattr(item.change, 'patchset'):
+            sha = item.change.patchset
+        elif hasattr(item.change, 'newrev'):
+            sha = item.change.newrev
+
+        url = item.formatStatusUrl()
+
+        state = None
+        if self._action == 'enqueue':
+            state = 'pending'
+        elif self._action == 'start':
+            state = 'running'
+        elif self._action == 'success':
+            state = 'success'
+        elif self._action == 'failure':
+            state = 'failed'
+        elif self._action == 'cancelled':
+            state = 'canceled'
+        elif self._action == 'dequeued':
+            state = 'canceled'
+        if state:
+            description = 'Zuul %s status: %s' % (item.pipeline.name,
+                                                  state)
+
+            for job in item.getJobs():
+                name = '%s.%s' % (item.pipeline.name, job.name)
+                log.debug(
+                    'Reporting change %s, params %s, '
+                    'state: %s, name: %s, description: %s, url: %s',
+                    item.change, self.config, state, name, description, url)
+
+                self.connection.setCommitStatus(
+                    project, item.change.number, sha, state,
+                    zuul_event_id=item.event, name=name, target_url=url,
+                    description=description)
+
 
 def getSchema():
     gitlab_reporter = v.Schema({
         'comment': bool,
         'approval': bool,
         'merge': bool,
+        'status': bool,
         'label': scalar_or_list(str),
         'unlabel': scalar_or_list(str),
     })
