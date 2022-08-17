ad03402dece90c843e16b7d24d2a90c242ff66bd
James E. Blair <jim@acmegating.com>
2022-08-17 15:12:42 -0700

    Deprecate Ansible 2, make Ansible 5 default
    Change-Id: I2576d0dcec7c8f7bbb76bdd469fd992874742edc

--- tests/unit/test_web.py
+++ tests/unit/test_web.py
@@ -1066,7 +1066,7 @@ class TestWeb(BaseTestWeb):
 
         job_params = {
             'job': 'project-test1',
-            'ansible_version': '2.9',
+            'ansible_version': '5',
             'timeout': None,
             'post_timeout': None,
             'items': [],
@@ -1164,7 +1164,7 @@ class TestWeb(BaseTestWeb):
             "noop")
 
         job_params = {
-            'ansible_version': '2.9',
+            'ansible_version': '5',
             'branch': 'master',
             'extra_vars': {},
             'group_vars': {},
