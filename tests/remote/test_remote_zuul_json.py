c0dcc9a27640c099e994c41785c9ce511e2deced
James E. Blair <jim@acmegating.com>
2022-08-17 15:29:14 -0700

    Add Ansible 6
    Change-Id: I0d450d9385b9aaab22d2d87fb47798bf56525f50

--- tests/remote/test_remote_zuul_json.py
+++ tests/remote/test_remote_zuul_json.py
@@ -166,3 +166,11 @@ class TestZuulJSON5(AnsibleZuulTestCase, FunctionalZuulJSONMixIn):
     def setUp(self):
         super().setUp()
         self._setUp()
+
+
+class TestZuulJSON6(AnsibleZuulTestCase, FunctionalZuulJSONMixIn):
+    ansible_version = '6'
+
+    def setUp(self):
+        super().setUp()
+        self._setUp()
