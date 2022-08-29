a42341b0ac2428c8e3f1d5f74ab40f2e5896810c
James E. Blair <jim@acmegating.com>
2022-08-29 16:05:13 -0700

    WIP: Tracing: implement span save/restore
    This adds API methods to the Tracing class to allow us to save and
    restore spans using ZooKeeper data.
    
    Additionally, we subclass the tracing.Span class so that we can
    transparently handle timestamps which are stored as floating point
    numbers rather than integer nanoseconds.
    
    To exercise the new features, emit spans for QueueItems and BuildSets.
    
    Change-Id: If55b06572b5e95f8c21611b2a3c23f7fd224a547

--- tests/unit/test_tracing.py
+++ tests/unit/test_tracing.py
@@ -12,7 +12,11 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import time
+
 from tests.base import iterate_timeout, ZuulTestCase
+
+from opentelemetry import trace as trace_api
 
 
 def attributes_to_dict(attrlist):
@@ -26,16 +30,90 @@ class TestTracing(ZuulTestCase):
     config_file = 'zuul-tracing.conf'
     tenant_config_file = "config/single-tenant/main.yaml"
 
-    def test_tracing(self):
-        self.scheds.first.sched.tracing.test()
+    def test_tracing_api(self):
+        tr = self.scheds.first.sched.tracing
+
+        # We have a lot of timestamps stored as floats, so make sure
+        # our root span is a ZuulSpan that can handle that input.
+        span_info = tr.startSavedSpan('parent-trace', start_time=time.time(),
+                                      attributes={'foo': 'bar'})
+
+        # Simulate a reconstructed root span
+        span = tr.restoreSpan(span_info)
+
+        # Within the root span, use the more typical OpenTelemetry
+        # context manager api.
+        with trace_api.use_span(span):
+            with tr.tracer.start_span('child1-trace') as child1_span:
+                link = trace_api.Link(child1_span.context,
+                                      attributes={'relationship': 'prev'})
+
+        # Make sure that we can manually start and stop a child span,
+        # and that it is a ZuulSpan as well.
+        child = tr.startSpan('child2-trace', span, start_time=time.time(),
+                             links=[link])
+        child.end(end_time=time.time())
+
+        # End our root span manually.
+        span.end(end_time=time.time())
+
         for _ in iterate_timeout(60, "request to arrive"):
-            if self.otlp.requests:
+            if len(self.otlp.requests) == 3:
                 break
-        req = self.otlp.requests[0]
-        self.log.debug("Received:\n%s", req)
-        attrs = attributes_to_dict(req.resource_spans[0].resource.attributes)
+        req1 = self.otlp.requests[0]
+        self.log.debug("Received:\n%s", req1)
+        attrs = attributes_to_dict(req1.resource_spans[0].resource.attributes)
         self.assertEqual({"service.name": "zuultest"}, attrs)
         self.assertEqual("zuul",
-                         req.resource_spans[0].scope_spans[0].scope.name)
-        span = req.resource_spans[0].scope_spans[0].spans[0]
-        self.assertEqual("test-trace", span.name)
+                         req1.resource_spans[0].scope_spans[0].scope.name)
+        span1 = req1.resource_spans[0].scope_spans[0].spans[0]
+        self.assertEqual("child1-trace", span1.name)
+
+        req2 = self.otlp.requests[1]
+        self.log.debug("Received:\n%s", req2)
+        span2 = req2.resource_spans[0].scope_spans[0].spans[0]
+        self.assertEqual("child2-trace", span2.name)
+        self.assertEqual(span2.links[0].span_id, span1.span_id)
+        attrs = attributes_to_dict(span2.links[0].attributes)
+        self.assertEqual({"relationship": "prev"}, attrs)
+
+        req3 = self.otlp.requests[2]
+        self.log.debug("Received:\n%s", req3)
+        span3 = req3.resource_spans[0].scope_spans[0].spans[0]
+        self.assertEqual("parent-trace", span3.name)
+        attrs = attributes_to_dict(span3.attributes)
+        self.assertEqual({"foo": "bar"}, attrs)
+
+        self.assertEqual(span1.trace_id, span3.trace_id)
+        self.assertEqual(span2.trace_id, span3.trace_id)
+
+    def test_tracing(self):
+        A = self.fake_gerrit.addFakeChange('org/project', 'master', 'A')
+        A.addApproval('Code-Review', 2)
+        self.fake_gerrit.addEvent(A.addApproval('Approved', 1))
+        self.waitUntilSettled()
+
+        for _ in iterate_timeout(60, "request to arrive"):
+            if len(self.otlp.requests) >= 2:
+                break
+
+        buildset = self.getSpan('BuildSet')
+        self.log.debug("Received:\n%s", buildset)
+        item = self.getSpan('QueueItem')
+        self.log.debug("Received:\n%s", item)
+        self.assertEqual(item.trace_id, buildset.trace_id)
+        self.assertNotEqual(item.span_id, buildset.span_id)
+        self.assertTrue(buildset.start_time_unix_nano >=
+                        item.start_time_unix_nano)
+        self.assertTrue(buildset.end_time_unix_nano <=
+                        item.end_time_unix_nano)
+        item_attrs = attributes_to_dict(item.attributes)
+        self.assertTrue(item_attrs['ref_number'] == "1")
+        self.assertTrue(item_attrs['ref_patchset'] == "1")
+        self.assertTrue('zuul_event_id' in item_attrs)
+
+    def getSpan(self, name):
+        for req in self.otlp.requests:
+            span = req.resource_spans[0].scope_spans[0].spans[0]
+            if span.name == name:
+                return span
