00f0708d051b680b4f5a18f927b791081054081f
Ian Wienand <iwienand@redhat.com>
2022-08-30 11:50:05 +1000

    web: console -- clarify package: comment
    Clarify the comment from I8a7e8172f784fc69aa0abb2e6787c63c33d3f802 to
    make it clearer that we have only seen this behaviour with package: on
    yum/dnf/Redhat-ish platforms.
    
    Change-Id: Idc14c5799f0c8e66440f23b175780a00c82bd589

--- web/src/containers/build/Console.jsx
+++ web/src/containers/build/Console.jsx
@@ -51,13 +51,13 @@ class TaskOutput extends React.Component {
   renderResults(value) {
     const interesting_results = []
 
-    // This was written to assume "value" is an array of
-    // key/value mappings to output.  This seems to be a
-    // good assumption for the most part, but "package:" for
-    // whatever reason outputs a result that is just an array of
-    // strings with what packages were installed.  So, if we
-    // see an array of strings as the value, we just swizzle
-    // that into a key/value so it displays usefully.
+    // This was written to assume "value" is an array of key/value
+    // mappings to output.  This seems to be a good assumption for the
+    // most part, but "package:" on at least some distros --
+    // RedHat/yum/dnf we've found -- outputs a result that is just an
+    // array of strings with what packages were installed.  So, if we
+    // see an array of strings as the value, we just swizzle that into
+    // a key/value so it displays usefully.
     const isAllStrings = value.every(i => typeof i === 'string')
     if (isAllStrings) {
       value = [ {output: [...value]} ]
