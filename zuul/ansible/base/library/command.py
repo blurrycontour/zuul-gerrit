3fbf3abfb9b81be8f79a7c2bb7e8dbd24c27829a
Ian Wienand <iwienand@redhat.com>
2022-08-31 14:50:19 +1000

    [wip] disable streaming writing via var
    Change-Id: I0273993c3ece4363098e4bf30bfc4308bb69a8b4

--- zuul/ansible/base/library/command.py
+++ zuul/ansible/base/library/command.py
@@ -270,6 +270,8 @@ class Console(object):
         # special-casing for any of this path.
         if log_uuid == 'in-loop-ignore':
             self.logfile_name = os.devnull
+        elif log_uuid == 'skip':
+            self.logfile_name = os.devnull
         else:
             self.logfile_name = LOG_STREAM_FILE.format(log_uuid=log_uuid)
 
@@ -493,6 +495,7 @@ def zuul_run_command(self, args, zuul_log_id, check_rc=False, close_fds=True, ex
     fail_json_kwargs = None
 
     try:
+        self.log("HERE: UUID is - %s " % zuul_log_id)
         if self._debug:
             self.log('Executing: ' + self._clean_args(args))
 
