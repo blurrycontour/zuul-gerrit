b56de0c51695c2ced4f4ce4cb0ce7c462bdcf405
Simon Westphahl <simon.westphahl@bmw.de>
2022-09-23 11:58:51 +0200

    Trace received Github events
    We'll create a span when zuul-web receives a Github webhook event which
    is then linked to the span for the event pre-processing step.
    
    The pre-processing span context will be added to the trigger events and
    with Icd240712b86cc22e55fb67f6787a0974d5308043 complete tracing of the
    whole chain from receiving a Github event until a change is enqueued.
    
    This also adds a span context field to `ChangeManagementEvent`s, as the
    Github driver also adds dequeue events to the pipeline event queues.
    Creating a link from the dequeue event to the queue item span will be
    done as a separate change.
    
    Change-Id: I1734a3a9e44f0ae01f5ed3453f8218945c90db58

--- zuul/model.py
+++ zuul/model.py
@@ -6239,6 +6239,8 @@ class ChangeManagementEvent(ManagementEvent):
         self.oldrev = oldrev or '0000000000000000000000000000000000000000'
         self.newrev = newrev or '0000000000000000000000000000000000000000'
         self.timestamp = time.time()
+        span = trace.get_current_span()
+        self.span_context = tracing.getSpanContext(span)
 
     def toDict(self):
         d = super().toDict()
@@ -6252,12 +6254,14 @@ class ChangeManagementEvent(ManagementEvent):
         d["oldrev"] = self.oldrev
         d["newrev"] = self.newrev
         d["timestamp"] = self.timestamp
+        d["span_context"] = self.span_context
         return d
 
     def updateFromDict(self, d):
         super().updateFromDict(d)
         self.type = d.get("type")
         self.timestamp = d.get("timestamp")
+        self.span_contex = d.get("span_context")
 
     @classmethod
     def fromDict(cls, data):
