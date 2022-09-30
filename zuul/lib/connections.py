e21028aebf05622349c68044b5df07c4dde8f623
Artem Goncharov <artem.goncharov@gmail.com>
2022-09-30 08:47:14 +0200

    Implement Gitea driver
    This change implements gitea support in Zuul. The driver is based on
    existing github/gitlab/pagure drivers since gitea is having certain
    similarities with them.
    Gitea API is having few issue not allowing to easily implement what
    other git providers can:
    - it is not possible to have labeled/unlabeled triggers, instead only
      label_changed (as the webhook reports) due to absence of the label
      change information
    - need to explicitly fetch branch definition on every event to be able
      to evaluate merge-ability (similar to Github it is required to
      re-calculate requirements on Zuul side)
    - absence of review dismissal event as such
    - different permission model
    
    Tests are coming in the follow-up to make code review more compact.
    
    Change-Id: I6b986de5697f0662144d520f4200b4c8e7183961

--- zuul/lib/connections.py
+++ zuul/lib/connections.py
@@ -23,6 +23,7 @@ from zuul.driver.sql.sqlreporter import SQLReporter
 import zuul.driver.zuul
 import zuul.driver.gerrit
 import zuul.driver.git
+import zuul.driver.gitea
 import zuul.driver.github
 import zuul.driver.smtp
 import zuul.driver.timer
@@ -53,6 +54,7 @@ class ConnectionRegistry(object):
         self.registerDriver(zuul.driver.zuul.ZuulDriver())
         self.registerDriver(zuul.driver.gerrit.GerritDriver())
         self.registerDriver(zuul.driver.git.GitDriver())
+        self.registerDriver(zuul.driver.gitea.GiteaDriver())
         self.registerDriver(zuul.driver.github.GithubDriver())
         self.registerDriver(zuul.driver.smtp.SMTPDriver())
         self.registerDriver(zuul.driver.timer.TimerDriver())
