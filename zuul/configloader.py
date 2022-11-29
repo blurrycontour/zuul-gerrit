c2ee99dd926a015e68707fa385fc4c4448556926
Simon Westphahl <simon.westphahl@bmw.de>
2022-11-29 07:31:18 +0100

    DNM: Debug config errros in Zuul web
        WARNING zuul.ConfigLoader: Zuul encountered an error while accessing
        the repo org/foobar. The error was:
    
          Configuration files missing from cache. Check Zuul scheduler logs
          for more information.
    
    Change-Id: Ib4d5eb00eb4396f7871c59e76e7368ab0004bad9

--- zuul/configloader.py
+++ zuul/configloader.py
@@ -2442,6 +2442,20 @@ class TenantParser(object):
         if not self.merger:
             with project_configuration_exceptions(source_context,
                                                   loading_errors):
+                self.log.error(
+                    "Invalid cache state for %s for given ltimes:\n"
+                    "project branch ltime = %s\n"
+                    "min_ltimes = %s\n"
+                    "unparsed config cache (ZK) ltime = %s\n"
+                    "unparsed branch cache (local) ltime = %s\n"
+                    "config cache valid = %s\n"
+                    "branch cache valid = %s\n",
+                    project.name,
+                    pb_ltime, min_ltimes, files_cache.ltime,
+                    branch_cache.getValidFor(tpc, ZUUL_CONF_ROOT, -1),
+                    files_cache.isValidFor(tpc, pb_ltime),
+                    branch_cache.getValidFor(tpc, ZUUL_CONF_ROOT, pb_ltime),
+                )
                 raise Exception(
                     "Configuration files missing from cache. "
                     "Check Zuul scheduler logs for more information.")
