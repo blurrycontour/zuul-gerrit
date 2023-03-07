0bfab69b00352b49a17a26a3049bee68d240f2ec
Simon Westphahl <simon.westphahl@bmw.de>
2023-03-07 11:28:52 +0100

    Select correct merge method for Github
    Starting with Github Enterprise 3.8[0] and github.com from September 2022
    on[1], the merge strategy changed from using merge-recursive to
    merge-ort[0].
    
    The merge-ort strategy is available in the Git client since version
    2.33.0 and became the default in 2.34.0[2].
    
    If not configured otherwise, we've so far used the default merge
    strategy of the Git client (which varies depending on the version). With
    this change, we are now explicitly choosing the default merge strategy
    based on the Github version. This way, we can reduce errors resulting
    from the use of different merge strategies in Zuul and Github.
    
    [0] https://docs.github.com/en/enterprise-server@3.8/admin/release-notes
    [1] https://github.blog/changelog/2022-09-12-merge-commits-now-created-using-the-merge-ort-strategy/
    [2] https://git-scm.com/docs/merge-strategies#Documentation/merge-strategies.txt-recursive
    
    Change-Id: I354a76fa8985426312344818320980c67171d774

--- zuul/merger/merger.py
+++ zuul/merger/merger.py
@@ -1127,6 +1127,12 @@ class Merger(object):
             elif mode == zuul.model.MERGER_MERGE_RESOLVE:
                 commit = repo.merge(item['ref'], 'resolve',
                                     zuul_event_id=zuul_event_id)
+            elif mode == zuul.model.MERGER_MERGE_RECURSIVE:
+                commit = repo.merge(item['ref'], 'recursive',
+                                    zuul_event_id=zuul_event_id)
+            elif mode == zuul.model.MERGER_MERGE_ORT:
+                commit = repo.merge(item['ref'], 'ort',
+                                    zuul_event_id=zuul_event_id)
             elif mode == zuul.model.MERGER_CHERRY_PICK:
                 commit = repo.cherryPick(item['ref'],
                                          zuul_event_id=zuul_event_id)
