12f7387bc39d163453929053b5486b5f191cc595
James E. Blair <jim@acmegating.com>
2023-08-05 16:23:58 -0700

    Add job failure output detection regexes
    This allows users to trigger the new early failure detection by
    matching regexes in the streaming job output.
    
    For example, if a unit test job outputs something sufficiently
    unique on failure, one could write a regex that matches that and
    triggers the early failure detection before the playbook completes.
    
    For hour-long unit test jobs, this could save a considerable amount
    of time.
    
    Note that this adds the google-re2 library to the Ansible venvs.  It
    has manylinux wheels available, so is easy to install with
    zuul-manage-ansible.  In Zuul itself, we use the fb-re2 library which
    requires compilation and is therefore more difficult to use with
    zuul-manage-ansible.  Presumably using fb-re2 to validate the syntax
    and then later actually using google-re2 to run the regexes is
    sufficient.  We may want to switch Zuul to use google-re2 later for
    consistency.
    
    Change-Id: Ifc9454767385de4c96e6da6d6f41bcb936aa24cd

--- zuul/executor/server.py
+++ zuul/executor/server.py
@@ -978,6 +978,10 @@ class AnsibleJob(object):
         with executor_server.zk_context as ctx:
             self.job = FrozenJob.fromZK(ctx, arguments["job_ref"])
         self.arguments["zuul"].update(zuul_params_from_job(self.job))
+        if self.job.failure_output:
+            self.failure_output = json.dumps(self.job.failure_output)
+        else:
+            self.failure_output = '[]'
 
         self.zuul_event_id = self.arguments["zuul_event_id"]
         # Record ansible version being used for the cleanup phase
@@ -2744,6 +2748,7 @@ class AnsibleJob(object):
                     if not key.startswith("ZUUL_")}
         env_copy.update(self.ssh_agent.env)
         env_copy['ZUUL_JOB_LOG_CONFIG'] = self.jobdir.logging_json
+        env_copy['ZUUL_JOB_FAILURE_OUTPUT'] = self.failure_output
         env_copy['ZUUL_JOBDIR'] = self.jobdir.root
         if self.executor_server.log_console_port != DEFAULT_STREAM_PORT:
             env_copy['ZUUL_CONSOLE_PORT'] = str(
@@ -2873,6 +2878,8 @@ class AnsibleJob(object):
                         self.log.info("Early failure in job")
                         self.executor_server.updateBuildStatus(
                             self.build_request, {'pre_fail': True})
+                        # No need to pre-fail again
+                        allow_pre_fail = False
                 else:
                     idx += 1
                 if idx < BUFFER_LINES_FOR_SYNTAX:
