d135ff069f17226c41f9392695b0142745f67005
James E. Blair <jim@acmegating.com>
2023-09-16 10:45:54 -0700

    WIP: Refactor bundle in sql connection
    This is a sketch of what it might look like if we refactored bundles so
    that a buildset supported multiple simultaneous changes.  The change information
    is removed from the buildset table and placed in a ref table.  Buildsets are
    associated with refs many-to-many via the buildset_ref table.  Builds are also
    associated with refs, many-to-one, so that we can support multiple builds with
    the same job name in a buildset, but we still know which change they are for.
    
    The output from the smoke test is below.  This represents a single buildset
    with two changes, each change runs the same linters job (resulting in two
    builds).
    
    zuul_buildset
    +----+---------------+------------+----------+---------+---------+----------+---------------------+------------------------+---------------------+---------------------+
    | id |      uuid     |   tenant   | pipeline |  result | message | event_id |   event_timestamp   | first_build_start_time | last_build_end_time |       updated       |
    +----+---------------+------------+----------+---------+---------+----------+---------------------+------------------------+---------------------+---------------------+
    | 1  | buildset uuid | tenant-one |  check   | SUCCESS |   None  | event id | 2023-09-16 17:44:35 |          None          |         None        | 2023-09-16 17:44:35 |
    +----+---------------+------------+----------+---------+---------+----------+---------------------+------------------------+---------------------+---------------------+
    zuul_ref
    +----+----------+--------+----------+-------------------+-----------------------------+--------+--------+--------+
    | id | project  | change | patchset |        ref        |           ref_url           | oldrev | newrev | branch |
    +----+----------+--------+----------+-------------------+-----------------------------+--------+--------+--------+
    | 1  | project1 |   1    |    1     | refs/changes/01/1 | http://review.example.com/1 |  None  |  None  | master |
    | 2  | project2 |   2    |    1     | refs/changes/02/2 | http://review.example.com/2 |  None  |  None  | master |
    +----+----------+--------+----------+-------------------+-----------------------------+--------+--------+--------+
    zuul_buildset_ref
    +-------------+--------+
    | buildset_id | ref_id |
    +-------------+--------+
    |      1      |   1    |
    |      1      |   2    |
    +-------------+--------+
    zuul_build
    +----+-------------+------+--------+----------+---------+---------------------+---------------------+--------+---------+--------------+-------+------+---------+
    | id | buildset_id | uuid | ref_id | job_name |  result |      start_time     |       end_time      | voting | log_url | error_detail | final | held | nodeset |
    +----+-------------+------+--------+----------+---------+---------------------+---------------------+--------+---------+--------------+-------+------+---------+
    | 1  |      1      | None |   1    | linters  | SUCCESS | 2023-09-16 17:44:35 | 2023-09-16 17:44:35 |  None  |   None  |     None     |  None | None |   None  |
    | 2  |      1      | None |   2    | linters  | SUCCESS | 2023-09-16 17:44:35 | 2023-09-16 17:44:35 |  None  |   None  |     None     |  None | None |   None  |
    +----+-------------+------+--------+----------+---------+---------------------+---------------------+--------+---------+--------------+-------+------+---------+
    
    Change-Id: I21f3f3dfc8f93a23744856e5b82b3c948c118dc2

---
+++ zuul/driver/sql/alembic/versions/f7843ddf1552_bundle_refactor.py
@@ -0,0 +1,270 @@
+# Copyright 2023 Acme Gating, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""bundle_refactor
+
+Revision ID: f7843ddf1552
+Revises: 151893067f91
+Create Date: 2023-09-16 09:25:00.674820
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'f7843ddf1552'
+down_revision = '151893067f91'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+REF_TABLE = 'zuul_ref'
+BUILD_TABLE = 'zuul_build'
+BUILDSET_TABLE = 'zuul_buildset'
+BUILDSET_REF_TABLE = 'zuul_buildset_ref'
+
+
+def mysql_unhex(x):
+    return f"unhex({x})"
+
+
+def mysql_int_to_bin(x):
+    return f"cast({x} as unsigned)"
+
+
+def pg_unhex(x):
+    return f"decode({x}, 'hex')"
+
+
+def pg_int_to_bin(x):
+    return f"decode(lpad(to_hex(cast({x} as integer)), 40, '0'), 'hex')"
+
+
+def upgrade(table_prefix=''):
+    prefixed_ref = table_prefix + REF_TABLE
+    prefixed_build = table_prefix + BUILD_TABLE
+    prefixed_buildset = table_prefix + BUILDSET_TABLE
+    prefixed_buildset_ref = table_prefix + BUILDSET_REF_TABLE
+
+    connection = op.get_bind()
+    dialect_name = connection.engine.dialect.name
+    if dialect_name == 'mysql':
+        binary = sa.BINARY
+        unhex = mysql_unhex
+        int_to_bin = mysql_int_to_bin
+    elif dialect_name == 'postgresql':
+        binary = sa.LargeBinary
+        unhex = pg_unhex
+        int_to_bin = pg_int_to_bin
+    else:
+        raise Exception("Unsupported dialect {dialect_name}")
+
+    # Normalize data in buildset table
+    connection.execute(sa.text(
+        f"update {prefixed_buildset} set patchset=null where patchset = ''"
+    ))
+    connection.execute(sa.text(
+        f"update {prefixed_buildset} set oldrev=null where oldrev = ''"
+    ))
+    connection.execute(sa.text(
+        f"update {prefixed_buildset} set newrev=null where newrev = ''"
+    ))
+    connection.execute(sa.text(
+        f"update {prefixed_buildset} set branch=null where branch = ''"
+    ))
+    connection.execute(sa.text(
+        f"update {prefixed_buildset} set ref=null where ref = ''"
+    ))
+    connection.execute(sa.text(
+        f"update {prefixed_buildset} set ref_url=null where ref_url = ''"
+    ))
+
+    # Create zuul_ref table.
+    op.create_table(
+        prefixed_ref,
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('project', sa.String(255), nullable=False),
+        sa.Column('ref', sa.String(255)),
+        sa.Column('ref_url', sa.String(255)),
+        sa.Column('change', sa.Integer),
+        sa.Column('patchset', binary(20)),
+        sa.Column('oldrev', binary(20)),
+        sa.Column('newrev', binary(20)),
+        sa.Column('branch', sa.String(255)),
+        sa.Column('patchset_temp', sa.String(255)),
+        sa.Column('oldrev_temp', sa.String(255)),
+        sa.Column('newrev_temp', sa.String(255)),
+    )
+
+    # Add mapping table for buildset <-> ref
+    op.create_table(
+        prefixed_buildset_ref,
+        sa.Column('buildset_id', sa.Integer,
+                  sa.ForeignKey(
+                      prefixed_buildset + ".id")),
+        sa.Column('ref_id', sa.Integer,
+                  sa.ForeignKey(
+                      prefixed_ref + ".id")),
+        sa.PrimaryKeyConstraint("buildset_id", "ref_id"),
+    )
+    op.create_index(
+        table_prefix + 'buildset_ref_buildset_id_idx',
+        prefixed_buildset_ref, ['buildset_id'])
+    op.create_index(
+        table_prefix + 'buildset_ref_ref_id_idx',
+        prefixed_buildset_ref, ['ref_id'])
+
+    # Copy data from buildset to ref
+    quote = connection.engine.dialect.identifier_preparer.quote
+    query = f"""
+        insert into {prefixed_ref}
+            (project, {quote('change')}, patchset_temp,
+             ref, ref_url, oldrev_temp, newrev_temp)
+        select distinct
+            bs.project, bs.change, bs.patchset,
+            bs.ref, bs.ref_url, bs.oldrev, bs.newrev
+        from {prefixed_buildset} bs"""
+    connection.execute(sa.text(query))
+
+    # Populate buildset_ref table
+    query = f"""
+        insert into {prefixed_buildset_ref}
+        select {prefixed_buildset}.id, {prefixed_ref}.id
+        from {prefixed_buildset}, {prefixed_ref}
+        where ({prefixed_buildset}.project = {prefixed_ref}.project
+               or ({prefixed_buildset}.project is null
+                   and {prefixed_ref}.project is null))
+        and ({prefixed_buildset}.change = {prefixed_ref}.change
+             or ({prefixed_buildset}.change is null
+                 and {prefixed_ref}.change is null))
+        and ({prefixed_buildset}.patchset = {prefixed_ref}.patchset_temp
+             or ({prefixed_buildset}.patchset is null
+                 and {prefixed_ref}.patchset_temp is null))
+        and ({prefixed_buildset}.ref = {prefixed_ref}.ref
+             or ({prefixed_buildset}.ref is null
+                 and {prefixed_ref}.ref is null))
+        and ({prefixed_buildset}.ref_url = {prefixed_ref}.ref_url
+             or ({prefixed_buildset}.ref_url is null
+                 and {prefixed_ref}.ref_url is null))
+        and ({prefixed_buildset}.oldrev = {prefixed_ref}.oldrev_temp
+             or ({prefixed_buildset}.oldrev is null
+                 and {prefixed_ref}.oldrev_temp is null))
+        and ({prefixed_buildset}.newrev = {prefixed_ref}.newrev_temp
+             or ({prefixed_buildset}.newrev is null
+                 and {prefixed_ref}.newrev_temp is null))
+    """
+    connection.execute(sa.text(query))
+
+    # Add the ref_id column to the build table
+    with op.batch_alter_table(prefixed_build) as batch_op:
+        batch_op.add_column(
+            sa.Column('ref_id', sa.Integer, sa.ForeignKey(
+                prefixed_ref + ".id"))
+        )
+        batch_op.create_index(
+            table_prefix + 'build_ref_id_idx',
+            ['ref_id'])
+
+    # Link all builds to their buildset's ref
+    if dialect_name == 'mysql':
+        query = f"""
+            update {prefixed_build}
+            inner join {prefixed_buildset}
+              on {prefixed_build}.buildset_id = {prefixed_buildset}.id
+            inner join {prefixed_buildset_ref}
+              on {prefixed_buildset_ref}.buildset_id = {prefixed_buildset}.id
+            set {prefixed_build}.ref_id = {prefixed_buildset_ref}.ref_id
+        """
+    elif dialect_name == 'postgresql':
+        query = f"""
+        with t as (
+            select {prefixed_build}.id as id,
+                   {prefixed_buildset_ref}.ref_id as ref_id
+            from {prefixed_build}
+            inner join {prefixed_buildset}
+              on {prefixed_build}.buildset_id = {prefixed_buildset}.id
+            inner join {prefixed_buildset_ref}
+              on {prefixed_buildset_ref}.buildset_id = {prefixed_buildset}.id
+        )
+        update {prefixed_build}
+        set ref_id = t.ref_id
+        from t
+        where {prefixed_build}.id=t.id
+        """
+    else:
+        raise Exception("Unsupported dialect {dname}")
+    connection.execute(sa.text(query))
+
+    # Drop the obsolete buildset indexes and columns
+    with op.batch_alter_table(prefixed_buildset) as batch_op:
+        batch_op.drop_index('project_pipeline_idx')
+        batch_op.drop_index('project_change_idx')
+        batch_op.drop_index('change_idx')
+        batch_op.drop_column('project')
+        batch_op.drop_column('change')
+        batch_op.drop_column('patchset')
+        batch_op.drop_column('ref')
+        batch_op.drop_column('ref_url')
+        batch_op.drop_column('oldrev')
+        batch_op.drop_column('newrev')
+        batch_op.drop_column('branch')
+        # This is not strictly related to this change, but is obsolete
+        batch_op.drop_column('zuul_ref')
+
+    # Update zuul_ref table to use new column formats
+    query = f"""
+        update {prefixed_ref}
+        set patchset =
+            case
+              when length(patchset_temp)=40
+              then {unhex('patchset_temp')}
+              else {int_to_bin('patchset_temp')}
+            end,
+        oldrev =
+            case
+              when length(oldrev_temp)=40
+              then {unhex('oldrev_temp')}
+              else {int_to_bin('oldrev_temp')}
+            end,
+        newrev =
+            case
+              when length(newrev_temp)=40
+              then {unhex('newrev_temp')}
+              else {int_to_bin('newrev_temp')}
+            end
+    """
+    connection.execute(sa.text(query))
+
+    with op.batch_alter_table(prefixed_ref) as batch_op:
+        # Drop temporary columns
+        batch_op.drop_column('patchset_temp')
+        batch_op.drop_column('oldrev_temp')
+        batch_op.drop_column('newrev_temp')
+        # Create replacement indexes
+        batch_op.create_index(
+            table_prefix + 'project_change_idx',
+            ['project', 'change'])
+        batch_op.create_index(
+            table_prefix + 'change_idx',
+            ['change'])
+        # Create the unique constraint across our new columns
+        batch_op.create_unique_constraint(
+            table_prefix + 'ref_unique',
+            ['project', 'ref', 'change', 'patchset', 'oldrev', 'newrev'],
+        )
+
+
+def downgrade():
+    raise Exception("Downgrades not supported")
