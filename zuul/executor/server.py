bf44bb0143d79ca7e92c35e7cbe1718c0f080b3f
James E. Blair <jim@acmegating.com>
2024-01-08 15:28:03 -0800

    WIP Finish circular dependency refactor
    This change completes the circular dependency refactor.
    
    The principal change is that queue items may now include
    more than one change simultaneously in the case of circular
    dependencies.
    
    In dependent pipelines, the two-phase reporting process is
    simplified because it happens during processing of a single
    pipeline.
    
    In independent pipelines, non-live items are still used for
    linear depnedencies, but multi-change items are used for
    circular dependencies.
    
    Previously changes were enqueued recursively and then
    bundles were made out of the resulting items.  Since we now
    need to enqueue entire cycles in one queue item, the
    dependency graph generation is performed at the start of
    enqueing the first change in a cycle.
    
    Some tests exercise situations where Zuul is processing
    events for old patchsets of changes.  The new change query
    sequence mentioned in the previous paragraph necessitates
    more accurate information about out-of-date patchsets than
    the previous sequence, therefore the Gerrit driver has been
    updated to query and return more data about non-current
    patchsets.
    
    This change is not backwards compatible with the existing
    ZK schema, and will require Zuul systems delete all pipeline
    states during the upgrade.  A later change will implement
    a helper command for this.
    
    All backwards compatability handling for the last several
    model_api versions which were added to prepare for this
    upgrade have been removed.  In general, all model data
    structures involving frozen jobs are now indexed by the
    frozen job's uuid and no longer include the job name since
    a job name no longer uniquely identifies a job in a buildset
    (either the uuid or the (job name, change) tuple must be
    used to identify it).
    
    Job deduplication is simplified and now only needs to
    consider jobs within the same buildset.
    
    The reporting in several drivers has been updated to support
    reporting information about multiple changes in a queue item.
    
    TODO:
    * upgrade process
    * web status js
    * finish release notes (unused sections intentionally left in draft)
    
    Future changes?
    * remove remaining (unrelated) model api backwards compat
    * clean up safety check
      * consider reworking dependency change process
        * maybe inside the pipeline processor we dequeue item and then add
          an enqueue event for each existing change on the item
    * api enqueue/dequeue by item id
    
    Change-Id: I0b9e4d3f9936b1e66a08142fc36866269dc287f1

--- zuul/executor/server.py
+++ zuul/executor/server.py
@@ -4193,7 +4193,7 @@ class ExecutorServer(BaseMergeServer):
 
         event = BuildStartedEvent(
             build_request.uuid, build_request.build_set_uuid,
-            build_request.job_name, build_request.job_uuid,
+            build_request.job_uuid,
             build_request.path, data, build_request.event_id)
         self.result_events[build_request.tenant_name][
             build_request.pipeline_name].put(event)
@@ -4201,7 +4201,7 @@ class ExecutorServer(BaseMergeServer):
     def updateBuildStatus(self, build_request, data):
         event = BuildStatusEvent(
             build_request.uuid, build_request.build_set_uuid,
-            build_request.job_name, build_request.job_uuid,
+            build_request.job_uuid,
             build_request.path, data, build_request.event_id)
         self.result_events[build_request.tenant_name][
             build_request.pipeline_name].put(event)
@@ -4216,7 +4216,7 @@ class ExecutorServer(BaseMergeServer):
 
         event = BuildPausedEvent(
             build_request.uuid, build_request.build_set_uuid,
-            build_request.job_name, build_request.job_uuid,
+            build_request.job_uuid,
             build_request.path, data, build_request.event_id)
         self.result_events[build_request.tenant_name][
             build_request.pipeline_name].put(event)
@@ -4283,7 +4283,7 @@ class ExecutorServer(BaseMergeServer):
         updater = self.executor_api.getRequestUpdater(build_request)
         event = BuildCompletedEvent(
             build_request.uuid, build_request.build_set_uuid,
-            build_request.job_name, build_request.job_uuid,
+            build_request.job_uuid,
             build_request.path, result, build_request.event_id)
         build_request.state = BuildRequest.COMPLETED
         updated = False
