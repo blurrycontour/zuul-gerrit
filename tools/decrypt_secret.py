94b9e9bbd660403b69b60bd45d2a7fbe948f4668
Francisco Seruca Salgado <fserucas@redhat.com>
2024-02-08 15:44:56 +0000

    Update decrypt_secret.py script
    This change updates the decrypt_secret.py script to decrypt
    Zuul jobs secrets, based on the dumped Zookeeper files using
    zuul-admin export-keys command.
    
    usage: decrypt_secret.py [-h] [--list] dumppath config file zkpath
    
    Decrypt a Zuul secret.
    
    positional arguments:
      dumppath    Path to the zuul-admin export-keys command output
      config      Zuul.conf file with Keystore password
      file        The YAML file with secrets
      zkpath      Path to the project key in Zookeeper
    
    options:
      -h, --help  show this help message and exit
      --list      list projects from exported keys
    
    Change-Id: Ieeed05640a1bab8388b9727acd2ba5f01d090cca

--- tools/decrypt_secret.py
+++ tools/decrypt_secret.py
@@ -16,22 +16,66 @@ import argparse
 from zuul.lib import encryption
 import zuul.configloader
 import zuul.model
-
+import json
+import zuul.lib.keystorage
+import configparser
+import textwrap
 
 DESCRIPTION = """Decrypt a Zuul secret.
 """
 
 
+def getProjectSecretsKeys(keyfile, path, password_bytes):
+    """Return the public and private keys"""
+    keys = keyfile.get("keys")
+    projectkeys = keys.get(path + "/secrets")
+    if projectkeys is None:
+        print(f"Project path {path} does not exist")
+        exit(1)
+    pk = projectkeys["keys"][0]["private_key"]
+    pem_private_key = pk.encode("utf-8")
+    private_key, public_key = encryption.deserialize_rsa_keypair(
+        pem_private_key, password_bytes)
+
+    return private_key, public_key
+
+
 def main():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
-    parser.add_argument('private_key',
-                        help="The path to the private key")
+    parser.add_argument('dumppath',
+                        help="Path to the zuul-admin export-keys \
+                        command output")
+    parser.add_argument('config',
+                        help="Zuul.conf file with Keystore password")
     parser.add_argument('file',
                         help="The YAML file with secrets")
+    parser.add_argument('zkpath',
+                        help="Path to the project key in Zookeeper")
+    parser.add_argument('--list',
+                        help="list projects from exported keys",
+                        action='store_true')
     args = parser.parse_args()
 
-    (private_secrets_key, public_secrets_key) = \
-        encryption.deserialize_rsa_keypair(open(args.private_key, 'rb').read())
+    keysfile = json.load(open(args.dumppath))
+    if (args.list):
+        for key, _ in keysfile['keys'].items():
+            if "secrets" in key:
+                print(key[:-8])
+        exit(0)
+
+    zuulconfig = configparser.ConfigParser()
+    zuulconfig.read(args.config)
+    try:
+        password = zuulconfig.get("keystore", "password").encode("utf-8")
+    except configparser.NoSectionError as section:
+        print(section)
+        exit(1)
+    except configparser.NoOptionError as key:
+        print(key)
+        exit(1)
+
+    priv, pub = getProjectSecretsKeys(keysfile, args.zkpath, password)
+
     parser = zuul.configloader.SecretParser(None)
     sc = zuul.model.SourceContext(None, 'project', None, 'master',
                                   'path', False)
@@ -42,8 +86,22 @@ def main():
             continue
         s = element['secret']
         secret = parser.fromYaml(s)
-        print(secret.name)
-        print(secret.decrypt(private_secrets_key).secret_data)
+        data = secret.decrypt(priv).secret_data
+        output = textwrap.dedent(
+            '''
+            - secret:
+                name: {}
+                data:
+            '''.format(secret.name))
+
+        twrap = textwrap.TextWrapper(break_long_words=False,
+                                     initial_indent=' ' * 8,
+                                     subsequent_indent=' ' * 10)
+        for k, v in data.items():
+            output += twrap.fill(k + ": " + v)
+            if list(data.keys())[-1] != k:
+                output += '\n'
+        print(output)
 
 
 if __name__ == '__main__':
