5ac02b0f2bc80df6f530a4ccb9e6b56254867f8f
Felix Edel <felix.edel@bmw.de>
2024-02-22 10:18:49 +0100

    Implement new status page
    The current status page shows a lot of information about the different
    pipelines, queues and even the individual queue items and their jobs.
    This information can become quite overwhelming - especially for larger
    tenants with a lot of pipelines and longer (gate) queues.
    
    For operators, this makes it hard to get a clear picture of what's going
    on and for the individual developers it's hard to find their individual
    changes (or queues).
    
    The new status page tries to provide a more "operator-oriented" view by
    showing the most imporant information about a tenant in a very concised
    manner. The idea is to focus only on the relevant information to get an
    answer to the following questions:
    
      1. What is the current state of the tenant?
      2. Which items are failing?
      3. Which pipelines/queues are succeeding (or failing)?
      4. Which queues are piling up items?
    
    To achieve this, the new status page focuses on higher-level information
    like pipelines and queues, but doesn't show more detailed ones like the
    individual queue items and their jobs. Also, the queues are much more
    prominent in this new status view.
    
    To compensate the missing information, the next change implements an
    additional "pipeline details view".
    
    The general idea behind this rework is to provide both a
    "operator-oriented" view and a "developer-oriented" view without mixing
    both into a single view (which is kind of what the current status view
    tries to do). Instead of showing all information on a single page, the
    new approach splits that information into different scopes:
    
      1. status / pipeline overview
      2. pipeline and queue details
      3. individual change
    
    Based on those scopes, the pages that handle those focus only on the
    relevant information, but don't show too many details about the other
    scopes. The changes in this stack currently cover 1. and 2., whereby 3.
    is already existing in form of the "single change panel/view".
    
    The change doesn't touch the current StatusPage, but creates a new one
    for easier review. The old status page will be cleaned up in a follow-up
    commit.
    
    Change-Id: I255e84ba562bb87da6bdbd44b51adbcc73136c47

---
+++ web/src/containers/status/PipelineSummary.jsx
@@ -0,0 +1,152 @@
+// Copyright 2024 BMW Group
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import {
+  Badge,
+  Button,
+  Card,
+  CardTitle,
+  CardBody,
+  Flex,
+  FlexItem,
+  Tooltip,
+} from '@patternfly/react-core'
+
+import { SquareIcon } from '@patternfly/react-icons'
+
+import { PipelineIcon, getQueueItemIconConfig } from './Misc'
+
+function QueueItemSquare({ item }) {
+  const iconConfig = getQueueItemIconConfig(item)
+  return (
+    <Button
+      variant="plain"
+      className={`zuul-item-square zuul-item-square-${iconConfig.variant}`}
+    >
+      <SquareIcon />
+    </Button>
+  )
+}
+
+QueueItemSquare.propTypes = {
+  item: PropTypes.object,
+}
+
+
+function QueueSummary({ pipeline, pipelineType }) {
+  // Dependent pipelines usually come with named queues, so we will
+  // visualize each queue individually. For other pipeline types, we
+  // will consolidate all heads as a single queue to simplify the
+  // visualization (e.g. independent pipelines like check where each
+  // change/item is enqueued in it's own queue by design).
+  if (['dependent'].indexOf(pipelineType) > -1) {
+    return (
+      pipeline.change_queues.map((queue) => (
+        <Flex key={`${queue.name}${queue.branch}`}>
+          <FlexItem>
+            <Card isPlain className="zuul-compact-card">
+              <CardTitle>
+                {queue.name}
+                {queue.branch ? ` (${queue.branch})` : ''}
+              </CardTitle>
+              <CardBody style={{ paddingBottom: '0' }}>
+                {queue.heads.map((head) => (
+                  head.map((item) => <QueueItemSquare item={item} key={item.id} />)
+                ))}
+              </CardBody>
+            </Card>
+          </FlexItem>
+        </Flex>
+      ))
+    )
+  } else {
+    return (
+      <Flex
+        display={{ default: 'inlineFlex' }}
+        spaceItems={{ default: 'spaceItemsNone' }}
+      >
+        {pipeline.change_queues.map((queue) => (
+          queue.heads.map((head) => (
+            head.map((item) => (
+              <FlexItem key={item.id}>
+                <QueueItemSquare item={item} />
+              </FlexItem>
+            ))
+          ))
+        ))}
+      </Flex>
+    )
+  }
+}
+
+QueueSummary.propTypes = {
+  pipeline: PropTypes.object,
+  pipelineType: PropTypes.string,
+}
+
+function PipelineSummary({ pipeline }) {
+
+  const countItems = (pipeline) => {
+    let count = 0
+    pipeline.change_queues.map(queue => (
+      queue.heads.map(head => (
+        head.map(() => (
+          count++
+        ))
+      ))
+    ))
+    return count
+  }
+
+  const pipelineType = pipeline.manager || 'unknown'
+  const itemCount = countItems(pipeline)
+
+  return (
+    <Card className="zuul-pipeline-summary zuul-compact-card">
+      <CardTitle
+        style={pipelineType !== 'dependent' ? { paddingBottom: '8px' } : {}}
+      >
+        <PipelineIcon pipelineType={pipelineType} />
+        {pipeline.name}
+        <Tooltip
+          content={
+            itemCount === 1
+              ? <div>{itemCount} item enqueued</div>
+              : <div>{itemCount} items enqueued</div>
+          }
+        >
+          <Badge
+            isRead
+            style={{ marginLeft: 'var(--pf-global--spacer--sm)', verticalAlign: '0.1em' }}
+          >
+            {itemCount}
+          </Badge>
+        </Tooltip>
+      </CardTitle>
+      <CardBody>
+        <QueueSummary pipeline={pipeline} pipelineType={pipelineType} />
+      </CardBody>
+
+    </Card>
+  )
+}
+
+PipelineSummary.propTypes = {
+  pipeline: PropTypes.object,
+}
+
+export default PipelineSummary
