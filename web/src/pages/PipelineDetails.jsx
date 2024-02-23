efaf68a3513556ea1ec75e2e42af148c228f1873
Felix Edel <felix.edel@bmw.de>
2024-02-23 15:06:34 +0100

    Implement PipelineDetails view
    As an addition to the new status page, this implements a detail view for
    a single pipeline. The pipelines details view can be reached by clicking
    on the title of a pipeline on the new status page.
    
    The information that is shown on this page is basically the same that
    was shown on the old status page. However, by keeping the focus on a
    single pipeline, the information can be presented in a more prominent
    way using the whole screen/page. On the old status page the information
    for each pipeline was very "squished" as each pipeline only got a little
    bit of space.
    
    Some key features:
    
      1. Queues in dependent pipelines are more prominent, so it's clear
         what is enqueued where.
      2. Non-live items are shown in a more compact way and also don't
         provide any actions (like dequeue and promote).
    
    This change copies and adapts a few methods from the
    containers/status/ItemPanel.jsx file to render job-specific information
    (like progress, result and links). I didn't bother to "lift" those to
    more generic files as this would also require some adaption to make them
    "backward-compatible" (support the old and new styling). On the long run
    the single change panel (ItemPanel.jsx) will also be replaced and we can
    deal with that later.
    
    Additionally, this aligns the layout and styling of the QueueItemPopover
    to match the QueueItem. Where possible those parts are also moved to a
    common file, so they are only defined once.
    
    Please note that the "branching" feature is currently missing, so items
    are not branched out in case of e.g. a MERGE_CONFLICT in a dependent
    pipeline. This feature will be implemented in a follow-up change.
    
    Change-Id: I9737c80337d8156f8fc8fc521c540aa09d4a6a18

---
+++ web/src/pages/PipelineDetails.jsx
@@ -0,0 +1,175 @@
+// Copyright 2024 BMW Group
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+
+import {
+  Gallery,
+  GalleryItem,
+  Grid,
+  GridItem,
+  PageSection,
+  PageSectionVariants,
+  Title,
+  Text,
+  TextContent,
+  TextVariants,
+} from '@patternfly/react-core'
+import { StreamIcon } from '@patternfly/react-icons'
+
+import ChangeQueue from '../containers/status/ChangeQueue'
+import { PipelineIcon } from '../containers/status/Misc'
+import { fetchStatusIfNeeded } from '../actions/status'
+import { EmptyPage } from '../containers/Errors'
+import { Fetching } from '../containers/Fetching'
+
+function PipelineStats({ pipeline }) {
+  return (
+    <>
+      <Title headingLevel="h4" style={{ margin: 0 }}>Events</Title>
+      Trigger: {pipeline.trigger_events} <br />
+      Management: {pipeline.management_events} <br />
+      Result: {pipeline.result_events} <br />
+    </>
+  )
+}
+
+PipelineStats.propTypes = {
+  pipeline: PropTypes.object.isRequired,
+}
+
+function PipelineDetails({ pipeline }) {
+
+  const pipelineType = pipeline.manager || 'unknown'
+
+  return (
+    <>
+      <Title headingLevel="h1">
+        <PipelineIcon pipelineType={pipelineType} />
+        {pipeline.name}
+      </Title>
+      <Grid hasGutter>
+        <GridItem span={10}>
+          <TextContent>
+            <Text component={TextVariants.p}>
+              {pipeline.description}
+            </Text>
+          </TextContent>
+        </GridItem>
+        <GridItem span={2}>
+          <PipelineStats pipeline={pipeline} />
+        </GridItem>
+      </Grid>
+    </>
+  )
+}
+
+PipelineDetails.propTypes = {
+  pipeline: PropTypes.object.isRequired,
+}
+
+function PipelineDetailsPage({ pipeline, isFetching, tenant, darkMode, fetchStatusIfNeeded }) {
+
+  useEffect(() => {
+    document.title = 'Zuul Pipeline Details'
+    if (tenant.name) {
+      fetchStatusIfNeeded(tenant)
+    }
+  }, [tenant, fetchStatusIfNeeded])
+
+  if (pipeline === undefined || isFetching) {
+    return <Fetching />
+  }
+
+  if (!pipeline) {
+    return (
+      <EmptyPage
+        title="This pipeline does not exist"
+        icon={StreamIcon}
+        linkTarget={`${tenant.linkPrefix}/status`}
+        linkText="Back to status page"
+      />
+    )
+  }
+
+  return (
+    <>
+      <PageSection variant={darkMode ? PageSectionVariants.dark : PageSectionVariants.light}>
+        <PipelineDetails pipeline={pipeline} />
+      </PageSection>
+      <PageSection variant={darkMode ? PageSectionVariants.dark : PageSectionVariants.light}>
+        <Title headingLevel="h3">
+          <StreamIcon
+            style={{
+              marginRight: 'var(--pf-global--spacer--sm)',
+              verticalAlign: '-0.1em',
+            }}
+          />{' '}
+          Queues
+        </Title>
+        <Gallery
+          hasGutter
+          minWidths={{
+            default: '450px',
+          }}
+        >
+
+          {pipeline.change_queues.map((queue, idx) => (
+            <GalleryItem key={idx}>
+              <ChangeQueue queue={queue} />
+            </GalleryItem>
+          ))}
+        </Gallery>
+      </PageSection>
+    </>
+  )
+}
+
+PipelineDetailsPage.propTypes = {
+  match: PropTypes.object.isRequired,
+  pipeline: PropTypes.object,
+  isFetching: PropTypes.bool,
+  tenant: PropTypes.object,
+  darkMode: PropTypes.bool,
+  fetchStatusIfNeeded: PropTypes.func.isRequired,
+}
+
+function mapStateToProps(state, ownProps) {
+  let pipeline = null
+  if (state.status.status) {
+    // Filter the state for this specific pipeline
+    state.status.status.pipelines.filter(
+      ppl => (ppl.name === ownProps.match.params.pipelineName)
+    ).map(
+      ppl => (pipeline = ppl)
+    )
+  }
+
+  return {
+    pipeline,
+    isFetching: state.status.isFetching,
+    tenant: state.tenant,
+    darkMode: state.preferences.darkMode,
+  }
+}
+
+const mapDispatchToProps = { fetchStatusIfNeeded }
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(withRouter(PipelineDetailsPage))
