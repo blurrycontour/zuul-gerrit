bc2168f6298610ba4f12d34bb4da57f485163b09
Felix Edel <felix.edel@bmw.de>
2024-04-24 11:05:56 +0200

    Show queue lengths and fetching state on status page
    This shows the queue lengths information to the new status page. As I
    didn't come up with something new, simply visualize them the same way as
    on the old status page.
    
    Additionally, properly visualize the fetching state so we don't show
    0 queue lengths while the status.json is loaded from the backend.
    
    Change-Id: I7422357efb13c3a749078b9b4d05b6582399677a

--- web/src/pages/PipelineOverview.jsx
+++ web/src/pages/PipelineOverview.jsx
@@ -27,9 +27,30 @@ import {
 import PipelineSummary from '../containers/status/PipelineSummary'
 
 import { fetchStatusIfNeeded } from '../actions/status'
+import { Fetching } from '../containers/Fetching'
 
 
-function PipelineOverviewPage({ pipelines, tenant, darkMode, fetchStatusIfNeeded }) {
+function TenantStats({ stats }) {
+  return (
+    <>
+      <p>
+        Queue lengths:{' '}
+        <span>
+          {stats.trigger_event_queue ? stats.trigger_event_queue.length : '0'}
+        </span> trigger events,{' '}
+        <span>
+          {stats.management_event_queue ? stats.management_event_queue.length : '0'}
+        </span> management events.
+      </p>
+    </>
+  )
+}
+
+TenantStats.propTypes = {
+  stats: PropTypes.object,
+}
+
+function PipelineOverviewPage({ pipelines, stats, isFetching, tenant, darkMode, fetchStatusIfNeeded }) {
 
   useEffect(() => {
     document.title = 'Zuul Status'
@@ -38,8 +59,15 @@ function PipelineOverviewPage({ pipelines, tenant, darkMode, fetchStatusIfNeeded
     }
   }, [tenant, fetchStatusIfNeeded])
 
+  if (isFetching) {
+    return <Fetching />
+  }
+
   return (
     <>
+      <PageSection variant={darkMode ? PageSectionVariants.dark : PageSectionVariants.light}>
+        <TenantStats stats={stats} />
+      </PageSection>
       <PageSection variant={darkMode ? PageSectionVariants.dark : PageSectionVariants.light}>
         <Gallery
           hasGutter
@@ -52,9 +80,7 @@ function PipelineOverviewPage({ pipelines, tenant, darkMode, fetchStatusIfNeeded
               <PipelineSummary pipeline={pipeline} tenant={tenant} />
             </GalleryItem>
           ))}
-
         </Gallery>
-
       </PageSection>
     </>
   )
@@ -63,6 +89,8 @@ function PipelineOverviewPage({ pipelines, tenant, darkMode, fetchStatusIfNeeded
 
 PipelineOverviewPage.propTypes = {
   pipelines: PropTypes.array,
+  stats: PropTypes.object,
+  isFetching: PropTypes.bool,
   tenant: PropTypes.object,
   preferences: PropTypes.object,
   darkMode: PropTypes.bool,
@@ -83,11 +111,16 @@ const countItems = (pipeline) => {
 
 function mapStateToProps(state) {
   let pipelines = []
+  let stats = {}
   if (state.status.status) {
     // TODO (felix): Make filtering optional via a switch (default: on)
     pipelines = state.status.status.pipelines.map(ppl => (
       { ...ppl, _count: countItems(ppl) }
     )).filter(ppl => ppl._count > 0)
+    stats = {
+      trigger_event_queue: state.status.status.trigger_event_queue,
+      management_event_queue: state.status.status.management_event_queue,
+    }
   }
 
   // TODO (felix): Here we could also order the pipelines by any
@@ -96,6 +129,8 @@ function mapStateToProps(state) {
   // The sorting could also be done via the filter toolbar.
   return {
     pipelines,
+    stats,
+    isFetching: state.status.isFetching,
     tenant: state.tenant,
     darkMode: state.preferences.darkMode,
   }
