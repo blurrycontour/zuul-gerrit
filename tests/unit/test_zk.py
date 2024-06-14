5fb345134760a0dd5525cd33d98c84f9faa15b9e
Simon Westphahl <simon.westphahl@bmw.de>
2024-06-14 16:25:00 +0200

    Add ZKObject-based launcher API
    This change adds two new ZKObjects, namely the NodesetRequest and a
    ProviderNode. A launcher API that operates on cached data provides an
    interface that will be used by the launcher server later on.
    
    This change only adds the API and exercises it in a unittest. Following
    changes will use it as part of the launcher client/server.
    
    Change-Id: I38528ca75002abca2bdcc7aee2173d0cdcf3129e

--- tests/unit/test_zk.py
+++ tests/unit/test_zk.py
@@ -17,6 +17,7 @@ from collections import defaultdict
 import json
 import queue
 import threading
+import time
 import uuid
 from unittest import mock
 
@@ -38,6 +39,7 @@ from zuul.zk.exceptions import LockException
 from zuul.zk.executor import ExecutorApi
 from zuul.zk.job_request_queue import JobRequestEvent
 from zuul.zk.merger import MergerApi
+from zuul.zk.launcher import LauncherApi
 from zuul.zk.layout import LayoutStateStore, LayoutState
 from zuul.zk.locks import locked
 from zuul.zk.nodepool import ZooKeeperNodepool
@@ -48,7 +50,11 @@ from zuul.zk.sharding import (
     NODE_BYTE_SIZE_LIMIT,
 )
 from zuul.zk.components import (
-    BaseComponent, ComponentRegistry, ExecutorComponent, COMPONENT_REGISTRY
+    BaseComponent,
+    ComponentRegistry,
+    ExecutorComponent,
+    LauncherComponent,
+    COMPONENT_REGISTRY
 )
 from tests.base import (
     BaseTestCase, HoldableExecutorApi, HoldableMergerApi,
@@ -2178,3 +2184,146 @@ class TestPipelineInit(ZooKeeperBaseTestCase):
         pipeline.manager = mock.Mock()
         pipeline.state.refresh(context)
         self.assertEqual(pipeline.state.layout_uuid, layout.uuid)
+
+
+class TestLauncherApi(ZooKeeperBaseTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.component_info = LauncherComponent(self.zk_client, "test")
+        self.component_info.state = self.component_info.RUNNING
+        self.component_info.register()
+        self.api = LauncherApi(
+            self.zk_client, self.component_registry, self.component_info,
+            lambda: None)
+        self.addCleanup(self.api.stop)
+
+    def test_launcher(self):
+        labels = ["foo", "bar"]
+        context = ZKContext(self.zk_client, None, None, self.log)
+        request = model.NodesetRequest.new(
+            context,
+            tenant_name="tenant",
+            pipeline_name="check",
+            buildset_uuid=uuid.uuid4().hex,
+            job_uuid=uuid.uuid4().hex,
+            job_name="foobar",
+            labels=labels,
+            priority=100,
+            request_time=time.time(),
+            zuul_event_id=None,
+            span_info=None,
+        )
+
+        # Wait for request to show up in the cache
+        for _ in iterate_timeout(10, "request to show up"):
+            request_list = self.api.getNodesetRequests()
+            if len(request_list):
+                break
+
+        self.assertEqual(len(request_list), 1)
+        for req in request_list:
+            request = self.api.getNodesetRequest(req.uuid)
+            self.assertIs(request, req)
+            self.assertIsNotNone(request)
+            self.assertEqual(labels, request.labels)
+            self.assertIsNotNone(request._zstat)
+            self.assertIsNotNone(request.getPath())
+
+        self.assertIsNotNone(request.acquireLock(self.zk_client))
+        self.assertTrue(request.hasLock())
+        for _ in iterate_timeout(10, "request to be locked"):
+            if request.is_locked:
+                break
+
+        # Create provider nodes for the requested labels
+        for i, label in enumerate(request.labels):
+            node = model.ProviderNode.new(
+                context, request_id=request.uuid, uuid=uuid.uuid4().hex,
+                label=label)
+
+        # Wait for the nodes to show up in the cache
+        for _ in iterate_timeout(10, "nodes to show up"):
+            provider_nodes = self.api.getProviderNodes()
+            if len(provider_nodes) == 2:
+                break
+
+        # Accept and update the nodeset request
+        request.updateAttributes(
+            context,
+            state=model.NodesetRequest.State.ACCEPTED,
+            provider_nodes=[n.uuid for n in provider_nodes])
+
+        # "Fulfill" requested provider nodes
+        for node in self.api.getMatchingProviderNodes():
+            self.assertIsNotNone(node.acquireLock(self.zk_client))
+            self.assertTrue(node.hasLock())
+            for _ in iterate_timeout(10, "node to be locked"):
+                if node.is_locked:
+                    break
+            node.updateAttributes(
+                context,
+                state=model.ProviderNode.State.READY
+            )
+            node.releaseLock()
+            self.assertFalse(node.hasLock())
+            for _ in iterate_timeout(10, "node to be unlocked"):
+                if not node.is_locked:
+                    break
+
+        # Wait for nodes to show up be ready and unlocked
+        for _ in iterate_timeout(10, "nodes to be ready"):
+            requested_nodes = [self.api.getProviderNode(ni)
+                               for ni in request.provider_nodes]
+            if len(requested_nodes) != 2:
+                continue
+            if all(n.state == model.ProviderNode.State.READY
+                   and not n.is_locked for n in requested_nodes):
+                break
+
+        # Mark the request as fulfilled + unlock
+        request.updateAttributes(
+            context,
+            state=model.NodesetRequest.State.FULFILLED,
+        )
+        request.releaseLock()
+        self.assertFalse(request.hasLock())
+        for _ in iterate_timeout(10, "request to be unlocked"):
+            if not request.is_locked:
+                break
+
+        # Should be a no-op
+        self.api.cleanupNodes()
+
+        # Remove request and wait for it to be removed from the cache
+        request.delete(context)
+        for _ in iterate_timeout(10, "request to be removed"):
+            request_list = self.api.getNodesetRequests()
+            if not len(request_list):
+                break
+
+        not_request = self.api.getNodesetRequest(request.uuid)
+        self.assertIsNone(not_request)
+
+        # Make sure we still have the provider nodes
+        provider_nodes = self.api.getProviderNodes()
+        self.assertEqual(len(provider_nodes), 2)
+
+        # Mark nodes as used
+        for node in provider_nodes:
+            self.assertIsNotNone(node.acquireLock(self.zk_client))
+            for _ in iterate_timeout(10, "wait for lock to show up"):
+                if node.is_locked:
+                    break
+            node.updateAttributes(
+                context,
+                state=model.ProviderNode.State.USED,
+            )
+            node.releaseLock()
+
+        # Cleanup used nodes and wait for them to be removed from the cache
+        for _ in iterate_timeout(10, "nodes to be removed"):
+            self.api.cleanupNodes()
+            provider_nodes = self.api.getProviderNodes()
+            if not provider_nodes:
+                break
