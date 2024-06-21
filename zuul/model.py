57233adceac2b6db476c76e073df293663742702
Simon Westphahl <simon.westphahl@bmw.de>
2024-06-21 14:14:12 +0200

    Support provider node subclasses in launcher API
    Different drivers will need to store additional information about a node
    instance in Zookeeper. However, we also want to keep all the nodes in
    the same Zookeeper path (/zuul/nodes/<node-uuid>) and with that also in
    the same tree cache.
    
    This can be accomplished by making `ProviderNode` an abstract base class
    and automatically registering subclasses with the parent class.
    
    Since this might be a useful pattern for other objects as well, this is
    provided as a separate mixin-class to allow allow variable ZKObjects in
    the same ZK path. The actual ZKObject type is encoded as a
    zero-terminated byte string before the actual (compressed) data.
    
    Change-Id: Ibb337cf7aeb808acc54eb22686d624dcb90db6e5

--- zuul/model.py
+++ zuul/model.py
@@ -1919,7 +1919,7 @@ class NodesetRequest(zkobject.LockableZKObject):
                 f" labels={self.labels}, path={self.getPath()}>")
 
 
-class ProviderNode(zkobject.LockableZKObject):
+class ProviderNode(zkobject.VariableZKObjectMixin, zkobject.LockableZKObject):
 
     class State(StrEnum):
         REQUESTED = "requested"
@@ -1953,8 +1953,8 @@ class ProviderNode(zkobject.LockableZKObject):
         )
 
     def __repr__(self):
-        return (f"<ProviderNode uuid={self.uuid}, state={self.state},"
-                f" path={self.getPath()}>")
+        return (f"<{self.__class__.__name__} uuid={self.uuid},"
+                f" state={self.state}, path={self.getPath()}>")
 
     def getPath(self):
         return f"{self.ROOT}/{self.NODES_PATH}/{self.uuid}"
