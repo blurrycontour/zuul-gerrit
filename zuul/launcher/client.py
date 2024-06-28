4ae2075b44032e7f64aa7faebd24bd7a804fbbdb
Simon Westphahl <simon.westphahl@bmw.de>
2024-06-28 11:56:54 +0200

    wip: Add launcher client/server implementation
    To Do:
    * e2e tests that also exercise the launcher client
    * doc strings
    
    Change-Id: I9bbf78a45044787d015c40f153b6d4ee5fa996f8

--- zuul/launcher/client.py
+++ zuul/launcher/client.py
@@ -13,11 +13,51 @@
 # under the License.
 
 import logging
+import time
+
+from zuul.model import NodesetRequest
+from zuul.lib import tracing
+from zuul.lib.logutil import get_annotated_logger
+
+from opentelemetry import trace
 
 
 class LauncherClient:
     log = logging.getLogger("zuul.LauncherClient")
+    tracer = trace.get_tracer("zuul")
 
     def __init__(self, config, sched):
         self.config = config
         self.sched = sched
+
+    def requestNodeset(self, item, job, priority):
+        log = get_annotated_logger(self.log, item.event)
+        labels = [n.label for n in job.nodeset.getNodes()]
+
+        buildset = item.current_build_set
+        parent_span = tracing.restoreSpan(buildset.span_info)
+        request_time = time.time()
+        with trace.use_span(parent_span):
+            request_span = self.tracer.start_span(
+                "NodesetRequest", start_time=request_time)
+        span_info = tracing.getSpanInfo(request_span)
+
+        with self.sched.createZKContext(None, self.log) as ctx:
+            state = (NodesetRequest.State.REQUESTED if job.nodeset.nodes
+                     else NodesetRequest.State.FULFILLED)
+            request = NodesetRequest.new(
+                ctx,
+                state=state,
+                tenant_name=item.pipeline.tenant.name,
+                pipeline_name=item.pipeline.name,
+                buildset_uuid=buildset.uuid,
+                job_uuid=job.uuid,
+                job_name=job.name,
+                labels=labels,
+                priority=priority,
+                # relative_priority,
+                request_time=request_time,
+                zuul_event_id=item.event.zuul_event_id,
+                span_info=span_info,
+            )
+            log.info("Submitted nodeset request %s", request)
