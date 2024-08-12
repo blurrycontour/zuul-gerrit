a1890f5ab2098d131f5aa78a741d257f42842cf4
James E. Blair <jim@acmegating.com>
2024-08-12 15:12:42 -0700

    Handle zuul_return format errors
    This change does two things:
    
    First, it corrects an error in the zuul_return schema validation
    which would, in a job where someone returned an artifact with a
    missing required attribute such as "url", cause an exception in
    the SQL reporter (which would lead to incomplete build results in
    the db, and possibly other pipeline-related errors).
    
    This change is made by correcting the voluptuous schema so that
    the "Required" flag is on the key, not the value.
    
    Secondly, it performs schema validation within the zuul_return
    module itself.  This will cause such errors to become user-visible
    at the time they occur (during the job) rather than post-facto.
    To accomplish this, the schema definition is moved into the
    shared zuul/ansible module, and a dependency on voluptuous within
    the in-job ansible virtualenvs is added.
    
    The first fix is still needed even with the second because users
    can return data to Zuul without using the zuul_return plugin.
    
    Change-Id: I6991863b2b5986b8067124b92c141c3c59240d18

---
+++ zuul/ansible/schema.py
@@ -0,0 +1,61 @@
+# Copyright 2024 Acme Gating, LLC
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+
+# This file incorporates work covered by the following copyright and
+# permission notice:
+
+#   Copyright 2018-2019 Red Hat, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+
+import voluptuous as vs
+
+
+artifact = {
+    vs.Required('name'): str,
+    vs.Required('url'): str,
+    'metadata': dict,
+}
+
+artifact_data = {
+    'zuul': {
+        'log_url': str,
+        'artifacts': [artifact],
+        vs.Extra: object,
+    },
+    vs.Extra: object,
+}
+
+warning_data = {
+    'zuul': {
+        'log_url': str,
+        'warnings': [str],
+        vs.Extra: object,
+    },
+    vs.Extra: object,
+}
+
+artifact_schema = vs.Schema(artifact_data)
+warning_schema = vs.Schema(warning_data)
