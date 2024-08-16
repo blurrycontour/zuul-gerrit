8fa815d6fc84b62048af3d49301f0cf45f0b5301
Benjamin Schanzel <benjamin.schanzel@bmw.de>
2024-08-16 13:58:57 +0200

    Add a switch to expand all jobs in PipelineDetails
    This adds a "Show all jobs" switch to the pipeline details page that
    toggles the expandable jobs pane on all queue items at once.
    
    This implements the functionality of the "Expand by default" toggle that
    exists on the current status page.
    
    Change-Id: Id1de863f921914ae23d0aeb48bfc9d931a1bf0a0

--- web/src/containers/status/QueueItem.jsx
+++ web/src/containers/status/QueueItem.jsx
@@ -12,7 +12,7 @@
 // License for the specific language governing permissions and limitations
 // under the License.
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect, useDispatch } from 'react-redux'
 
@@ -152,11 +152,15 @@ PromoteModal.propTypes = {
   onCancel: PropTypes.func.isRequired,
 }
 
-function QueueItem({ item, pipeline, tenant, user }) {
+function QueueItem({ item, pipeline, tenant, user, jobsExpanded}) {
   const [isAdminActionsOpen, setIsAdminActionsOpen] = useState(false)
   const [isDequeueModalOpen, setIsDequeueModalOpen] = useState(false)
   const [isPromoteModalOpen, setIsPromoteModalOpen] = useState(false)
-  const [isJobsExpanded, setIsJobsExpanded] = useState(false)
+  const [isJobsExpanded, setIsJobsExpanded] = useState(jobsExpanded)
+
+  useEffect(() => {
+    setIsJobsExpanded(jobsExpanded)
+  }, [jobsExpanded])
 
   const dispatch = useDispatch()
 
@@ -336,6 +340,7 @@ QueueItem.propTypes = {
   pipeline: PropTypes.object,
   tenant: PropTypes.object,
   user: PropTypes.object,
+  jobsExpanded: PropTypes.bool,
 }
 
 export default connect(state => ({
