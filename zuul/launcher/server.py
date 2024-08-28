fa71c1e1298c885c8e007d3b7d33dc935458ed6a
Simon Westphahl <simon.westphahl@bmw.de>
2024-08-28 14:54:03 +0200

    Handle create state-machine errors as node failure
    Until we have a better picture of which type of exceptions should lead
    to a node failure and which ones can be retried (state-machine level),
    we'll treat all exceptions from the create state-machine as fatal
    errors and fail the node request.
    
    This change also adds the missing `RuntimeConfigurationException`.
    
    Change-Id: Iac0291d1c987781df1844fc4b9b64ef12912ad4f

--- zuul/launcher/server.py
+++ zuul/launcher/server.py
@@ -273,10 +273,11 @@ class Launcher:
                         self._checkNode(node, log)
                     if node.state == model.ProviderNode.State.READY:
                         node.releaseLock()
-                except ProviderNodeError as err:
+                # TODO: check which errors are recoverable or can be
+                # retried (state machine level).
+                except Exception:
                     state = model.ProviderNode.State.FAILED
-                    log.exception("Marking node %s as %s: %s", node,
-                                  state, err)
+                    log.exception("Marking node %s as %s", node, state)
                     with self.createZKContext(node._lock, self.log) as ctx:
                         node.updateAttributes(ctx, state=state)
 
