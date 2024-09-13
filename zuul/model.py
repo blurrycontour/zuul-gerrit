e8f0a37e1f7e7932b0489eb1b2d01174e030794d
Simon Westphahl <simon.westphahl@bmw.de>
2024-09-13 15:22:18 +0200

    Add support for 'min-ready' provider nodes
    The available 'min-ready' nodes for a provider will be shared by all
    tenants that have the specific label configuration and
    connection/endpoint configured.
    
    A 'min-ready' nodes is considered usable if its label config hash
    is available on any of the request tenant's providers and has the same
    connection/endpoint.
    
    In case the min-ready count is not satisfied for a tenant a random
    provider in that tenant will be picked to spawn the missing nodes.
    
    In order to avoid race conditions when creating min-ready nodes, the
    launchers will calculate a score for the canonical label name, with the
    launcher that has the highest score being responsible for creating
    the missing nodes.
    
    Co-Authored-By: James E. Blair <jim@acmegating.com>
    Change-Id: I14d61719da6d4719cd147edeb6b35d7cae4ee93e

--- zuul/model.py
+++ zuul/model.py
@@ -1658,12 +1658,13 @@ class Label(ConfigObject):
     Labels are associated with provider-specific instance types.
     """
 
-    def __init__(self, name, image, flavor, description):
+    def __init__(self, name, image, flavor, description, min_ready):
         super().__init__()
         self.name = name
         self.image = image
         self.flavor = flavor
         self.description = description
+        self.min_ready = min_ready
 
     @property
     def canonical_name(self):
@@ -1685,7 +1686,8 @@ class Label(ConfigObject):
         return (self.name == other.name and
                 self.image == other.image and
                 self.flavor == other.flavor and
-                self.description == other.description)
+                self.description == other.description and
+                self.min_ready == other.min_ready)
 
     def toDict(self):
         sc = self.source_context
@@ -1695,6 +1697,7 @@ class Label(ConfigObject):
             'image': self.image,
             'flavor': self.flavor,
             'description': self.description,
+            'min_ready': self.min_ready,
         }
 
     def validateReferences(self, layout):
@@ -1850,24 +1853,50 @@ class ProviderConfig(ConfigObject):
             parent_name = parent_section.parent
             previous_section = parent_section
         # Provide defaults from the images/flavors/labels objects
+        image_hashes = {}
         for image in config.get('images', []):
             layout_image = self._dropNone(
                 layout.images[image['name']].toDict())
             image.update(ProviderConfig._mergeDict(layout_image, image))
             # This is used for identifying unique image configurations
             # across multiple providers.
-            hasher = hashlib.sha256()
-            hasher.update(json.dumps(image, sort_keys=True).encode('utf8'))
-            image['config_hash'] = hasher.hexdigest()
+            image['config_hash'] = hashlib.sha256(
+                json.dumps(image, sort_keys=True).encode("utf8")).hexdigest()
+            image_hashes[image['name']] = image['config_hash']
+        flavor_hashes = {}
         for flavor in config.get('flavors', []):
             layout_flavor = self._dropNone(
                 layout.flavors[flavor['name']].toDict())
             flavor.update(ProviderConfig._mergeDict(layout_flavor, flavor))
+            flavor['config_hash'] = hashlib.sha256(
+                json.dumps(flavor, sort_keys=True).encode("utf8")).hexdigest()
+            flavor_hashes[flavor['name']] = flavor['config_hash']
         for label in config.get('labels', []):
             layout_label = self._dropNone(
                 layout.labels[label['name']].toDict())
             label.update(ProviderConfig._mergeDict(layout_label, label))
+            try:
+                label['config_hash'] = self._getLabelConfigHash(
+                    label, image_hashes, flavor_hashes)
+            except Exception:
+                # We might miss some flavor or label, but this will be
+                # caught later during config validation.
+                label['config_hash'] = None
         return config
+
+    def _getLabelConfigHash(self, label, image_hashes, flavor_hashes):
+        label_hash = hashlib.sha256(
+            json.dumps(label, sort_keys=True).encode("utf8")
+        ).hexdigest()
+        image_hash = image_hashes[label["image"]]
+        flavor_hash = flavor_hashes[label["flavor"]]
+        # XOR relevant config hashes
+        _hash = (
+            int(label_hash, 16)
+            ^ int(image_hash, 16)
+            ^ int(flavor_hash, 16)
+        )
+        return f"{_hash:x}"
 
 
 class Node(ConfigObject):
@@ -2394,6 +2423,7 @@ class ProviderNode(zkobject.PolymorphicZKObjectMixin,
         FAILED = "failed"
         IN_USE = "in-use"
         USED = "used"
+        OUTDATED = "outdated"
         HOLD = "hold"
 
     FINAL_STATES = (
@@ -2411,6 +2441,7 @@ class ProviderNode(zkobject.PolymorphicZKObjectMixin,
         State.BUILDING,
         State.FAILED,
         State.USED,
+        State.OUTDATED,
     )
 
     ROOT = "/zuul/nodes"
@@ -2421,9 +2452,11 @@ class ProviderNode(zkobject.PolymorphicZKObjectMixin,
         super().__init__()
         self._set(
             uuid=uuid4().hex,
-            request_id="",
+            request_id=None,
+            zuul_event_id=None,
             state=self.State.REQUESTED,
             label="",
+            label_config_hash=None,
             tags={},
             connection_name="",
             create_state={},
@@ -2457,7 +2490,8 @@ class ProviderNode(zkobject.PolymorphicZKObjectMixin,
 
     def __repr__(self):
         return (f"<{self.__class__.__name__} uuid={self.uuid},"
-                f" state={self.state}, path={self.getPath()}>")
+                f" label={self.label}, state={self.state},"
+                f" path={self.getPath()}>")
 
     def getPath(self):
         return self._getPath(self.uuid)
@@ -2478,8 +2512,10 @@ class ProviderNode(zkobject.PolymorphicZKObjectMixin,
         data = dict(
             uuid=self.uuid,
             request_id=self.request_id,
+            zuul_event_id=self.zuul_event_id,
             state=self.state,
             label=self.label,
+            label_config_hash=self.label_config_hash,
             tags=self.tags,
             connection_name=self.connection_name,
             create_state=self.create_state,
