de838f20b4264b4f750d7e64b5cfd38ee1ea93ef
James E. Blair <jim@acmegating.com>
2025-02-06 10:45:07 -0800

    Launcher: improve nodescan testing
    This improves the nodescan testing by:
    
    * Removing the fake handling from the production code
    * Adds explicit host-key-checking: false settings to most nodepool
      test fixtures.
    * Adding explicit success/failure tests for nodescan with the full
      node request lifecycle.
    * Moves the test-only fakes into their own file so they can be shared
      with the new launcher nodescan tests as well as the dedicated
      nodescan unit tests.
    * Allows for customizable timeouts in the requestNodes helper method
      since the failing test takes a while.
    
    It also moves the boot-timeout parameter from labels to images, and,
    more importantly, allows it to be set on the section object itself.
    Typically a boot-timeout would be a characteristic of the cloud (ie,
    the cloud is slow to boot anything) or an image (this image is slow
    to boot).  It's less likely to be a characteristic of a specific label.
    
    Change-Id: If6361cb437ad28472f154f3af9dde190d43c0d1e

--- zuul/launcher/server.py
+++ zuul/launcher/server.py
@@ -264,8 +264,6 @@ class NodescanRequest:
     CONNECTING_KEY = 'connecting key'
     NEGOTIATING_KEY = 'negotiating key'
     COMPLETE = 'complete'
-    # For unit testing
-    FAKE = False
 
     def __init__(self, node, log):
         self.state = self.START
@@ -282,10 +280,6 @@ class NodescanRequest:
             self.gather_hostkeys = False
         self.ip = node.interface_ip
         self.port = node.connection_port
-        if 'fake' not in self.ip and not self.FAKE:
-            addrinfo = socket.getaddrinfo(self.ip, self.port)[0]
-            self.family = addrinfo[0]
-            self.sockaddr = addrinfo[4]
         self.sock = None
         self.transport = None
         self.event = None
@@ -412,14 +406,9 @@ class NodescanRequest:
             if not self.host_key_checking:
                 self.state = self.COMPLETE
             else:
-                if 'fake' in self.ip or self.FAKE:
-                    if self.gather_hostkeys:
-                        self.keys = ['ssh-rsa FAKEKEY']
-                    self.state = self.COMPLETE
-                else:
-                    self.init_connection_attempts += 1
-                    self._connect()
-                    self.state = self.CONNECTING_INIT
+                self.init_connection_attempts += 1
+                self._connect()
+                self.state = self.CONNECTING_INIT
 
         if self.state == self.CONNECTING_INIT:
             if not socket_ready:
@@ -946,7 +935,7 @@ class Launcher:
             label_config_hash=label.config_hash,
             max_ready_age=label.max_ready_age,
             host_key_checking=label.host_key_checking,
-            boot_timeout=label.boot_timeout,
+            boot_timeout=image.boot_timeout,
             request_id=request.uuid,
             zuul_event_id=request.zuul_event_id,
             connection_name=provider.connection_name,
@@ -1024,7 +1013,6 @@ class Launcher:
                         with node.activeContext(ctx):
                             node.setState(state)
                         self.wake_event.set()
-
             # Deallocate ready node w/o a request for re-use
             if (node.request_id and not request
                     and node.state == node.State.READY):
@@ -1212,7 +1200,7 @@ class Launcher:
                     label_config_hash=label.config_hash,
                     max_ready_age=label.max_ready_age,
                     host_key_checking=label.host_key_checking,
-                    boot_timeout=label.boot_timeout,
+                    boot_timeout=image.boot_timeout,
                     request_id=None,
                     connection_name=provider.connection_name,
                     zuul_event_id=uuid.uuid4().hex,
