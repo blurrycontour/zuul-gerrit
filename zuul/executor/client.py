14f0f19aed29bb702eda169ac8fb06d860d7c6fa
Dong Zhang <dong.zhang@bmw.de>
2025-02-26 16:23:03 +0100

    Prepare oidc token for playbook execution in executor.
    The OIDC secrets are decrypted togheter with normal secrets,
    but the OIDC tokens are created right before the playbooks
    execution if needed for those playbooks.
    
    Specification: https://zuul-ci.org/docs/zuul/latest/developer/specs/zuul-workload-identity-federation.html
    
    Change-Id: I6dcc0e807da86fd07deae0505d313c8297f1c096

--- zuul/executor/client.py
+++ zuul/executor/client.py
@@ -65,6 +65,13 @@ class ExecutorClient(object):
             uuid, self.sched.connections,
             job, item, pipeline, dependent_changes, merger_items,
             redact_secrets_and_keys=False)
+
+        # Pass webroot to the executor for generating oidc token
+        web_root = self.sched.abide.tenants.get(
+            pipeline.tenant.name, self.sched.globals).web_root
+        if web_root:
+            params["zuul"]["web_root"] = web_root.split("/t/")[0].rstrip("/")
+
         # TODO: deprecate and remove this variable?
         params["zuul"]["_inheritance_path"] = list(job.inheritance_path)
 
